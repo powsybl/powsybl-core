02ca0e271b5c359a1eeef9e2e74918fd5a1e740e
Ioannis Konstantelos <ik303@ee-ik303.ic.ac.uk>
2016-04-26 17:29:14 +0100

    Removed copyright from insert_ind.m

--- sampling/sources/insert_ind.m
+++ sampling/sources/insert_ind.m
@@ -1,11 +1,3 @@
-%
-% Copyright (c) 2016, Ioannis Konstantelos <i.konstantelos@imperial.ac.uk>
-% and Mingyang Sun <mingyang.sun11@imperial.ac.uk> – Imperial College London
-% This Source Code Form is subject to the terms of the Mozilla Public
-% License, v. 2.0. If a copy of the MPL was not distributed with this
-% file, You can obtain one at http://mozilla.org/MPL/2.0/.
-%
-
 function NewMat=insert_ind(Mat,Ind,Val2Insert,Dim);
 %---------------------------------------------------------------------------
 % insert_ind function                                               General
