d0a6d8250f052d03433e3167131ce240b9cb26b2
massimoferraro <massimo.ferraro@quinary.com>
2016-09-23 16:06:27 +0200

    added parameter for stopping WCA when there are limit violations on the base state

--- wca-integration/src/main/java/eu/itesla_project/wca/WCAImpl.java
+++ wca-integration/src/main/java/eu/itesla_project/wca/WCAImpl.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2016, All partners of the iTesla project (http://www.itesla-project.eu/consortium)
+ * Copyright (c) 2016, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -594,7 +595,7 @@ public class WCAImpl implements WCA, WCAConstants, AmplConstants {
                     } else {
                         // check limits on base state
                         List<LimitViolation> baseStateLimitViolations = CURRENT_FILTER.apply(Security.checkLimits(network));
-                        if (baseStateLimitViolations.size() > 0) {
+                        if (baseStateLimitViolations.size() > 0 && parameters.isStopWCAifBaseStateLimitViolations()) {
                             for (Contingency contingency : contingencies) {
                                 clusters.add(CompletableFuture.completedFuture(new WCAClusterImpl(contingency,
                                                                                                   WCAClusterNum.FOUR,
