810edc816919e52cbafc1fb43e54c68da42bbb52
Mathieu Bague <mathieu.bague@rte-france.com>
2016-10-18 10:18:17 +0200

    Move ComponentDefaultConfigTest and XmlPlatformConfigTest to commons.config

--- commons/src/test/java/eu/itesla_project/commons/config/XmlPlatformConfigTest.java
+++ commons/src/test/java/eu/itesla_project/commons/config/XmlPlatformConfigTest.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package eu.itesla_project.commons.io;
+package eu.itesla_project.commons.config;
 
 import java.io.IOException;
 import java.io.Writer;
