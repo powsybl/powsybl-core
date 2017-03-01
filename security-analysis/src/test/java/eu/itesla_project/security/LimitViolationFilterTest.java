483b5873db201084fb3ee3b9878ffa50eea47607
Massimo Ferraro <massimo.ferraro@quinary.com>
2017-03-01 18:46:24 +0100

    Added country filter to LimitViolationFilter (#71)
    Added country filter to LimitViolationFilter
    
    * Fix assertion statements in test

--- security-analysis/src/test/java/eu/itesla_project/security/LimitViolationFilterTest.java
+++ security-analysis/src/test/java/eu/itesla_project/security/LimitViolationFilterTest.java
@@ -1,26 +1,32 @@
 /**
- * Copyright (c) 2016, RTE (http://www.rte-france.com)
+ * Copyright (c) 2016-2017, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package eu.itesla_project.security;
 
-import com.google.common.jimfs.Configuration;
-import com.google.common.jimfs.Jimfs;
-import eu.itesla_project.commons.config.InMemoryPlatformConfig;
-import eu.itesla_project.commons.config.MapModuleConfig;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import java.nio.file.FileSystem;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import java.nio.file.FileSystem;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.EnumSet;
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
 
-import static org.junit.Assert.*;
+import eu.itesla_project.commons.config.InMemoryPlatformConfig;
+import eu.itesla_project.commons.config.MapModuleConfig;
+import eu.itesla_project.iidm.network.Country;
+import eu.itesla_project.iidm.network.Identifiable;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
@@ -38,23 +44,30 @@ public class LimitViolationFilterTest {
         moduleConfig = platformConfig.createModuleConfig("limit-violation-default-filter");
         moduleConfig.setStringListProperty("violationTypes", Arrays.asList("CURRENT", "LOW_VOLTAGE"));
         moduleConfig.setStringProperty("minBaseVoltage", "150");
+        moduleConfig.setStringListProperty("countries", Arrays.asList("FR", "BE"));
     }
 
     @After
     public void tearDown() throws Exception {
         fileSystem.close();
     }
+    
     @Test
     public void load() throws Exception {
         LimitViolationFilter filter = LimitViolationFilter.load(platformConfig);
-        assertEquals(filter.getViolationTypes(), EnumSet.of(LimitViolationType.CURRENT, LimitViolationType.LOW_VOLTAGE));
-        assertTrue(filter.getMinBaseVoltage() == 150f);
+        assertEquals(EnumSet.of(LimitViolationType.CURRENT, LimitViolationType.LOW_VOLTAGE), filter.getViolationTypes());
+        assertEquals(150f, filter.getMinBaseVoltage(), 0f);
+        assertEquals(EnumSet.of(Country.FR, Country.BE), filter.getCountries());
         filter.setViolationTypes(EnumSet.of(LimitViolationType.HIGH_VOLTAGE));
-        assertEquals(filter.getViolationTypes(), EnumSet.of(LimitViolationType.HIGH_VOLTAGE));
+        assertEquals(EnumSet.of(LimitViolationType.HIGH_VOLTAGE), filter.getViolationTypes());
         filter.setMinBaseVoltage(225f);
-        assertTrue(filter.getMinBaseVoltage() == 225f);
+        filter.setCountries(EnumSet.of(Country.FR));
+        assertEquals(EnumSet.of(Country.FR), filter.getCountries());
+        assertEquals(225f, filter.getMinBaseVoltage(), 0f);
         filter.setViolationTypes(null);
         assertNull(filter.getViolationTypes());
+        filter.setCountries(null);
+        assertNull(filter.getCountries());
         try {
             filter.setViolationTypes(EnumSet.noneOf(LimitViolationType.class));
             fail();
@@ -65,6 +78,56 @@ public class LimitViolationFilterTest {
             fail();
         } catch (Exception ignored) {
         }
+        try {
+            filter.setCountries(EnumSet.noneOf(Country.class));
+            fail();
+        } catch (Exception ignored) {
+        }
     }
 
+    @Test
+    public void apply() throws Exception {
+        Identifiable line1 = Mockito.mock(Identifiable.class);
+        Mockito.when(line1.getId()).thenReturn("line1");
+        LimitViolation line1Violation = new LimitViolation(line1, LimitViolationType.CURRENT, 1000f, "", 1, 1100f, Country.FR, 380f);
+        Identifiable line2 = Mockito.mock(Identifiable.class);
+        Mockito.when(line2.getId()).thenReturn("line2");
+        LimitViolation line2Violation = new LimitViolation(line2, LimitViolationType.CURRENT, 900f, "", 1, 950f, Country.BE, 220f);
+        Identifiable voltageLeve1 = Mockito.mock(Identifiable.class);
+        Mockito.when(voltageLeve1.getId()).thenReturn("voltageLeve11");
+        LimitViolation voltageLeve11Violation = new LimitViolation(voltageLeve1, LimitViolationType.HIGH_VOLTAGE, 200f, "", 1, 250f, Country.FR, 220f);
+        
+        LimitViolationFilter filter = new LimitViolationFilter();
+        List<LimitViolation> filteredViolations = filter.apply(Arrays.asList(line1Violation, line2Violation, voltageLeve11Violation));
+        assertEquals(3, filteredViolations.size());
+        
+        filter = new LimitViolationFilter();
+        filter.setViolationTypes(EnumSet.of(LimitViolationType.HIGH_VOLTAGE));
+        filteredViolations = filter.apply(Arrays.asList(line1Violation, line2Violation, voltageLeve11Violation));
+        checkFilteredViolations(filteredViolations, "voltageLeve11", LimitViolationType.HIGH_VOLTAGE, 220f, Country.FR);
+        
+        filter = new LimitViolationFilter();
+        filter.setMinBaseVoltage(300f);
+        filteredViolations = filter.apply(Arrays.asList(line1Violation, line2Violation, voltageLeve11Violation));
+        checkFilteredViolations(filteredViolations, "line1", LimitViolationType.CURRENT, 380f, Country.FR);
+        
+        filter = new LimitViolationFilter();
+        filter.setCountries(EnumSet.of(Country.BE));
+        filteredViolations = filter.apply(Arrays.asList(line1Violation, line2Violation, voltageLeve11Violation));
+        checkFilteredViolations(filteredViolations, "line2", LimitViolationType.CURRENT, 220f, Country.BE);
+        
+        filter = new LimitViolationFilter(EnumSet.of(LimitViolationType.CURRENT), 300f, EnumSet.of(Country.FR));
+        filteredViolations = filter.apply(Arrays.asList(line1Violation, line2Violation, voltageLeve11Violation));
+        checkFilteredViolations(filteredViolations, "line1", LimitViolationType.CURRENT, 380f, Country.FR);
+        
+    }
+    
+    private void checkFilteredViolations(List<LimitViolation> filteredViolations, String equipmentId, LimitViolationType violationType, 
+                                         float baseVoltage, Country country) {
+        assertEquals(1, filteredViolations.size());
+        assertEquals(equipmentId, filteredViolations.get(0).getSubject().getId());
+        assertEquals(violationType, filteredViolations.get(0).getLimitType());
+        assertEquals(baseVoltage, filteredViolations.get(0).getBaseVoltage(), 0f);
+        assertEquals(country, filteredViolations.get(0).getCountry());
+    }
 }
