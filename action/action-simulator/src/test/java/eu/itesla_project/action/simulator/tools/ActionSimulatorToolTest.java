bfe62df36ab943e781034ac060bac59133970dae
Mathieu BAGUE <mathieu.bague@gmail.com>
2017-07-27 14:25:16 +0200

    Rename action.simulator.tool package (#163)

--- action/action-simulator/src/test/java/eu/itesla_project/action/simulator/tools/ActionSimulatorToolTest.java
+++ action/action-simulator/src/test/java/eu/itesla_project/action/simulator/tools/ActionSimulatorToolTest.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package eu.itesla_project.action.simulator.tool;
+package eu.itesla_project.action.simulator.tools;
 
 import eu.itesla_project.commons.tools.AbstractToolTest;
 import eu.itesla_project.commons.tools.Command;
