55a2dc7ad4f504269a152e6a1e4fe45cfce53c8a
yichen88 <tang.yichenyves@gmail.com>
2017-10-09 14:24:12 +0200

    fix package name

--- action/action-util/src/main/groovy/com/powsybl/action/util/FixPhaseShifterTapTaskExtension.groovy
+++ action/action-util/src/main/groovy/com/powsybl/action/util/FixPhaseShifterTapTaskExtension.groovy
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package eu.itesla_project.action.util
+package com.powsybl.action.util
 
 import com.google.auto.service.AutoService
 import com.powsybl.action.dsl.spi.DslTaskExtension
