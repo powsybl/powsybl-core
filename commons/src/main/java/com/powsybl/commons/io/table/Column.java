605a1a3d09fd8226f84a6d477239633b661575d7
calinbanc <32734781+calinbanc@users.noreply.github.com>
2017-10-31 16:38:19 +0100

    Refactoring table number horizontal format (#44)

--- commons/src/main/java/com/powsybl/commons/io/table/Column.java
+++ commons/src/main/java/com/powsybl/commons/io/table/Column.java
@@ -6,6 +6,9 @@
  */
 package com.powsybl.commons.io.table;
 
+import java.text.NumberFormat;
+import java.util.Objects;
+
 /**
  *
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
@@ -14,12 +17,34 @@ public class Column {
 
     private final String name;
 
+    private HorizontalAlignment horizontalAlignment;
+
+    private NumberFormat numberFormat = null;
+
     public Column(String name) {
         this.name = name;
+        this.horizontalAlignment = HorizontalAlignment.LEFT;
     }
 
     public String getName() {
         return name;
     }
 
+    public HorizontalAlignment getHorizontalAlignment() {
+        return horizontalAlignment;
+    }
+
+    public Column setHorizontalAlignment(HorizontalAlignment horizontalAlignment) {
+        this.horizontalAlignment = Objects.requireNonNull(horizontalAlignment);
+        return this;
+    }
+
+    public NumberFormat getNumberFormat() {
+        return numberFormat;
+    }
+
+    public Column setNumberFormat(NumberFormat numberFormat) {
+        this.numberFormat = Objects.requireNonNull(numberFormat);
+        return this;
+    }
 }
