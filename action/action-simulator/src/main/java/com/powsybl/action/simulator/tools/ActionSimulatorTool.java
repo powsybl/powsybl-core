e42790e7ec286957b2f8bde0a37923eb4a2df91e
calinbanc <calinbanc@yahoo.com>
2017-11-02 16:38:57 +0100

    iAL save case to file

--- action/action-simulator/src/main/java/com/powsybl/action/simulator/tools/ActionSimulatorTool.java
+++ action/action-simulator/src/main/java/com/powsybl/action/simulator/tools/ActionSimulatorTool.java
@@ -44,7 +44,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
