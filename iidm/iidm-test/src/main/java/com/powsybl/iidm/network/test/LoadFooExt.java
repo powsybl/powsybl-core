972eb1ca2e487d47a8b9615de90f22bc19d6e491
Mathieu BAGUE <mathieu.bague@rte-france.com>
2017-11-17 18:32:47 +0100

    Fix XML extension export to regroup all the extensions for the same identifiable

---
+++ iidm/iidm-test/src/main/java/com/powsybl/iidm/network/test/LoadFooExt.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2017, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.network.test;
+
+import com.powsybl.iidm.network.Identifiable;
+import com.powsybl.iidm.network.Load;
+
+/**
+ * @author Mathieu Bague <mathieu.bague@rte-france.com>
+ */
+public class LoadFooExt implements Identifiable.Extension<Load> {
+
+    private final Load load;
+
+    public LoadFooExt(Load load) {
+        this.load = load;
+    }
+
+    @Override
+    public String getName() {
+        return "loadFoo";
+    }
+
+    @Override
+    public Load getIdentifiable() {
+        return load;
+    }
+}
