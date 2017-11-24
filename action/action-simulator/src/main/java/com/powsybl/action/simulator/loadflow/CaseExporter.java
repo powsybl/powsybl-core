24ece94f4469c990f68392584c73bcfb28b786f5
calinbanc <calinbanc@yahoo.com>
2017-11-24 14:11:52 +0100

    new rule for the name of the case file

--- action/action-simulator/src/main/java/com/powsybl/action/simulator/loadflow/CaseExporter.java
+++ action/action-simulator/src/main/java/com/powsybl/action/simulator/loadflow/CaseExporter.java
@@ -43,12 +43,12 @@ public class CaseExporter extends DefaultLoadFlowActionSimulatorObserver {
     }
 
     private void exportNetwork(Contingency contingency, Network network, int round) {
-        String rootFileName;
+        String suffix;
         if (null != contingency) {
-            rootFileName = contingency.getId() + String.valueOf("_Round_" + round);
+            suffix = contingency.getId();
         } else {
-            rootFileName = "N_situation" + String.valueOf("_Round_" + round);
+            suffix = "N_situation";
         }
-        Exporters.export(outputCaseFormat, network, new Properties(), outputCaseFolder.resolve(rootFileName + "_" + network.getId() + "." + outputCaseFormat + ".gz"));
+        Exporters.export(outputCaseFormat, network, new Properties(), outputCaseFolder.resolve(network.getId() + "_" + suffix + "_Round_" + round + "." + outputCaseFormat + ".gz"));
     }
 }
