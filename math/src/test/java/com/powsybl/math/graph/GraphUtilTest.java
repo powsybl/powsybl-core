0d3eb11567467c52e2c71a7603afc5212c08bf6f
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2018-01-22 18:44:14 +0100

    Evolution of node-breaker manipulation (#163)
    * Evolution of node-breaker manipulation:
     - removing equipments do not remove adjacent switches any more
     - possibility to remove switches in node-breaker API
     - possibility to traverse the node-breaker graph
     - the clean method now removes useless nodes
     - higher level methods in a separate utility module to manipulate the node breaker topology
    
    * Following code review:
    - check for null arguments for public methods
    - static imports
    - small syntax improvements

---
+++ math/src/test/java/com/powsybl/math/graph/GraphUtilTest.java
@@ -0,0 +1,32 @@
+package com.powsybl.math.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Sylvain Leclerc <sylvain.leclerc@rte-france.com>
+ */
+public class GraphUtilTest {
+
+    @Test
+    public void removeIsolatedVertices() throws Exception {
+        UndirectedGraph<Object, Object> graph = new UndirectedGraphImpl<>();
+
+        graph.addVertex();
+        graph.addVertex();
+        graph.addVertex();
+        graph.addEdge(1, 2, null);
+        graph.setVertexObject(0, new Object());
+
+        assertEquals(3, graph.getVertexCount());
+        // Vertex 0 is isolated but has an associated object: it must not be removed.
+        GraphUtil.removeIsolatedVertices(graph);
+        assertEquals(3, graph.getVertexCount());
+        graph.setVertexObject(0, null);
+        // Now vertex 0 must be removed.
+        GraphUtil.removeIsolatedVertices(graph);
+        assertEquals(2, graph.getVertexCount());
+    }
+
+}
