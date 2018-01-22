b82acf7eaa7b9b11617374947878c8e4183bfa81
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2018-01-22 15:00:23 +0100

    Add time series creation helper (#168)
    Add time series creation helper

--- math/src/main/java/com/powsybl/math/timeseries/StringTimeSeries.java
+++ math/src/main/java/com/powsybl/math/timeseries/StringTimeSeries.java
@@ -7,6 +7,7 @@
 package com.powsybl.math.timeseries;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
@@ -14,6 +15,17 @@ import java.util.List;
 public class StringTimeSeries extends AbstractTimeSeries<StringPoint, StringArrayChunk, StringTimeSeries> implements TimeSeries<StringPoint, StringTimeSeries> {
 
     private static final String[] NULL_ARRAY = new String[] {null};
+
+    public static StringTimeSeries create(String name, TimeSeriesIndex index, String[] values) {
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(index);
+        Objects.requireNonNull(values);
+        if (index.getPointCount() != values.length) {
+            throw new IllegalArgumentException("Bad number of values " + values.length + ", expected " + index.getPointCount());
+        }
+        return new StringTimeSeries(new TimeSeriesMetadata(name, TimeSeriesDataType.STRING, index),
+                new UncompressedStringArrayChunk(0, values));
+    }
 
     public StringTimeSeries(TimeSeriesMetadata metadata, StringArrayChunk... chunks) {
         super(metadata, chunks);
