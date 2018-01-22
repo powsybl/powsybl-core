713d19a6724295607c451ae339fb9b4e8ef3707e
Mathieu BAGUE <mathieu.bague@gmail.com>
2018-01-22 13:48:49 +0100

    Add extensions for current limit violations (#166)

---
+++ security-analysis/src/main/java/com/powsybl/security/interceptors/SecurityAnalysisInterceptors.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.security.interceptors;
+
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.powsybl.commons.util.ServiceLoaderCache;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author Mathieu Bague <mathieu.bague@rte-france.com>
+ */
+public final class SecurityAnalysisInterceptors {
+
+    private static final Supplier<Map<String, SecurityAnalysisInterceptorExtension>> EXTENSIONS
+        = Suppliers.memoize(SecurityAnalysisInterceptors::loadExtensions);
+
+    private static Map<String, SecurityAnalysisInterceptorExtension> loadExtensions() {
+        return new ServiceLoaderCache<>(SecurityAnalysisInterceptorExtension.class).getServices().stream()
+            .collect(Collectors.toMap(SecurityAnalysisInterceptorExtension::getName, e -> e));
+    }
+
+    public static Set<String> getExtensionNames() {
+        return EXTENSIONS.get().keySet();
+    }
+
+    public static SecurityAnalysisInterceptor createInterceptor(String name) {
+        Objects.requireNonNull(name);
+
+        SecurityAnalysisInterceptorExtension extension = EXTENSIONS.get().get(name);
+
+        return extension == null ? null : extension.createInterceptor();
+    }
+
+    private SecurityAnalysisInterceptors() {
+    }
+}
--- security-analysis/src/main/java/com/powsybl/security/json/CurrentExtensionSerializer.java
+++ security-analysis/src/main/java/com/powsybl/security/json/CurrentExtensionSerializer.java
@@ -1,10 +1,10 @@
 /**
- * Copyright (c) 2017, RTE (http://www.rte-france.com)
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.powsybl.security.extensions;
+package com.powsybl.security.json;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
@@ -15,6 +15,7 @@ import com.google.auto.service.AutoService;
 import com.powsybl.commons.PowsyblException;
 import com.powsybl.commons.extensions.ExtensionJsonSerializer;
 import com.powsybl.security.LimitViolation;
+import com.powsybl.security.extensions.CurrentExtension;
 
 import java.io.IOException;
 
@@ -22,11 +23,11 @@ import java.io.IOException;
  * @author Mathieu Bague <mathieu.bague@rte-france.com>
  */
 @AutoService(ExtensionJsonSerializer.class)
-public class PreContingencyJson implements ExtensionJsonSerializer<LimitViolation, PreContingency> {
+public class CurrentExtensionSerializer implements ExtensionJsonSerializer<LimitViolation, CurrentExtension> {
 
     @Override
     public String getExtensionName() {
-        return "PreContingency";
+        return "Current";
     }
 
     @Override
@@ -35,23 +36,23 @@ public class PreContingencyJson implements ExtensionJsonSerializer<LimitViolatio
     }
 
     @Override
-    public Class<? super PreContingency> getExtensionClass() {
-        return PreContingency.class;
+    public Class<? super CurrentExtension> getExtensionClass() {
+        return CurrentExtension.class;
     }
 
     @Override
-    public void serialize(PreContingency extension, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+    public void serialize(CurrentExtension extension, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
         jsonGenerator.writeStartObject();
-        jsonGenerator.writeNumberField("value", extension.getPreContingencyValue());
+        jsonGenerator.writeNumberField("preContingencyValue", extension.getPreContingencyValue());
         jsonGenerator.writeEndObject();
     }
 
     @Override
-    public PreContingency deserialize(JsonParser parser, DeserializationContext deserializationContext) throws IOException {
+    public CurrentExtension deserialize(JsonParser parser, DeserializationContext deserializationContext) throws IOException {
         float value = Float.NaN;
 
         while (parser.nextToken() != JsonToken.END_OBJECT) {
-            if (parser.getCurrentName().equals("value")) {
+            if (parser.getCurrentName().equals("preContingencyValue")) {
                 parser.nextToken();
                 value = parser.readValueAs(Float.class);
             } else {
@@ -59,6 +60,6 @@ public class PreContingencyJson implements ExtensionJsonSerializer<LimitViolatio
             }
         }
 
-        return new PreContingency(value);
+        return new CurrentExtension(value);
     }
 }
