60c0f3384044a3f3b82d9f9bbe1ccb074ac3e223
Luma Zamarreño <zamarrenolm@aia.es>
2018-03-05 13:38:45 +0100

    realized that there was already a three windings transformer network factory. Adapted the unit test to check the existing sample

--- iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/ThreeWindingsTransformerXmlTest.java
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/ThreeWindingsTransformerXmlTest.java
@@ -7,10 +7,13 @@
 package com.powsybl.iidm.xml;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import org.junit.Test;
 
 import com.powsybl.commons.AbstractConverterTest;
+import com.powsybl.commons.datasource.FileDataSource;
+import com.powsybl.iidm.network.test.ThreeWindingsTransformerNetworkFactory;
 import com.powsybl.iidm.network.test.ThreeWindingsTransformerTestCaseFactory;
 
 /**
@@ -20,7 +23,7 @@ public class ThreeWindingsTransformerXmlTest extends AbstractConverterTest {
 
     @Test
     public void roundTripTest() throws IOException {
-        roundTripXmlTest(ThreeWindingsTransformerTestCaseFactory.create(),
+        roundTripXmlTest(ThreeWindingsTransformerNetworkFactory.createWithCurrentLimits(),
                          NetworkXml::writeAndValidate,
                          NetworkXml::read,
                          "/threeWindingsTransformerRoundTripRef.xml");
