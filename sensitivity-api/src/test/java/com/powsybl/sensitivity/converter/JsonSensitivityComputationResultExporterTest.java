121c4f5f7008ff5833b19bbe90964437a9476d9b
MURGEY Sébastien <sebastien.murgey@rte-france.com>
2018-06-29 11:19:57 +0200

    Solve issue in roundtrip tests

--- sensitivity-api/src/test/java/com/powsybl/sensitivity/converter/JsonSensitivityComputationResultExporterTest.java
+++ sensitivity-api/src/test/java/com/powsybl/sensitivity/converter/JsonSensitivityComputationResultExporterTest.java
@@ -1,31 +1,64 @@
 package com.powsybl.sensitivity.converter;
 
+import com.powsybl.commons.AbstractConverterTest;
 import com.powsybl.sensitivity.SensitivityComputationResults;
 import com.powsybl.sensitivity.json.SensitivityComputationResultJsonSerializer;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 
-import static org.junit.Assert.*;
+public class JsonSensitivityComputationResultExporterTest extends AbstractConverterTest {
 
-public class JsonSensitivityComputationResultExporterTest {
+    private static SensitivityComputationResults create() throws IOException {
+        byte[] inputBytes = IOUtils.toByteArray(JsonSensitivityComputationResultExporterTest.class.getResourceAsStream("/resultsExport.json"));
+        return SensitivityComputationResultJsonSerializer.read(new InputStreamReader(new ByteArrayInputStream(inputBytes)));
+    }
+
+    private static SensitivityComputationResults read(Path jsonFile) {
+        Objects.requireNonNull(jsonFile);
+
+        try (InputStream is = Files.newInputStream(jsonFile)) {
+            return SensitivityComputationResultJsonSerializer.read(new InputStreamReader(is));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static void write(SensitivityComputationResults results, Path jsonFile) {
+        Objects.requireNonNull(results);
+        Objects.requireNonNull(jsonFile);
+
+        try (OutputStream os = Files.newOutputStream(jsonFile)) {
+            SensitivityComputationResultExporter exporter = new JsonSensitivityComputationResultExporter();
+            exporter.export(results, new OutputStreamWriter(os));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static void writeViaExporters(SensitivityComputationResults results, Path jsonFile) {
+        Objects.requireNonNull(results);
+        Objects.requireNonNull(jsonFile);
+        SensitivityComputationResultExporters.export(results, jsonFile, "JSON");
+    }
 
     @Test
-    public void export() throws IOException {
-        JsonSensitivityComputationResultExporter exporter = new JsonSensitivityComputationResultExporter();
+    public void roundTripTest() throws IOException {
+        roundTripTest(create(), JsonSensitivityComputationResultExporterTest::write, JsonSensitivityComputationResultExporterTest::read, "/resultsExport.json");
+    }
 
-        byte[] inputBytes = IOUtils.toByteArray(getClass().getResourceAsStream("/resultsExport.json"));
-        SensitivityComputationResults results = SensitivityComputationResultJsonSerializer.read(new InputStreamReader(new ByteArrayInputStream(inputBytes)));
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        exporter.export(results, new OutputStreamWriter(baos));
-        byte[] outputBytes = baos.toByteArray();
-
-        assertArrayEquals(inputBytes, outputBytes);
+    @Test
+    public void roundTripViaExportersTest() throws IOException {
+        roundTripTest(create(), JsonSensitivityComputationResultExporterTest::writeViaExporters, JsonSensitivityComputationResultExporterTest::read, "/resultsExport.json");
     }
 }
--- sensitivity-api/src/test/java/com/powsybl/sensitivity/converter/SensitivityComputationResultExportersTest.java
+++ sensitivity-api/src/test/java/com/powsybl/sensitivity/converter/SensitivityComputationResultExportersTest.java
@@ -1,15 +1,7 @@
 package com.powsybl.sensitivity.converter;
 
-import com.powsybl.sensitivity.SensitivityComputationResults;
-import com.powsybl.sensitivity.json.SensitivityComputationResultJsonSerializer;
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Objects;
 
 import static org.junit.Assert.*;
@@ -26,17 +18,5 @@ public class SensitivityComputationResultExportersTest {
     public void testExporterForUnavailableFormat() {
         SensitivityComputationResultExporter exporter = SensitivityComputationResultExporters.getExporter("Invalid");
         assertTrue(Objects.isNull(exporter));
-    }
-
-    @Test
-    public void testExportWithFormat() throws IOException {
-
-        byte[] inputBytes = IOUtils.toByteArray(getClass().getResourceAsStream("/resultsExport.json"));
-        SensitivityComputationResults results = SensitivityComputationResultJsonSerializer.read(new InputStreamReader(new ByteArrayInputStream(inputBytes)));
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        SensitivityComputationResultExporters.export(results, new OutputStreamWriter(baos), "JSON");
-        byte[] outputBytes = baos.toByteArray();
-        assertArrayEquals(inputBytes, outputBytes);
     }
 }
