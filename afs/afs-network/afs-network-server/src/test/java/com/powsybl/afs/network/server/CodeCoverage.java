9bd931d4b93ef006f6041085491f66ded14b48ca
baguemat <mathieu.bague@rte-france.com>
2018-07-04 11:34:43 +0200

    Add dummy unit tests to initialize the code coverage

---
+++ afs/afs-network/afs-network-server/src/test/java/com/powsybl/afs/network/server/CodeCoverage.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.afs.network.server;
+
+import org.junit.Test;
+
+/**
+ * @author Mathieu Bague <mathieu.bague at rte-france.com>
+ */
+public class CodeCoverage {
+
+    @Test
+    public void testNothing() {
+    }
+}
