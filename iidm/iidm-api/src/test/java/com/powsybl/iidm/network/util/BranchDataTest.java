fa76d39ba61a11238de75dd770f58bbf160341ee
Luma Zamarreño <zamarrenolm@aia.es>
2018-07-09 10:30:51 +0200

    adjust tests for branch flow calculation; consider disconnected ends

--- iidm/iidm-api/src/test/java/com/powsybl/iidm/network/util/BranchDataTest.java
+++ iidm/iidm-api/src/test/java/com/powsybl/iidm/network/util/BranchDataTest.java
@@ -2,7 +2,6 @@ package com.powsybl.iidm.network.util;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,79 +10,126 @@ import com.powsybl.iidm.network.TwoTerminalsConnectable.Side;
 
 public class BranchDataTest {
 
-    private BranchTestCase cas2EntsoeLoadFlowExplicitLine;
-    private BranchTestCase cas2EntsoeLoadFlowExplicitPhaseShiftTransformer;
-    private Flow           cas2EntsoeLoadFlowExplicitLoad;
-    private Flow           cas2EntsoeLoadFlowExplicitGenerator;
+    // Some tests for a transmission line disconnected at one end
+    
+    @Test
+    public void testDanglingLine() {
+        BranchTestCase t = lineEnd2Disconnected();
 
-    @Before
-    public void setUp() {
-        BranchTestCase t;
+        // The expected values at the other end will be the same when we disconnect end 1 or end 2
+        // If we use the same voltage at the connected end
+        double expectedU = 381.9095;
+        double expectedTheta = -0.000503;
 
-        // Input data from ENTSO-E CASv2.0 test configuration ENTSOE_LoadFlowExplicit
+        // First obtain results when end 2 is disconnected
+        BranchData b2disconnected = checkTestCase("End 2 disconnected", t);
+        assertEquals(expectedU, b2disconnected.getComputedU2(), t.config.tolerance);
+        assertEquals(expectedTheta, b2disconnected.getComputedTheta2(), t.config.tolerance);
 
-        t = new BranchTestCase();
-        t.branch.id = "FFNOD0L41__FNO";
-        t.branch.end1.ratedU = 380;
-        t.branch.end2.ratedU = 380;
-        t.branch.end1.r = 4.1956;
-        t.branch.end1.x = 12.73;
-        t.bus1.u = 395.906724888442;
-        t.bus2.u = 397.1;
-        t.bus1.theta = Math.toRadians(-2.717121983205);
-        t.bus2.theta = Math.toRadians(0);
-        t.expectedFlow1.p = -534.9869;
-        t.expectedFlow1.q = 153.1046;
-        t.expectedFlow2.p = 543.2755;
-        t.expectedFlow2.q = -127.9559;
-        cas2EntsoeLoadFlowExplicitLine = t;
-
-        t = new BranchTestCase();
-        t.branch.id = "FNOD041__FNOD021__1_PT";
-        t.branch.end1.ratedU = 380;
-        t.branch.end2.ratedU = 380;
-        t.branch.end1.r = 4.1956;
-        t.branch.end1.x = 12.73;
-        t.bus1.u = 395.906724888442;
-        t.bus2.u = 397.1;
-        t.bus1.theta = Math.toRadians(-2.717121983205);
-        t.bus2.theta = Math.toRadians(0);
-        t.expectedFlow1.p = 202.9869;
-        t.expectedFlow1.q = -75.1046;
-        t.expectedFlow2.p = -201.7355;
-        t.expectedFlow2.q = 78.9091;
-
-        t.branch.end2.tap.rho = 0.997829;
-        t.branch.end2.tap.alpha = Math.toRadians(-3.77605);
-        cas2EntsoeLoadFlowExplicitPhaseShiftTransformer = t;
-
-        cas2EntsoeLoadFlowExplicitLoad = new Flow();
-        cas2EntsoeLoadFlowExplicitLoad.id = "Load";
-        cas2EntsoeLoadFlowExplicitLoad.p = 332.0;
-        cas2EntsoeLoadFlowExplicitLoad.q = -78.0;
-
-        cas2EntsoeLoadFlowExplicitGenerator = new Flow();
-        cas2EntsoeLoadFlowExplicitGenerator.id = "Generator";
-        cas2EntsoeLoadFlowExplicitGenerator.p = -341.54;
-        cas2EntsoeLoadFlowExplicitGenerator.q = 49.0468;
+        // Now change the disconnected end and check the same results are obtained
+        t.bus2.u = t.bus1.u;
+        t.bus2.theta = t.bus1.theta;
+        t.bus1.u = Double.NaN;
+        t.bus1.theta = Double.NaN;
+        t.branch.end2.connected = true;
+        t.branch.end1.connected = false;
+        t.expectedFlow2.p = t.expectedFlow1.p;
+        t.expectedFlow2.q = t.expectedFlow1.q;
+        t.expectedFlow1.p = Double.NaN;
+        t.expectedFlow1.q = Double.NaN;
+        BranchData b1disconnected = checkTestCase("End 1 disconnected", t);
+        assertEquals(expectedU, b1disconnected.getComputedU1(), t.config.tolerance);
+        assertEquals(expectedTheta, b1disconnected.getComputedTheta1(), t.config.tolerance);
     }
 
     @Test
+    public void testDanglingLineDifferentY() {
+        BranchTestCase t = lineEnd2Disconnected();
+        t.branch.id = "Dangling-Y1-Y2-different";
+        // End 1 admittance to ground has different value of End 2
+        t.branch.end1.b = t.branch.end2.b * 2;
+        double expectedU;
+        double expectedTheta;
+
+        // When end 2 is disconnected, the voltage at end 2
+        // should be the same that was obtained when Y1 = Y2
+        // because the voltage at end 2 depends only on Ytr and Y2
+        expectedU = 381.9095;
+        expectedTheta = -0.000503;
+        // But the flow seen at end 1 will be different
+        t.expectedFlow1.p = 0.0072927;
+        t.expectedFlow1.q = -43.392559;
+        BranchData b2disconnected = checkTestCase("End 2 disconnected", t);
+        assertEquals(expectedU, b2disconnected.getComputedU2(), t.config.tolerance);
+        assertEquals(expectedTheta, b2disconnected.getComputedTheta2(), t.config.tolerance);
+
+        // Now when we disconnect end 1 both the voltage drop and
+        // the expected values for flow are different
+        t.bus2.u = t.bus1.u;
+        t.bus2.theta = t.bus1.theta;
+        t.bus1.u = Double.NaN;
+        t.bus1.theta = Double.NaN;
+        t.branch.end2.connected = true;
+        t.branch.end1.connected = false;
+        expectedU = 383.838188;
+        expectedTheta = -0.001010;
+        t.expectedFlow1.p = Double.NaN;
+        t.expectedFlow1.q = Double.NaN;
+        t.expectedFlow2.p = 0.02946635;
+        t.expectedFlow2.q = -43.611687;
+        BranchData b1disconnected = checkTestCase("End 1 disconnected", t);
+        assertEquals(expectedU, b1disconnected.getComputedU1(), t.config.tolerance);
+        assertEquals(expectedTheta, b1disconnected.getComputedTheta1(), t.config.tolerance);
+    }
+
+    private BranchTestCase lineEnd2Disconnected() {
+        BranchTestCase t = new BranchTestCase();
+        t.config.asTransformer = false;
+
+        t.branch.id = "Dangling";
+        t.branch.end1.ratedU = 380;
+        t.branch.end2.ratedU = 380;
+        t.branch.end1.r = 5;
+        t.branch.end1.x = 50.0;
+        t.branch.end1.g = 0;
+        t.branch.end1.b = 0.0001;
+        t.branch.end2.g = 0;
+        t.branch.end2.b = 0.0001;
+
+        t.branch.end1.connected = true;
+        t.branch.end2.connected = false;
+        t.bus1.u = 380;
+        t.bus1.theta = 0;
+        t.bus2.u = Double.NaN;
+        t.bus2.theta = Double.NaN;
+
+        t.expectedFlow2.p = Double.NaN;
+        t.expectedFlow2.q = Double.NaN;
+        t.expectedFlow1.p = 0.007293;
+        t.expectedFlow1.q = -28.952559;
+        return t;
+    }
+
+    // Test cases related to ENTSO-E CASv2.0 test configuration ENTSOE_LoadFlowExplicit
+    // There are two parallel branches between a generator and a load bus: 
+    // a transmission line and a phase shift transformer
+
+    @Test
     public void testCAS2EntsoeLoadFlowExplicitLine() {
-        checkTestCase(cas2EntsoeLoadFlowExplicitLine);
+        checkTestCase("line", cas2EntsoeLoadFlowExplicitLine());
     }
 
     @Test
     public void testCAS2EntsoeLoadFlowExplicitPhaseShiftTransformer() {
-        checkTestCase(cas2EntsoeLoadFlowExplicitPhaseShiftTransformer);
+        checkTestCase("pst", cas2EntsoeLoadFlowExplicitPhaseShiftTransformer());
     }
 
     @Test
     public void testCAS2EntsoeLoadFlowExplicitLineAndPhaseShifter() {
-        BranchTestCase line = cas2EntsoeLoadFlowExplicitLine;
-        BranchTestCase pst = cas2EntsoeLoadFlowExplicitPhaseShiftTransformer;
-        Flow load = cas2EntsoeLoadFlowExplicitLoad;
-        Flow generator = cas2EntsoeLoadFlowExplicitGenerator;
+        BranchTestCase line = cas2EntsoeLoadFlowExplicitLine();
+        BranchTestCase pst = cas2EntsoeLoadFlowExplicitPhaseShiftTransformer();
+        Flow load = cas2EntsoeLoadFlowExplicitLoad();
+        Flow generator = cas2EntsoeLoadFlowExplicitGenerator();
 
         // Obtain flows at both ends of line and phase shifter
         BranchData pline = piModelFlows(line);
@@ -108,10 +154,10 @@ public class BranchDataTest {
 
     @Test
     public void testCAS2EntsoeLoadFlowExplicitLineAndPhaseShifterMovedToEnd1() {
-        BranchTestCase line = cas2EntsoeLoadFlowExplicitLine;
-        BranchTestCase pst = cas2EntsoeLoadFlowExplicitPhaseShiftTransformer;
-        Flow load = cas2EntsoeLoadFlowExplicitLoad;
-        Flow generator = cas2EntsoeLoadFlowExplicitGenerator;
+        BranchTestCase line = cas2EntsoeLoadFlowExplicitLine();
+        BranchTestCase pst = cas2EntsoeLoadFlowExplicitPhaseShiftTransformer();
+        Flow load = cas2EntsoeLoadFlowExplicitLoad();
+        Flow generator = cas2EntsoeLoadFlowExplicitGenerator();
 
         // Move phase shift from end 2 to end 1
         pst.branch.end1.tap.rho = 1 / pst.branch.end2.tap.rho;
@@ -140,54 +186,120 @@ public class BranchDataTest {
         checkBusBalance("End 2", 1e-2, 1e-4, line2, pst2, generator);
     }
 
-    private void checkTestCase(BranchTestCase t) {
+    private BranchTestCase cas2EntsoeLoadFlowExplicitLine() {
+        BranchTestCase t = new BranchTestCase();
+        t.branch.id = "FFNOD0L41__FNO";
+        t.branch.end1.ratedU = 380;
+        t.branch.end2.ratedU = 380;
+        t.branch.end1.r = 4.1956;
+        t.branch.end1.x = 12.73;
+        t.bus1.u = 395.906724888442;
+        t.bus2.u = 397.1;
+        t.bus1.theta = Math.toRadians(-2.717121983205);
+        t.bus2.theta = Math.toRadians(0);
+        t.expectedFlow1.p = -534.9869;
+        t.expectedFlow1.q = 153.1046;
+        t.expectedFlow2.p = 543.2755;
+        t.expectedFlow2.q = -127.9559;
+        return t;
+    }
+
+    private BranchTestCase cas2EntsoeLoadFlowExplicitPhaseShiftTransformer() {
+        BranchTestCase t = new BranchTestCase();
+        t.branch.id = "FNOD041__FNOD021__1_PT";
+        t.branch.end1.ratedU = 380;
+        t.branch.end2.ratedU = 380;
+        t.branch.end1.r = 4.1956;
+        t.branch.end1.x = 12.73;
+        t.bus1.u = 395.906724888442;
+        t.bus2.u = 397.1;
+        t.bus1.theta = Math.toRadians(-2.717121983205);
+        t.bus2.theta = Math.toRadians(0);
+        t.expectedFlow1.p = 202.9869;
+        t.expectedFlow1.q = -75.1046;
+        t.expectedFlow2.p = -201.7355;
+        t.expectedFlow2.q = 78.9091;
+
+        t.branch.end2.tap.rho = 0.997829;
+        t.branch.end2.tap.alpha = Math.toRadians(-3.77605);
+        return t;
+    };
+
+    private Flow cas2EntsoeLoadFlowExplicitLoad() {
+        Flow f = new Flow();
+        f.id = "Load";
+        f.p = 332.0;
+        f.q = -78.0;
+        return f;
+    }
+
+    private Flow cas2EntsoeLoadFlowExplicitGenerator() {
+        Flow f = new Flow();
+        f.id = "Generator";
+        f.p = -341.54;
+        f.q = 49.0468;
+        return f;
+    }
+
+    private BranchData checkTestCase(String title, BranchTestCase t) {
         BranchData b = piModelFlows(t);
-        LOG.debug("");
-        LOG.debug("Results for " + b.getId());
-        LOG.debug(String.format("   end1 computed = %14.6f  %14.6f",
-                b.getComputedP1(),
-                b.getComputedQ1()));
-        LOG.debug(String.format("   end1 expected = %14.6f  %14.6f",
-                t.expectedFlow1.p,
-                t.expectedFlow1.q));
-        LOG.debug(String.format("   end1 diff     = %14.6f  %14.6f",
-                Math.abs(t.expectedFlow1.p - b.getComputedP1()),
-                Math.abs(t.expectedFlow1.q - b.getComputedQ1())));
+        logTestCase(title, t, b);
         assertEquals(t.expectedFlow1.p, b.getComputedP1(), t.config.tolerance);
         assertEquals(t.expectedFlow2.p, b.getComputedP2(), t.config.tolerance);
         assertEquals(t.expectedFlow1.q, b.getComputedQ1(), t.config.tolerance);
         assertEquals(t.expectedFlow2.q, b.getComputedQ2(), t.config.tolerance);
+        return b;
     }
 
     private BranchData piModelFlows(BranchTestCase t) {
         // Compute final PI model values
-        double rho0 = t.branch.end2.ratedU / t.branch.end1.ratedU;
-        double rho0Square = rho0 * rho0;
-        double r0 = t.branch.end1.r * rho0Square + t.branch.end2.r;
-        double x0 = t.branch.end1.x * rho0Square + t.branch.end2.x;
-        double g0 = t.branch.end1.g / rho0Square + t.branch.end2.g;
-        double b0 = t.branch.end1.b / rho0Square + t.branch.end2.b;
-        double r = r0 * (1 + t.branch.end1.tap.r / 100);
-        double x = x0 * (1 + t.branch.end1.tap.x / 100);
+        double r;
+        double x;
         double g1;
         double b1;
         double g2;
         double b2;
-        if (t.config.specificCompatibility) {
-            g1 = g0 / 2 * (1 + t.branch.end1.tap.g / 100);
-            b1 = b0 / 2 * (1 + t.branch.end1.tap.b / 100);
-            g2 = b0 / 2;
-            b2 = b0 / 2;
+        double rho1;
+        double alpha1;
+        double rho2;
+        double alpha2;
+
+        if (t.config.asTransformer) {
+            double rho0 = t.branch.end2.ratedU / t.branch.end1.ratedU;
+            double rho0Square = rho0 * rho0;
+            double r0 = t.branch.end1.r * rho0Square + t.branch.end2.r;
+            double x0 = t.branch.end1.x * rho0Square + t.branch.end2.x;
+            double g0 = t.branch.end1.g / rho0Square + t.branch.end2.g;
+            double b0 = t.branch.end1.b / rho0Square + t.branch.end2.b;
+            r = r0 * (1 + t.branch.end1.tap.r / 100);
+            x = x0 * (1 + t.branch.end1.tap.x / 100);
+            if (t.config.specificCompatibility) {
+                g1 = g0 / 2 * (1 + t.branch.end1.tap.g / 100);
+                b1 = b0 / 2 * (1 + t.branch.end1.tap.b / 100);
+                g2 = g0 / 2;
+                b2 = b0 / 2;
+            } else {
+                g1 = g0;
+                b1 = b0;
+                g2 = 0;
+                b2 = 0;
+            }
+            rho1 = rho0 * t.branch.end1.tap.rho;
+            rho2 = t.branch.end2.tap.rho;
+            alpha1 = t.branch.end1.tap.alpha;
+            alpha2 = t.branch.end2.tap.alpha;
         } else {
-            g1 = g0;
-            b1 = b0;
-            g2 = 0;
-            b2 = 0;
+            r = t.branch.end1.r;
+            x = t.branch.end1.x;
+            g1 = t.branch.end1.g;
+            b1 = t.branch.end1.b;
+            g2 = t.branch.end2.g;
+            b2 = t.branch.end2.b;
+            rho1 = t.branch.end1.tap.rho;
+            rho2 = t.branch.end2.tap.rho;
+            alpha1 = t.branch.end1.tap.alpha;
+            alpha2 = t.branch.end2.tap.alpha;
         }
-        double rho1 = rho0 * t.branch.end1.tap.rho;
-        double rho2 = t.branch.end2.tap.rho;
-        double alpha1 = t.branch.end1.tap.alpha;
-        double alpha2 = t.branch.end2.tap.alpha;
 
         // Build final PI model
         // (and compute flows on it)
@@ -198,7 +310,7 @@ public class BranchDataTest {
                 alpha1, alpha2,
                 g1, g2, b1, b2,
                 t.expectedFlow1.p, t.expectedFlow1.q, t.expectedFlow2.p, t.expectedFlow2.q,
-                t.bus1.connected, t.bus2.connected,
+                t.branch.end1.connected, t.branch.end2.connected,
                 t.bus1.mainComponent, t.bus2.mainComponent,
                 t.config.epsilonX, t.config.applyReactanceCorrection);
     }
@@ -208,6 +320,37 @@ public class BranchDataTest {
         Flow mismatch = sum(flows);
         assertEquals(0, mismatch.p, ptol);
         assertEquals(0, mismatch.q, qtol);
+    }
+
+    private void logTestCase(String title, BranchTestCase t, BranchData b) {
+        LOG.debug("");
+        LOG.debug("Results for " + title + " branch " + b.getId());
+        LOG.debug("End1");
+        LOG.debug(String.format("    V          = %14.6f  %14.6f",
+                b.getComputedU1(),
+                b.getComputedTheta1()));
+        LOG.debug(String.format("    S expected = %14.6f  %14.6f",
+                t.expectedFlow1.p,
+                t.expectedFlow1.q));
+        LOG.debug(String.format("    S actual   = %14.6f  %14.6f",
+                b.getComputedP1(),
+                b.getComputedQ1()));
+        LOG.debug(String.format("    diff       = %14.6f  %14.6f",
+                Math.abs(t.expectedFlow1.p - b.getComputedP1()),
+                Math.abs(t.expectedFlow1.q - b.getComputedQ1())));
+        LOG.debug("End2");
+        LOG.debug(String.format("    V          = %14.6f  %14.6f",
+                b.getComputedU2(),
+                b.getComputedTheta2()));
+        LOG.debug(String.format("    S expected = %14.6f  %14.6f",
+                t.expectedFlow2.p,
+                t.expectedFlow2.q));
+        LOG.debug(String.format("    S actual   = %14.6f  %14.6f",
+                b.getComputedP2(),
+                b.getComputedQ2()));
+        LOG.debug(String.format("    diff       = %14.6f  %14.6f",
+                Math.abs(t.expectedFlow2.p - b.getComputedP2()),
+                Math.abs(t.expectedFlow2.q - b.getComputedQ2())));
     }
 
     private void logBusBalance(String title, Flow... flows) {
@@ -253,12 +396,13 @@ public class BranchDataTest {
     }
 
     static class BranchEnd {
-        double ratedU = 1;
-        double r;
-        double x;
-        double g      = 0;
-        double b      = 0;
-        Tap    tap    = new Tap();
+        double  ratedU    = 1;
+        double  r;
+        double  x;
+        double  g         = 0;
+        double  b         = 0;
+        Tap     tap       = new Tap();
+        boolean connected = true;
     }
 
     static class Tap {
@@ -273,7 +417,6 @@ public class BranchDataTest {
     static class Bus {
         double  u;
         double  theta;
-        boolean connected     = true;
         boolean mainComponent = true;
     }
 
@@ -284,6 +427,7 @@ public class BranchDataTest {
     }
 
     static class Config {
+        boolean asTransformer            = true;
         double  tolerance                = 0.01;
         boolean specificCompatibility    = true;
         boolean applyReactanceCorrection = false;
