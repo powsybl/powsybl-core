a5429f031d5bbde00a2f658c5bc9966239c606b4
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2018-07-17 17:23:46 +0200

    Add etc et lib

--- itools-plugin/src/main/java/com/powsybl/itools/plugin/ItoolsMojo.java
+++ itools-plugin/src/main/java/com/powsybl/itools/plugin/ItoolsMojo.java
@@ -13,12 +13,19 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
 import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -31,6 +38,18 @@ public class ItoolsMojo extends AbstractMojo {
 
     @Parameter(readonly = true, defaultValue = "${project}")
     private MavenProject project;
+
+    @Parameter
+    private String bundleName;
+
+    @Parameter(defaultValue = "8G")
+    private String javaXmx;
+
+    @Parameter
+    private File[] binDirectories;
+
+    @Parameter
+    private File[] libDirectories;
 
     private static void zip(Path folder, Path zipFilePath) throws IOException {
         try (ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(zipFilePath))) {
@@ -53,17 +72,51 @@ public class ItoolsMojo extends AbstractMojo {
         }
     }
 
+    private void copyDir(Path srcDir, Path destDir) {
+        try (Stream<Path> stream = Files.list(srcDir)) {
+            stream.forEach(file -> {
+                if (Files.isRegularFile(file)) {
+                    try {
+                        Files.copy(file, destDir.resolve(file.getFileName().toString()), StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                }
+            });
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private void writeItoolsConf(BufferedWriter writer) throws IOException {
+        Objects.requireNonNull(writer);
+        Objects.requireNonNull(javaXmx);
+        writer.write("#itools_cache_dir=");
+        writer.newLine();
+        writer.write("#itools_config_dir=");
+        writer.newLine();
+        writer.write("itools_config_name=config");
+        writer.newLine();
+        writer.write("java_xmx=");
+        writer.write(javaXmx);
+        writer.newLine();
+        writer.write("mpi_tasks=3");
+        writer.newLine();
+        writer.write("mpi_hosts=localhost");
+        writer.newLine();
+    }
+
     @Override
     public void execute() {
         try {
-            String bundleName = project.getBuild().getFinalName();
+            String bundleNameNotNull = bundleName != null ? bundleName : project.getBuild().getFinalName();
             Path targetDir = Paths.get(project.getBuild().getDirectory());
             Path bundleDir = targetDir.resolve(bundleName);
 
             // copy jars
             Path javaDir = bundleDir.resolve("share").resolve("java");
             Files.createDirectories(javaDir);
-            for (Artifact artifact : (Set<Artifact>) project.getArtifacts()) {
+            for (Artifact artifact : project.getArtifacts()) {
                 Path jar = artifact.getFile().toPath();
                 getLog().info("Add jar " + jar + " to bundle");
                 Files.copy(jar, javaDir.resolve(jar.getFileName()), StandardCopyOption.REPLACE_EXISTING);
@@ -74,11 +127,40 @@ public class ItoolsMojo extends AbstractMojo {
             Files.createDirectories(binDir);
             for (String script : Arrays.asList("itools", "itools.bat", "powsyblsh")) {
                 getLog().info("Add script " + script + " to bundle");
-                Files.copy(ItoolsMojo.class.getResourceAsStream("/" + script), binDir.resolve(script), StandardCopyOption.REPLACE_EXISTING);
+                Path file = binDir.resolve(script);
+                Files.copy(ItoolsMojo.class.getResourceAsStream("/" + script), file, StandardCopyOption.REPLACE_EXISTING);
+                Set<PosixFilePermission> perms = EnumSet.of(PosixFilePermission.OWNER_READ,
+                                                            PosixFilePermission.OWNER_WRITE,
+                                                            PosixFilePermission.OWNER_EXECUTE,
+                                                            PosixFilePermission.GROUP_READ,
+                                                            PosixFilePermission.GROUP_EXECUTE);
+                Files.setPosixFilePermissions(file, perms);
+            }
+            if (binDirectories != null) {
+                for (File dir : binDirectories) {
+                    copyDir(dir.toPath(), binDir);
+                }
+            }
+
+            // create etc directory
+            Path etcDir = bundleDir.resolve("etc");
+            Files.createDirectories(etcDir);
+            Files.copy(ItoolsMojo.class.getResourceAsStream("/logback-itools.xml"), etcDir.resolve("logback-itools.xml"), StandardCopyOption.REPLACE_EXISTING);
+            try (BufferedWriter writer = Files.newBufferedWriter(etcDir.resolve("itools.conf"), StandardCharsets.UTF_8)) {
+                writeItoolsConf(writer);
+            }
+
+            // create lib dir
+            Path libDir = bundleDir.resolve("lib");
+            Files.createDirectories(libDir);
+            if (libDirectories != null) {
+                for (File dir : libDirectories) {
+                    copyDir(dir.toPath(), libDir);
+                }
             }
 
             getLog().info("Zip bundle");
-            zip(bundleDir, targetDir.resolve(bundleName + ".zip"));
+            zip(bundleDir, targetDir.resolve(bundleNameNotNull + ".zip"));
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
