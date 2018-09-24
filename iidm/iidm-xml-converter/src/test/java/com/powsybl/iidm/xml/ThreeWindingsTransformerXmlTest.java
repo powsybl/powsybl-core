71d1195b13f127de8f0edec70a538f031fbafdff
yichen88 <tang.yichenyves@gmail.com>
2018-09-24 10:03:43 +0200

    Rename the step attributes #79

--- iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/ThreeWindingsTransformerXmlTest.java
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/ThreeWindingsTransformerXmlTest.java
@@ -7,11 +7,16 @@
 package com.powsybl.iidm.xml;
 
 import java.io.IOException;
+import java.io.InputStream;
 
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.ThreeWindingsTransformer;
 import org.junit.Test;
 
 import com.powsybl.commons.AbstractConverterTest;
 import com.powsybl.iidm.network.test.ThreeWindingsTransformerNetworkFactory;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Luma Zamarreno <zamarrenolm at aia.es>
@@ -24,5 +29,17 @@ public class ThreeWindingsTransformerXmlTest extends AbstractConverterTest {
                          NetworkXml::writeAndValidate,
                          NetworkXml::read,
                          "/threeWindingsTransformerRoundTripRef.xml");
+    }
+
+    @Test
+    public void testReadV10() {
+        InputStream in = getClass().getResourceAsStream("/refsV1_0/threeWindingsTransformerRoundTripRef.xml");
+        Network read = NetworkXml.read(in);
+        ThreeWindingsTransformer.Leg2or3 leg2 = read.getThreeWindingsTransformer("3WT").getLeg2();
+        ThreeWindingsTransformer.Leg2or3 leg3 = read.getThreeWindingsTransformer("3WT").getLeg3();
+        assertEquals(0.9801, leg2.getRatioTapChanger().getStep(0).getR(), 0.0);
+        assertEquals(0.1089, leg2.getRatioTapChanger().getStep(1).getX(), 0.0);
+        assertEquals(0.8264462809917356, leg3.getRatioTapChanger().getStep(1).getG(), 0.0);
+        assertEquals(0.09090909090909093, leg3.getRatioTapChanger().getStep(2).getB(), 0.0);
     }
 }
