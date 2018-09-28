93885cc8dd4c6f64ad46c1b4d9bf546acf9c5151
ralambotianamio <miora.ralambotiana@gmail.com>
2018-09-28 16:55:42 +0200

    add licence

--- action/action-dsl/src/test/java/com/powsybl/action/dsl/ThrowExceptionsSameIdTest.java
+++ action/action-dsl/src/test/java/com/powsybl/action/dsl/ThrowExceptionsSameIdTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2017, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.action.dsl;
 
 import com.powsybl.commons.PowsyblException;
@@ -9,6 +15,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+/**
+ * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
+ */
 public class ThrowExceptionsSameIdTest {
 
     @Rule
