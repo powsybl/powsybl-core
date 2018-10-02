4131bf176ad55ad634743a8404d67b24b229d46e
calinbanc <calinbanc@yahoo.com>
2018-10-02 14:28:46 +0200

    extendable contingency

--- contingency-api/src/main/java/com/powsybl/contingency/json/ContingencyDeserializer.java
+++ contingency-api/src/main/java/com/powsybl/contingency/json/ContingencyDeserializer.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017, RTE (http://www.rte-france.com)
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -11,6 +11,12 @@ import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.powsybl.commons.extensions.Extension;
+import com.powsybl.commons.extensions.ExtensionJsonSerializer;
+import com.powsybl.commons.extensions.ExtensionProviders;
+import com.powsybl.commons.json.JsonUtil;
 import com.powsybl.contingency.Contingency;
 import com.powsybl.contingency.ContingencyElement;
 
@@ -22,17 +28,24 @@ import java.util.List;
 /**
  *
  * @author Massimo Ferraro <massimo.ferraro@techrain.it>
+ * @author Teofil Calin BANC <teofil-calin.banc at rte-france.com>
  */
 public class ContingencyDeserializer extends StdDeserializer<Contingency> {
+
+    private static final Supplier<ExtensionProviders<ExtensionJsonSerializer>> SUPPLIER =
+            Suppliers.memoize(() -> ExtensionProviders.createProvider(ExtensionJsonSerializer.class, "security-analysis"));
 
     public ContingencyDeserializer() {
         super(Contingency.class);
     }
 
     @Override
-    public Contingency deserialize(JsonParser parser, DeserializationContext ctx) throws IOException {
+    public Contingency deserialize(JsonParser parser, DeserializationContext deserializationContext) throws IOException {
         String id = null;
         List<ContingencyElement> elements = Collections.emptyList();
+
+        List<Extension<Contingency>> extensions = Collections.emptyList();
+
 
         while (parser.nextToken() != JsonToken.END_OBJECT) {
             switch (parser.getCurrentName()) {
@@ -46,11 +59,19 @@ public class ContingencyDeserializer extends StdDeserializer<Contingency> {
                     });
                     break;
 
+                case "extensions":
+                    parser.nextToken();
+                    extensions = JsonUtil.readExtensions(parser, deserializationContext, SUPPLIER.get());
+                    break;
+
                 default:
                     throw new AssertionError("Unexpected field: " + parser.getCurrentName());
             }
         }
 
-        return new Contingency(id, elements);
+        Contingency contingency = new Contingency(id, elements);
+        SUPPLIER.get().addExtensions(contingency, extensions);
+
+        return contingency;
     }
 }
