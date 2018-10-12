ddb61771be44e3d7313dc950ab6b80f7e1af8f39
ralambotianamio <miora.ralambotiana@gmail.com>
2018-10-12 15:38:13 +0200

    add licence + author

--- commons/src/main/java/com/powsybl/commons/config/VersionConfig.java
+++ commons/src/main/java/com/powsybl/commons/config/VersionConfig.java
@@ -1,5 +1,14 @@
+/**
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.commons.config;
 
+/**
+ * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
+ */
 public class VersionConfig {
 
     private final String[] version;
