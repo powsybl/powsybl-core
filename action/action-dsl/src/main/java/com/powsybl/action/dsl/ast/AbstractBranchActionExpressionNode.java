60d0cae34cc538afe66b4b83a1ffa67fd3b60c9b
Mathieu BAGUE <mathieu.bague@gmail.com>
2018-12-01 10:25:01 +0100

    Fix author and copyright information

--- action/action-dsl/src/main/java/com/powsybl/action/dsl/ast/AbstractBranchActionExpressionNode.java
+++ action/action-dsl/src/main/java/com/powsybl/action/dsl/ast/AbstractBranchActionExpressionNode.java
@@ -1,19 +1,21 @@
 /**
- * Copyright (c) 2017, RTE (http://www.rte-france.com)
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package com.powsybl.action.dsl.ast;
 
-
 import java.util.List;
 import java.util.Objects;
 
-
+/**
+ * @author Olivier Bretteville <olivier.bretteville at rte-france.com>
+ */
 public abstract class AbstractBranchActionExpressionNode extends AbstractActionExpressionNode {
 
     private final List<String> branchIds;
+    
     private final float limitReduction;
 
     public AbstractBranchActionExpressionNode(List<String> branchIds, float limitReduction) {
