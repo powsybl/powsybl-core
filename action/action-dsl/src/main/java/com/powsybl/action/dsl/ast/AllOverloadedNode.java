3be0016cf0d994724b1f852dc28d878530c42be5
Mathieu BAGUE <mathieu.bague@gmail.com>
2018-12-01 10:26:11 +0100

    Fix author information

--- action/action-dsl/src/main/java/com/powsybl/action/dsl/ast/AllOverloadedNode.java
+++ action/action-dsl/src/main/java/com/powsybl/action/dsl/ast/AllOverloadedNode.java
@@ -6,10 +6,11 @@
  */
 package com.powsybl.action.dsl.ast;
 
-
 import java.util.List;
 
-
+/**
+ * @author Olivier Bretteville <olivier.bretteville at rte-france.com>
+ */
 public class AllOverloadedNode extends AbstractBranchActionExpressionNode {
 
     public AllOverloadedNode(List<String> branchIds, float limitReduction) {
