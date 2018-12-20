ed662011ee8ccb42e9e3575465ca55b9702cc732
Mathieu BAGUE <mathieu.bague@rte-france.com>
2018-12-20 10:31:52 +0100

    Fix coding style

--- iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/NetworksTest.java
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/NetworksTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018, All partners of the iTesla project (http://www.itesla-project.eu/consortium)
+ * Copyright (c) 2018, RTE (https://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -13,6 +13,7 @@ import com.powsybl.iidm.network.util.Networks;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+
 /**
  * @author Chamseddine BENHAMED <chamseddine.benhamed at rte-france.com>
  */
@@ -20,23 +21,22 @@ public class NetworksTest {
 
     @Test
     public void printBalanceSummaryTest()  {
-
         Network network = EurostagTutorialExample1Factory.create();
         CustomLogger logger = new CustomLogger("LoggerForTest");
         logger.setLogLevel("debug");
         Networks.printBalanceSummary("", network, logger);
         assertEquals("[DEBUG] Active balance at step '':\n" +
-                        "+-----------------------+--------------------------------+----------------------------------+\n" +
-                        "|                       | Main CC connected/disconnected | Others CC connected/disconnected |\n" +
-                        "+-----------------------+--------------------------------+----------------------------------+\n" +
-                        "| Bus count             | 4                              | 0                                |\n" +
-                        "| Load count            | 1           | 0                | 0           | 0                  |\n" +
-                        "| Load (MW)             | 600.0       | 0.0              | 0.0         | 0.0                |\n" +
-                        "| Generator count       | 1           | 0                | 0           | 0                  |\n" +
-                        "| Max generation (MW)   | 9999.99     | 0.0              | 0.0         | 0.0                |\n" +
-                        "| Generation (MW)       | 607.0       | 0.0              | 0.0         | 0.0                |\n" +
-                        "| Shunt at nom V (MVar) | 0.0 0.0 (0) | 0.0 0.0 (0)      | 0.0 0.0 (0) | 0.0 0.0 (0)        |\n" +
-                        "+-----------------------+-------------+------------------+-------------+--------------------+" + System.lineSeparator(),
+                     "+-----------------------+--------------------------------+----------------------------------+\n" +
+                     "|                       | Main CC connected/disconnected | Others CC connected/disconnected |\n" +
+                     "+-----------------------+--------------------------------+----------------------------------+\n" +
+                     "| Bus count             | 4                              | 0                                |\n" +
+                     "| Load count            | 1           | 0                | 0           | 0                  |\n" +
+                     "| Load (MW)             | 600.0       | 0.0              | 0.0         | 0.0                |\n" +
+                     "| Generator count       | 1           | 0                | 0           | 0                  |\n" +
+                     "| Max generation (MW)   | 9999.99     | 0.0              | 0.0         | 0.0                |\n" +
+                     "| Generation (MW)       | 607.0       | 0.0              | 0.0         | 0.0                |\n" +
+                     "| Shunt at nom V (MVar) | 0.0 0.0 (0) | 0.0 0.0 (0)      | 0.0 0.0 (0) | 0.0 0.0 (0)        |\n" +
+                     "+-----------------------+-------------+------------------+-------------+--------------------+" + System.lineSeparator(),
                 logger.getContent());
     }
 }
