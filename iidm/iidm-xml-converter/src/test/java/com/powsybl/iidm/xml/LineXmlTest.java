35597d923e061735e87d6d028d869d124f486462
yichen88 <tang.yichenyves@gmail.com>
2019-01-15 12:59:29 +0100

    Add lines xml test

---
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/LineXmlTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.xml;
+
+import com.powsybl.commons.AbstractConverterTest;
+import com.powsybl.iidm.network.test.NoEquipmentNetworkFactory;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author Yichen TANG <yichen.tang at rte-france.com>
+ */
+public class LineXmlTest extends AbstractConverterTest {
+
+    @Test
+    public void tieLine() throws IOException {
+        roundTripXmlTest(NoEquipmentNetworkFactory.createWithTieLine(),
+                NetworkXml::writeAndValidate,
+                NetworkXml::read, "/tieLineRef.xml");
+    }
+
+    @Test
+    public void danglingLine() throws IOException {
+        roundTripXmlTest(NoEquipmentNetworkFactory.createWithDanglingLine(),
+                NetworkXml::writeAndValidate,
+                NetworkXml::read, "/danglingLineRef.xml");
+    }
+}
