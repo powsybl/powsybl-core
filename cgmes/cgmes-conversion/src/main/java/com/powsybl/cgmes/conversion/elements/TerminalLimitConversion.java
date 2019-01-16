9b0bb7f9c5517e3c753bc344f2adf6ff45221502
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2019-01-16 15:31:15 +0100

    Fix

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/TerminalLimitConversion.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/TerminalLimitConversion.java
@@ -8,10 +8,7 @@
 package com.powsybl.cgmes.conversion.elements;
 
 import com.powsybl.cgmes.conversion.Conversion;
-import com.powsybl.iidm.network.Branch;
-import com.powsybl.iidm.network.Connectable;
-import com.powsybl.iidm.network.Identifiable;
-import com.powsybl.iidm.network.Terminal;
+import com.powsybl.iidm.network.*;
 import com.powsybl.triplestore.api.PropertyBag;
 
 /**
@@ -21,14 +18,24 @@ public class TerminalLimitConversion extends AbstractIdentifiedObjectConversion 
 
     public TerminalLimitConversion(PropertyBag l, Conversion.Context context) {
         super("OperationalLimitSet", l, context);
+        // limit can associated to a terminal, a branch or a dangling line
         terminalId = l.getId("Terminal");
-        terminal = context.terminalMapping().find(terminalId);
+        if (terminalId != null) {
+            terminal = context.terminalMapping().find(terminalId);
+        }
+        equipmentId = l.getId("Equipment");
+        if (equipmentId != null) {
+            branch = context.network().getBranch(equipmentId);
+            if (branch == null) {
+                danglingLine = context.network().getDanglingLine(equipmentId);
+            }
+        }
     }
 
     @Override
     public boolean valid() {
-        if (terminal == null) {
-            missing(String.format("Terminal %s", terminalId));
+        if (terminal == null && branch == null && danglingLine == null) {
+            missing(String.format("Terminal %s or Equipment %s", terminalId, equipmentId));
             return false;
         }
         return true;
@@ -37,11 +44,10 @@ public class TerminalLimitConversion extends AbstractIdentifiedObjectConversion 
     @Override
     public void convert() {
         String limitTypeName = p.getLocal("operationalLimitTypeName");
-        String limitSubtype = p.getLocal("OperationalLimitSubtype");
+        String limitSubtype = p.getLocal("OperationalLimitSubclass");
         String limitType = p.getLocal("limitType");
         double value = p.asDouble("value");
-        int terminalNumber = context.terminalMapping().number(terminalId);
-        Connectable eq = terminal.getConnectable();
+        Identifiable eq = null;
         boolean assigned = false;
         if (limitTypeName.equals("PATL") || "LimitTypeKind.patl".equals(limitType)) {
             if (value <= 0) {
@@ -51,24 +57,41 @@ public class TerminalLimitConversion extends AbstractIdentifiedObjectConversion 
                     // Enhancement: we should be able to use a CurrentLimitsAdder (an owner)
                     // to avoid checking the class of the equipment
                     // In terminal mapping insert a CurrentLimitsAdder instead of a Branch.side
-                    if (eq instanceof Branch) {
-                        Branch b = (Branch) eq;
-                        if (terminalNumber == 1) {
-                            b.newCurrentLimits1().setPermanentLimit(value).add();
-                            assigned = true;
-                        } else if (terminalNumber == 2) {
-                            b.newCurrentLimits2().setPermanentLimit(value).add();
-                            assigned = true;
+                    if (terminal != null) {
+                        int terminalNumber = context.terminalMapping().number(terminalId);
+                        Connectable connectable = terminal.getConnectable();
+                        if (connectable instanceof Branch) {
+                            Branch b = (Branch) connectable;
+                            if (terminalNumber == 1) {
+                                b.newCurrentLimits1().setPermanentLimit(value).add();
+                                assigned = true;
+                            } else if (terminalNumber == 2) {
+                                b.newCurrentLimits2().setPermanentLimit(value).add();
+                                assigned = true;
+                            }
                         }
+                        eq = connectable;
+                    } else if (branch != null) {
+                        // set limit at both sides
+                        branch.newCurrentLimits1().setPermanentLimit(value).add();
+                        branch.newCurrentLimits2().setPermanentLimit(value).add();
+                        eq = branch;
+                    } else if (danglingLine != null) {
+                        danglingLine.newCurrentLimits().setPermanentLimit(value).add();
+                        eq = danglingLine;
                     }
+                } else if (limitSubtype.equals("ApparentPowerLimit")) {
+                    // TODO
+                } else if (limitSubtype.equals("VoltageLimit")) {
+                    // TODO
                 }
             }
         }
         if (!assigned) {
             String reason = String.format(
                     "Not assigned for %s %s. Limit id, type, terminal : %s, %s, %s",
-                    className(eq),
-                    eq.getId(),
+                    eq != null ? className(eq) : "",
+                    eq != null ? eq.getId() : "",
                     id,
                     type,
                     terminal);
@@ -87,5 +110,9 @@ public class TerminalLimitConversion extends AbstractIdentifiedObjectConversion 
     }
 
     private final String terminalId;
-    private final Terminal terminal;
+    private Terminal terminal;
+
+    private final String equipmentId;
+    private Branch branch;
+    private DanglingLine danglingLine;
 }
