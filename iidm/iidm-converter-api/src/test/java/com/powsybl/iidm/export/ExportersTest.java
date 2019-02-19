e275ccf8c097106fc3dee5bdaa5b9f31ca285b28
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2019-02-19 13:00:49 +0100

    fix checkstyle

--- iidm/iidm-converter-api/src/test/java/com/powsybl/iidm/export/ExportersTest.java
+++ iidm/iidm-converter-api/src/test/java/com/powsybl/iidm/export/ExportersTest.java
@@ -23,7 +23,6 @@ import java.util.Collections;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
