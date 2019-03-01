98ee76b6a80391ebf3ebb77d48772f23effc0a28
MioRtia <miora.ralambotiana@rte-france.com>
2019-03-01 08:39:31 +0100

    add convert params in tools importing network (#651)

--- action/action-simulator/src/test/java/com/powsybl/action/simulator/tools/ActionSimulatorToolTest.java
+++ action/action-simulator/src/test/java/com/powsybl/action/simulator/tools/ActionSimulatorToolTest.java
@@ -46,7 +46,7 @@ public class ActionSimulatorToolTest extends AbstractToolTest {
         ActionSimulatorTool tool = new ActionSimulatorTool();
         Command command = tool.getCommand();
 
-        assertCommand(command, "action-simulator", 13, 2);
+        assertCommand(command, "action-simulator", 15, 2);
         assertOption(command.getOptions(), "case-file", true, true);
         assertOption(command.getOptions(), "dsl-file", true, true);
         assertOption(command.getOptions(), "contingencies", false, true);
