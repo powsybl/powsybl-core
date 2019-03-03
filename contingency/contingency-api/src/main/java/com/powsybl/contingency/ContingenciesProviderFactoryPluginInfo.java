1457dd81a484687e34592ffcdadaf7145c7d01e0
pengbo-ruidian <47722616+pengbo-ruidian@users.noreply.github.com>
2019-03-03 19:54:51 +0100

    see #483: add SecurityAnalysis in itools pluginInfo. (#630)
    * see #483: add SecurityAnalysis in itools pluginInfo.
    
    * Revert "see #483: add SecurityAnalysis in itools pluginInfo."
    
    This reverts commit d0541f9632983567210754548d9769ae2136e0d1.
    
    * add SecurityAnalysisFactory in itools pluginInfo. https://github.com/powsybl/powsybl-core/issues/483
    
    * change class.fullname to class.simipleName for better display.
    
    * remove autoservice in SA factory impl.
    
    * remove empty line before import
    
    * Revert "remove autoservice in SA factory impl."
    
    This reverts commit 476d600dcd2ab61c6be7c47e0519ff59df7c464b.
    
    * Add similar for LoadFlow and ContingenciesProvider factories
    Fix the tool for a pretty print output

--- contingency/contingency-api/src/main/java/com/powsybl/contingency/ContingenciesProviderFactoryPluginInfo.java
+++ contingency/contingency-api/src/main/java/com/powsybl/contingency/ContingenciesProviderFactoryPluginInfo.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016, RTE (http://www.rte-france.com)
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -7,15 +7,15 @@
 package com.powsybl.contingency;
 
 import com.google.auto.service.AutoService;
+import com.powsybl.commons.plugins.PluginInfo;
 
 /**
  * @author Mathieu Bague <mathieu.bague at rte-france.com>
  */
-@AutoService(ContingenciesProviderFactory.class)
-public class EmptyContingencyListProviderFactory implements ContingenciesProviderFactory {
+@AutoService(PluginInfo.class)
+public class ContingenciesProviderFactoryPluginInfo extends PluginInfo<ContingenciesProviderFactory> {
 
-    @Override
-    public ContingenciesProvider create() {
-        return new EmptyContingencyListProvider();
+    public ContingenciesProviderFactoryPluginInfo() {
+        super(ContingenciesProviderFactory.class);
     }
 }
