7fdd2ff70026c7d7bb912eabe7818bd552a851b6
BENHAMED CHAMSEDDINE <chamseddine.benhamed@rte-france.com>
2019-03-07 16:35:24 +0100

    deleing unused import -_-

--- iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/XMLImporterTest.java
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/XMLImporterTest.java
@@ -22,7 +22,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 
 import static org.junit.Assert.*;
