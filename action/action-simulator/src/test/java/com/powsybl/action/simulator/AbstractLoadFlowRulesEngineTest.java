c9a18ab5bfabad93fd4286cfd4f6d4227d00729c
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2019-03-28 16:38:19 +0100

    change loadflowengine test for sonar coverage

--- action/action-simulator/src/test/java/com/powsybl/action/simulator/AbstractLoadFlowRulesEngineTest.java
+++ action/action-simulator/src/test/java/com/powsybl/action/simulator/AbstractLoadFlowRulesEngineTest.java
@@ -15,9 +15,7 @@ import com.powsybl.action.simulator.loadflow.LoadFlowActionSimulatorObserver;
 import com.powsybl.commons.io.table.TableFormatterConfig;
 import com.powsybl.computation.ComputationManager;
 import com.powsybl.iidm.network.Network;
-import com.powsybl.loadflow.LoadFlow;
-import com.powsybl.loadflow.LoadFlowFactory;
-import com.powsybl.loadflow.LoadFlowResult;
+import com.powsybl.loadflow.mock.LoadFlowFactoryMock;
 import groovy.lang.GroovyCodeSource;
 import groovy.lang.GroovyShell;
 import org.junit.After;
@@ -25,7 +23,6 @@ import org.junit.Before;
 import org.mockito.Mockito;
 
 import java.io.InputStreamReader;
-import java.util.concurrent.CompletableFuture;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
@@ -50,26 +47,11 @@ public abstract class AbstractLoadFlowRulesEngineTest {
     public void setUp() {
         network = createNetwork();
         ComputationManager computationManager = Mockito.mock(ComputationManager.class);
-        LoadFlow loadFlow = Mockito.mock(LoadFlow.class);
-        LoadFlowFactory loadFlowFactory = Mockito.mock(LoadFlowFactory.class);
-        Mockito.when(loadFlowFactory.create(Mockito.any(Network.class), Mockito.any(ComputationManager.class), Mockito.anyInt()))
-                .thenReturn(loadFlow);
-        LoadFlowResult loadFlowResult = Mockito.mock(LoadFlowResult.class);
-        Mockito.when(loadFlowResult.isOk())
-                .thenReturn(true);
-        Mockito.when(loadFlow.getName()).thenReturn("load flow mock");
-        Mockito.when(loadFlow.run(Mockito.anyString(), Mockito.any()))
-                .thenReturn(CompletableFuture.completedFuture(loadFlowResult));
         LoadFlowActionSimulatorObserver observer = createObserver();
         GroovyCodeSource src = new GroovyCodeSource(new InputStreamReader(getClass().getResourceAsStream(getDslFile())), "test", GroovyShell.DEFAULT_CODE_BASE);
         actionDb = new ActionDslLoader(src).load(network);
-        engine = new LoadFlowActionSimulator(network, computationManager, new LoadFlowActionSimulatorConfig(LoadFlowFactory.class, 3, false, false),
-                new TableFormatterConfig(), applyIfWorks(), observer) {
-            @Override
-            protected LoadFlowFactory newLoadFlowFactory() {
-                return loadFlowFactory;
-            }
-        };
+        engine = new LoadFlowActionSimulator(network, computationManager, new LoadFlowActionSimulatorConfig(LoadFlowFactoryMock.class, 3, false, false),
+                new TableFormatterConfig(), applyIfWorks(), observer);
     }
 
     protected boolean applyIfWorks() {
