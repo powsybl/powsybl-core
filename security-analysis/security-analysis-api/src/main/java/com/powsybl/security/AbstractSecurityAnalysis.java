13ef4d49fdb0fe61839984e686795ac3f243a6e2
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2019-04-29 14:39:28 +0200

    Add contingencies to limit violation detector API. (#772)
    * Add Contingency information to limit violation detector API.
    
    Limit violation detectors can now adapt their behaviour to the current contingency.
    Security analyses should now call the corresponding methods.
    At the same time, some javadoc is added for for LimitViolation class,
    and some utilities for limit violations in general
    
    * Restore moved classes to ensure smooth transition.
    
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- security-analysis/security-analysis-api/src/main/java/com/powsybl/security/AbstractSecurityAnalysis.java
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/AbstractSecurityAnalysis.java
@@ -7,6 +7,7 @@
 package com.powsybl.security;
 
 import com.powsybl.iidm.network.Network;
+import com.powsybl.security.detectors.DefaultLimitViolationDetector;
 import com.powsybl.security.interceptors.RunningContext;
 import com.powsybl.security.interceptors.SecurityAnalysisInterceptor;
 
--- security-analysis/security-analysis-api/src/main/java/com/powsybl/security/DefaultLimitViolationDetector.java
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/DefaultLimitViolationDetector.java
@@ -6,85 +6,13 @@
  */
 package com.powsybl.security;
 
-import com.powsybl.iidm.network.*;
-import com.powsybl.iidm.network.util.LimitViolationUtils;
-
-import java.util.*;
-import java.util.function.Consumer;
-
 /**
  * Implements the default behaviour for limit violation detection.
  *
+ * @deprecated moved to package {@link com.powsybl.security.detectors}.
  * @author Sylvain Leclerc <sylvain.leclerc at rte-france.com>
  */
-public class DefaultLimitViolationDetector extends AbstractLimitViolationDetector {
-
-    private final float limitReduction;
-    private final Set<Security.CurrentLimitType> currentLimitTypes;
-
-    public DefaultLimitViolationDetector(float limitReduction, Collection<Security.CurrentLimitType> currentLimitTypes) {
-        if (limitReduction <= 0) {
-            throw new IllegalArgumentException("Bad limit reduction " + limitReduction);
-        }
-        this.limitReduction = limitReduction;
-        this.currentLimitTypes = EnumSet.copyOf(Objects.requireNonNull(currentLimitTypes));
-    }
-
-    public DefaultLimitViolationDetector(Collection<Security.CurrentLimitType> currentLimitTypes) {
-        this(1.0f, currentLimitTypes);
-    }
-
-    public DefaultLimitViolationDetector() {
-        this(EnumSet.allOf(Security.CurrentLimitType.class));
-    }
-
-    private void checkPermanentLimit(Branch branch, Branch.Side side, double value, Consumer<LimitViolation> consumer) {
-
-        if (LimitViolationUtils.checkPermanentLimit(branch, side, limitReduction, value)) {
-            consumer.accept(new LimitViolation(branch.getId(),
-                    branch.getName(),
-                    LimitViolationType.CURRENT,
-                    null,
-                    Integer.MAX_VALUE,
-                    branch.getCurrentLimits(side).getPermanentLimit(),
-                    limitReduction,
-                    value,
-                    side));
-        }
-    }
-
-    @Override
-    public void checkCurrent(Branch branch, Branch.Side side, double value, Consumer<LimitViolation> consumer) {
-
-        Branch.Overload overload = LimitViolationUtils.checkTemporaryLimits(branch, side, limitReduction, value);
-
-        if (currentLimitTypes.contains(Security.CurrentLimitType.TATL) && (overload != null)) {
-            consumer.accept(new LimitViolation(branch.getId(),
-                    branch.getName(),
-                    LimitViolationType.CURRENT,
-                    overload.getPreviousLimitName(),
-                    overload.getTemporaryLimit().getAcceptableDuration(),
-                    overload.getPreviousLimit(),
-                    limitReduction,
-                    value,
-                    side));
-        } else if (currentLimitTypes.contains(Security.CurrentLimitType.PATL)) {
-            checkPermanentLimit(branch, side, value, consumer);
-        }
-    }
-
-    @Override
-    public void checkVoltage(Bus bus, double value, Consumer<LimitViolation> consumer) {
-        VoltageLevel vl = bus.getVoltageLevel();
-        if (!Double.isNaN(vl.getLowVoltageLimit()) && value <= vl.getLowVoltageLimit()) {
-            consumer.accept(new LimitViolation(vl.getId(), vl.getName(), LimitViolationType.LOW_VOLTAGE,
-                    vl.getLowVoltageLimit(), limitReduction, value));
-        }
-
-        if (!Double.isNaN(vl.getHighVoltageLimit()) && value >= vl.getHighVoltageLimit()) {
-            consumer.accept(new LimitViolation(vl.getId(), vl.getName(), LimitViolationType.HIGH_VOLTAGE,
-                    vl.getHighVoltageLimit(), limitReduction, value));
-        }
-    }
+@Deprecated
+public class DefaultLimitViolationDetector extends com.powsybl.security.detectors.DefaultLimitViolationDetector {
 
 }
