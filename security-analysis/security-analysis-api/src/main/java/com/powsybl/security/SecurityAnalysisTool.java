a5999c533a54294c741e4cb9611530e443109ba0
MioRtia <miora.ralambotiana@rte-france.com>
2019-05-06 18:09:52 +0200

    Tests independent of local TableFormatterConfig (#658)
    * deprecated ValidationWriter#create(id, class, writer, verbose, validationType, compareResults) + modify impl
    * checkBuses is not static
    
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- security-analysis/security-analysis-api/src/main/java/com/powsybl/security/SecurityAnalysisTool.java
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/SecurityAnalysisTool.java
@@ -124,6 +124,9 @@ public class SecurityAnalysisTool implements Tool {
                 options.addOption(Option.builder().longOpt(EXTERNAL)
                         .desc("external execution")
                         .build());
+                options.addOption(Option.builder().longOpt(SKIP_POSTPROC_OPTION)
+                        .desc("skip network importer post processors (when configured)")
+                        .build());
                 options.addOption(createImportParametersFileOption());
                 options.addOption(createImportParameterOption());
                 options.addOption(Option.builder().longOpt(OUTPUT_LOG_OPTION)
@@ -142,6 +145,10 @@ public class SecurityAnalysisTool implements Tool {
                 );
             }
         };
+    }
+
+    protected TableFormatterConfig createTableFormatterConfig() {
+        return TableFormatterConfig.load();
     }
 
     private static Optional<String> getOptionValue(CommandLine line, String option) {
@@ -224,7 +231,9 @@ public class SecurityAnalysisTool implements Tool {
 
         context.getOutputStream().println("Loading network '" + caseFile + "'");
         Properties inputParams = readProperties(line, ConversionToolUtils.OptionType.IMPORT, context);
-        Network network = Importers.loadNetwork(caseFile, context.getShortTimeExecutionComputationManager(), ImportConfig.load(), inputParams);
+        boolean skipPostProc = line.hasOption(SKIP_POSTPROC_OPTION);
+        ImportConfig importConfig = (!skipPostProc) ? ImportConfig.load() : new ImportConfig();
+        Network network = Importers.loadNetwork(caseFile, context.getShortTimeExecutionComputationManager(), importConfig, inputParams);
         network.getVariantManager().allowVariantMultiThreadAccess(true);
 
         LimitViolationFilter limitViolationFilter = LimitViolationFilter.load();
@@ -262,7 +271,7 @@ public class SecurityAnalysisTool implements Tool {
         } else {
             // To avoid the closing of System.out
             Writer writer = new OutputStreamWriter(context.getOutputStream());
-            Security.print(result, network, writer, new AsciiTableFormatterFactory(), TableFormatterConfig.load());
+            Security.print(result, network, writer, new AsciiTableFormatterFactory(), createTableFormatterConfig());
         }
     }
 }
