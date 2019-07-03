0402837de5829ba6a9f31228d16c79e265307506
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2019-07-03 10:02:33 +0200

    correct checkstyle
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/conformity/modified/CgmesConformity1ModifiedConversionTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/conformity/modified/CgmesConformity1ModifiedConversionTest.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.nio.file.FileSystem;
 
 import com.powsybl.iidm.network.*;
-import com.powsybl.iidm.network.impl.NetworkFactoryImpl;
 import org.junit.*;
 
 import com.google.common.jimfs.Jimfs;
