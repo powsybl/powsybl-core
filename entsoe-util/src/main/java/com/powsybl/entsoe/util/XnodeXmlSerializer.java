4d6a810f44205400571a2f1a4585b2137d194224
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2019-07-06 17:36:31 +0200

    Active power control (#840)
    Open source active power control extension
    
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- entsoe-util/src/main/java/com/powsybl/entsoe/util/XnodeXmlSerializer.java
+++ entsoe-util/src/main/java/com/powsybl/entsoe/util/XnodeXmlSerializer.java
@@ -7,53 +7,23 @@
 package com.powsybl.entsoe.util;
 
 import com.google.auto.service.AutoService;
+import com.powsybl.commons.extensions.AbstractExtensionXmlSerializer;
 import com.powsybl.commons.extensions.ExtensionXmlSerializer;
 import com.powsybl.commons.xml.XmlReaderContext;
 import com.powsybl.commons.xml.XmlWriterContext;
 import com.powsybl.iidm.network.DanglingLine;
 
 import javax.xml.stream.XMLStreamException;
-import java.io.InputStream;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
 @AutoService(ExtensionXmlSerializer.class)
-public class XnodeXmlSerializer implements ExtensionXmlSerializer<DanglingLine, Xnode> {
+public class XnodeXmlSerializer extends AbstractExtensionXmlSerializer<DanglingLine, Xnode> {
 
-    @Override
-    public String getExtensionName() {
-        return "xnode";
-    }
-
-    @Override
-    public String getCategoryName() {
-        return "network";
-    }
-
-    @Override
-    public Class<Xnode> getExtensionClass() {
-        return Xnode.class;
-    }
-
-    @Override
-    public boolean hasSubElements() {
-        return false;
-    }
-
-    @Override
-    public InputStream getXsdAsStream() {
-        return getClass().getResourceAsStream("/xsd/xnode.xsd");
-    }
-
-    @Override
-    public String getNamespaceUri() {
-        return "http://www.itesla_project.eu/schema/iidm/ext/xnode/1_0";
-    }
-
-    @Override
-    public String getNamespacePrefix() {
-        return "xn";
+    public XnodeXmlSerializer() {
+        super("xnode", "network", Xnode.class, false, "xnode.xsd",
+                "http://www.itesla_project.eu/schema/iidm/ext/xnode/1_0", "xn");
     }
 
     @Override
