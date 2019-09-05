f316dda3837def511d409c1e0684903bfbca54aa
Thomas ADAM <52197093+tadam50@users.noreply.github.com>
2019-09-05 07:14:53 +0200

    iidm notify update on TieLineImpl.HalfLineImpl changes (#912)
    * Add notification on HalfLine changes
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/AbstractConnectable.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/AbstractConnectable.java
@@ -61,6 +61,10 @@ abstract class AbstractConnectable<I extends Connectable<I>> extends AbstractIde
         network.getListeners().notifyRemoval(this);
     }
 
+    protected void notifyUpdate(Supplier<String> attribute, Object oldValue, Object newValue) {
+        getNetwork().getListeners().notifyUpdate(this, attribute, oldValue, newValue);
+    }
+
     protected void notifyUpdate(String attribute, Object oldValue, Object newValue) {
         getNetwork().getListeners().notifyUpdate(this, attribute, oldValue, newValue);
     }
