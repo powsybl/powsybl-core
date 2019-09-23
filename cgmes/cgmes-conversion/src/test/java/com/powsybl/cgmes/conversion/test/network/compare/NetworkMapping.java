b50b023cd03c9022ccd4b2d6861fbb0c96f5e587
Luma Zamarreño <zamarrenolm@aia.es>
2019-09-23 14:58:50 +0200

    complete the mapping between networks taking into account calculated buses
    Signed-off-by: Luma Zamarreño <zamarrenolm@aia.es>

--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/network/compare/NetworkMapping.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/network/compare/NetworkMapping.java
@@ -25,6 +25,8 @@ public class NetworkMapping {
         this.explicitActuals = new HashMap<>();
         this.expectedPrefixed = false;
         this.actualPrefixed = false;
+        this.calculatedExpecteds = computeCalculated(this.expected);
+        this.calculatedActuals = computeCalculated(this.actual);
     }
 
     public Identifiable findExpected(Identifiable a) {
@@ -33,6 +35,9 @@ public class NetworkMapping {
             return e;
         }
         String eid = applyPrefixToActual(a.getId());
+        if (isCalculated(a, this.actual)) {
+            return calculatedExpecteds.get(eid);
+        }
         return expected.getIdentifiable(eid);
     }
 
@@ -42,6 +47,9 @@ public class NetworkMapping {
             return a;
         }
         String aid = applyPrefixToExpected(e.getId());
+        if (isCalculated(e, this.expected)) {
+            return calculatedActuals.get(aid);
+        }
         return actual.getIdentifiable(aid);
     }
 
@@ -88,10 +96,33 @@ public class NetworkMapping {
         return id;
     }
 
+    private boolean isCalculated(Identifiable i, Network network) {
+        // An identifiable is considered as calculated
+        // if it can not be found in the list of identifiables of the Network
+        return network.getIdentifiable(i.getId()) == null;
+    }
+
+    private Map<String, Identifiable> computeCalculated(Network n) {
+        Map<String, Identifiable> calculated = new HashMap<>();
+        addCalculatedBuses(calculated, n);
+        return calculated;
+    }
+
+    private void addCalculatedBuses(Map<String, Identifiable> calculated, Network n) {
+        // Add all calculated buses in the bus-breaker view
+        n.getBusBreakerView().getBuses().forEach(b -> {
+            if (isCalculated(b, n)) {
+                calculated.put(b.getId(), b);
+            }
+        });
+    }
+
     private final Network expected;
     private final Network actual;
     private boolean expectedPrefixed;
     private boolean actualPrefixed;
     private Map<String, String> explicitExpecteds;
     private Map<String, String> explicitActuals;
+    private final Map<String, Identifiable> calculatedExpecteds;
+    private final Map<String, Identifiable> calculatedActuals;
 }
