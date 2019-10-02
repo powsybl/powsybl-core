8568093513b4893c7a5c943075289597f40f3543
Paul <paul.bui-quang@runbox.com>
2019-10-02 17:07:24 +0200

    Add include scripts in AbstractScript nodes
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

---
+++ afs/afs-core/src/main/java/com/powsybl/afs/OrderedDependencyManager.java
@@ -0,0 +1,96 @@
+package com.powsybl.afs;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class OrderedDependencyManager {
+
+    private ProjectFile projectFile;
+
+    private List<ProjectDependency<ProjectNode>> dependencyCache = null;
+
+    public OrderedDependencyManager(ProjectFile projectFile) {
+        this.projectFile = projectFile;
+        projectFile.addListener(new DefaultProjectFileListener() {
+            @Override
+            public void dependencyChanged(String name) {
+                dependencyCache = null;
+            }
+        });
+    }
+
+    public void appendDependencies(String name, List<ProjectNode> projectNodes) {
+        List<ProjectNode> nodes = getDependencies(name);
+        nodes.addAll(projectNodes);
+        setDependencies(name, nodes);
+    }
+
+    public void insertDependencies(String name, int index, List<ProjectNode> projectNodes) {
+        List<ProjectNode> nodes = getDependencies(name);
+        nodes.addAll(index, projectNodes);
+        setDependencies(name, nodes);
+    }
+
+    public void removeDependency(String name, int index) {
+        List<ProjectNode> nodes = getDependencies(name);
+        nodes.remove(index);
+        setDependencies(name, nodes);
+    }
+
+    public void removeDependencies(String name, List<String> nodeIds) {
+        List<ProjectNode> nodes = getDependencies(name);
+        setDependencies(name, nodes.stream().filter(dep -> !nodeIds.contains(dep.getId())).collect(Collectors.toList()));
+    }
+
+    public void setDependencies(String name, List<ProjectNode> projectNodes) {
+        getDependenciesFor(name).map(el -> el.getRight().getName()).forEach(projectFile::removeDependencies);
+        for (int i = 0; i < projectNodes.size(); i++) {
+            projectFile.setDependencies(name + "_" + i, Collections.singletonList(projectNodes.get(i)));
+        }
+    }
+
+    public List<ProjectNode> getDependencies(String name) {
+        return getDependenciesFor(name)
+                .sorted(Comparator.comparing(ImmutablePair::getLeft))
+                .map(dep -> dep.getRight().getProjectNode())
+                .collect(Collectors.toList());
+    }
+
+    public <T extends ProjectNode> List<T> getDependencies(String name, Class<T> nodeClass) {
+        return getDependenciesFor(name)
+                .filter(dep -> nodeClass.isAssignableFrom(dep.getRight().getProjectNode().getClass()))
+                .sorted(Comparator.comparing(ImmutablePair::getLeft))
+                .map(dep -> nodeClass.cast(dep.getRight().getProjectNode()))
+                .collect(Collectors.toList());
+    }
+
+    private List<ProjectDependency<ProjectNode>> getDependencyCache() {
+        if (dependencyCache == null) {
+            dependencyCache = projectFile.getDependencies();
+        }
+        return dependencyCache;
+    }
+
+    private Stream<ImmutablePair<String, ProjectDependency<ProjectNode>>> getDependenciesFor(String name) {
+        Pattern pattern = Pattern.compile(name + "_(\\d+)");
+        return getDependencyCache()
+                .stream()
+                .map(dep -> {
+                    Matcher depMatch = pattern.matcher(dep.getName());
+                    if (depMatch.matches()) {
+                        return ImmutablePair.of(depMatch.group(1), dep);
+                    }
+                    return null;
+                })
+                .filter(Objects::nonNull);
+    }
+
+}
--- afs/afs-ext-base/src/main/java/com/powsybl/afs/ext/base/AbstractScript.java
+++ afs/afs-ext-base/src/main/java/com/powsybl/afs/ext/base/AbstractScript.java
@@ -7,22 +7,23 @@
 package com.powsybl.afs.ext.base;
 
 import com.google.common.io.CharStreams;
+import com.powsybl.afs.OrderedDependencyManager;
 import com.powsybl.afs.ProjectFile;
 import com.powsybl.afs.ProjectFileCreationContext;
 import com.powsybl.afs.storage.events.AppStorageListener;
 import com.powsybl.afs.storage.events.NodeDataUpdated;
 import com.powsybl.afs.storage.events.NodeEvent;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
-public abstract class AbstractModificationScript extends ProjectFile implements StorableScript {
+public abstract class AbstractScript<T extends AbstractScript> extends ProjectFile implements StorableScript {
 
     private static final String  NODE_DATA_UPDATED = "NODE_DATA_UPDATED";
 
@@ -32,7 +33,11 @@ public abstract class AbstractModificationScript extends ProjectFile implements 
 
     private final AppStorageListener l = eventList -> processEvents(eventList.getEvents(), info.getId(), listeners);
 
-    public AbstractModificationScript(ProjectFileCreationContext context, int codeVersion, String scriptContentName) {
+    private static final String INCLUDED_SCRIPTS_DEPENDENCY_NAME = "scriptIncludes";
+    private static final String DEFAULT_SCRIPTS_DELIMITER = "\n\n";
+    protected final OrderedDependencyManager orderedDependencyManager = new OrderedDependencyManager(this);
+
+    public AbstractScript(ProjectFileCreationContext context, int codeVersion, String scriptContentName) {
         super(context, codeVersion);
         this.scriptContentName = Objects.requireNonNull(scriptContentName);
         storage.getEventsBus().addListener(l);
@@ -40,7 +45,7 @@ public abstract class AbstractModificationScript extends ProjectFile implements 
 
     private void processEvents(List<NodeEvent> events, String nodeId, List<ScriptListener> listeners) {
         for (NodeEvent event : events) {
-            if (event.getType().equals(NODE_DATA_UPDATED)) {
+            if (NODE_DATA_UPDATED.equals(event.getType())) {
                 NodeDataUpdated dataUpdated = (NodeDataUpdated) event;
                 if (dataUpdated.getId().equals(nodeId) && scriptContentName.equals(dataUpdated.getDataName())) {
                     for (ScriptListener listener : listeners) {
@@ -49,6 +54,32 @@ public abstract class AbstractModificationScript extends ProjectFile implements 
                 }
             }
         }
+    }
+
+    public void addScript(T includeScript) {
+        orderedDependencyManager.appendDependencies(INCLUDED_SCRIPTS_DEPENDENCY_NAME, Collections.singletonList(includeScript));
+    }
+
+    public void removeScript(String scriptNodeId) {
+        orderedDependencyManager.removeDependencies(INCLUDED_SCRIPTS_DEPENDENCY_NAME, Collections.singletonList(scriptNodeId));
+    }
+
+    @Override
+    public String readScript(boolean withIncludes) {
+        String ownContent = readScript();
+        if (withIncludes) {
+            String includesScript = orderedDependencyManager
+                    .getDependencies(INCLUDED_SCRIPTS_DEPENDENCY_NAME, AbstractScript.class)
+                    .stream()
+                    // TODO add commented header for script ? (script project node name ?)
+                    .map(script -> script.readScript(true))
+                    .collect(Collectors.joining(DEFAULT_SCRIPTS_DELIMITER));
+            if (StringUtils.isNotBlank(includesScript)) {
+                includesScript += DEFAULT_SCRIPTS_DELIMITER;
+            }
+            return includesScript + ownContent;
+        }
+        return ownContent;
     }
 
     @Override
@@ -63,7 +94,7 @@ public abstract class AbstractModificationScript extends ProjectFile implements 
     @Override
     public void writeScript(String content) {
         try (Reader reader = new StringReader(content);
-             Writer writer = new OutputStreamWriter(storage.writeBinaryData(info.getId(), scriptContentName), StandardCharsets.UTF_8)) {
+            Writer writer = new OutputStreamWriter(storage.writeBinaryData(info.getId(), scriptContentName), StandardCharsets.UTF_8)) {
             CharStreams.copy(reader, writer);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
