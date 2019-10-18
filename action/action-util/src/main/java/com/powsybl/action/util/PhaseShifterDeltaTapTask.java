237ff0355a80944cb302595c9a8840d87f13664e
AMROUN Hamou Ext <hamou.amroun@rte-france.com>
2019-10-18 16:47:16 +0200

    Added licence on new classes created in this branch
    Signed-off-by: AMROUN Hamou Ext <hamou.amroun@rte-france.com>

--- action/action-util/src/main/java/com/powsybl/action/util/PhaseShifterDeltaTapTask.java
+++ action/action-util/src/main/java/com/powsybl/action/util/PhaseShifterDeltaTapTask.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2017, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.action.util;
 
 import com.powsybl.commons.PowsyblException;
