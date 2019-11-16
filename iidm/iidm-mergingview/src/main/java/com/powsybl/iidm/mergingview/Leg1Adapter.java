b82c95fe6948274afd9f44392e1c8340c1b63927
Thomas ADAM <52197093+tadam50@users.noreply.github.com>
2019-11-16 07:55:16 +0100

    Merging view implementation : empty adapters (#1001)
    * Merging view step : empty adapters implementation (throw Implemented exception)
    * Increase code coverage
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

---
+++ iidm/iidm-mergingview/src/main/java/com/powsybl/iidm/mergingview/Leg1Adapter.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.mergingview;
+
+import com.powsybl.iidm.network.CurrentLimits;
+import com.powsybl.iidm.network.CurrentLimitsAdder;
+import com.powsybl.iidm.network.ThreeWindingsTransformer;
+import com.powsybl.iidm.network.ThreeWindingsTransformer.Leg1;
+
+/**
+ * @author Thomas Adam <tadam at silicom.fr>
+ */
+public class Leg1Adapter extends AbstractAdapter<ThreeWindingsTransformer.Leg1> implements ThreeWindingsTransformer.Leg1 {
+
+    protected Leg1Adapter(final Leg1 delegate, final MergingViewIndex index) {
+        super(delegate, index);
+    }
+
+    // -------------------------------
+    // Not implemented methods -------
+    // -------------------------------
+    @Override
+    public TerminalAdapter getTerminal() {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public double getR() {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public Leg1Adapter setR(final double r) {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public double getX() {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public Leg1Adapter setX(final double x) {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public double getRatedU() {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public Leg1Adapter setRatedU(final double ratedU) {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public CurrentLimits getCurrentLimits() {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public CurrentLimitsAdder newCurrentLimits() {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public double getG() {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public Leg1Adapter setG(final double g) {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public double getB() {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+    @Override
+    public Leg1Adapter setB(final double b) {
+        throw MergingView.NOT_IMPLEMENTED_EXCEPTION;
+    }
+
+}
