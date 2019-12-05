56333685fb9948e1d3f4d69b49c448c361712139
MioRtia <miora.ralambotiana@rte-france.com>
2019-12-05 13:33:01 +0100

    Add methods for CGMES validation (#1042)
    Add methods for CGMES validation.
    CGMES conversion context extension.
    
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

---
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/CgmesConversionContextExtensionTest.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.cgmes.conversion;
+
+import com.powsybl.cgmes.conformity.test.CgmesConformity1Catalog;
+import com.powsybl.iidm.network.Network;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
+ */
+public class CgmesConversionContextExtensionTest {
+
+    @Test
+    public void test() {
+        Properties properties = new Properties();
+        properties.put(CgmesImport.STORE_CGMES_CONVERSION_CONTEXT_AS_NETWORK_EXTENSION, "true");
+        Network network = new CgmesImport().importData(CgmesConformity1Catalog.microGridBaseCaseBE().dataSource(), properties);
+        CgmesConversionContextExtension extension = network.getExtension(CgmesConversionContextExtension.class);
+        assertNotNull(extension);
+        assertTrue(extension.getContext().config().storeCgmesConversionContextAsNetworkExtension());
+    }
+}
