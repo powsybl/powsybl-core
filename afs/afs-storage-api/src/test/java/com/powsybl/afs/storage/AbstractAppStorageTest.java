a3f1c416351238835c2e2380ca94c3833615cb69
Paul Bui-Quang <paul.buiquang@rte-france.com>
2019-12-06 09:21:51 +0100

    Fixes
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

--- afs/afs-storage-api/src/test/java/com/powsybl/afs/storage/AbstractAppStorageTest.java
+++ afs/afs-storage-api/src/test/java/com/powsybl/afs/storage/AbstractAppStorageTest.java
@@ -12,7 +12,6 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import com.google.common.io.ByteStreams;
 import com.powsybl.afs.storage.events.*;
-import com.powsybl.commons.PowsyblException;
 import com.powsybl.commons.datasource.DataSource;
 import com.powsybl.timeseries.*;
 import org.junit.After;
@@ -172,9 +171,9 @@ public abstract class AbstractAppStorageTest {
         NodeInfo testDataInfo = storage.createNode(testFolderInfo.getId(), "data", DATA_FILE_CLASS, "", 0, new NodeGenericMetadata());
         NodeInfo testData2Info = storage.createNode(testFolderInfo.getId(), "data2", DATA_FILE_CLASS, "", 0,
                 new NodeGenericMetadata().setString("s1", "v1")
-                        .setDouble("d1", 1d)
-                        .setInt("i1", 2)
-                        .setBoolean("b1", false));
+                                         .setDouble("d1", 1d)
+                                         .setInt("i1", 2)
+                                         .setBoolean("b1", false));
         NodeInfo testData3Info = storage.createNode(testFolderInfo.getId(), "data3", DATA_FILE_CLASS, "", 0, new NodeGenericMetadata());
 
         storage.setConsistent(testDataInfo.getId());
@@ -184,11 +183,11 @@ public abstract class AbstractAppStorageTest {
 
         // check events
         assertEventStack(new NodeCreated(testDataInfo.getId(), testFolderInfo.getId()),
-                new NodeCreated(testData2Info.getId(), testFolderInfo.getId()),
-                new NodeCreated(testData3Info.getId(), testFolderInfo.getId()),
-                new NodeConsistent(testDataInfo.getId()),
-                new NodeConsistent(testData2Info.getId()),
-                new NodeConsistent(testData3Info.getId()));
+                     new NodeCreated(testData2Info.getId(), testFolderInfo.getId()),
+                     new NodeCreated(testData3Info.getId(), testFolderInfo.getId()),
+                     new NodeConsistent(testDataInfo.getId()),
+                     new NodeConsistent(testData2Info.getId()),
+                     new NodeConsistent(testData3Info.getId()));
 
         // check info are correctly stored even with metadata
         assertEquals(testData2Info, storage.getNodeInfo(testData2Info.getId()));
@@ -244,7 +243,7 @@ public abstract class AbstractAppStorageTest {
 
         // check event
         assertEventStack(new DependencyAdded(testDataInfo.getId(), "mylink"),
-                new BackwardDependencyAdded(testData2Info.getId(), "mylink"));
+                         new BackwardDependencyAdded(testData2Info.getId(), "mylink"));
 
         // check dependency state
         assertEquals(ImmutableSet.of(new NodeDependency("mylink", testData2Info)), storage.getDependencies(testDataInfo.getId()));
@@ -258,7 +257,7 @@ public abstract class AbstractAppStorageTest {
 
         // check event
         assertEventStack(new DependencyAdded(testDataInfo.getId(), "mylink2"),
-                new BackwardDependencyAdded(testData2Info.getId(), "mylink2"));
+                         new BackwardDependencyAdded(testData2Info.getId(), "mylink2"));
 
         assertEquals(ImmutableSet.of(new NodeDependency("mylink", testData2Info), new NodeDependency("mylink2", testData2Info)), storage.getDependencies(testDataInfo.getId()));
         assertEquals(ImmutableSet.of(testDataInfo), storage.getBackwardDependencies(testData2Info.getId()));
@@ -269,7 +268,7 @@ public abstract class AbstractAppStorageTest {
 
         // check event
         assertEventStack(new DependencyRemoved(testDataInfo.getId(), "mylink2"),
-                new BackwardDependencyRemoved(testData2Info.getId(), "mylink2"));
+                         new BackwardDependencyRemoved(testData2Info.getId(), "mylink2"));
 
         assertEquals(ImmutableSet.of(new NodeDependency("mylink", testData2Info)), storage.getDependencies(testDataInfo.getId()));
         assertEquals(ImmutableSet.of(testDataInfo), storage.getBackwardDependencies(testData2Info.getId()));
@@ -365,10 +364,10 @@ public abstract class AbstractAppStorageTest {
 
         // 13) create double time series
         TimeSeriesMetadata metadata1 = new TimeSeriesMetadata("ts1",
-                TimeSeriesDataType.DOUBLE,
-                ImmutableMap.of("var1", "value1"),
-                RegularTimeSeriesIndex.create(Interval.parse("2015-01-01T00:00:00Z/2015-01-01T01:15:00Z"),
-                        Duration.ofMinutes(15)));
+                                                              TimeSeriesDataType.DOUBLE,
+                                                              ImmutableMap.of("var1", "value1"),
+                                                              RegularTimeSeriesIndex.create(Interval.parse("2015-01-01T00:00:00Z/2015-01-01T01:15:00Z"),
+                                                                                            Duration.ofMinutes(15)));
         storage.createTimeSeries(testData2Info.getId(), metadata1);
         storage.flush();
 
@@ -386,8 +385,8 @@ public abstract class AbstractAppStorageTest {
         assertTrue(storage.getTimeSeriesMetadata(testData3Info.getId(), Sets.newHashSet("ts1")).isEmpty());
 
         // 14) add data to double time series
-        storage.addDoubleTimeSeriesData(testData2Info.getId(), 0, "ts1", Arrays.asList(new UncompressedDoubleDataChunk(2, new double[]{1d, 2d}),
-                new UncompressedDoubleDataChunk(5, new double[]{3d})));
+        storage.addDoubleTimeSeriesData(testData2Info.getId(), 0, "ts1", Arrays.asList(new UncompressedDoubleDataChunk(2, new double[] {1d, 2d}),
+                                                                                       new UncompressedDoubleDataChunk(5, new double[] {3d})));
         storage.flush();
 
         // check event
@@ -400,17 +399,17 @@ public abstract class AbstractAppStorageTest {
         // check double time series data query
         Map<String, List<DoubleDataChunk>> doubleTimeSeriesData = storage.getDoubleTimeSeriesData(testData2Info.getId(), Sets.newHashSet("ts1"), 0);
         assertEquals(1, doubleTimeSeriesData.size());
-        assertEquals(Arrays.asList(new UncompressedDoubleDataChunk(2, new double[]{1d, 2d}),
-                new UncompressedDoubleDataChunk(5, new double[]{3d})),
-                doubleTimeSeriesData.get("ts1"));
+        assertEquals(Arrays.asList(new UncompressedDoubleDataChunk(2, new double[] {1d, 2d}),
+                                   new UncompressedDoubleDataChunk(5, new double[] {3d})),
+                     doubleTimeSeriesData.get("ts1"));
         assertTrue(storage.getDoubleTimeSeriesData(testData3Info.getId(), Sets.newHashSet("ts1"), 0).isEmpty());
 
         // 15) create a second string time series
         TimeSeriesMetadata metadata2 = new TimeSeriesMetadata("ts2",
-                TimeSeriesDataType.STRING,
-                ImmutableMap.of(),
-                RegularTimeSeriesIndex.create(Interval.parse("2015-01-01T00:00:00Z/2015-01-01T01:15:00Z"),
-                        Duration.ofMinutes(15)));
+                                                              TimeSeriesDataType.STRING,
+                                                              ImmutableMap.of(),
+                                                              RegularTimeSeriesIndex.create(Interval.parse("2015-01-01T00:00:00Z/2015-01-01T01:15:00Z"),
+                                                                                            Duration.ofMinutes(15)));
         storage.createTimeSeries(testData2Info.getId(), metadata2);
         storage.flush();
 
@@ -423,8 +422,8 @@ public abstract class AbstractAppStorageTest {
         assertEquals(1, metadataList.size());
 
         // 16) add data to double time series
-        storage.addStringTimeSeriesData(testData2Info.getId(), 0, "ts2", Arrays.asList(new UncompressedStringDataChunk(2, new String[]{"a", "b"}),
-                new UncompressedStringDataChunk(5, new String[]{"c"})));
+        storage.addStringTimeSeriesData(testData2Info.getId(), 0, "ts2", Arrays.asList(new UncompressedStringDataChunk(2, new String[] {"a", "b"}),
+                                                                                       new UncompressedStringDataChunk(5, new String[] {"c"})));
         storage.flush();
 
         // check event
@@ -433,9 +432,9 @@ public abstract class AbstractAppStorageTest {
         // check string time series data query
         Map<String, List<StringDataChunk>> stringTimeSeriesData = storage.getStringTimeSeriesData(testData2Info.getId(), Sets.newHashSet("ts2"), 0);
         assertEquals(1, stringTimeSeriesData.size());
-        assertEquals(Arrays.asList(new UncompressedStringDataChunk(2, new String[]{"a", "b"}),
-                new UncompressedStringDataChunk(5, new String[]{"c"})),
-                stringTimeSeriesData.get("ts2"));
+        assertEquals(Arrays.asList(new UncompressedStringDataChunk(2, new String[] {"a", "b"}),
+                                   new UncompressedStringDataChunk(5, new String[] {"c"})),
+                     stringTimeSeriesData.get("ts2"));
 
         // 17) clear time series
         storage.clearTimeSeries(testData2Info.getId());
@@ -547,14 +546,9 @@ public abstract class AbstractAppStorageTest {
         checkMetadataEquality(metadata, node.getGenericMetadata());
         discardEvents(17);
 
-        try {
-            storage.setMetadata(node.getId(), cloneMetadata(metadata));
-            storage.flush();
-            assertEventStack(new NodeMetadataUpdated(node.getId(), metadata));
-        } catch (PowsyblException e) {
-            assertThat(e).hasMessage("Not implemented");
-            return;
-        }
+        storage.setMetadata(node.getId(), cloneMetadata(metadata));
+        storage.flush();
+        assertEventStack(new NodeMetadataUpdated(node.getId(), metadata));
 
         metadata.setString("test", "test");
         assertThat(node.getGenericMetadata().getStrings().keySet().size()).isEqualTo(0);
@@ -619,5 +613,4 @@ public abstract class AbstractAppStorageTest {
         clone.getDoubles().putAll(metadata.getDoubles());
         return clone;
     }
-
 }
