0579ff69aab1ccee0f168f3fbfc279a7d26cc4e9
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2019-12-12 17:45:23 +0100

    add test
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/StaticVarCompensatorXmlTest.java
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/StaticVarCompensatorXmlTest.java
@@ -6,6 +6,8 @@
  */
 package com.powsybl.iidm.xml;
 
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.StaticVarCompensator;
 import com.powsybl.iidm.network.test.SvcTestCaseFactory;
 import org.junit.Test;
 
@@ -20,11 +22,30 @@ public class StaticVarCompensatorXmlTest extends AbstractXmlConverterTest {
 
     @Test
     public void roundTripTest() throws IOException {
-        roundTripVersionnedXmlTest("staticVarCompensatorRoundTripRef.xml", IidmXmlVersion.V_1_0);
+        roundTripVersionnedXmlTest("staticVarCompensatorRoundTripRef.xml");
 
-        roundTripXmlTest(SvcTestCaseFactory.create(),
+        Network network = SvcTestCaseFactory.create();
+        roundTripXmlTest(network,
                 NetworkXml::writeAndValidate,
                 NetworkXml::read,
                 getVersionDir(CURRENT_IIDM_XML_VERSION) + "staticVarCompensatorRoundTripRef.xml");
+
+        // Add a SVC with a remote regulating terminal
+        network.getVoltageLevel("VL2")
+                .newStaticVarCompensator()
+                .setId("SVC3")
+                .setConnectableBus("B2")
+                .setBus("B2")
+                .setBmin(0.0002)
+                .setBmax(0.0008)
+                .setRegulationMode(StaticVarCompensator.RegulationMode.VOLTAGE)
+                .setVoltageSetPoint(390.0)
+                .setReactivePowerSetPoint(1.0)
+                .setRegulatingTerminal(network.getLoad("L2").getTerminal())
+                .add();
+        roundTripXmlTest(network,
+                NetworkXml::writeAndValidate,
+                NetworkXml::read,
+                getVersionDir(CURRENT_IIDM_XML_VERSION) + "regulatingStaticVarCompensatorRoundTripRef.xml");
     }
 }
