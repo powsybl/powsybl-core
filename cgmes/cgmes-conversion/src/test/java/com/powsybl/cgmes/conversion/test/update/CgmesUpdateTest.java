0490ec285c0f1af0e2fed791f544d30de8e011ea
Elena Kaltakova <kaltakovae@aia.es>
2019-12-17 12:55:31 +0100

    fix CI build failure
    Signed-off-by: Elena Kaltakova <kaltakovae@aia.es>

--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/update/CgmesUpdateTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/update/CgmesUpdateTest.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
