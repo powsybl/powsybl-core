c4fda5bc0d4cf1b52352dfaef8a254b51b898e23
Thomas ADAM <tadam@silicom.fr>
2020-01-20 10:14:18 +0100

    Merging view step : DanglingLine adapter implementation
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>

---
+++ iidm/iidm-mergingview/src/main/java/com/powsybl/iidm/mergingview/DanglingLineAdderAdapter.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.mergingview;
+
+import com.powsybl.iidm.network.DanglingLine;
+import com.powsybl.iidm.network.DanglingLineAdder;
+
+/**
+ * @author Thomas Adam <tadam at silicom.fr>
+ */
+public class DanglingLineAdderAdapter extends AbstractInjectionAdderAdapter<DanglingLineAdder> implements DanglingLineAdder {
+
+    DanglingLineAdderAdapter(final DanglingLineAdder delegate, final MergingViewIndex index) {
+        super(delegate, index);
+    }
+
+    @Override
+    public DanglingLine add() {
+        checkAndSetUniqueId();
+        return getIndex().getDanglingLine(getDelegate().add());
+    }
+
+    // -------------------------------
+    // Simple delegated methods ------
+    // -------------------------------
+    @Override
+    public DanglingLineAdder setP0(final double p0) {
+        getDelegate().setP0(p0);
+        return this;
+    }
+
+    @Override
+    public DanglingLineAdder setQ0(final double q0) {
+        getDelegate().setQ0(q0);
+        return this;
+    }
+
+    @Override
+    public DanglingLineAdder setR(final double r) {
+        getDelegate().setR(r);
+        return this;
+    }
+
+    @Override
+    public DanglingLineAdder setX(final double x) {
+        getDelegate().setX(x);
+        return this;
+    }
+
+    @Override
+    public DanglingLineAdder setG(final double g) {
+        getDelegate().setG(g);
+        return this;
+    }
+
+    @Override
+    public DanglingLineAdder setB(final double b) {
+        getDelegate().setB(b);
+        return this;
+    }
+
+    @Override
+    public DanglingLineAdder setUcteXnodeCode(final String ucteXnodeCode) {
+        getDelegate().setUcteXnodeCode(ucteXnodeCode);
+        return this;
+    }
+}
