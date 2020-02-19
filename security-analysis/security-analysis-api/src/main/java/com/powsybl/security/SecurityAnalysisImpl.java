d9b0359916b8c2b8cde0255683c5a736e9321130
Jon Harper <jon.harper87@gmail.com>
2020-02-19 13:35:31 +0100

    SecurityAnalysisImpl: Clone a batch variants before the loadflows and delete them after
    cloneVariant is not thread safe, so it must be done before the computations.
    
    Signed-off-by: Jon Harper <jon.harper87@gmail.com>

--- security-analysis/security-analysis-api/src/main/java/com/powsybl/security/SecurityAnalysisImpl.java
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/SecurityAnalysisImpl.java
@@ -6,6 +6,7 @@
  */
 package com.powsybl.security;
 
+import com.powsybl.commons.exceptions.UncheckedInterruptedException;
 import com.powsybl.computation.ComputationManager;
 import com.powsybl.contingency.ContingenciesProvider;
 import com.powsybl.contingency.Contingency;
@@ -20,7 +21,11 @@ import com.powsybl.security.interceptors.SecurityAnalysisInterceptor;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
@@ -77,7 +82,7 @@ public class SecurityAnalysisImpl extends AbstractSecurityAnalysis {
 
                     SecurityAnalysisResultBuilder resultBuilder = createResultBuilder(workingStateId);
 
-                    CompletableFuture<Void>[] futures;
+                    CompletableFuture<Void> future;
 
                     if (loadFlowResult.isOk()) {
 
@@ -88,47 +93,65 @@ public class SecurityAnalysisImpl extends AbstractSecurityAnalysis {
 
                         List<Contingency> contingencies = contingenciesProvider.getContingencies(network);
 
-                        futures = new CompletableFuture[contingencies.size()];
+                        CompletableFuture<Void>[] futures = new CompletableFuture[contingencies.size()];
 
                         String hash = UUID.randomUUID().toString();
+                        int workerCount = Math.min(computationManager.getResourcesStatus().getAvailableCores(), contingencies.size());
+                        List<String> variantIds = IntStream.range(0, workerCount).mapToObj(i -> hash + "_" + i).collect(Collectors.toList());
+                        network.getVariantManager().cloneVariant(VariantManagerConstants.INITIAL_VARIANT_ID, variantIds);
+
+                        BlockingQueue<String> queue = new ArrayBlockingQueue<>(variantIds.size(), false, variantIds);
                         for (int i = 0; i < contingencies.size(); i++) {
                             Contingency contingency = contingencies.get(i);
-
-                            String postContStateId = hash + "_" + contingency.getId();
 
                             // run one loadflow per contingency
                             futures[i] = CompletableFuture
                                     .supplyAsync(() -> {
-                                        network.getVariantManager().cloneVariant(VariantManagerConstants.INITIAL_VARIANT_ID, postContStateId);
-                                        network.getVariantManager().setWorkingVariant(postContStateId);
-
-                                        // apply the contingency on the network
-                                        contingency.toTask().modify(network, computationManager);
-
-                                        return null;
+                                        try {
+                                            return queue.take();
+                                        } catch (InterruptedException e) {
+                                            Thread.currentThread().interrupt();
+                                            throw new UncheckedInterruptedException(e);
+                                        }
                                     }, computationManager.getExecutor())
-                                    .thenComposeAsync(aVoid -> LoadFlow.runAsync(network, postContStateId, computationManager, postContParameters), computationManager.getExecutor())
-                                    .handleAsync((lfResult, throwable) -> {
-                                        network.getVariantManager().setWorkingVariant(postContStateId);
+                                    .thenCompose(postContStateId -> {
+                                        return CompletableFuture
+                                                .supplyAsync(() -> {
+                                                    network.getVariantManager().cloneVariant(VariantManagerConstants.INITIAL_VARIANT_ID, postContStateId, true);
+                                                    network.getVariantManager().setWorkingVariant(postContStateId);
 
-                                        resultBuilder.contingency(contingency)
-                                                .setComputationOk(lfResult.isOk());
-                                        violationDetector.checkAll(contingency, network, resultBuilder::addViolation);
-                                        resultBuilder.endContingency();
-                                        network.getVariantManager().removeVariant(postContStateId);
+                                                    // apply the contingency on the network
+                                                    contingency.toTask().modify(network, computationManager);
 
-                                        return null;
-                                    }, computationManager.getExecutor());
+                                                    return null;
+                                                }, computationManager.getExecutor())
+                                                .thenComposeAsync(aVoid -> LoadFlow.runAsync(network, postContStateId, computationManager, postContParameters), computationManager.getExecutor())
+                                                .handleAsync((lfResult, throwable) -> {
+                                                    network.getVariantManager().setWorkingVariant(postContStateId);
+                                                    resultBuilder.contingency(contingency)
+                                                            .setComputationOk(lfResult.isOk());
+                                                    violationDetector.checkAll(contingency, network, resultBuilder::addViolation);
+                                                    resultBuilder.endContingency();
+                                                    queue.add(postContStateId); //Will always work because we are putting back in the queue the id we took
+                                                    return null;
+                                                }, computationManager.getExecutor());
+                                    });
                         }
+                        future = CompletableFuture.allOf(futures).whenComplete((aVoid, throwable) ->
+                            //We clean up after the computation.
+                            //Note that this is only executed when all the futures complete (normally or exceptionally),
+                            //so it will fail if the exception is generated in the few lines outside of the future.
+                            variantIds.stream().forEach(network.getVariantManager()::removeVariant)
+                        );
                     } else {
                         resultBuilder.preContingency()
                                 .setComputationOk(false)
                                 .endPreContingency()
                                 .build();
-                        futures = new CompletableFuture[0];
+                        future = CompletableFuture.completedFuture(null);
                     }
 
-                    return CompletableFuture.allOf(futures)
+                    return future
                         .thenApplyAsync(aVoid -> {
                             network.getVariantManager().setWorkingVariant(workingStateId);
                             return resultBuilder.build();
