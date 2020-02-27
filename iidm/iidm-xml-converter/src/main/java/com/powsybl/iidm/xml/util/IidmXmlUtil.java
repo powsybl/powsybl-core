0d9283bdecaed6dc9d47162e69c376c428d9abc0
Mathieu BAGUE <mathieu.bague@rte-france.com>
2020-02-27 19:19:54 +0100

    Add unit tests
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

--- iidm/iidm-xml-converter/src/main/java/com/powsybl/iidm/xml/util/IidmXmlUtil.java
+++ iidm/iidm-xml-converter/src/main/java/com/powsybl/iidm/xml/util/IidmXmlUtil.java
@@ -34,14 +34,17 @@ public final class IidmXmlUtil {
         }
     }
 
+    private static PowsyblException createException(String rootElementName, String elementName, ErrorMessage type, IidmXmlVersion version, IidmXmlVersion contextVersion, String reason) {
+        return new PowsyblException(rootElementName + "." + elementName + " is " + type.message + " for IIDM-XML version " + contextVersion.toString(".") + ". " + reason + version.toString("."));
+    }
+
     /**
      * Assert that the context's IIDM-XML version equals or is less recent than a given IIDM-XML version.
      * If not, throw an exception with a given type of error message.
      */
     public static <C extends AbstractNetworkXmlContext> void assertMaximumVersion(String rootElementName, String elementName, ErrorMessage type, IidmXmlVersion maxVersion, C context) {
         if (context.getVersion().compareTo(maxVersion) > 0) {
-            throw new PowsyblException(rootElementName + "." + elementName + " is " + type.message + " for IIDM-XML version " + context.getVersion().toString(".") + ". " +
-                    "IIDM-XML version should be <= " + maxVersion.toString("."));
+            throw createException(rootElementName, elementName, type, maxVersion, context.getVersion(), "IIDM-XML version should be <= ");
         }
     }
 
@@ -51,8 +54,7 @@ public final class IidmXmlUtil {
      */
     public static <C extends AbstractNetworkXmlContext> void assertMinimumVersion(String rootElementName, String elementName, ErrorMessage type, IidmXmlVersion minVersion, C context) {
         if (context.getVersion().compareTo(minVersion) < 0) {
-            throw new PowsyblException(rootElementName + "." + elementName + " is " + type.message + " for IIDM-XML version " + context.getVersion().toString(".") + ". " +
-                    "IIDM-XML version should be >= " + minVersion.toString("."));
+            throw createException(rootElementName, elementName, type, minVersion, context.getVersion(), "IIDM-XML version should be >= ");
         }
     }
 
