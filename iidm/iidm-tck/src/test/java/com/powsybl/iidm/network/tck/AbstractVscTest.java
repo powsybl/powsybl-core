25b0c4c445b8ec65912d99ebfcd7228319291466
Thomas ADAM <tadam@silicom.fr>
2020-04-03 17:45:05 +0200

    Increase code coverage
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>

--- iidm/iidm-tck/src/test/java/com/powsybl/iidm/network/tck/AbstractVscTest.java
+++ iidm/iidm-tck/src/test/java/com/powsybl/iidm/network/tck/AbstractVscTest.java
@@ -81,7 +81,11 @@ public abstract class AbstractVscTest {
         assertSame(cs2, hvdcLine.getConverterStation(HvdcLine.Side.TWO));
 
         assertEquals(2, hvdcLine.getConverterStation1().getTerminal().getBusView().getBus().getConnectedComponent().getBusStream().count());
+        assertEquals(2, hvdcLine.getConverterStation1().getTerminal().getBusView().getBus().getConnectedComponent().getSize());
+        assertTrue(hvdcLine.getConverterStation1().getTerminal().getBusView().getBus().getConnectedComponent().getBuses().iterator().hasNext());
         assertEquals(1, hvdcLine.getConverterStation1().getTerminal().getBusView().getBus().getSynchronousComponent().getBusStream().count());
+        assertEquals(1, hvdcLine.getConverterStation1().getTerminal().getBusView().getBus().getSynchronousComponent().getSize());
+        assertTrue(hvdcLine.getConverterStation1().getTerminal().getBusView().getBus().getSynchronousComponent().getBuses().iterator().hasNext());
     }
 
     @Test
