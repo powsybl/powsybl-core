aec13a376906836be8cdc7c4668ee28436375fa0
MioRtia <miora.ralambotiana@rte-france.com>
2020-04-08 09:42:13 +0200

    Support XIIDM serialization/deserialization with new shunt API (#1244)
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/ShuntCompensatorXmlTest.java
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/ShuntCompensatorXmlTest.java
@@ -11,7 +11,9 @@ import com.powsybl.iidm.export.ExportOptions;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.ShuntCompensator;
 import com.powsybl.iidm.network.test.ShuntTestCaseFactory;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.io.IOException;
 
@@ -24,8 +26,11 @@ import static org.junit.Assert.fail;
  */
 public class ShuntCompensatorXmlTest extends AbstractXmlConverterTest {
 
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
     @Test
-    public void test() throws IOException {
+    public void linearShuntTest() throws IOException {
         Network network = ShuntTestCaseFactory.create();
         ShuntCompensator sc = network.getShuntCompensator("SHUNT");
         sc.setProperty("test", "test");
@@ -39,25 +44,35 @@ public class ShuntCompensatorXmlTest extends AbstractXmlConverterTest {
     }
 
     @Test
-    public void unsupportedReadTest() {
-        testForAllPreviousVersions(IidmXmlVersion.V_1_2, this::read);
+    public void nonLinearShuntTest() throws IOException {
+        Network network = ShuntTestCaseFactory.createNonLinear();
+        ShuntCompensator sc = network.getShuntCompensator("SHUNT");
+        sc.setProperty("test", "test");
+        roundTripXmlTest(network,
+                NetworkXml::writeAndValidate,
+                NetworkXml::read,
+                getVersionedNetworkPath("nonLinearShuntRoundTripRef.xml", CURRENT_IIDM_XML_VERSION));
+
+        // backward compatibility from version 1.2
+        roundTripVersionedXmlFromMinToCurrentVersionTest("nonLinearShuntRoundTripRef.xml", IidmXmlVersion.V_1_2);
+
+        // check that it fails for versions previous to 1.2
+        testForAllPreviousVersions(IidmXmlVersion.V_1_2, version -> {
+            try {
+                ExportOptions options = new ExportOptions().setVersion(version.toString("."));
+                NetworkXml.write(network, options, tmpDir.resolve("fail"));
+                fail();
+            } catch (PowsyblException e) {
+                assertEquals("Non linear shunts are not supported for IIDM-XML version " + version.toString(".") + ". IIDM-XML version should be >= 1.2",
+                        e.getMessage());
+            }
+        });
     }
 
     @Test
     public void unsupportedWriteTest() {
         Network network = ShuntTestCaseFactory.create();
         testForAllPreviousVersions(IidmXmlVersion.V_1_2, v -> write(network, v.toString(".")));
-    }
-
-    private void read(IidmXmlVersion version) {
-        try {
-            NetworkXml.read(getVersionedNetworkAsStream("faultyShunt.xml", version));
-            fail();
-        } catch (PowsyblException e) {
-            assertEquals("shunt.regulatingTerminal is not supported for IIDM-XML version " +
-                            version.toString(".") + ". IIDM-XML version should be >= 1.2",
-                    e.getMessage());
-        }
     }
 
     private void write(Network network, String version) {
