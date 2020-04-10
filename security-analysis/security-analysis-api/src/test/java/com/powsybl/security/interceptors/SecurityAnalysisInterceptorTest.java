bc9a08b612a6df14998bea548ef7179ab24680a4
yichen88 <30594441+yichen88@users.noreply.github.com>
2020-04-10 16:18:19 +0200

    Improve security analysis interceptor (#1247)
    * Change the design of the SecurityAnalysisResultBuilder (#1207)
    
    * Change the design of the SecurityAnalysisResultBuilder
    
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>
    (cherry picked from commit 63cc9c74bcfaff7dd16ec78d7100f2a607a4eef2)
    
    * Improve security analysis interceptor
    
    Add two more methods in interceptor and two contexts correspondent.
    
    Make interceptors protected
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    Improve interceptor
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Consume extensions while building post result.
    
    The post builder takes extensions when it created.
    Binding extensions on the ContingencyContext before the context is consumed by interceptors.
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Add deprecated annotation
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Some fix.
    
    Remove Violation/Contingency out of its context and make it independent in parameters.
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Fix binding extensions by builder.
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Fix CurrentLimitViolationInterceptor to use new method
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Some fix
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Refactor
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Refactor
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Refactor
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Fix names in test
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Some clean
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Rename and add javadoc
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Add default behavior in new api to smoothly convert to old api
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Add javadoc to explain more the interceptor/context design.
    
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>
    
    * Some fix
    
    Signed-off-by: yichen88 <tang.yichenyves@gmail.com>
    
    * Small fixes
    
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>
    
    Co-authored-by: Mathieu BAGUE <mathieu.bague@rte-france.com>
    Co-authored-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- security-analysis/security-analysis-api/src/test/java/com/powsybl/security/interceptors/SecurityAnalysisInterceptorTest.java
+++ security-analysis/security-analysis-api/src/test/java/com/powsybl/security/interceptors/SecurityAnalysisInterceptorTest.java
@@ -6,11 +6,15 @@
  */
 package com.powsybl.security.interceptors;
 
+import com.powsybl.security.LimitViolationsResult;
+import com.powsybl.security.PostContingencyResult;
+import com.powsybl.security.SecurityAnalysisResult;
 import org.junit.Test;
 
 import java.util.Collections;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 /**
  * @author Mathieu Bague <mathieu.bague at rte-france.com>
@@ -37,6 +41,60 @@ public class SecurityAnalysisInterceptorTest {
             fail();
         } catch (IllegalArgumentException e) {
             // Nothing to do
+        }
+    }
+
+    @Test
+    public void testDefaultNewApi() {
+        RunningContext runningContext = mock(RunningContext.class);
+        LimitViolationsResult preRes = mock(LimitViolationsResult.class);
+        PostContingencyResult postRes = mock(PostContingencyResult.class);
+        SecurityAnalysisResult allRes = mock(SecurityAnalysisResult.class);
+        OldInterceptor oldInterceptor = new OldInterceptor(runningContext, preRes, postRes, allRes);
+        oldInterceptor.onPreContingencyResult(preRes, runningContext);
+        oldInterceptor.onPostContingencyResult(postRes, runningContext);
+        oldInterceptor.onSecurityAnalysisResult(allRes, runningContext);
+
+        for (int i = 0; i < 3; i++) {
+            assertTrue(oldInterceptor.allMethodsCalled[i]);
+        }
+    }
+
+    static class OldInterceptor implements SecurityAnalysisInterceptor {
+
+        private final RunningContext runningContext;
+        private final LimitViolationsResult preRes;
+        private final PostContingencyResult postRes;
+        private final SecurityAnalysisResult allRes;
+
+        private boolean[] allMethodsCalled = new boolean[3];
+
+        public OldInterceptor(RunningContext runningContext, LimitViolationsResult preRes, PostContingencyResult postRes, SecurityAnalysisResult allRes) {
+            this.runningContext = runningContext;
+            this.preRes = preRes;
+            this.postRes = postRes;
+            this.allRes = allRes;
+        }
+
+        @Override
+        public void onPreContingencyResult(RunningContext context, LimitViolationsResult preContingencyResult) {
+            assertSame(runningContext, context);
+            assertSame(preRes, preContingencyResult);
+            allMethodsCalled[0] = true;
+        }
+
+        @Override
+        public void onPostContingencyResult(RunningContext context, PostContingencyResult postContingencyResult) {
+            assertSame(runningContext, context);
+            assertSame(postRes, postContingencyResult);
+            allMethodsCalled[1] = true;
+        }
+
+        @Override
+        public void onSecurityAnalysisResult(RunningContext context, SecurityAnalysisResult result) {
+            assertSame(runningContext, context);
+            assertSame(allRes, result);
+            allMethodsCalled[2] = true;
         }
     }
 
