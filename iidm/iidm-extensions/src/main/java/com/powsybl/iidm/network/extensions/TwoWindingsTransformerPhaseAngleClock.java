8545f97843ce1f22b7337ba2ae5d229cd1f293e6
jlabous <56879412+jlabous@users.noreply.github.com>
2020-05-05 15:37:15 +0200

    Issue 1206 two windings transformer phase angle clock (#1272)
    * implement new extension API on TwoWindingsTransformerPhaseAngleClock
    
    Signed-off-by: Jérémy LABOUS <jlabous@silicom.fr>
    
    * fix ambiguous reference in java 8
    
    Signed-off-by: Jérémy LABOUS <jlabous@silicom.fr>
    
    * take remark in account
    
    Signed-off-by: Jérémy LABOUS <jlabous@silicom.fr>
    
    Co-authored-by: MioRtia <miora.ralambotiana@rte-france.com>

--- iidm/iidm-extensions/src/main/java/com/powsybl/iidm/network/extensions/TwoWindingsTransformerPhaseAngleClock.java
+++ iidm/iidm-extensions/src/main/java/com/powsybl/iidm/network/extensions/TwoWindingsTransformerPhaseAngleClock.java
@@ -1,44 +1,31 @@
 /**
- * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package com.powsybl.iidm.network.extensions;
 
-import com.powsybl.commons.PowsyblException;
-import com.powsybl.commons.extensions.AbstractExtension;
+import com.powsybl.commons.extensions.Extension;
 import com.powsybl.iidm.network.TwoWindingsTransformer;
 
 /**
- * @author José Antonio Marqués <marquesja at aia.es>
+ * @author Jérémy LABOUS <jlabous at silicom.fr>
  */
-public class TwoWindingsTransformerPhaseAngleClock extends AbstractExtension<TwoWindingsTransformer> {
-
-    private int phaseAngleClock;
-
-    public TwoWindingsTransformerPhaseAngleClock(TwoWindingsTransformer twt, int phaseAngleClock) {
-        super(twt);
-        this.phaseAngleClock = checkPhaseAngleClock(phaseAngleClock);
-    }
+public interface TwoWindingsTransformerPhaseAngleClock extends Extension<TwoWindingsTransformer> {
 
     @Override
-    public String getName() {
+    default String getName() {
         return "twoWindingsTransformerPhaseAngleClock";
     }
 
-    public int getPhaseAngleClock() {
-        return phaseAngleClock;
-    }
+    /**
+     * Get the phase angle displacement represented in clock hours.
+     */
+    int getPhaseAngleClock();
 
-    public void setPhaseAngleClock(int phaseAngleClock) {
-        this.phaseAngleClock = checkPhaseAngleClock(phaseAngleClock);
-    }
-
-    private static int checkPhaseAngleClock(int phaseAngleClock) {
-        if (phaseAngleClock < 0 || phaseAngleClock > 11) {
-            throw new PowsyblException("Unexpected value for phaseAngleClock: " + phaseAngleClock);
-        }
-        return phaseAngleClock;
-    }
+    /**
+     * Set the phase angle displacement represented in clock hours
+     */
+    void setPhaseAngleClock(int phaseAngleClock);
 }
