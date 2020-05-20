4c0f24382c477e6ade7af16557f4981448035b31
Mathieu BAGUE <mathieu.bague@rte-france.com>
2020-05-20 18:24:47 +0200

    Curves management for dynamic simulation
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

---
+++ dsl/src/main/java/com/powsybl/dsl/GroovyScripts.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.dsl;
+
+import groovy.lang.GroovyCodeSource;
+import groovy.lang.GroovyShell;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+/**
+ * @author Mathieu Bague <mathieu.bague@rte-france.com>
+ */
+public final class GroovyScripts {
+
+    private GroovyScripts() {
+    }
+
+    public static GroovyCodeSource load(InputStream inputStream) {
+        Objects.requireNonNull(inputStream);
+        return new GroovyCodeSource(new InputStreamReader(inputStream, StandardCharsets.UTF_8), "script", GroovyShell.DEFAULT_CODE_BASE);
+    }
+
+    public static GroovyCodeSource load(Path path) {
+        Objects.requireNonNull(path);
+        try (Reader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            return new GroovyCodeSource(reader, "script", GroovyShell.DEFAULT_CODE_BASE);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+}
---
+++ dynamic-simulation/dynamic-simulation-dsl/src/test/java/com/powsybl/dynamicsimulation/groovy/DummyCurve.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.dynamicsimulation.groovy;
+
+import com.powsybl.dynamicsimulation.Curve;
+
+import java.util.Objects;
+
+/**
+ * @author Mathieu Bague <mathieu.bague@rte-france.com>
+ */
+public class DummyCurve implements Curve {
+
+    private final String id;
+
+    private final String variable;
+
+    public DummyCurve(String id, String variable) {
+        this.id = Objects.requireNonNull(id);
+        this.variable = Objects.requireNonNull(variable);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getVariable() {
+        return variable;
+    }
+
+}
