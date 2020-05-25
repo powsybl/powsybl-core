a2983d360e6ec97aaa423187abb848a0670350b8
marqueslanauja <51124986+marqueslanauja@users.noreply.github.com>
2020-05-25 08:45:52 +0200

    Cgmes conversion . Add phaseTapChangerLinear (#1306)
    * add PhaseTapChangerLinear
    * Pretty code. Add unitary tests
    * Apply reviewer comments
    
    Signed-off-by: José Antonio Marqués <marquesja@aia.es>

--- cgmes/cgmes-conformity/src/test/java/com/powsybl/cgmes/conformity/test/CgmesConformity1ModifiedCatalog.java
+++ cgmes/cgmes-conformity/src/test/java/com/powsybl/cgmes/conformity/test/CgmesConformity1ModifiedCatalog.java
@@ -38,6 +38,26 @@ public final class CgmesConformity1ModifiedCatalog {
                         "MicroGridTestConfiguration_TP_BD.xml"));
     }
 
+    public static TestGridModelResources microT4BePhaseTapChangerLinear() {
+        String base = ENTSOE_CONFORMITY_1
+                + "/MicroGrid/Type4_T4/CGMES_v2.4.15_MicroGridTestConfiguration_T4_BE_BB_Complete_v2/";
+        String baseModified = ENTSOE_CONFORMITY_1_MODIFIED
+                + "/MicroGrid/Type4_T4/BE_BB_PhaseTapChangerLinear/";
+        String baseBoundary = ENTSOE_CONFORMITY_1
+                + "/MicroGrid/Type4_T4/CGMES_v2.4.15_MicroGridTestConfiguration_BD_v2/";
+        return new TestGridModelResources(
+                "MicroGrid-T4-Invalid-SVC-mode",
+                null,
+                new ResourceSet(baseModified,
+                        "MicroGridTestConfiguration_T4_BE_EQ_V2.xml",
+                        "MicroGridTestConfiguration_T4_BE_SSH_V2.xml"),
+                new ResourceSet(base,
+                        "MicroGridTestConfiguration_T4_BE_SV_V2.xml",
+                        "MicroGridTestConfiguration_T4_BE_TP_V2.xml"),
+                new ResourceSet(baseBoundary, "MicroGridTestConfiguration_EQ_BD.xml",
+                        "MicroGridTestConfiguration_TP_BD.xml"));
+    }
+
     public static TestGridModelResources microGridBaseCaseBEPtcSide2() {
         String base = ENTSOE_CONFORMITY_1_MODIFIED
                 + "/MicroGrid/BaseCase/BC_BE_v2_ptc_side_2/";
