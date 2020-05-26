74c518f03049dfcae644410a31a9d708af53e88b
Anne Tilloy <anne.tilloy@rte-france.com>
2020-05-26 09:25:35 +0200

    First import of extension "VoltagePerReactivePowerControl".
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

---
+++ iidm/iidm-extensions/src/main/java/com/powsybl/iidm/network/extensions/VoltagePerReactivePowerControlImpl.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.network.extensions;
+
+import com.powsybl.commons.extensions.AbstractExtension;
+import com.powsybl.iidm.network.StaticVarCompensator;
+
+/**
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+public class VoltagePerReactivePowerControlImpl extends AbstractExtension<StaticVarCompensator> implements VoltagePerReactivePowerControl {
+
+    private double slope;
+
+    public VoltagePerReactivePowerControlImpl(StaticVarCompensator svc, double slope) {
+        super(svc);
+        this.slope = slope;
+    }
+
+    @Override
+    public double getSlope() {
+        return slope;
+    }
+
+    public VoltagePerReactivePowerControl setSlope(double slope) {
+        this.slope = slope;
+        return this;
+    }
+}
