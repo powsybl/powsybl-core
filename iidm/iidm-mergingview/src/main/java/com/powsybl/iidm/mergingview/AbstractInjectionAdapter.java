eb15e3bbcd55cba62fcc1ac69890cb46108512e1
Mathieu BAGUE <mathieu.bague@rte-france.com>
2020-06-04 09:51:45 +0200

    Add AbstractConnectableAdapter class and refactor the code (#1331)
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

--- iidm/iidm-mergingview/src/main/java/com/powsybl/iidm/mergingview/AbstractInjectionAdapter.java
+++ iidm/iidm-mergingview/src/main/java/com/powsybl/iidm/mergingview/AbstractInjectionAdapter.java
@@ -6,47 +6,20 @@
  */
 package com.powsybl.iidm.mergingview;
 
-import com.powsybl.iidm.network.ConnectableType;
 import com.powsybl.iidm.network.Injection;
 import com.powsybl.iidm.network.Terminal;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author Thomas Adam <tadam at silicom.fr>
  */
-abstract class AbstractInjectionAdapter<I extends Injection<I>> extends AbstractIdentifiableAdapter<I> implements Injection<I> {
+abstract class AbstractInjectionAdapter<I extends Injection<I>> extends AbstractConnectableAdapter<I> implements Injection<I> {
 
     protected AbstractInjectionAdapter(I delegate, MergingViewIndex index) {
         super(delegate, index);
     }
 
     @Override
-    public Terminal getTerminal() {
+    public final Terminal getTerminal() {
         return getIndex().getTerminal(getDelegate().getTerminal());
-    }
-
-    @Override
-    public List<? extends Terminal> getTerminals() {
-        return getDelegate().getTerminals().stream()
-                                           .map(getIndex()::getTerminal)
-                                           .collect(Collectors.toList());
-    }
-
-    // -------------------------------
-    // Simple delegated methods ------
-    // -------------------------------
-    @Override
-    public ConnectableType getType() {
-        return getDelegate().getType();
-    }
-
-    // -------------------------------
-    // Not implemented methods -------
-    // -------------------------------
-    @Override
-    public void remove() {
-        throw MergingView.createNotImplementedException();
     }
 }
