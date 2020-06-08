a693c4ef34519ac44cdd100f4df20c171a1debfa
jlabous <56879412+jlabous@users.noreply.github.com>
2020-06-08 14:22:19 +0200

    implement new extension API on CgmesModelExtension (#1339)
    * implement new extension API on CgmesModelExtension
    
    Signed-off-by: Jérémy LABOUS <jlabous@silicom.fr>
    
    * take remark in account
    
    Signed-off-by: Jérémy LABOUS <jlabous@silicom.fr>
    
    Co-authored-by: MioRtia <miora.ralambotiana@rte-france.com>

---
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/CgmesModelExtensionAdderImplProvider.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.cgmes.conversion;
+
+import com.google.auto.service.AutoService;
+import com.powsybl.commons.extensions.ExtensionAdderProvider;
+import com.powsybl.iidm.network.Network;
+
+/**
+ * @author Jérémy LABOUS <jlabous at silicom.fr>
+ */
+@AutoService(ExtensionAdderProvider.class)
+public class CgmesModelExtensionAdderImplProvider implements
+        ExtensionAdderProvider<Network, CgmesModelExtension, CgmesModelExtensionAdderImpl> {
+
+    @Override
+    public String getImplementationName() {
+        return "Default";
+    }
+
+    @Override
+    public Class<CgmesModelExtensionAdderImpl> getAdderClass() {
+        return CgmesModelExtensionAdderImpl.class;
+    }
+
+    @Override
+    public CgmesModelExtensionAdderImpl newAdder(Network extendable) {
+        return new CgmesModelExtensionAdderImpl(extendable);
+    }
+}
