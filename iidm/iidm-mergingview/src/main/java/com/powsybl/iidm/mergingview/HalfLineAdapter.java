deb2449af89dd1bc7041ce93ec72262068ac9004
Mathieu BAGUE <mathieu.bague@rte-france.com>
2020-06-11 17:20:49 +0200

    MergedLine implements TieLine instead of Line HalfLineAdapter adapt a DanglingLine to a HalfLine
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

---
+++ iidm/iidm-mergingview/src/main/java/com/powsybl/iidm/mergingview/HalfLineAdapter.java
@@ -0,0 +1,157 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.iidm.mergingview;
+
+import com.powsybl.iidm.network.DanglingLine;
+import com.powsybl.iidm.network.TieLine;
+
+import java.util.Objects;
+
+/**
+ * @author Mathieu Bague <mathieu.bague@rte-france.com>
+ */
+public class HalfLineAdapter implements TieLine.HalfLine {
+
+    private final DanglingLine danglingLine;
+
+    private double g1;
+
+    private double g2;
+
+    private double b1;
+
+    private double b2;
+
+    public HalfLineAdapter(DanglingLine danglingLine) {
+        this.danglingLine = Objects.requireNonNull(danglingLine);
+        this.g1 = danglingLine.getG() / 2.0;
+        this.g2 = danglingLine.getG() / 2.0;
+        this.b1 = danglingLine.getB() / 2.0;
+        this.b2 = danglingLine.getB() / 2.0;
+    }
+
+    DanglingLine getDanglingLine() {
+        return danglingLine;
+    }
+
+    @Override
+    public String getId() {
+        return danglingLine.getId();
+    }
+
+    @Override
+    public String getName() {
+        return danglingLine.getName();
+    }
+
+    @Override
+    public double getXnodeP() {
+        return danglingLine.getP0();
+    }
+
+    @Override
+    public TieLine.HalfLine setXnodeP(double p) {
+        danglingLine.setP0(p);
+        return this;
+    }
+
+    @Override
+    public double getXnodeQ() {
+        return danglingLine.getQ0();
+    }
+
+    @Override
+    public TieLine.HalfLine setXnodeQ(double q) {
+        danglingLine.setQ0(q);
+        return this;
+    }
+
+    @Override
+    public double getR() {
+        return danglingLine.getR();
+    }
+
+    @Override
+    public TieLine.HalfLine setR(double r) {
+        danglingLine.setR(r);
+        return this;
+    }
+
+    @Override
+    public double getX() {
+        return danglingLine.getX();
+    }
+
+    @Override
+    public TieLine.HalfLine setX(double x) {
+        danglingLine.setX(x);
+        return this;
+    }
+
+    @Override
+    public double getG1() {
+        return g1;
+    }
+
+    @Override
+    public TieLine.HalfLine setG1(double g1) {
+        this.g1 = g1;
+        danglingLine.setG(g1 + g2);
+        return this;
+    }
+
+    @Override
+    public double getG2() {
+        return g2;
+    }
+
+    @Override
+    public TieLine.HalfLine setG2(double g2) {
+        this.g2 = g2;
+        danglingLine.setG(g1 + g2);
+        return this;
+    }
+
+    public TieLine.HalfLine setG(double g) {
+        this.g1 = g / 2.0;
+        this.g2 = g / 2.0;
+        danglingLine.setG(g);
+        return this;
+    }
+
+    @Override
+    public double getB1() {
+        return b1;
+    }
+
+    @Override
+    public TieLine.HalfLine setB1(double b1) {
+        this.b1 = b1;
+        danglingLine.setB(b1 + b2);
+        return this;
+    }
+
+    @Override
+    public double getB2() {
+        return b2;
+    }
+
+    @Override
+    public TieLine.HalfLine setB2(double b2) {
+        this.b2 = b2;
+        danglingLine.setB(b1 + b2);
+        return this;
+    }
+
+    public TieLine.HalfLine setB(double b) {
+        this.b1 = b / 2.0;
+        this.b2 = b / 2.0;
+        danglingLine.setB(b);
+        return this;
+    }
+}
