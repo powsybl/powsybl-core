6f97babe3f263719681480cc5dd4dac404141140
Etienne Lesot <etienne.lesot@rte-france.com>
2020-06-17 10:49:06 +0200

    add Voltage Extension class
    Signed-off-by: Etienne Lesot <etienne.lesot@rte-france.com>

---
+++ commons/src/main/java/com/powsybl/commons/extensions/AbstractPrecontingencyValueExtension.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.commons.extensions;
+
+/**
+ * Abstract extension for Voltage and Current Extensions
+ *
+ * @author Etienne Lesot <etienne.lesot at rte-france.com>
+ */
+
+public abstract class AbstractPrecontingencyValueExtension<T> extends AbstractExtension<T> {
+
+    private final double preContingencyValue;
+
+    public AbstractPrecontingencyValueExtension(double value) {
+        this.preContingencyValue = checkValue(value);
+    }
+
+    public double getPreContingencyValue() {
+        return preContingencyValue;
+    }
+
+    private static double checkValue(double value) {
+        if (Double.isNaN(value)) {
+            throw new IllegalArgumentException("Value is undefined");
+        }
+        return value;
+    }
+}
