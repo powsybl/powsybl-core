02ed8dddb9196bfbc4858bdb2162055ddcf5f1ff
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2020-07-03 10:10:02 +0200

    Implementation of LoadDetail extension (handle fixed and variable part of active and reactive power)
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
    
    tmp
    
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

---
+++ iidm/iidm-extensions/src/main/java/com/powsybl/iidm/network/extensions/LoadDetailXmlSerializer.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.network.extensions;
+
+import com.google.auto.service.AutoService;
+import com.powsybl.commons.extensions.ExtensionXmlSerializer;
+import com.powsybl.commons.xml.XmlReaderContext;
+import com.powsybl.commons.xml.XmlUtil;
+import com.powsybl.commons.xml.XmlWriterContext;
+import com.powsybl.iidm.network.Load;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.InputStream;
+
+/**
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+@AutoService(ExtensionXmlSerializer.class)
+public class LoadDetailXmlSerializer implements ExtensionXmlSerializer<Load, LoadDetail> {
+
+    @Override
+    public String getExtensionName() {
+        return "detail";
+    }
+
+    @Override
+    public String getCategoryName() {
+        return "network";
+    }
+
+    @Override
+    public Class<? super LoadDetail> getExtensionClass() {
+        return LoadDetail.class;
+    }
+
+    @Override
+    public boolean hasSubElements() {
+        return false;
+    }
+
+    @Override
+    public InputStream getXsdAsStream() {
+        return getClass().getResourceAsStream("/xsd/loadDetail.xsd");
+    }
+
+    @Override
+    public String getNamespaceUri() {
+        return "http://www.itesla_project.eu/schema/iidm/ext/load_detail/1_0";
+    }
+
+    @Override
+    public String getNamespacePrefix() {
+        return "ld";
+    }
+
+    @Override
+    public void write(LoadDetail detail, XmlWriterContext context) throws XMLStreamException {
+        XmlUtil.writeFloat("fixedActivePower", detail.getFixedActivePower(), context.getExtensionsWriter());
+        XmlUtil.writeFloat("fixedReactivePower", detail.getFixedReactivePower(), context.getExtensionsWriter());
+        XmlUtil.writeFloat("variableActivePower", detail.getVariableActivePower(), context.getExtensionsWriter());
+        XmlUtil.writeFloat("variableReactivePower", detail.getVariableReactivePower(), context.getExtensionsWriter());
+    }
+
+    @Override
+    public LoadDetail read(Load load, XmlReaderContext context) throws XMLStreamException {
+        float fixedActivePower = XmlUtil.readOptionalFloatAttribute(context.getReader(), "subLoad1ActivePower");
+        if (Float.isNaN(fixedActivePower)) {
+            fixedActivePower = XmlUtil.readFloatAttribute(context.getReader(), "fixedActivePower");
+        }
+        float fixedReactivePower = XmlUtil.readOptionalFloatAttribute(context.getReader(), "subLoad1ReactivePower");
+        if (Float.isNaN(fixedReactivePower)) {
+            fixedReactivePower = XmlUtil.readFloatAttribute(context.getReader(), "fixedReactivePower");
+        }
+        float variableActivePower = XmlUtil.readOptionalFloatAttribute(context.getReader(), "subLoad2ActivePower");
+        if (Float.isNaN(variableActivePower)) {
+            variableActivePower = XmlUtil.readFloatAttribute(context.getReader(), "variableActivePower");
+        }
+        float variableReactivePower = XmlUtil.readOptionalFloatAttribute(context.getReader(), "subLoad2ReactivePower");
+        if (Float.isNaN(variableReactivePower)) {
+            variableReactivePower = XmlUtil.readFloatAttribute(context.getReader(), "variableReactivePower");
+        }
+        load.newExtension(LoadDetailAdder.class)
+                .withFixedActivePower(fixedActivePower)
+                .withFixedReactivePower(fixedReactivePower)
+                .withVariableActivePower(variableActivePower)
+                .withVariableReactivePower(variableReactivePower)
+                .add();
+        return load.getExtension(LoadDetail.class);
+    }
+}
