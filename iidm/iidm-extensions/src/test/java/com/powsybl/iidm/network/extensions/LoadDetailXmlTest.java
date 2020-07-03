5fe6087df6a5943afb65498966590f5856f690ae
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2020-07-03 14:38:25 +0200

    Add tests
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- iidm/iidm-extensions/src/test/java/com/powsybl/iidm/network/extensions/LoadDetailXmlTest.java
+++ iidm/iidm-extensions/src/test/java/com/powsybl/iidm/network/extensions/LoadDetailXmlTest.java
@@ -9,11 +9,14 @@ package com.powsybl.iidm.network.extensions;
 import com.powsybl.commons.AbstractConverterTest;
 import com.powsybl.iidm.network.*;
 import com.powsybl.iidm.xml.NetworkXml;
-import org.joda.time.DateTime;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+import static com.powsybl.iidm.network.extensions.LoadDetailTest.createTestNetwork;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -22,45 +25,11 @@ import static org.junit.Assert.assertNotNull;
  */
 public class LoadDetailXmlTest extends AbstractConverterTest {
 
-    private static Network createTestNetwork() {
-        Network network = NetworkFactory.create("test", "test");
-        network.setCaseDate(DateTime.parse("2016-06-27T12:27:58.535+02:00"));
-        Substation s = network.newSubstation()
-                .setId("S")
-                .setCountry(Country.FR)
-                .add();
-        VoltageLevel vl = s.newVoltageLevel()
-                .setId("VL")
-                .setNominalV(400)
-                .setTopologyKind(TopologyKind.BUS_BREAKER)
-                .add();
-        vl.getBusBreakerView().newBus()
-                .setId("B")
-                .add();
-        vl.newLoad()
-                .setId("L")
-                .setBus("B")
-                .setConnectableBus("B")
-                .setP0(100)
-                .setQ0(50)
-                .add();
-        return network;
-    }
-
     @Test
     public void test() throws IOException {
         Network network = createTestNetwork();
 
-        // extends load
-        Load load = network.getLoad("L");
-        assertNotNull(load);
-        load.newExtension(LoadDetailAdder.class)
-                .withFixedActivePower(40f)
-                .withFixedReactivePower(20f)
-                .withVariableActivePower(60f)
-                .withVariableReactivePower(30f)
-                .add();
-        LoadDetail detail = load.getExtension(LoadDetail.class);
+        LoadDetail detail = network.getLoad("L").getExtension(LoadDetail.class);
         assertNotNull(detail);
 
         Network network2 = roundTripXmlTest(network,
@@ -76,5 +45,20 @@ public class LoadDetailXmlTest extends AbstractConverterTest {
         assertEquals(detail.getFixedReactivePower(), detail2.getFixedReactivePower(), 0f);
         assertEquals(detail.getVariableActivePower(), detail2.getVariableActivePower(), 0f);
         assertEquals(detail.getVariableReactivePower(), detail2.getVariableReactivePower(), 0f);
+    }
+
+    @Test
+    public void testOld() throws IOException {
+        Network network = NetworkXml.read(getClass().getResourceAsStream("/loadDetailOldRef.xml"));
+
+        LoadDetail detail = network.getLoad("L").getExtension(LoadDetail.class);
+        assertNotNull(detail);
+
+        Path tmp = tmpDir.resolve("data");
+        NetworkXml.writeAndValidate(network, tmp);
+
+        try (InputStream is = Files.newInputStream(tmp)) {
+            compareXml(getClass().getResourceAsStream("/loadDetailRef.xml"), is);
+        }
     }
 }
