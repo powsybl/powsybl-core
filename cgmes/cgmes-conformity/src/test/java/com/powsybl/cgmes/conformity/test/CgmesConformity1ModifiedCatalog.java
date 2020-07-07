faa6635578c478c3610fb6aa4c39eb5fc3fd3110
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2020-07-07 13:56:14 +0200

    modification indent
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- cgmes/cgmes-conformity/src/test/java/com/powsybl/cgmes/conformity/test/CgmesConformity1ModifiedCatalog.java
+++ cgmes/cgmes-conformity/src/test/java/com/powsybl/cgmes/conformity/test/CgmesConformity1ModifiedCatalog.java
@@ -472,15 +472,15 @@ public final class CgmesConformity1ModifiedCatalog {
         String baseOriginal = ENTSOE_CONFORMITY_1
                 + "/MiniGrid/BusBranch/CGMES_v2.4.15_MiniGridTestConfiguration_BaseCase_v3/";
         return new TestGridModelResources(
-                "MiniGrid-BusBranch-PhaseAngleClockZero",
-                null,
-                new ResourceSet(base,
-                        "MiniGridTestConfiguration_BC_EQ_v3.0.0.xml"),
-                new ResourceSet(baseOriginal,
-                        "MiniGridTestConfiguration_BC_SSH_v3.0.0.xml",
-                        "MiniGridTestConfiguration_BC_TP_v3.0.0.xml",
-                        "MiniGridTestConfiguration_BC_DL_v3.0.0.xml",
-                        "MiniGridTestConfiguration_BC_SV_v3.0.0.xml"));
+            "MiniGrid-BusBranch-PhaseAngleClockZero",
+            null,
+            new ResourceSet(base,
+                "MiniGridTestConfiguration_BC_EQ_v3.0.0.xml"),
+            new ResourceSet(baseOriginal,
+                "MiniGridTestConfiguration_BC_SSH_v3.0.0.xml",
+                "MiniGridTestConfiguration_BC_TP_v3.0.0.xml",
+                "MiniGridTestConfiguration_BC_DL_v3.0.0.xml",
+                "MiniGridTestConfiguration_BC_SV_v3.0.0.xml"));
     }
 
     public static TestGridModelResources miniBusBranchT2xPhaseAngleClock1NonZero() {
@@ -489,15 +489,15 @@ public final class CgmesConformity1ModifiedCatalog {
         String baseOriginal = ENTSOE_CONFORMITY_1
                 + "/MiniGrid/BusBranch/CGMES_v2.4.15_MiniGridTestConfiguration_BaseCase_v3/";
         return new TestGridModelResources(
-                "MiniGrid-BusBranch-T2xPhaseAngleClock1NonZero",
-                null,
-                new ResourceSet(base,
-                        "MiniGridTestConfiguration_BC_EQ_v3.0.0.xml"),
-                new ResourceSet(baseOriginal,
-                        "MiniGridTestConfiguration_BC_SSH_v3.0.0.xml",
-                        "MiniGridTestConfiguration_BC_TP_v3.0.0.xml",
-                        "MiniGridTestConfiguration_BC_DL_v3.0.0.xml",
-                        "MiniGridTestConfiguration_BC_SV_v3.0.0.xml"));
+            "MiniGrid-BusBranch-T2xPhaseAngleClock1NonZero",
+            null,
+            new ResourceSet(base,
+                "MiniGridTestConfiguration_BC_EQ_v3.0.0.xml"),
+            new ResourceSet(baseOriginal,
+                "MiniGridTestConfiguration_BC_SSH_v3.0.0.xml",
+                "MiniGridTestConfiguration_BC_TP_v3.0.0.xml",
+                "MiniGridTestConfiguration_BC_DL_v3.0.0.xml",
+                "MiniGridTestConfiguration_BC_SV_v3.0.0.xml"));
     }
 
     public static TestGridModelResources miniBusBranchT3xAllPhaseAngleClockNonZero() {
@@ -506,15 +506,15 @@ public final class CgmesConformity1ModifiedCatalog {
         String baseOriginal = ENTSOE_CONFORMITY_1
                 + "/MiniGrid/BusBranch/CGMES_v2.4.15_MiniGridTestConfiguration_BaseCase_v3/";
         return new TestGridModelResources(
-                "MiniGrid-BusBranch-T3xAllPhaseAngleClockNonZero",
-                null,
-                new ResourceSet(base,
-                        "MiniGridTestConfiguration_BC_EQ_v3.0.0.xml"),
-                new ResourceSet(baseOriginal,
-                        "MiniGridTestConfiguration_BC_SSH_v3.0.0.xml",
-                        "MiniGridTestConfiguration_BC_TP_v3.0.0.xml",
-                        "MiniGridTestConfiguration_BC_DL_v3.0.0.xml",
-                        "MiniGridTestConfiguration_BC_SV_v3.0.0.xml"));
+            "MiniGrid-BusBranch-T3xAllPhaseAngleClockNonZero",
+            null,
+            new ResourceSet(base,
+                "MiniGridTestConfiguration_BC_EQ_v3.0.0.xml"),
+            new ResourceSet(baseOriginal,
+                "MiniGridTestConfiguration_BC_SSH_v3.0.0.xml",
+                "MiniGridTestConfiguration_BC_TP_v3.0.0.xml",
+                "MiniGridTestConfiguration_BC_DL_v3.0.0.xml",
+                "MiniGridTestConfiguration_BC_SV_v3.0.0.xml"));
     }
 
     public static TestGridModelResources miniNodeBreakerLimitsforEquipment() {
