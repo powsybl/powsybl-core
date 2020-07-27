7c311fb133dbeb0ee124cf9ef81c86de7011c315
Florian Dupuy <florian.dupuy@rte-france.com>
2020-07-27 12:15:06 +0200

    SlackTerminal serialisation corrections
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- iidm/iidm-xml-converter/src/main/java/com/powsybl/iidm/xml/extensions/SlackTerminalXmlSerializer.java
+++ iidm/iidm-xml-converter/src/main/java/com/powsybl/iidm/xml/extensions/SlackTerminalXmlSerializer.java
@@ -6,11 +6,8 @@
  */
 package com.powsybl.iidm.xml.extensions;
 
-import com.google.auto.service.AutoService;
 import com.powsybl.commons.extensions.AbstractExtensionXmlSerializer;
-import com.powsybl.commons.extensions.ExtensionXmlSerializer;
 import com.powsybl.commons.xml.XmlReaderContext;
-import com.powsybl.commons.xml.XmlUtil;
 import com.powsybl.commons.xml.XmlWriterContext;
 import com.powsybl.iidm.network.Terminal;
 import com.powsybl.iidm.network.VoltageLevel;
@@ -25,34 +22,33 @@ import javax.xml.stream.XMLStreamException;
 /**
  * @author Florian Dupuy <florian.dupuy at rte-france.com>
  */
-@AutoService(ExtensionXmlSerializer.class)
+// FIXME(floriand-e2r): uncomment line below once extension export is done only if exportable (that is, non-cleanable for the SlackTerminal)
+// @AutoService(ExtensionXmlSerializer.class)
 public class SlackTerminalXmlSerializer extends AbstractExtensionXmlSerializer<VoltageLevel, SlackTerminal> {
 
     private static final String ELEM_TERMINAL_REF = "terminalRef";
 
     public SlackTerminalXmlSerializer() {
-        super("slackTerminal", "network", SlackTerminal.class, true, "slackTerminal.xsd",
+        super("slackTerminal", "network", SlackTerminal.class, false, "slackTerminal.xsd",
             "http://www.powsybl.org/schema/iidm/ext/slack_terminal/1_0", "slt");
     }
 
     @Override
     public void write(SlackTerminal slackTerminal, XmlWriterContext context) throws XMLStreamException {
-        TerminalRefXml.writeTerminalRef(slackTerminal.getTerminal(), (NetworkXmlWriterContext) context, getNamespaceUri(), ELEM_TERMINAL_REF);
+        Terminal terminal = slackTerminal.getTerminal();
+        if (terminal != null) {
+            TerminalRefXml.writeTerminalRefAttribute(slackTerminal.getTerminal(), (NetworkXmlWriterContext) context);
+        }
     }
 
     @Override
     public SlackTerminal read(VoltageLevel voltageLevel, XmlReaderContext context) throws XMLStreamException {
         NetworkXmlReaderContext networkContext = (NetworkXmlReaderContext) context;
-        XmlUtil.readUntilEndElement(getExtensionName(), networkContext.getReader(), () -> {
-            if (networkContext.getReader().getLocalName().equals(ELEM_TERMINAL_REF)) {
-                String id = networkContext.getAnonymizer().deanonymizeString(networkContext.getReader().getAttributeValue(null, "id"));
-                String side = networkContext.getReader().getAttributeValue(null, "side");
-                Terminal terminal = TerminalRefXml.readTerminalRef(voltageLevel.getNetwork(), id, side);
-                voltageLevel.newExtension(SlackTerminalAdder.class).withTerminal(terminal).add();
-            } else {
-                throw new AssertionError("Unexpected element: " + networkContext.getReader().getLocalName());
-            }
-        });
+        String id = networkContext.getAnonymizer().deanonymizeString(networkContext.getReader().getAttributeValue(null, "id"));
+        String side = networkContext.getReader().getAttributeValue(null, "side");
+        Terminal terminal = TerminalRefXml.readTerminalRef(voltageLevel.getNetwork(), id, side);
+        voltageLevel.newExtension(SlackTerminalAdder.class).withTerminal(terminal).add();
+
         return voltageLevel.getExtension(SlackTerminal.class);
     }
 
--- iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/extensions/SlackTerminalXmlTest.java
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/extensions/SlackTerminalXmlTest.java
@@ -16,19 +16,21 @@ import com.powsybl.iidm.network.extensions.SlackTerminalAdder;
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
 import com.powsybl.iidm.xml.NetworkXml;
 import org.joda.time.DateTime;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * @author Florian Dupuy <florian.dupuy at rte-france.com>
  */
 public class SlackTerminalXmlTest extends AbstractConverterTest {
 
+    // FIXME(floriand-e2r) Remove ignore annotation once extension export is done only if exportable (that is, non-cleanable for the SlackTerminal)
     @Test
+    @Ignore
     public void test() throws IOException {
         Network network = EurostagTutorialExample1Factory.create();
         network.setCaseDate(DateTime.parse("2019-05-27T12:17:02.504+02:00"));
@@ -51,6 +53,43 @@ public class SlackTerminalXmlTest extends AbstractConverterTest {
                 NetworkXml::read,
                 AbstractConverterTest::compareTxt,
                 "/slackTerminal.xml");
+
+        VoltageLevel vl2 = network2.getVoltageLevel(voltageLevelId);
+        assertNotNull(vl2);
+        SlackTerminal s = vl2.getExtension(SlackTerminal.class);
+        assertNotNull(s);
+
+        assertEquals(s.getTerminal().getBusBreakerView().getBus().getId(), busId);
+    }
+
+    // FIXME(floriand-e2r) Remove ignore annotation once extension export is done only if exportable (that is, non-cleanable for the SlackTerminal)
+    @Test
+    @Ignore
+    public void testNoTerminal() throws IOException {
+        Network network = EurostagTutorialExample1Factory.create();
+
+        String voltageLevelId = "VLHV2";
+        VoltageLevel vl = network.getVoltageLevel(voltageLevelId);
+        assertNotNull(vl);
+
+        String busId = "NHV2";
+        Bus bus = vl.getBusBreakerView().getBus(busId);
+        assertNotNull(bus);
+
+        Terminal t = bus.getConnectedTerminals().iterator().next();
+        assertNotNull(t);
+
+        vl.newExtension(SlackTerminalAdder.class).withTerminal(t).add();
+
+        SlackTerminal st = vl.getExtension(SlackTerminal.class);
+        assertNotNull(st);
+        assertTrue(st.setTerminal(null).isCleanable());
+
+        Network network2 = roundTripTest(network,
+            NetworkXml::writeAndValidate,
+            NetworkXml::read,
+            AbstractConverterTest::compareTxt,
+            "/slackTerminal.xml");
 
         VoltageLevel vl2 = network2.getVoltageLevel(voltageLevelId);
         assertNotNull(vl2);
