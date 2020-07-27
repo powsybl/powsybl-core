85c389e0fa215a743f4e6bc537e37de15bd7e02d
Florian Dupuy <florian.dupuy@rte-france.com>
2020-07-27 16:38:49 +0200

    TerminalFinder with max
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- iidm/iidm-util/src/main/java/com/powsybl/iidm/network/util/TerminalFinder.java
+++ iidm/iidm-util/src/main/java/com/powsybl/iidm/network/util/TerminalFinder.java
@@ -14,26 +14,48 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 /**
- * A class which allows to choose a terminal, according to given filters, among several terminals
+ * A class which allows to choose the best terminal, according to given comparator, among several terminals.
  * @author Florian Dupuy <florian.dupuy at rte-france.com>
  */
 public final class TerminalFinder {
 
-    private final List<Predicate<Terminal>> rules;
+    private final Comparator<Terminal> comparator;
 
-    public static TerminalFinder getDefault() {
+    private static final Comparator<Terminal> DEFAULT_COMPARATOR = getComparator(getDefaultRules());
+
+    private static Comparator<Terminal> getComparator(List<Predicate<Terminal>> predicates) {
+        return (o1, o2) -> {
+            if (o1.equals(o2)) {
+                return 0;
+            }
+            for (Predicate<Terminal> p : predicates) {
+                if (p.test(o1)) {
+                    return 1;
+                } else if (p.test(o2)) {
+                    return -1;
+                }
+            }
+            return 1;
+        };
+    }
+
+    private static List<Predicate<Terminal>> getDefaultRules() {
         List<Predicate<Terminal>> rules = new ArrayList<>();
         rules.add(t -> t.getConnectable() instanceof BusbarSection);
         rules.add(t -> t.getConnectable() instanceof Injection);
         rules.add(t -> t.getConnectable() instanceof Branch);
         rules.add(t -> t.getConnectable() instanceof ThreeWindingsTransformer);
         rules.add(t -> t.getConnectable() instanceof HvdcConverterStation);
-        rules.add(t -> true);
-        return new TerminalFinder(rules);
+        rules.add(Objects::nonNull);
+        return rules;
     }
 
-    public TerminalFinder(List<Predicate<Terminal>> rules) {
-        this.rules = Objects.requireNonNull(rules);
+    public static TerminalFinder getDefault() {
+        return new TerminalFinder(DEFAULT_COMPARATOR);
+    }
+
+    public TerminalFinder(Comparator<Terminal> comparator) {
+        this.comparator = Objects.requireNonNull(comparator);
     }
 
     /**
@@ -49,24 +71,7 @@ public final class TerminalFinder {
      * @return the first terminal satisfying a rule (rules are checked in ascending order)
      */
     public Terminal find(Stream<? extends Terminal> terminals) {
-        Map<Predicate<Terminal>, Terminal> resultMap = new HashMap<>();
-        List<Predicate<Terminal>> rulesNotFound = new LinkedList<>(rules);
-        terminals.forEach(t -> {
-            Iterator<Predicate<Terminal>> iterator = rulesNotFound.iterator();
-            while (iterator.hasNext()) {
-                Predicate<Terminal> predicate = iterator.next();
-                if (predicate.test(t)) {
-                    resultMap.put(predicate, t);
-                    iterator.remove();
-                }
-            }
-        });
-        for (Predicate<Terminal> predicate : rules) {
-            if (resultMap.containsKey(predicate)) {
-                return resultMap.get(predicate);
-            }
-        }
-        return null;
+        return terminals.max(comparator).orElse(null);
     }
 
 }
