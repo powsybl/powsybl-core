8fc524855a3e117b0fc1b4e4417073d28ca5ec30
Luma <zamarrenolm@aia.es>
2020-07-28 11:15:31 +0200

    export SV without using original CGMES model (wip)
    Signed-off-by: Luma <zamarrenolm@aia.es>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/CgmesExport.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/CgmesExport.java
@@ -7,17 +7,35 @@
 
 package com.powsybl.cgmes.conversion;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
+import java.util.UUID;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 
 import com.google.auto.service.AutoService;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import com.powsybl.cgmes.conversion.update.CgmesUpdate;
 import com.powsybl.cgmes.conversion.update.StateVariablesAdder;
 import com.powsybl.cgmes.model.CgmesModel;
 import com.powsybl.cgmes.model.CgmesModelException;
 import com.powsybl.cgmes.model.CgmesModelFactory;
+import com.powsybl.commons.PowsyblException;
 import com.powsybl.commons.datasource.DataSource;
+import com.powsybl.commons.exceptions.UncheckedXmlStreamException;
 import com.powsybl.iidm.export.Exporter;
+import com.powsybl.iidm.network.Bus;
+import com.powsybl.iidm.network.DanglingLine;
 import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.TopologyKind;
+
+import javanet.staxutils.IndentingXMLStreamWriter;
 
 /**
  * @author Luma Zamarreño <zamarrenolm at aia.es>
@@ -27,6 +45,110 @@ public class CgmesExport implements Exporter {
 
     @Override
     public void export(Network network, Properties params, DataSource ds) {
+        if (Boolean.valueOf(params.getProperty("cgmes.export.usingOnlyNetwork"))) {
+            exportUsingOnlyNetwork(network, params, ds);
+        } else {
+            exportUsingOriginalCgmesModel(network, params, ds);
+        }
+    }
+
+    private static final Supplier<XMLOutputFactory> XML_OUTPUT_FACTORY_SUPPLIER = Suppliers.memoize(XMLOutputFactory::newFactory);
+    public static final String RDF_NAMESPACE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+    public static final String CIM_NAMESPACE = "http://iec.ch/TC57/2013/CIM-schema-cim16#";
+    private static final String MD_NAMESPACE = "http://iec.ch/TC57/61970-552/ModelDescription/1#";
+    private static final boolean INDENT = true;
+
+    private void exportUsingOnlyNetwork(Network network, Properties params, DataSource ds) {
+        CgmesModelExtension ext = network.getExtension(CgmesModelExtension.class);
+        if (ext != null) {
+            CgmesModel cgmesSource = ext.getCgmesModel();
+            if (cgmesSource != null) {
+                throw new CgmesModelException("CGMES model should not be available in Network");
+            }
+        }
+        String baseName = network.getProperty("baseName");
+        try (OutputStream os = ds.newOutputStream(baseName + "_SV.xml", false);
+                BufferedOutputStream bos = new BufferedOutputStream(os)) {
+            try {
+                writeSV(network, initializeWriter(bos));
+            } catch (XMLStreamException e) {
+                throw new UncheckedXmlStreamException(e);
+            }
+        } catch (IOException x) {
+            throw new PowsyblException("Exporting to CGMES using only Network");
+        }
+    }
+
+    private static void writeSV(Network network, XMLStreamWriter writer) throws XMLStreamException {
+        // FIXME(Luma)
+        // Node-breaker: build topological nodes
+        // Bus-branch: reuse topological nodes (stored in bus-breaker view buses)
+        boolean nodeBreaker = network.getVoltageLevelStream().anyMatch(vl -> vl.getTopologyKind() == TopologyKind.NODE_BREAKER);
+        System.err.println("node-breaker : " + nodeBreaker);
+        for (Bus b : network.getBusBreakerView().getBuses()) {
+            // FIXME(Luma) for bus-branch the bus id in the bus-breaker view contains the topoNode id
+            // For node-breaker, we can try to use aliases or properties???
+            String topologicalNode = b.getId();
+            writeSvVoltage(writer, topologicalNode, b.getV(), b.getAngle());
+        }
+        // Voltages at boundary nodes of dangling lines
+        for (DanglingLine dl : network.getDanglingLines()) {
+            // FIXME(Luma) We should store an alias for the topological node for the boundary bus of the dangling line
+            String topologicalNode = "XXX";
+            writeSvVoltage(writer, topologicalNode, Double.valueOf(dl.getProperty("v", "NaN")), Double.valueOf(dl.getProperty("angle", "NaN")));
+        }
+        writer.writeEndElement();
+    }
+
+    private static void writeSvVoltage(XMLStreamWriter writer, String topologicalNode, double v, double angle) throws XMLStreamException {
+        // FIXME(Luma) remove this reference block
+        // <cim:SvVoltage rdf:ID="_d4548915-4f00-4507-ba54-350cafcee1af">
+        //     <cim:SvVoltage.angle>-18.20656</cim:SvVoltage.angle>
+        //     <cim:SvVoltage.v>128.575378</cim:SvVoltage.v>
+        //     <cim:SvVoltage.TopologicalNode rdf:resource="#_0471bd2a-c766-11e1-8775-005056c00008" />
+        // </cim:SvVoltage>
+        writer.writeStartElement("cim:SvVoltage");
+        writer.writeAttribute("rdf:ID", getUniqueId());
+        writer.writeStartElement("cim:SvVoltage.angle");
+        writer.writeCharacters(Double.toString(angle));
+        writer.writeEndElement();
+        writer.writeStartElement("cim:SvVoltage.v");
+        writer.writeCharacters(Double.toString(v));
+        writer.writeEndElement();
+        writer.writeEmptyElement("cim:SvVoltage.TopologicalNode");
+        writer.writeAttribute("rdf:resource", "#" + topologicalNode);
+        writer.writeEndElement();
+    }
+
+    private static String getUniqueId() {
+        return UUID.randomUUID().toString();
+    }
+
+    private static XMLStreamWriter initializeWriter(OutputStream os) throws XMLStreamException {
+        XMLStreamWriter writer;
+        writer = XML_OUTPUT_FACTORY_SUPPLIER.get().createXMLStreamWriter(os, StandardCharsets.UTF_8.toString());
+        if (INDENT) {
+            IndentingXMLStreamWriter indentingWriter = new IndentingXMLStreamWriter(writer);
+            indentingWriter.setIndent("    ");
+            writer = indentingWriter;
+        }
+        writer.writeStartDocument(StandardCharsets.UTF_8.toString(), "1.0");
+        // FIXME(Luma) remove this reference line and consider adding entsoe namespace
+        // <rdf:RDF xmlns:cim="http://iec.ch/TC57/2013/CIM-schema-cim16#"
+        //          xmlns:entsoe="http://entsoe.eu/CIM/SchemaExtension/3/1#"
+        //          xmlns:md="http://iec.ch/TC57/61970-552/ModelDescription/1#"
+        //          xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+        writer.setPrefix("rdf", RDF_NAMESPACE);
+        writer.setPrefix("cim", CIM_NAMESPACE);
+        writer.setPrefix("md", MD_NAMESPACE);
+        writer.writeStartElement(RDF_NAMESPACE, "RDF");
+        writer.writeNamespace("rdf", RDF_NAMESPACE);
+        writer.writeNamespace("cim", CIM_NAMESPACE);
+        writer.writeNamespace("md", MD_NAMESPACE);
+        return writer;
+    }
+
+    private void exportUsingOriginalCgmesModel(Network network, Properties params, DataSource ds) {
 
         // Right now the network must contain the original CgmesModel
         // In the future it should be possible to export to CGMES
--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/update/StateVariablesAdderTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/update/StateVariablesAdderTest.java
@@ -7,17 +7,31 @@
 package com.powsybl.cgmes.conversion.test.update;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+
+import javax.xml.transform.Source;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.xmlunit.builder.DiffBuilder;
+import org.xmlunit.builder.Input;
+import org.xmlunit.diff.DefaultNodeMatcher;
+import org.xmlunit.diff.Diff;
+import org.xmlunit.diff.Difference;
+import org.xmlunit.diff.ElementSelectors;
 
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
@@ -37,7 +51,6 @@ import com.powsybl.commons.datasource.ReadOnlyDataSource;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.NetworkFactory;
 import com.powsybl.triplestore.api.PropertyBags;
-import com.powsybl.triplestore.api.TripleStoreFactory;
 
 /**
  * @author Elena Kaltakova <kaltakovae at aia.es>
@@ -65,11 +78,91 @@ public class StateVariablesAdderTest {
         importExportTest(CgmesConformity1Catalog.smallNodeBreaker().dataSource());
     }
 
-    void importExportTest(ReadOnlyDataSource ds) throws IOException {
-        importExportTest(ds, TripleStoreFactory.defaultImplementation());
+    @Test
+    public void svAlternatives() throws IOException {
+        exportUsingCgmesModelUsingOnlyNetworkAndCompareSV(CgmesConformity1Catalog.smallBusBranch().dataSource());
+        exportUsingCgmesModelUsingOnlyNetworkAndCompareSV(CgmesConformity1Catalog.smallNodeBreaker().dataSource());
     }
 
-    private void importExportTest(ReadOnlyDataSource ds, String impl) throws IOException {
+    private void exportUsingCgmesModelUsingOnlyNetworkAndCompareSV(ReadOnlyDataSource ds) throws IOException {
+        Network network0 = cgmesImport.importData(ds, NetworkFactory.findDefault(), importParameters("false"));
+        NetworkChanges.modifyStateVariables(network0);
+        network0.setProperty("baseName",  ds.getBaseName());
+
+        CgmesExport e = new CgmesExport();
+
+        // Export modified network to new CGMES using two different variants
+        DataSource tmpUsingCgmes = tmpDataSource("usingCgmes");
+        DataSource tmpUsingOnlyNetwork = tmpDataSource("usingOnlyNetwork");
+        Properties ep = new Properties();
+        e.export(network0, ep, tmpUsingCgmes);
+        ep.setProperty("cgmes.export.usingOnlyNetwork", "true");
+        network0.removeExtension(CgmesModelExtension.class);
+        e.export(network0, ep, tmpUsingOnlyNetwork);
+
+        // Compare resulting SV of both variants
+        System.err.println("Output files:");
+        String sv1 = tmpUsingCgmes.listNames(".*SV.*").parallelStream().findFirst().orElse("-");
+        String sv2 = tmpUsingOnlyNetwork.listNames(".*SV.*").parallelStream().findFirst().orElse("-");
+        System.err.println("   using CGMES " + sv1);
+        System.err.println("   using CGMES " + sv2);
+        assertTrue(sv1.contains(ds.getBaseName()));
+        assertTrue(sv2.contains(ds.getBaseName()));
+        try (InputStream is1 = tmpUsingCgmes.newInputStream(sv1)) {
+            try (InputStream is2 = tmpUsingOnlyNetwork.newInputStream(sv2)) {
+                compareXmlStepByStep(is1, is2);
+            }
+        }
+    }
+
+    protected static void compareXmlStepByStep(InputStream expected, InputStream actual) {
+        Source control = Input.fromStream(expected).build();
+        Source test = Input.fromStream(actual).build();
+        Map<String, String> prefixUris = new HashMap<>(2);
+        prefixUris.put("cim", CgmesExport.CIM_NAMESPACE);
+        prefixUris.put("rdf", CgmesExport.RDF_NAMESPACE);
+        Diff myDiff = DiffBuilder.compare(control).withTest(test)
+                .ignoreWhitespace()
+                .ignoreComments()
+                .withNodeFilter(node -> {
+                    String localName = node.getLocalName();
+                    // FIXME(Luma) Only checking voltages at this stage
+                    return localName != null && (localName.equals("RDF") || localName.startsWith("SvVoltage"));
+                })
+                .withAttributeFilter(attr -> {
+                    // Identifiers of SV objects are not persistent,
+                    // can be completely ignored for comparison with control
+                    String elementName = attr.getOwnerElement().getLocalName();
+                    boolean ignored = elementName != null && elementName.startsWith("Sv") && attr.getLocalName().equals("ID");
+                    System.err.println("elem " + elementName + ", attr " + attr + ", ignored ? " + ignored);
+                    return !ignored;
+                })
+                // FIXME(Luma) Trying to compare SvVoltage objects
+                // that have same topological node
+                // (does not work)
+                .withNodeMatcher(new DefaultNodeMatcher(
+                        ElementSelectors.conditionalBuilder()
+                            .whenElementIsNamed("SvVoltage")
+                            .thenUse(ElementSelectors.byXPath("./cim:SvVoltage.TopologicalNode/@rdf:resource", prefixUris, ElementSelectors.byNameAndText))
+                            .elseUse(ElementSelectors.byName)
+                            .build()))
+                .withComparisonListeners((comparison, outcome) -> {
+                    System.err.println("comparison " + comparison.getType() + " was " + outcome);
+                    System.err.println("    " + comparison.getControlDetails().getXPath());
+                    System.err.println("    " + comparison.getTestDetails().getXPath());
+                    System.err.printf("");
+                })
+                .build();
+        boolean hasDiff = myDiff.hasDifferences();
+        if (hasDiff) {
+            for (Difference diff : myDiff.getDifferences()) {
+                System.out.println(diff.getComparison().toString());
+            }
+        }
+        assertFalse(hasDiff);
+    }
+
+    private void importExportTest(ReadOnlyDataSource ds) throws IOException {
         Network network0 = cgmesImport.importData(ds, NetworkFactory.findDefault(), importParameters("false"));
         CgmesModelExtension ext0 = network0.getExtension(CgmesModelExtension.class);
         if (ext0 == null) {
@@ -81,8 +174,8 @@ public class StateVariablesAdderTest {
         PropertyBags fullModel0 = cgmes0.fullModel(CgmesSubset.STATE_VARIABLES.getProfile());
         NetworkChanges.modifyStateVariables(network0);
 
-        // Export modified network to new cgmes
-        DataSource tmp = tmpDataSource(impl);
+        // Export modified network to new CGMES
+        DataSource tmp = tmpDataSource("export-modified");
         CgmesExport e = new CgmesExport();
         e.export(network0, new Properties(), tmp);
 
@@ -112,8 +205,8 @@ public class StateVariablesAdderTest {
         new Comparison(network0, network1, new ComparisonConfig()).compareBuses();
     }
 
-    private DataSource tmpDataSource(String impl) throws IOException {
-        Path exportFolder = fileSystem.getPath("impl-" + impl);
+    private DataSource tmpDataSource(String name) throws IOException {
+        Path exportFolder = fileSystem.getPath(name);
         if (Files.exists(exportFolder)) {
             FileUtils.cleanDirectory(exportFolder.toFile());
         }
