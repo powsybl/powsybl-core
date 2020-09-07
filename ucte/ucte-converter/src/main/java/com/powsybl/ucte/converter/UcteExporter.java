b0bb6dea5aae718cb867242e4d24775744e207f0
Kacem NAJJARI <belgacem.najjari@rte-france.com>
2020-09-07 13:22:17 +0200

    fix sonar
    Signed-off-by: Kacem NAJJARI <belgacem.najjari@rte-france.com>

--- ucte/ucte-converter/src/main/java/com/powsybl/ucte/converter/UcteExporter.java
+++ ucte/ucte-converter/src/main/java/com/powsybl/ucte/converter/UcteExporter.java
@@ -840,7 +840,7 @@ public class UcteExporter implements Exporter {
     private static Integer getPermanentLimit(Branch<?> branch) {
         Optional<Double> permanentLimit1 = Optional.ofNullable(branch.getCurrentLimits1()).map(CurrentLimits::getPermanentLimit);
         Optional<Double> permanentLimit2 = Optional.ofNullable(branch.getCurrentLimits2()).map(CurrentLimits::getPermanentLimit);
-        if (permanentLimit1.isPresent() && permanentLimit2.isPresent()) {
+        if (permanentLimit1.isPresent() & permanentLimit2.isPresent()) {
             return (int) Double.min(permanentLimit1.get(), permanentLimit2.get());
         } else if (permanentLimit1.isPresent()) {
             return permanentLimit1.get().intValue();
