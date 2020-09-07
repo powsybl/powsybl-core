0a4753d7f095a68a8cb7c943134c63183bfa8e23
Kacem NAJJARI <belgacem.najjari@rte-france.com>
2020-09-07 14:09:26 +0200

    fix sonar getPermanentLimit

--- ucte/ucte-converter/src/main/java/com/powsybl/ucte/converter/UcteExporter.java
+++ ucte/ucte-converter/src/main/java/com/powsybl/ucte/converter/UcteExporter.java
@@ -837,10 +837,12 @@ public class UcteExporter implements Exporter {
     private static Integer getPermanentLimit(Branch<?> branch) {
         Optional<Double> permanentLimit1 = Optional.ofNullable(branch.getCurrentLimits1()).map(CurrentLimits::getPermanentLimit);
         Optional<Double> permanentLimit2 = Optional.ofNullable(branch.getCurrentLimits2()).map(CurrentLimits::getPermanentLimit);
-        if (permanentLimit1.isPresent() & permanentLimit2.isPresent()) {
-            return (int) Double.min(permanentLimit1.get(), permanentLimit2.get());
-        } else if (permanentLimit1.isPresent()) {
-            return permanentLimit1.get().intValue();
+        if (permanentLimit1.isPresent()) {
+            if (permanentLimit2.isPresent()) {
+                return (int) Double.min(permanentLimit1.get(), permanentLimit2.get());
+            } else {
+                return permanentLimit1.get().intValue();
+            }
         } else if (permanentLimit2.isPresent()) {
             return permanentLimit2.get().intValue();
         } else {
