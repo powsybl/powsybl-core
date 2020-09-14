e77711980adcdbb8ce987558c71b6f21d767695e
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2020-09-14 16:12:51 +0200

    Add test
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

---
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/extensions/MergedCimCharacteristicsTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.cgmes.conversion.extensions;
+
+import com.powsybl.cgmes.conversion.elements.CgmesTopologyKind;
+import com.powsybl.iidm.mergingview.MergingView;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
+import com.powsybl.iidm.network.test.NetworkTest1Factory;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
+ */
+public class MergedCimCharacteristicsTest {
+
+    @Test
+    public void test() {
+        Network network1 = EurostagTutorialExample1Factory.create();
+        network1.newExtension(CimCharacteristicsAdder.class)
+                .setCimVersion(14)
+                .setTopologyKind(CgmesTopologyKind.BUS_BRANCH)
+                .add();
+
+        Network network2 = NetworkTest1Factory.create();
+        network2.newExtension(CimCharacteristicsAdder.class)
+                .setCimVersion(16)
+                .setTopologyKind(CgmesTopologyKind.NODE_BREAKER)
+                .add();
+
+        MergingView network = MergingView.create("network", "test");
+        network.merge(network1, network2);
+        CimCharacteristics mergedExt = network.getExtension(CimCharacteristics.class);
+        assertNotNull(mergedExt);
+        assertEquals(14, mergedExt.getCimVersion());
+        assertEquals(CgmesTopologyKind.NODE_BREAKER, mergedExt.getTopologyKind());
+    }
+}
