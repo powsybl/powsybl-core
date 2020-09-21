a8ccb2e3a56b2242e6b39985665d5d930cd7a5ee
Luma <zamarrenolm@aia.es>
2020-09-21 19:10:38 +0200

    store boundary topological node as an alias of dangling line
    Signed-off-by: Luma <zamarrenolm@aia.es>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/AbstractConnectorConversion.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/AbstractConnectorConversion.java
@@ -10,6 +10,7 @@ package com.powsybl.cgmes.conversion.elements;
 import java.util.List;
 
 import com.powsybl.cgmes.conversion.Context;
+import com.powsybl.cgmes.model.CgmesNames;
 import com.powsybl.cgmes.model.PowerFlow;
 import com.powsybl.commons.PowsyblException;
 import com.powsybl.iidm.network.DanglingLine;
@@ -99,6 +100,7 @@ public abstract class AbstractConnectorConversion extends AbstractConductingEqui
                 .add();
         }
         addAliases(dl);
+        dl.addAlias(boundaryNode, CgmesNames.TOPOLOGICAL_NODE);
         context.convertedTerminal(terminalId(modelSide), dl.getTerminal(), 1, powerFlow(modelSide));
 
         // If we do not have power flow at model side and we can compute it,
