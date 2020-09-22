94dbdc2341c2a205a1de8a41411eb495adbe689c
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2020-09-22 16:47:46 +0200

    Add test
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/update/StateVariablesExportTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/update/StateVariablesExportTest.java
@@ -11,6 +11,7 @@ import com.powsybl.cgmes.conversion.CgmesImport;
 import com.powsybl.cgmes.conversion.update.CgmesExportContext;
 import com.powsybl.cgmes.conversion.update.StateVariablesExport;
 import com.powsybl.commons.AbstractConverterTest;
+import com.powsybl.commons.datasource.ReadOnlyDataSource;
 import com.powsybl.computation.DefaultComputationManagerConfig;
 import com.powsybl.iidm.import_.ImportConfig;
 import com.powsybl.iidm.import_.Importers;
@@ -44,13 +45,42 @@ import static org.junit.Assert.assertEquals;
  */
 public class StateVariablesExportTest extends AbstractConverterTest {
 
+    private static final String CONFORMITY_DIR = "/conformity/cas-1.1.3-data-4.0.3/";
+
     @Test
-    public void test() throws IOException, XMLStreamException {
+    public void microGridBE() throws IOException, XMLStreamException {
+        String microGridDir = "MicroGrid/BaseCase/";
+        String microGridBeDir = microGridDir + "CGMES_v2.4.15_MicroGridTestConfiguration_BC_BE_v2/";
+        String microGridBd = microGridDir + "CGMES_v2.4.15_MicroGridTestConfiguration_BD_v2/";
+        test(CgmesConformity1Catalog.microGridBaseCaseBE().dataSource(),
+                CONFORMITY_DIR + microGridBeDir + "MicroGridTestConfiguration_BC_BE_EQ_V2.xml",
+                CONFORMITY_DIR + microGridBeDir + "MicroGridTestConfiguration_BC_BE_TP_V2.xml",
+                CONFORMITY_DIR + microGridBeDir + "MicroGridTestConfiguration_BC_BE_SSH_V2.xml",
+                CONFORMITY_DIR + microGridBd + "MicroGridTestConfiguration_EQ_BD.xml",
+                CONFORMITY_DIR + microGridBd + "MicroGridTestConfiguration_TP_BD.xml",
+                2);
+    }
+
+    @Test
+    public void smallGrid() throws IOException, XMLStreamException {
+        String smallGridDir = "SmallGrid/BusBranch/";
+        String smallGridBaseDir = smallGridDir + "CGMES_v2.4.15_SmallGridTestConfiguration_BaseCase_Complete_v3.0.0/";
+        String smallGridBd = smallGridDir + "CGMES_v2.4.15_SmallGridTestConfiguration_Boundary_v3.0.0/";
+        test(CgmesConformity1Catalog.smallBusBranch().dataSource(),
+                CONFORMITY_DIR + smallGridBaseDir + "SmallGridTestConfiguration_BC_EQ_v3.0.0.xml",
+                CONFORMITY_DIR + smallGridBaseDir + "SmallGridTestConfiguration_BC_TP_v3.0.0.xml",
+                CONFORMITY_DIR + smallGridBaseDir + "SmallGridTestConfiguration_BC_SSH_v3.0.0.xml",
+                CONFORMITY_DIR + smallGridBd + "SmallGridTestConfiguration_EQ_BD_v3.0.0.xml",
+                CONFORMITY_DIR + smallGridBd + "SmallGridTestConfiguration_TP_BD_v3.0.0.xml",
+                4);
+    }
+
+    private void test(ReadOnlyDataSource dataSource, String eq, String tp, String ssh, String eqBd, String tpBd, int svVersion) throws IOException, XMLStreamException {
         Properties properties = new Properties();
         properties.put("iidm.import.cgmes.profile-used-for-initial-state-values", "SV");
 
-        // Import original micro grid BE
-        Network expected = new CgmesImport().importData(CgmesConformity1Catalog.microGridBaseCaseBE().dataSource(), properties);
+        // Import original
+        Network expected = new CgmesImport().importData(dataSource, properties);
 
         // Export SV
         Path test = tmpDir.resolve("test");
@@ -59,37 +89,37 @@ public class StateVariablesExportTest extends AbstractConverterTest {
             IndentingXMLStreamWriter indentingWriter = new IndentingXMLStreamWriter(writer);
             indentingWriter.setIndent("    ");
             writer = indentingWriter;
-            StateVariablesExport.write(expected, writer, new CgmesExportContext(expected).setSvVersion(2));
+            StateVariablesExport.write(expected, writer, new CgmesExportContext(expected).setSvVersion(svVersion));
         }
 
-        // Zip micro grid BE with new SV
-        try (OutputStream fos = Files.newOutputStream(tmpDir.resolve("repackaged-microGrid-BE.zip"));
+        // Zip with new SV
+        try (OutputStream fos = Files.newOutputStream(tmpDir.resolve("repackaged.zip"));
              ZipOutputStream zipOut = new ZipOutputStream(fos)) {
-            try (InputStream eqIs = CgmesConformity1Catalog.class.getResourceAsStream("/conformity/cas-1.1.3-data-4.0.3/MicroGrid/BaseCase/CGMES_v2.4.15_MicroGridTestConfiguration_BC_BE_v2/MicroGridTestConfiguration_BC_BE_EQ_V2.xml")) {
+            try (InputStream eqIs = CgmesConformity1Catalog.class.getResourceAsStream(eq)) {
                 zipFile("EQ.xml", eqIs, zipOut);
             }
-            try (InputStream tpIs = CgmesConformity1Catalog.class.getResourceAsStream("/conformity/cas-1.1.3-data-4.0.3/MicroGrid/BaseCase/CGMES_v2.4.15_MicroGridTestConfiguration_BC_BE_v2/MicroGridTestConfiguration_BC_BE_TP_V2.xml")) {
+            try (InputStream tpIs = CgmesConformity1Catalog.class.getResourceAsStream(tp)) {
                 zipFile("TP.xml", tpIs, zipOut);
             }
-            try (InputStream sshIs = CgmesConformity1Catalog.class.getResourceAsStream("/conformity/cas-1.1.3-data-4.0.3/MicroGrid/BaseCase/CGMES_v2.4.15_MicroGridTestConfiguration_BC_BE_v2/MicroGridTestConfiguration_BC_BE_SSH_V2.xml")) {
+            try (InputStream sshIs = CgmesConformity1Catalog.class.getResourceAsStream(ssh)) {
                 zipFile("SSH.xml", sshIs, zipOut);
             }
             try (InputStream svIs = Files.newInputStream(test)) {
                 zipFile("SV.xml", svIs, zipOut);
             }
-            try (InputStream eqBdIs = CgmesConformity1Catalog.class.getResourceAsStream("/conformity/cas-1.1.3-data-4.0.3/MicroGrid/BaseCase/CGMES_v2.4.15_MicroGridTestConfiguration_BD_v2/MicroGridTestConfiguration_EQ_BD.xml")) {
+            try (InputStream eqBdIs = CgmesConformity1Catalog.class.getResourceAsStream(eqBd)) {
                 zipFile("EQ_BD.xml", eqBdIs, zipOut);
             }
-            try (InputStream tpBdIs = CgmesConformity1Catalog.class.getResourceAsStream("/conformity/cas-1.1.3-data-4.0.3/MicroGrid/BaseCase/CGMES_v2.4.15_MicroGridTestConfiguration_BD_v2/MicroGridTestConfiguration_TP_BD.xml")) {
+            try (InputStream tpBdIs = CgmesConformity1Catalog.class.getResourceAsStream(tpBd)) {
                 zipFile("TP_BD.xml", tpBdIs, zipOut);
             }
         }
 
-        // Import micro grid BE with new SV
-        Network actual = Importers.loadNetwork(tmpDir.resolve("repackaged-microGrid-BE.zip"),
+        // Import with new SV
+        Network actual = Importers.loadNetwork(tmpDir.resolve("repackaged.zip"),
                 DefaultComputationManagerConfig.load().createShortTimeExecutionComputationManager(), ImportConfig.load(), properties);
 
-        // Export original micro grid BE and micro grid BE with new SV
+        // Export original and with new SV
         NetworkXml.writeAndValidate(expected, tmpDir.resolve("expected"));
         NetworkXml.writeAndValidate(actual, tmpDir.resolve("actual"));
 
@@ -116,6 +146,9 @@ public class StateVariablesExportTest extends AbstractConverterTest {
         Source test = Input.fromStream(actual).build();
         Diff myDiff = DiffBuilder.compare(control).withTest(test).ignoreWhitespace().ignoreComments().build();
         for (Difference diff : myDiff.getDifferences()) {
+            if (diff.getComparison().getControlDetails().getXPath().endsWith("forecastDistance")) {
+                continue;
+            }
             double exp = Double.parseDouble((String) diff.getComparison().getControlDetails().getValue());
             double act = Double.parseDouble((String) diff.getComparison().getTestDetails().getValue());
             assertEquals(exp, act, getDelta(diff.getComparison().getControlDetails().getXPath()));
