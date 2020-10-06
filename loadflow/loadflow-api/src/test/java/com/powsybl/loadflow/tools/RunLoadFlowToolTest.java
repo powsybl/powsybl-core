bb50df2057154c82ea1d41ba5ea3755ed6f709c8
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-10-06 21:29:12 +0200

    LoadFlowResult 1.1 (#1497)
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
    Co-authored-by: Anne Tilloy <48123713+annetill@users.noreply.github.com>
    (cherry picked from commit 78e90a6acd0740bb17ee83b1a6016904906a77e8)

---
+++ loadflow/loadflow-api/src/test/java/com/powsybl/loadflow/tools/RunLoadFlowToolTest.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.loadflow.tools;
+
+import com.powsybl.commons.AbstractConverterTest;
+import com.powsybl.commons.io.table.AsciiTableFormatterFactory;
+import com.powsybl.commons.io.table.TableFormatterConfig;
+import com.powsybl.loadflow.LoadFlowResult;
+import com.powsybl.loadflow.LoadFlowResultImpl;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Collections;
+import java.util.Locale;
+
+/**
+ *
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+public class RunLoadFlowToolTest extends AbstractConverterTest {
+
+    @Test
+    public void printLoadFlowResultTest() throws IOException {
+        LoadFlowResult result = new LoadFlowResultImpl(true, Collections.emptyMap(), "",
+                Collections.singletonList(new LoadFlowResultImpl.ComponentResultImpl(0, LoadFlowResult.ComponentResult.Status.CONVERGED, 8, "mySlack", 0.01)));
+        try (StringWriter writer = new StringWriter()) {
+            RunLoadFlowTool.printLoadFlowResult(result, writer, new AsciiTableFormatterFactory(), new TableFormatterConfig(Locale.US, "inv"));
+            writer.flush();
+            compareTxt(getClass().getResourceAsStream("/LoadFlowResultResult.txt"), writer.toString());
+        }
+    }
+}
