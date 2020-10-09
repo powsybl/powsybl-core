277f8127442bc9b281a8d63900296065a24a4c7e
Ghiles Abdellah <abdellah.ghiles@gmail.com>
2020-10-09 17:17:57 +0200

    consider network extension during AMPL export (#1514)
    Signed-off-by: Ghiles ABDELLAH <ghiles.abdellah@rte-france.com>
    
    Co-authored-by: Ghiles ABDELLAH <ghiles.abdellah@rte-france.com>
    Co-authored-by: MioRtia <miora.ralambotiana@rte-france.com>
    (cherry picked from commit b8b17c7984d03b6a5f05e82b4839062b0652547e)

---
+++ ampl-converter/src/test/java/com/powsybl/ampl/converter/FooNetworkExtensionWriter.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.ampl.converter;
+
+import com.google.auto.service.AutoService;
+import com.powsybl.commons.datasource.DataSource;
+import com.powsybl.commons.util.StringToIntMapper;
+import com.powsybl.iidm.network.Network;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+/**
+ * @author Ghiles Abdellah <ghiles.abdellah at rte-france.com>
+ */
+@AutoService(AmplExtensionWriter.class)
+public class FooNetworkExtensionWriter implements AmplExtensionWriter {
+
+    @Override
+    public String getName() {
+        return "FooNetwork";
+    }
+
+    @Override
+    public void write(List<AmplExtension> extensions, Network network, int variantIndex,
+                      StringToIntMapper<AmplSubset> mapper, DataSource dataSource, boolean append,
+                      AmplExportConfig config) throws IOException {
+        try (Writer writer = new OutputStreamWriter(dataSource.newOutputStream("foo-network-extension", "txt", false), StandardCharsets.UTF_8)) {
+            for (AmplExtension<FooNetworkExtension, Network> ext : extensions) {
+                writer.write(ext.getExtendedNum() + " " + ext.getExtension().getName() + "\n");
+            }
+        }
+
+    }
+
+}
