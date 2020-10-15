50fe60f35855e85ddbf18073c0cd5b077c62f4b8
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2020-10-15 14:38:48 +0200

    Add test (temporary until TCK is implemented for merging view)
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- iidm/iidm-mergingview/src/test/java/com/powsybl/iidm/mergingview/BusAdapterTest.java
+++ iidm/iidm-mergingview/src/test/java/com/powsybl/iidm/mergingview/BusAdapterTest.java
@@ -412,4 +412,134 @@ public class BusAdapterTest {
         busBreakerView.getSwitches().forEach(b -> assertTrue(b instanceof AbstractAdapter));
         assertEquals(1, busBreakerView.getSwitchCount());
     }
+
+    @Test
+    public void mainConnectedComponentWithSwitchTest() {
+        // TODO: delete this test when TCK tests merging view
+        Network network = Network.create("test_mcc", "test");
+
+        Substation s1 = network.newSubstation()
+                .setId("A")
+                .setCountry(Country.FR)
+                .add();
+        VoltageLevel vl1 = s1.newVoltageLevel()
+                .setId("B")
+                .setNominalV(225.0)
+                .setLowVoltageLimit(0.0)
+                .setTopologyKind(TopologyKind.NODE_BREAKER)
+                .add();
+        vl1.getNodeBreakerView().newBusbarSection()
+                .setId("C")
+                .setNode(0)
+                .add();
+        vl1.getNodeBreakerView().newSwitch()
+                .setId("D")
+                .setKind(SwitchKind.DISCONNECTOR)
+                .setRetained(false)
+                .setOpen(false)
+                .setNode1(0)
+                .setNode2(1)
+                .add();
+        vl1.getNodeBreakerView().newSwitch()
+                .setId("E")
+                .setKind(SwitchKind.BREAKER)
+                .setRetained(false)
+                .setOpen(false)
+                .setNode1(1)
+                .setNode2(2)
+                .add();
+
+        Substation s2 = network.newSubstation()
+                .setId("F")
+                .setCountry(Country.FR)
+                .add();
+        VoltageLevel vl2 = s2.newVoltageLevel()
+                .setId("G")
+                .setNominalV(225.0)
+                .setLowVoltageLimit(0.0)
+                .setTopologyKind(TopologyKind.NODE_BREAKER)
+                .add();
+        vl2.getNodeBreakerView().newBusbarSection()
+                .setId("H")
+                .setNode(0)
+                .add();
+        vl2.getNodeBreakerView().newBusbarSection()
+                .setId("I")
+                .setNode(1)
+                .add();
+        vl2.getNodeBreakerView().newSwitch()
+                .setId("J")
+                .setKind(SwitchKind.DISCONNECTOR)
+                .setRetained(true)
+                .setOpen(false)
+                .setNode1(0)
+                .setNode2(2)
+                .add();
+        vl2.getNodeBreakerView().newSwitch()
+                .setId("K")
+                .setKind(SwitchKind.DISCONNECTOR)
+                .setRetained(true)
+                .setOpen(false)
+                .setNode1(1)
+                .setNode2(3)
+                .add();
+        vl2.getNodeBreakerView().newSwitch()
+                .setId("L")
+                .setKind(SwitchKind.BREAKER)
+                .setRetained(true)
+                .setOpen(false)
+                .setNode1(2)
+                .setNode2(3)
+                .add();
+        vl2.getNodeBreakerView().newSwitch()
+                .setId("M")
+                .setKind(SwitchKind.BREAKER)
+                .setRetained(false)
+                .setOpen(false)
+                .setNode1(0)
+                .setNode2(4)
+                .add();
+
+        network.newLine()
+                .setId("N")
+                .setR(0.001)
+                .setX(0.1)
+                .setG1(0.0)
+                .setB1(0.0)
+                .setG2(0.0)
+                .setB2(0.0)
+                .setVoltageLevel1("B")
+                .setNode1(2)
+                .setVoltageLevel2("G")
+                .setNode2(4)
+                .add();
+
+        network.getBusView().getBuses().forEach(b -> {
+            if (b.getVoltageLevel() == vl1) {
+                b.setV(230.0).setAngle(0.5);
+            } else {
+                b.setV(220.0).setAngle(0.7);
+            }
+        });
+
+        MergingView mergingView = MergingView.create("merged", "test");
+        mergingView.merge(network);
+
+        assertEquals(2, mergingView.getBusView().getBusStream().count());
+        for (Bus b : mergingView.getBusView().getBuses()) {
+            assertTrue(b.isInMainConnectedComponent());
+        }
+
+        assertEquals(5, mergingView.getBusBreakerView().getBusStream().count());
+        for (Bus b : mergingView.getBusBreakerView().getBuses()) {
+            assertTrue(b.isInMainConnectedComponent());
+            if (b.getVoltageLevel().getId().equals(vl1.getId())) {
+                assertEquals(230.0, b.getV(), 0.0);
+                assertEquals(0.5, b.getAngle(), 0.0);
+            } else {
+                assertEquals(220.0, b.getV(), 0.0);
+                assertEquals(0.7, b.getAngle(), 0.0);
+            }
+        }
+    }
 }
