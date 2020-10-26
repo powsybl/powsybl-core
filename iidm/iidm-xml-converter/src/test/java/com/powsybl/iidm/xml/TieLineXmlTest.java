cf0088e4223669e460d3eba9fd5224a36734e48e
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2020-10-26 14:07:32 +0100

    Add a test
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/TieLineXmlTest.java
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/TieLineXmlTest.java
@@ -7,9 +7,14 @@
 
 package com.powsybl.iidm.xml;
 
+import com.powsybl.iidm.network.Branch;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.TieLine;
 import org.junit.Test;
 
 import java.io.IOException;
+
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * @author Mathieu Bague <mathieu.bague@rte-france.com>
@@ -19,5 +24,19 @@ public class TieLineXmlTest extends AbstractXmlConverterTest {
     @Test
     public void test() throws IOException {
         roundTripAllVersionedXmlTest("tieline.xml");
+    }
+
+    @Test
+    public void ignoreIncorrectBoundaryValues() {
+        Network network = NetworkXml.read(getVersionedNetworkAsStream("tielineNullBoundaryValues.xml", IidmXmlConstants.CURRENT_IIDM_XML_VERSION));
+        TieLine tl = (TieLine) network.getLine("NHV1_NHV2_1");
+        assertNotEquals(0.0, tl.getBoundaryPoint(Branch.Side.ONE).getP(), 0.0);
+        assertNotEquals(0.0, tl.getBoundaryPoint(Branch.Side.ONE).getQ(), 0.0);
+        assertNotEquals(0.0, tl.getBoundaryPoint(Branch.Side.ONE).getV(), 0.0);
+        assertNotEquals(0.0, tl.getBoundaryPoint(Branch.Side.ONE).getAngle(), 0.0);
+        assertNotEquals(0.0, tl.getBoundaryPoint(Branch.Side.TWO).getP(), 0.0);
+        assertNotEquals(0.0, tl.getBoundaryPoint(Branch.Side.TWO).getQ(), 0.0);
+        assertNotEquals(0.0, tl.getBoundaryPoint(Branch.Side.TWO).getV(), 0.0);
+        assertNotEquals(0.0, tl.getBoundaryPoint(Branch.Side.TWO).getAngle(), 0.0);
     }
 }
