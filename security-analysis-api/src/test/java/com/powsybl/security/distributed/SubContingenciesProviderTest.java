e8b9778713b2b490c11d8b302dfabbc7801eda07
Thomas ADAM <tadam@silicom.fr>
2020-11-27 17:37:22 +0100

    Revert bad commit
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>

--- security-analysis-api/src/test/java/com/powsybl/security/distributed/SubContingenciesProviderTest.java
+++ security-analysis-api/src/test/java/com/powsybl/security/distributed/SubContingenciesProviderTest.java
@@ -4,10 +4,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.powsybl.contingency;
+package com.powsybl.security.distributed;
 
 import com.google.common.collect.ImmutableList;
 import com.powsybl.computation.Partition;
+import com.powsybl.contingency.ContingenciesProvider;
+import com.powsybl.contingency.Contingency;
 import com.powsybl.iidm.network.Network;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -17,7 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * @author Sylvain Leclerc <sylvain.leclerc at rte-france.com>
