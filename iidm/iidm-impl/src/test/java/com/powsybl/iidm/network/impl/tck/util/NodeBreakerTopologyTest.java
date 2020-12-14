3e48882d584629669f0f59724e3ef23d7804fa5e
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-12-14 21:01:02 +0100

    Remove IIDM util module
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

---
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/tck/util/NodeBreakerTopologyTest.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2018, All partners of the iTesla project (http://www.itesla-project.eu/consortium)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.network.impl.tck.util;
+
+import com.powsybl.iidm.network.tck.util.AbstractNodeBreakerTopologyTest;
+
+/**
+ * @author Sylvain Leclerc <sylvain.leclerc at rte-france.com>
+ */
+public class NodeBreakerTopologyTest extends AbstractNodeBreakerTopologyTest {
+}
