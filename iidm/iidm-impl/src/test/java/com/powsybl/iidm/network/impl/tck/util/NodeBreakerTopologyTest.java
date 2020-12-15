64ad31d5bee09164e6d686ae834513f96a4d6b5f
Mathieu BAGUE <mathieu.bague@rte-france.com>
2020-12-15 09:21:39 +0100

    Small fixes
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

--- iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/tck/util/NodeBreakerTopologyTest.java
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/tck/util/NodeBreakerTopologyTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018, All partners of the iTesla project (http://www.itesla-project.eu/consortium)
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -9,7 +9,7 @@ package com.powsybl.iidm.network.impl.tck.util;
 import com.powsybl.iidm.network.tck.util.AbstractNodeBreakerTopologyTest;
 
 /**
- * @author Sylvain Leclerc <sylvain.leclerc at rte-france.com>
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
 public class NodeBreakerTopologyTest extends AbstractNodeBreakerTopologyTest {
 }
