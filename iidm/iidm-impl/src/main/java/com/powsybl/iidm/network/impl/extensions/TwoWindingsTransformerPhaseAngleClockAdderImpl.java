f8718a64fc3833b2aa1435c00a200bd7f2dfff14
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-12-15 11:36:27 +0100

    Fix com.powsybl.iidm.network.extensions split package
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

---
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/extensions/TwoWindingsTransformerPhaseAngleClockAdderImpl.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.network.impl.extensions;
+
+import com.powsybl.commons.extensions.AbstractExtensionAdder;
+import com.powsybl.iidm.network.TwoWindingsTransformer;
+import com.powsybl.iidm.network.extensions.TwoWindingsTransformerPhaseAngleClock;
+import com.powsybl.iidm.network.extensions.TwoWindingsTransformerPhaseAngleClockAdder;
+
+/**
+ * @author Jérémy LABOUS <jlabous at silicom.fr>
+ */
+public class TwoWindingsTransformerPhaseAngleClockAdderImpl
+    extends AbstractExtensionAdder<TwoWindingsTransformer, TwoWindingsTransformerPhaseAngleClock>
+    implements TwoWindingsTransformerPhaseAngleClockAdder {
+
+    private int phaseAngleClock = -1;
+
+    protected TwoWindingsTransformerPhaseAngleClockAdderImpl(TwoWindingsTransformer extendable) {
+        super(extendable);
+    }
+
+    @Override
+    protected TwoWindingsTransformerPhaseAngleClock createExtension(TwoWindingsTransformer extendable) {
+        return new TwoWindingsTransformerPhaseAngleClockImpl(extendable, phaseAngleClock);
+    }
+
+    @Override
+    public TwoWindingsTransformerPhaseAngleClockAdder withPhaseAngleClock(int phaseAngleClock) {
+        this.phaseAngleClock = phaseAngleClock;
+        return this;
+    }
+}
