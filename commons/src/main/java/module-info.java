31668b04ab32033e09974ff85e85649f56be3b9b
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-12-17 16:16:56 +0100

    Improve
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- commons/src/main/java/module-info.java
+++ commons/src/main/java/module-info.java
@@ -1,12 +1,19 @@
 module powsybl.commons {
+    requires auto.service; // No Automatic-Module-Name
     requires com.fasterxml.jackson.databind; // Real module
     requires com.google.common;
     requires java.xml.bind; // Real module
     requires net.java.truevfs.comp.zip;
     requires org.apache.commons.compress;
+    requires org.apache.commons.lang3;
     requires org.joda.time;
     requires org.slf4j;
     requires org.yaml.snakeyaml;
+    requires sirocco.text.table.formatter; // No Automatic-Module-Name
     requires stax.utils; // No Automatic-Module-Name
     requires trove4j; // No Automatic-Module-Name
+    requires univocity.parsers; // No Automatic-Module-Name
+
+    exports com.powsybl.commons;
+    exports com.powsybl.commons.json;
 }
--- commons/src/test/java/module-info.java
+++ commons/src/test/java/module-info.java
@@ -1,2 +1,5 @@
 module powsybl.commons.test {
+    requires auto.service; // No Automatic-Module-Name
+    requires com.fasterxml.jackson.databind; // Real module
+    requires powsybl.commons;
 }
