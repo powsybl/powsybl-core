4b1618a11966bb00ac14d745155241a81ea93f26
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-12-17 08:56:33 +0100

    Add snakeyaml
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- commons/src/main/java/module-info.java
+++ commons/src/main/java/module-info.java
@@ -1,11 +1,12 @@
 module powsybl.commons {
-    requires com.google.common;
-    requires org.slf4j;
-    requires org.joda.time;
     requires com.fasterxml.jackson.databind; // Real module
-    requires trove4j; // No Automatic-Module-Name
-    requires org.apache.commons.compress;
+    requires com.google.common;
     requires java.xml.bind; // Real module
-    requires stax.utils; // No Automatic-Module-Name
     requires net.java.truevfs.comp.zip;
+    requires org.apache.commons.compress;
+    requires org.joda.time;
+    requires org.slf4j;
+    requires org.yaml.snakeyaml;
+    requires stax.utils; // No Automatic-Module-Name
+    requires trove4j; // No Automatic-Module-Name
 }
