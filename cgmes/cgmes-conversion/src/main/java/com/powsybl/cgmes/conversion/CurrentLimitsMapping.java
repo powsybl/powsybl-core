0e648094b2056eacebf8d790b29e6d431667600a
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2021-01-11 16:43:50 +0100

    CGMES import of all kinds of limits
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/CurrentLimitsMapping.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/CurrentLimitsMapping.java
@@ -1,50 +1,31 @@
 /**
- * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package com.powsybl.cgmes.conversion;
 
-import com.google.common.collect.Iterables;
-import com.powsybl.iidm.network.CurrentLimits;
 import com.powsybl.iidm.network.CurrentLimitsAdder;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 import java.util.function.Supplier;
 
 /**
+ * @deprecated Use {@link LoadingLimitsMapping} instead.
+ *
  * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
  */
-public class CurrentLimitsMapping {
-
-    private final Map<String, CurrentLimitsAdder> adders = new HashMap<>();
-    private final Context context;
-
+@Deprecated
+public class CurrentLimitsMapping extends LoadingLimitsMapping {
     CurrentLimitsMapping(Context context) {
-        this.context = Objects.requireNonNull(context);
+        super(context);
     }
 
+    /**
+     * @deprecated Use {@link LoadingLimitsMapping#getLoadingLimitsAdder(String, Supplier)} instead.
+     */
+    @Deprecated
     public CurrentLimitsAdder getCurrentLimitsAdder(String id, Supplier<CurrentLimitsAdder> supplier) {
-        return adders.computeIfAbsent(id, s -> supplier.get());
-    }
-
-    void addAll() {
-        for (Map.Entry<String, CurrentLimitsAdder> entry : adders.entrySet()) {
-            if (!Double.isNaN(entry.getValue().getPermanentLimit()) || entry.getValue().hasTemporaryLimits()) {
-                CurrentLimits limits = entry.getValue().add();
-                if (Double.isNaN(limits.getPermanentLimit())) {
-                    double fixedPermanentLimit = Iterables.get(limits.getTemporaryLimits(), 0).getValue();
-                    context.fixed("Operational Limit Set of " + entry.getKey(),
-                            "An operational limit set without permanent limit is considered with permanent limit" +
-                                    "equal to lowest TATL value",
-                            Double.NaN, fixedPermanentLimit);
-                    limits.setPermanentLimit(fixedPermanentLimit);
-                }
-            }
-        }
-        adders.clear();
+        return (CurrentLimitsAdder) adders.computeIfAbsent(id, s -> supplier.get());
     }
 }
---
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/LoadingLimitsMapping.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.cgmes.conversion;
+
+import com.google.common.collect.Iterables;
+import com.powsybl.iidm.network.LoadingLimits;
+import com.powsybl.iidm.network.LoadingLimitsAdder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
+ */
+public class LoadingLimitsMapping {
+
+    protected final Map<String, LoadingLimitsAdder> adders = new HashMap<>();
+    private final Context context;
+
+    LoadingLimitsMapping(Context context) {
+        this.context = Objects.requireNonNull(context);
+    }
+
+    public LoadingLimitsAdder getLoadingLimitsAdder(String id, Supplier<LoadingLimitsAdder> supplier) {
+        return adders.computeIfAbsent(id, s -> supplier.get());
+    }
+
+    void addAll() {
+        for (Map.Entry<String, LoadingLimitsAdder> entry : adders.entrySet()) {
+            if (!Double.isNaN(entry.getValue().getPermanentLimit()) || entry.getValue().hasTemporaryLimits()) {
+                LoadingLimits limits = (LoadingLimits) entry.getValue().add();
+                if (Double.isNaN(limits.getPermanentLimit())) {
+                    double fixedPermanentLimit = Iterables.get(limits.getTemporaryLimits(), 0).getValue();
+                    context.fixed("Operational Limit Set of " + entry.getKey(),
+                            "An operational limit set without permanent limit is considered with permanent limit" +
+                                    "equal to lowest TATL value",
+                            Double.NaN, fixedPermanentLimit);
+                    limits.setPermanentLimit(fixedPermanentLimit);
+                }
+            }
+        }
+        adders.clear();
+    }
+}
