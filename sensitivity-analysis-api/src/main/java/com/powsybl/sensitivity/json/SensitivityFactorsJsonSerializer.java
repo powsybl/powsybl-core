1d2b81acc7cea21276180cd3a09623adf2399db8
EDWARDS Philippe <philippe.edwards@rte-france.com>
2021-01-15 14:34:45 +0100

    smarter lighter Json files

--- sensitivity-analysis-api/src/main/java/com/powsybl/sensitivity/json/SensitivityFactorsJsonSerializer.java
+++ sensitivity-analysis-api/src/main/java/com/powsybl/sensitivity/json/SensitivityFactorsJsonSerializer.java
@@ -6,18 +6,31 @@
  */
 package com.powsybl.sensitivity.json;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.powsybl.commons.PowsyblException;
 import com.powsybl.commons.json.JsonUtil;
 import com.powsybl.sensitivity.SensitivityFactor;
+import com.powsybl.sensitivity.SensitivityFunction;
+import com.powsybl.sensitivity.SensitivityVariable;
+import com.powsybl.sensitivity.factors.BranchFlowPerInjectionIncrease;
+import com.powsybl.sensitivity.factors.BranchFlowPerLinearGlsk;
+import com.powsybl.sensitivity.factors.BranchFlowPerPSTAngle;
+import com.powsybl.sensitivity.factors.BranchIntensityPerPSTAngle;
+import com.powsybl.sensitivity.factors.functions.BranchFlow;
+import com.powsybl.sensitivity.factors.functions.BranchIntensity;
+import com.powsybl.sensitivity.factors.variables.InjectionIncrease;
+import com.powsybl.sensitivity.factors.variables.LinearGlsk;
+import com.powsybl.sensitivity.factors.variables.PhaseTapChangerAngle;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * JSON Serialization utility class for sensitivity factors input
@@ -34,9 +47,53 @@ public final class SensitivityFactorsJsonSerializer {
      */
     public static Map<String, List<SensitivityFactor>> read(Reader reader) throws IOException {
         Objects.requireNonNull(reader);
+        ObjectMapper mapper = new ObjectMapper();
 
-        ObjectReader objectReader = JsonUtil.createObjectMapper().readerFor(new TypeReference<Map<String, List<SensitivityFactor>>>() { });
-        return objectReader.readValue(reader);
+        JsonParser jsonParser = mapper.getFactory().createParser(reader);
+        JsonToken jsonToken;
+        jsonToken = jsonParser.nextToken();
+        jsonToken = jsonParser.nextToken();
+
+        Map<String, SensitivityFunction> sensitivityFunctionMap = mapper
+            .readValue(jsonParser, new TypeReference<LinkedHashMap<String, SensitivityFunction>>() { });
+
+        jsonToken = jsonParser.nextToken();
+        Map<String, SensitivityVariable> sensitivityVariableMap = mapper
+            .readValue(jsonParser, new TypeReference<LinkedHashMap<String, SensitivityVariable>>() { });
+
+        jsonToken = jsonParser.nextToken();
+        Map<String, List<List<String>>> sensitivityFactorsStringMap = mapper
+            .readValue(jsonParser, new TypeReference<LinkedHashMap<String, List<List<String>>>>() { });
+        jsonParser.close();
+
+        Map<String, List<SensitivityFactor>> sensitivityFactorsMap = new HashMap<>();
+        for (String contingencyId : sensitivityFactorsStringMap.keySet()) {
+            List<SensitivityFactor> sensitivityFactors = new ArrayList<>();
+            for (List<String> sensitivityFactorString : sensitivityFactorsStringMap.get(contingencyId)) {
+                SensitivityFunction function = sensitivityFunctionMap.get(sensitivityFactorString.get(0));
+                SensitivityVariable variable = sensitivityVariableMap.get(sensitivityFactorString.get(1));
+                sensitivityFactors.add(makeSensitivityFactor(function, variable));
+            }
+            sensitivityFactorsMap.put(contingencyId, sensitivityFactors);
+        }
+        return sensitivityFactorsMap;
+    }
+
+    private static SensitivityFactor makeSensitivityFactor(SensitivityFunction function, SensitivityVariable variable) {
+        if (function instanceof BranchFlow) {
+            if (variable instanceof LinearGlsk) {
+                return new BranchFlowPerLinearGlsk((BranchFlow) function, (LinearGlsk) variable);
+            } else if (variable instanceof PhaseTapChangerAngle) {
+                return new BranchFlowPerPSTAngle((BranchFlow) function, (PhaseTapChangerAngle) variable);
+            } else if (variable instanceof InjectionIncrease) {
+                return new BranchFlowPerInjectionIncrease((BranchFlow) function, (InjectionIncrease) variable);
+            }
+        } else if (function instanceof BranchIntensity) {
+            if (variable instanceof PhaseTapChangerAngle) {
+                return new BranchIntensityPerPSTAngle((BranchIntensity) function, (PhaseTapChangerAngle) variable);
+            }
+        }
+        throw new PowsyblException("Unable to parse JsonSensitivityFactorProvider: unrecognizable sensitivity factor");
     }
 
     /**
@@ -48,9 +105,33 @@ public final class SensitivityFactorsJsonSerializer {
     public static void write(Map<String, List<SensitivityFactor>> sensitivityFactorsMap, Writer writer) throws IOException {
         Objects.requireNonNull(sensitivityFactorsMap);
         Objects.requireNonNull(writer);
+        ObjectMapper mapper = JsonUtil.createObjectMapper();
+        ObjectWriter objectWriter = mapper.writerWithDefaultPrettyPrinter();
+        JsonGenerator jsonGenerator = mapper.getFactory().createGenerator(writer);
 
-        ObjectWriter objectWriter = JsonUtil.createObjectMapper().writerWithDefaultPrettyPrinter();
-        objectWriter.forType(new TypeReference<Map<String, List<SensitivityFactor>>>() { }).writeValue(writer, sensitivityFactorsMap);
+        Map<String, SensitivityVariable> sensitivityVariableMap = new LinkedHashMap<>();
+        Map<String, SensitivityFunction> sensitivityFunctionMap = new LinkedHashMap<>();
+        Map<String, List<List<String>>> sensitivityFactorsStringMap = new LinkedHashMap<>();
+        for (String contingencyId : sensitivityFactorsMap.keySet()) {
+            List<List<String>> sensitivityFactorsString = new ArrayList<>();
+            for (SensitivityFactor sensitivityFactor : sensitivityFactorsMap.get(contingencyId)) {
+                List<String> functionVariablePair = new ArrayList<>();
+                functionVariablePair.add(sensitivityFactor.getFunction().getId());
+                functionVariablePair.add(sensitivityFactor.getVariable().getId());
+                sensitivityFactorsString.add(functionVariablePair);
+
+                sensitivityVariableMap.put(sensitivityFactor.getVariable().getId(), sensitivityFactor.getVariable());
+                sensitivityFunctionMap.put(sensitivityFactor.getFunction().getId(), sensitivityFactor.getFunction());
+            }
+            sensitivityFactorsStringMap.put(contingencyId, sensitivityFactorsString);
+        }
+
+        jsonGenerator.writeStartArray();
+        objectWriter.forType(new TypeReference<Map<String, SensitivityFunction>>() { }).writeValue(jsonGenerator, sensitivityFunctionMap);
+        objectWriter.forType(new TypeReference<Map<String, SensitivityVariable>>() { }).writeValue(jsonGenerator, sensitivityVariableMap);
+        objectWriter.forType(new TypeReference<Map<String, List<List<String>>>>() { }).writeValue(jsonGenerator, sensitivityFactorsStringMap);
+        jsonGenerator.writeEndArray();
+        jsonGenerator.close();
     }
 
     private SensitivityFactorsJsonSerializer() {
