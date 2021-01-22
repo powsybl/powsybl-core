e9922c1fddc62fa006bef482b18e08317524d4c0
Paul Bui-Quang <paul.buiquang@rte-france.com>
2021-01-22 16:34:39 +0100

    Add new iidm hvdc extensions (#1587)
    * Add new iidm hvdc extensions
    * Add tests
    
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>
    Co-authored-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

---
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/extensions/HvdcOperatorActiveRangeXmlTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+package com.powsybl.iidm.xml.extensions;
+
+import com.powsybl.commons.AbstractConverterTest;
+import com.powsybl.iidm.network.HvdcLine;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.extensions.HvdcOperatorActivePowerRange;
+import com.powsybl.iidm.network.extensions.HvdcOperatorActivePowerRangeAdder;
+import com.powsybl.iidm.network.test.HvdcTestNetwork;
+import com.powsybl.iidm.xml.NetworkXml;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static com.powsybl.iidm.xml.AbstractXmlConverterTest.getVersionedNetworkPath;
+import static com.powsybl.iidm.xml.IidmXmlConstants.CURRENT_IIDM_XML_VERSION;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Paul Bui-Quang <paul.buiquang at rte-france.com>
+ */
+public class HvdcOperatorActiveRangeXmlTest extends AbstractConverterTest {
+
+    @Test
+    public void test() throws IOException {
+        Network network = HvdcTestNetwork.createLcc();
+
+        HvdcLine hvdcLine = network.getHvdcLine("L");
+        hvdcLine.newExtension(HvdcOperatorActivePowerRangeAdder.class).withOprFromCS1toCS2(1.0f).withOprFromCS2toCS1(2.0f).add();
+        HvdcOperatorActivePowerRange extension = hvdcLine.getExtension(HvdcOperatorActivePowerRange.class);
+
+        Network network2 = roundTripXmlTest(network,
+                NetworkXml::writeAndValidate,
+                NetworkXml::read,
+                getVersionedNetworkPath("hvdcOperatorActiveRangeRef.xml", CURRENT_IIDM_XML_VERSION));
+
+        HvdcLine hvdcLine2 = network2.getHvdcLine("L");
+        assertEquals(hvdcLine2.getExtension(HvdcOperatorActivePowerRange.class), extension);
+    }
+}
