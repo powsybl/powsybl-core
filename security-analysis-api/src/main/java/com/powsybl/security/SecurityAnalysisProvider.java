18b2a614e0c061d0177d98c6add4e941296d3791
Thomas ADAM <52197093+tadam50@users.noreply.github.com>
2021-02-01 14:51:57 +0100

    Use ServiceLoader to create SecurityAnalysis (#1585)
    * Add SecurityAnalysisProvider
    * Add FIXME to rename later and fix comments issues
    * Remove runWithLog method in SecurityAnalysisProvider
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>

---
+++ security-analysis-api/src/main/java/com/powsybl/security/SecurityAnalysisProvider.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.security;
+
+import com.powsybl.commons.Versionable;
+import com.powsybl.commons.config.PlatformConfigNamedProvider;
+import com.powsybl.computation.ComputationManager;
+import com.powsybl.contingency.ContingenciesProvider;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.security.interceptors.SecurityAnalysisInterceptor;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Security analysis provider
+ *
+ * @author Thomas Adam <tadam at silicom.fr>
+ */
+public interface SecurityAnalysisProvider extends Versionable, PlatformConfigNamedProvider {
+
+    /**
+     * Run an asynchronous single security analysis job.
+     *
+     * @param network IIDM network on which the security analysis will be performed
+     * @param workingVariantId network variant ID on which the analysis will be performed
+     * @param detector
+     * @param filter
+     * @param computationManager
+     * @param parameters specific security analysis parameters
+     * @param contingenciesProvider provides list of contingencies
+     * @param interceptors
+     * @return a {@link CompletableFuture} on {@link SecurityAnalysisResult} that gathers security factor values
+     */
+    CompletableFuture<SecurityAnalysisResult> run(Network network,
+                                                  String workingVariantId,
+                                                  LimitViolationDetector detector,
+                                                  LimitViolationFilter filter,
+                                                  ComputationManager computationManager,
+                                                  SecurityAnalysisParameters parameters,
+                                                  ContingenciesProvider contingenciesProvider,
+                                                  List<SecurityAnalysisInterceptor> interceptors);
+}
