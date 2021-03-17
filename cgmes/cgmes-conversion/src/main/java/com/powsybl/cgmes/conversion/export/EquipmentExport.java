ed236e103958f13fefe12f7f9155e4f8535eee46
Marcos de Miguel <demiguelm@aia.es>
2021-03-17 17:36:20 +0100

    write: - Model Description - Substations - Generators
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/export/EquipmentExport.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/export/EquipmentExport.java
@@ -6,18 +6,19 @@
  */
 package com.powsybl.cgmes.conversion.export;
 
+import com.powsybl.cgmes.conversion.Conversion;
 import com.powsybl.cgmes.model.CgmesNames;
 import com.powsybl.commons.exceptions.UncheckedXmlStreamException;
-import com.powsybl.iidm.network.Load;
-import com.powsybl.iidm.network.Network;
-import com.powsybl.iidm.network.VoltageLevel;
+import com.powsybl.iidm.network.*;
 import com.powsybl.iidm.network.extensions.LoadDetail;
 
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import static com.powsybl.cgmes.model.CgmesNamespace.RDF_NAMESPACE;
 
@@ -27,6 +28,9 @@ import static com.powsybl.cgmes.model.CgmesNamespace.RDF_NAMESPACE;
 public final class EquipmentExport {
 
     private static final String EQ_BASEVOLTAGE_NOMINALV = "BaseVoltage.nominalVoltage";
+    private static final String EQ_GENERATINUNIT_MINP = "GeneratingUnit.minOperatingP";
+    private static final String EQ_GENERATINUNIT_MAXP = "GeneratingUnit.maxOperatingP";
+    private static final String EQ_GENERATINUNIT_INITIALP = "GeneratingUnit.initialP";
 
     public static void write(Network network, XMLStreamWriter writer) {
         write(network, writer, new CgmesExportContext(network));
@@ -34,15 +38,18 @@ public final class EquipmentExport {
 
     public static void write(Network network, XMLStreamWriter writer, CgmesExportContext context) {
         try {
-            Map<Double, String> baseVoltageIds = new HashMap<>();
             CgmesExportUtil.writeRdfRoot(context.getCimVersion(), writer);
             String cimNamespace = context.getCimNamespace();
 
-            // TODO write EQ Model Description
+            // TODO fill EQ Model Description
+            if (context.getCimVersion() == 16) {
+                CgmesExportUtil.writeModelDescription(writer, context.getEqModelDescription(), context);
+            }
 
-            writeBaseVoltages(network, baseVoltageIds, cimNamespace, writer);
-            writeVoltageLevels(network, baseVoltageIds, cimNamespace, writer);
-            writeEnergyConsumers(network, cimNamespace, writer);
+            writeSubstations(network, cimNamespace, writer);
+            writeVoltageLevels(network, cimNamespace, writer);
+            writeLoads(network, cimNamespace, writer);
+            writeGenerators(network, cimNamespace, writer);
 
             writer.writeEndDocument();
         } catch (XMLStreamException e) {
@@ -50,7 +57,42 @@ public final class EquipmentExport {
         }
     }
 
-    private static void writeBaseVoltages(Network network, Map<Double, String> baseVoltageIds, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
+    private static void writeSubstations(Network network, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
+        Map<Country, String> geographicalRegionIds = new HashMap<>();
+        Set<String> geographicalTags = new HashSet<>();
+        for (Substation substation : network.getSubstations()) {
+            Country country = substation.getCountry().get();
+            if (!geographicalRegionIds.containsKey(country)) {
+                String geographicalRegionId = CgmesExportUtil.getUniqueId();
+                geographicalRegionIds.put(country, geographicalRegionId);
+                writeEqGeographicalRegion(geographicalRegionId, country.toString(), cimNamespace, writer);
+            }
+            writeEqSubstation(substation.getId(), substation.getNameOrId(), geographicalRegionIds.get(country), cimNamespace, writer);
+        }
+    }
+
+    private static void writeEqGeographicalRegion(String id, String regionName, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
+        writer.writeStartElement(cimNamespace, "GeographicalRegion");
+        writer.writeAttribute(RDF_NAMESPACE, CgmesNames.ID, id);
+        writer.writeStartElement(cimNamespace, CgmesNames.NAME);
+        writer.writeCharacters(regionName);
+        writer.writeEndElement();
+        writer.writeEndElement();
+    }
+
+    private static void writeEqSubstation(String id, String substationName, String geographicalRegionId, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
+        writer.writeStartElement(cimNamespace, "Substation");
+        writer.writeAttribute(RDF_NAMESPACE, CgmesNames.ID, id);
+        writer.writeStartElement(cimNamespace, CgmesNames.NAME);
+        writer.writeCharacters(substationName);
+        writer.writeEndElement();
+        writer.writeEmptyElement(cimNamespace, "Substation.Region");
+        writer.writeAttribute(RDF_NAMESPACE, CgmesNames.RESOURCE, "#" + geographicalRegionId);
+        writer.writeEndElement();
+    }
+
+    private static void writeVoltageLevels(Network network, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
+        Map<Double, String> baseVoltageIds = new HashMap<>();
         for (VoltageLevel voltageLevel : network.getVoltageLevels()) {
             double nominalV = voltageLevel.getNominalV();
             if (!baseVoltageIds.containsKey(nominalV)) {
@@ -58,6 +100,7 @@ public final class EquipmentExport {
                 baseVoltageIds.put(nominalV, baseVoltageId);
                 writeEqBaseVoltages(baseVoltageId, nominalV, cimNamespace, writer);
             }
+            writeEqVoltageLevel(voltageLevel.getId(), voltageLevel.getNameOrId(), voltageLevel.getSubstation().getId(), baseVoltageIds.get(voltageLevel.getNominalV()), cimNamespace, writer);
         }
     }
 
@@ -73,12 +116,6 @@ public final class EquipmentExport {
         writer.writeEndElement();
     }
 
-    private static void writeVoltageLevels(Network network, Map<Double, String> baseVoltageIds, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
-        for (VoltageLevel voltageLevel : network.getVoltageLevels()) {
-            writeEqVoltageLevel(voltageLevel.getId(), voltageLevel.getNameOrId(), voltageLevel.getSubstation().getId(), baseVoltageIds.get(voltageLevel.getNominalV()), cimNamespace, writer);
-        }
-    }
-
     private static void writeEqVoltageLevel(String id, String voltageLevelName, String substationId, String baseVoltageId, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
         writer.writeStartElement(cimNamespace, "VoltageLevel");
         writer.writeAttribute(RDF_NAMESPACE, CgmesNames.ID, id);
@@ -92,7 +129,7 @@ public final class EquipmentExport {
         writer.writeEndElement();
     }
 
-    private static void writeEnergyConsumers(Network network, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
+    private static void writeLoads(Network network, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
         for (Load load : network.getLoads()) {
             writeEqEnergyConsumer(load.getId(), load.getNameOrId(), load.getExtension(LoadDetail.class), load.getTerminal().getVoltageLevel().getId(), cimNamespace, writer);
         }
@@ -123,6 +160,64 @@ public final class EquipmentExport {
             }
         }
         return "EnergyConsumer";
+    }
+
+    private static void writeGenerators(Network network, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
+        for (Generator generator : network.getGenerators()) {
+            String generatingUnit = generator.getProperty(Conversion.CGMES_PREFIX_ALIAS_PROPERTIES + "GeneratingUnit");
+            if (generatingUnit == null) {
+                generatingUnit = CgmesExportUtil.getUniqueId();
+            }
+            writeEqSynchronousMachine(generator.getId(), generator.getNameOrId(), generatingUnit, cimNamespace, writer);
+            String generatingUnitName = "GEN_" + generator.getNameOrId();
+            writeEqGeneratingUnit(generatingUnit, generatingUnitName, generator.getEnergySource(), generator.getMinP(), generator.getMaxP(), generator.getTargetP(), cimNamespace, writer);
+        }
+    }
+
+    private static void writeEqSynchronousMachine(String id, String generatorName, String generatingUnit, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
+        writer.writeStartElement(cimNamespace, "SynchronousMachine");
+        writer.writeAttribute(RDF_NAMESPACE, CgmesNames.ID, id);
+        writer.writeStartElement(cimNamespace, CgmesNames.NAME);
+        writer.writeCharacters(generatorName);
+        writer.writeEndElement();
+        writer.writeEmptyElement(cimNamespace, "RotatingMachine.GeneratingUnit");
+        writer.writeAttribute(RDF_NAMESPACE, CgmesNames.RESOURCE, "#" + generatingUnit);
+        writer.writeEndElement();
+    }
+
+    private static void writeEqGeneratingUnit(String id, String generatingUnitName, EnergySource energySource, double minP, double maxP, double initialP, String cimNamespace, XMLStreamWriter writer) throws XMLStreamException {
+        writer.writeStartElement(cimNamespace, generatingUnitClassName(energySource));
+        writer.writeAttribute(RDF_NAMESPACE, CgmesNames.ID, id);
+        writer.writeStartElement(cimNamespace, CgmesNames.NAME);
+        writer.writeCharacters(generatingUnitName);
+        writer.writeEndElement();
+        writer.writeStartElement(cimNamespace, EQ_GENERATINUNIT_MINP);
+        writer.writeCharacters(CgmesExportUtil.format(minP));
+        writer.writeEndElement();
+        writer.writeStartElement(cimNamespace, EQ_GENERATINUNIT_MAXP);
+        writer.writeCharacters(CgmesExportUtil.format(maxP));
+        writer.writeEndElement();
+        writer.writeStartElement(cimNamespace, EQ_GENERATINUNIT_INITIALP);
+        writer.writeCharacters(CgmesExportUtil.format(initialP));
+        writer.writeEndElement();
+        writer.writeEndElement();
+    }
+
+    private static String generatingUnitClassName(EnergySource energySource) {
+        if (EnergySource.HYDRO.equals(energySource)) {
+            return "HydroGeneratingUnit";
+        } else if (EnergySource.NUCLEAR.equals(energySource)) {
+            return "NuclearGeneratingUnit";
+        } else if (EnergySource.THERMAL.equals(energySource)) {
+            return "ThermalGeneratingUnit";
+        } else if (EnergySource.WIND.equals(energySource)) {
+            return "WindGeneratingUnit";
+        } else if (EnergySource.SOLAR.equals(energySource)) {
+            return "SolarGeneratingUnit";
+        } else if (EnergySource.OTHER.equals(energySource)) {
+            return "GeneratingUnit";
+        }
+        return "GeneratingUnit";
     }
 
     private EquipmentExport() {
