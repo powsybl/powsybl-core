2a41aaa482436e6ead207e649c99d650eabd82e8
Thomas ADAM <52197093+tadam50@users.noreply.github.com>
2021-03-22 14:20:16 +0100

    Refactoring security-analysis-api : SecurityAnalysisImpl (issue 1604) (#1648)
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>

---
+++ security-analysis/security-analysis-default/src/main/java/com/powsybl/security/impl/DefaultSecurityAnalysisProvider.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.security.impl;
+
+import com.google.auto.service.AutoService;
+import com.powsybl.computation.ComputationManager;
+import com.powsybl.contingency.ContingenciesProvider;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.security.*;
+import com.powsybl.security.interceptors.SecurityAnalysisInterceptor;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author Thomas Adam <tadam at silicom.fr>
+ */
+@AutoService(SecurityAnalysisProvider.class)
+public class DefaultSecurityAnalysisProvider implements SecurityAnalysisProvider {
+
+    private static final String PROVIDER_NAME = "DefaultSecurityAnalysis";
+    private static final String PROVIDER_VERSION = "1.0";
+
+    @Override
+    public CompletableFuture<SecurityAnalysisResult> run(Network network,
+                                                         String workingVariantId,
+                                                         LimitViolationDetector detector,
+                                                         LimitViolationFilter filter,
+                                                         ComputationManager computationManager,
+                                                         SecurityAnalysisParameters parameters,
+                                                         ContingenciesProvider contingenciesProvider,
+                                                         List<SecurityAnalysisInterceptor> interceptors) {
+        DefaultSecurityAnalysis securityAnalysis = new DefaultSecurityAnalysis(network, detector, filter, computationManager);
+        interceptors.forEach(securityAnalysis::addInterceptor);
+        return securityAnalysis.run(workingVariantId, parameters, contingenciesProvider);
+    }
+
+    @Override
+    public String getName() {
+        return PROVIDER_NAME;
+    }
+
+    @Override
+    public String getVersion() {
+        return PROVIDER_VERSION;
+    }
+}
---
+++ security-analysis/security-analysis-default/src/test/java/com/powsybl/security/impl/interceptors/SecurityAnalysisInterceptorMock.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.security.impl.interceptors;
+
+import com.powsybl.security.*;
+import com.powsybl.security.interceptors.DefaultSecurityAnalysisInterceptor;
+import com.powsybl.security.interceptors.SecurityAnalysisResultContext;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Mathieu Bague <mathieu.bague at rte-france.com>
+ */
+public class SecurityAnalysisInterceptorMock extends DefaultSecurityAnalysisInterceptor {
+
+    private int onPreContingencyResultCount = 0;
+
+    private int onPostContingencyResultCount = 0;
+
+    private int onSecurityAnalysisResultCount = 0;
+
+    @Override
+    public void onPreContingencyResult(LimitViolationsResult preContingencyResult, SecurityAnalysisResultContext context) {
+        super.onPreContingencyResult(preContingencyResult, context);
+
+        assertRunningContext(context);
+        assertPreContingencyResult(preContingencyResult);
+        onPreContingencyResultCount++;
+    }
+
+    @Override
+    public void onPostContingencyResult(PostContingencyResult postContingencyResult, SecurityAnalysisResultContext context) {
+        super.onPostContingencyResult(postContingencyResult, context);
+
+        assertRunningContext(context);
+        assertPostContingencyResult(postContingencyResult);
+        onPostContingencyResultCount++;
+    }
+
+    @Override
+    public void onSecurityAnalysisResult(SecurityAnalysisResult result, SecurityAnalysisResultContext context) {
+        super.onSecurityAnalysisResult(result, context);
+
+        assertRunningContext(context);
+        assertNotNull(result);
+        assertPreContingencyResult(result.getPreContingencyResult());
+        result.getPostContingencyResults().forEach(SecurityAnalysisInterceptorMock::assertPostContingencyResult);
+        onSecurityAnalysisResultCount++;
+    }
+
+    public int getOnPreContingencyResultCount() {
+        return onPreContingencyResultCount;
+    }
+
+    public int getOnPostContingencyResultCount() {
+        return onPostContingencyResultCount;
+    }
+
+    public int getOnSecurityAnalysisResultCount() {
+        return onSecurityAnalysisResultCount;
+    }
+
+    private static void assertRunningContext(SecurityAnalysisResultContext context) {
+        assertNotNull(context);
+        assertNotNull(context.getNetwork());
+        assertEquals("sim1", context.getNetwork().getId());
+        assertEquals("test", context.getNetwork().getSourceFormat());
+    }
+
+    private static void assertPreContingencyResult(LimitViolationsResult preContingencyResult) {
+        assertNotNull(preContingencyResult);
+        assertTrue(preContingencyResult.isComputationOk());
+        assertEquals(0, preContingencyResult.getLimitViolations().size());
+    }
+
+    private static void assertPostContingencyResult(PostContingencyResult postContingencyResult) {
+        assertNotNull(postContingencyResult);
+        assertTrue(postContingencyResult.getLimitViolationsResult().isComputationOk());
+        assertEquals(1, postContingencyResult.getLimitViolationsResult().getLimitViolations().size());
+        LimitViolation violation = postContingencyResult.getLimitViolationsResult().getLimitViolations().get(0);
+        assertEquals(LimitViolationType.CURRENT, violation.getLimitType());
+        assertEquals("NHV1_NHV2_1", violation.getSubjectId());
+    }
+}
