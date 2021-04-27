56913693722fe8f51b24672f9b29d37982ffb867
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2021-04-27 16:36:12 +0200

    Add unit test
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- math/src/main/java/com/powsybl/math/matrix/Matrix.java
+++ math/src/main/java/com/powsybl/math/matrix/Matrix.java
@@ -36,12 +36,19 @@ public interface Matrix {
         return m;
     }
 
-    static Matrix createFromRow(double[] c, MatrixFactory matrixFactory) {
-        Objects.requireNonNull(c);
+    /**
+     * Utility method for creating a single row matrix from a java array.
+     *
+     * @param r a row array
+     * @param matrixFactory matrix factory to allow creating the matrix with different implementations.
+     * @return the single row matrix
+     */
+    static Matrix createFromRow(double[] r, MatrixFactory matrixFactory) {
+        Objects.requireNonNull(r);
         Objects.requireNonNull(matrixFactory);
-        Matrix m = matrixFactory.create(1, c.length, c.length);
-        for (int j = 0; j < c.length; j++) {
-            m.set(0, j, c[j]);
+        Matrix m = matrixFactory.create(1, r.length, r.length);
+        for (int j = 0; j < r.length; j++) {
+            m.set(0, j, r[j]);
         }
         return m;
     }
