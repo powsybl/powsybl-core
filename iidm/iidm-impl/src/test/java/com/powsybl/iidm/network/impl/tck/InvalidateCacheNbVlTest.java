631e619e71d26bbb141168bbbeb2de69a8fa9ba9
Mathieu BAGUE <mathieu.bague@rte-france.com>
2021-05-03 15:13:53 +0200

    Fix coding style
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

--- iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/tck/InvalidateCacheNbVlTest.java
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/tck/InvalidateCacheNbVlTest.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
- package com.powsybl.iidm.network.impl.tck;
+package com.powsybl.iidm.network.impl.tck;
 
 import com.powsybl.iidm.network.tck.AbstractInvalidateCacheNbVlTest;
 
