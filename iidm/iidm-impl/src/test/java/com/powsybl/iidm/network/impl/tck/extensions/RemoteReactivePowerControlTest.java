319a9bda7ee5441f6862ce82a0e656d1768b4c25
Bertrand Rix <bertrand.rix@artelys.com>
2021-05-21 17:09:18 +0200

    Add a non abstract test class for RemoteReactivePowerControlTest.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

---
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/tck/extensions/RemoteReactivePowerControlTest.java
@@ -0,0 +1,9 @@
+package com.powsybl.iidm.network.impl.extensions;
+
+import com.powsybl.iidm.network.tck.extensions.AbstractRemoteReactivePowerControlTest;
+
+/**
+ * @author Bertrand Rix <bertrand.rix at artelys.com>
+ */
+public class RemoteReactivePowerControlTest extends AbstractRemoteReactivePowerControlTest {
+}
