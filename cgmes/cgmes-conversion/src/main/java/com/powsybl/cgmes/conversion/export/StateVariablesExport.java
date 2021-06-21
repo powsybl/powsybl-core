2361c1f07a617728bd0882ccc16fc6634960f47a
marqueslanauja <51124986+marqueslanauja@users.noreply.github.com>
2021-06-21 08:36:19 +0200

    CGMES export: ShuntCompensator, StaticVarCompensator, Battery export the active flow as 0.0. (#1770)
    Signed-off-by: José Antonio Marqués <marquesja@aia.es>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/export/StateVariablesExport.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/export/StateVariablesExport.java
@@ -286,31 +286,55 @@ public final class StateVariablesExport {
     private static void writePowerFlow(Terminal terminal, String cimNamespace, XMLStreamWriter writer, CgmesExportContext context) {
         String cgmesTerminal = ((Connectable<?>) terminal.getConnectable()).getAliasFromType(Conversion.CGMES_PREFIX_ALIAS_PROPERTIES + CgmesNames.TERMINAL1).orElse(null);
         if (cgmesTerminal != null) {
-            writePowerFlow(cgmesTerminal, terminal.getP(), terminal.getQ(), cimNamespace, writer);
+            writePowerFlow(cgmesTerminal, getTerminalP(terminal), terminal.getQ(), cimNamespace, writer);
         } else if (terminal.getConnectable() instanceof Load && terminal.getConnectable().isFictitious()) {
-            // Fictitious loads are created in IIDM to keep track of mismatches in the input case,
-            // These mismatches are given by SvInjection CGMES objects
-            // These loads have been taken into account as inputs for potential power flow analysis
-            // TODO(Luma) Not sure that its values should be written back as SvInjection objects
-            // Because in our output we should write our current mismatches
-            // Original mismatches, if they have been used, should be written as loads
-            // But that would mean to introduce a new object in the Equipment profile
-            Load svInjection = (Load) terminal.getConnectable();
-            Bus bus = svInjection.getTerminal().getBusView().getBus();
-            if (bus == null) {
-                LOG.warn("Fictitious load does not have a BusView bus. No SvInjection is written");
-            } else {
-                Set<String> topologicalNodes = context.getTopologicalNodesByBusViewBus(bus.getId());
-                if (topologicalNodes.isEmpty()) {
-                    LOG.warn("Fictitious load does not have a corresponding Topological Node. No SvInjection is written");
-                } else {
-                    // SvInjection will be assigned to the first of the TNs mapped to the bus
-                    String topologicalNode = topologicalNodes.iterator().next();
-                    writeSvInjection(svInjection, topologicalNode, cimNamespace, writer);
-                }
-            }
+            writeFictitiousLoadPowerFlow(terminal, cimNamespace, writer, context);
         } else {
             LOG.error("No defined CGMES terminal for {}", terminal.getConnectable().getId());
+        }
+    }
+
+    private static double getTerminalP(Terminal terminal) {
+        double p = terminal.getP();
+        if (!Double.isNaN(p)) {
+            return p;
+        }
+        // P is NaN
+        if (Double.isNaN(terminal.getQ())) {
+            return p;
+        }
+        // P is NaN and Q != NaN
+        if (terminal.getConnectable() instanceof StaticVarCompensator) {
+            return 0.0;
+        }
+        if (terminal.getConnectable() instanceof ShuntCompensator) {
+            return 0.0;
+        }
+        return p;
+    }
+
+    private static void writeFictitiousLoadPowerFlow(Terminal terminal, String cimNamespace, XMLStreamWriter writer,
+        CgmesExportContext context) {
+        // Fictitious loads are created in IIDM to keep track of mismatches in the input case,
+        // These mismatches are given by SvInjection CGMES objects
+        // These loads have been taken into account as inputs for potential power flow analysis
+        // TODO(Luma) Not sure that its values should be written back as SvInjection objects
+        // Because in our output we should write our current mismatches
+        // Original mismatches, if they have been used, should be written as loads
+        // But that would mean to introduce a new object in the Equipment profile
+        Load svInjection = (Load) terminal.getConnectable();
+        Bus bus = svInjection.getTerminal().getBusView().getBus();
+        if (bus == null) {
+            LOG.warn("Fictitious load does not have a BusView bus. No SvInjection is written");
+        } else {
+            Set<String> topologicalNodes = context.getTopologicalNodesByBusViewBus(bus.getId());
+            if (topologicalNodes.isEmpty()) {
+                LOG.warn("Fictitious load does not have a corresponding Topological Node. No SvInjection is written");
+            } else {
+                // SvInjection will be assigned to the first of the TNs mapped to the bus
+                String topologicalNode = topologicalNodes.iterator().next();
+                writeSvInjection(svInjection, topologicalNode, cimNamespace, writer);
+            }
         }
     }
 
--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/export/StateVariablesExportTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/export/StateVariablesExportTest.java
@@ -7,10 +7,13 @@
 package com.powsybl.cgmes.conversion.test.export;
 
 import com.powsybl.cgmes.conformity.test.CgmesConformity1Catalog;
+import com.powsybl.cgmes.conformity.test.CgmesConformity1ModifiedCatalog;
 import com.powsybl.cgmes.conversion.CgmesImport;
+import com.powsybl.cgmes.conversion.Conversion;
 import com.powsybl.cgmes.conversion.export.CgmesExportContext;
 import com.powsybl.cgmes.conversion.export.StateVariablesExport;
 import com.powsybl.cgmes.extensions.CgmesIidmMapping;
+import com.powsybl.cgmes.model.CgmesNames;
 import com.powsybl.commons.AbstractConverterTest;
 import com.powsybl.commons.datasource.ReadOnlyDataSource;
 import com.powsybl.commons.xml.XmlUtil;
@@ -18,10 +21,15 @@ import com.powsybl.computation.DefaultComputationManagerConfig;
 import com.powsybl.iidm.export.ExportOptions;
 import com.powsybl.iidm.import_.ImportConfig;
 import com.powsybl.iidm.import_.Importers;
+import com.powsybl.iidm.network.Connectable;
 import com.powsybl.iidm.network.Line;
+import com.powsybl.iidm.network.Load;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.NetworkFactory;
+import com.powsybl.iidm.network.ShuntCompensator;
+import com.powsybl.iidm.network.Terminal;
 import com.powsybl.iidm.xml.NetworkXml;
+
 import org.junit.Test;
 
 import javax.xml.stream.XMLStreamException;
@@ -34,6 +42,7 @@ import java.util.Collections;
 import java.util.Properties;
 import java.util.function.Consumer;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -69,6 +78,84 @@ public class StateVariablesExportTest extends AbstractConverterTest {
     @Test
     public void smallGridNodeBreaker() throws IOException, XMLStreamException {
         test(CgmesConformity1Catalog.smallNodeBreaker().dataSource(), 4);
+    }
+
+    @Test
+    public void miniBusBranchWithSvInjection() throws IOException, XMLStreamException {
+        test(CgmesConformity1ModifiedCatalog.smallBusBranchWithSvInjectio().dataSource(), 4);
+    }
+
+    @Test
+    public void miniBusBranchWithSvInjectionExportPQ() throws IOException, XMLStreamException {
+
+        Network network = importNetwork(CgmesConformity1ModifiedCatalog.smallBusBranchWithSvInjectio().dataSource());
+        String loadId = "_0448d86a-c766-11e1-8775-005056c00008";
+        Load load = network.getLoad(loadId);
+        String cgmesTerminal = getCgmesTerminal(load.getTerminal());
+
+        // Only when P and Q are NaN is not exported
+
+        load.getTerminal().setP(-0.12);
+        load.getTerminal().setQ(-13.03);
+        String sv = exportSvAsString(network, 4);
+        assertTrue(sv.contains(cgmesTerminal));
+
+        load.getTerminal().setP(Double.NaN);
+        load.getTerminal().setQ(-13.03);
+        String sv1 = exportSvAsString(network, 4);
+        assertTrue(sv1.contains(cgmesTerminal));
+
+        load.getTerminal().setP(-0.12);
+        load.getTerminal().setQ(Double.NaN);
+        String sv2 = exportSvAsString(network, 4);
+        assertTrue(sv2.contains(cgmesTerminal));
+
+        load.getTerminal().setP(Double.NaN);
+        load.getTerminal().setQ(Double.NaN);
+        String sv3 = exportSvAsString(network, 4);
+        assertFalse(sv3.contains(cgmesTerminal));
+    }
+
+    @Test
+    public void miniBusBranchWithSvInjectionExportQ() throws IOException, XMLStreamException {
+
+        Network network = importNetwork(CgmesConformity1ModifiedCatalog.smallBusBranchWithSvInjectio().dataSource());
+        String shuntCompensatorId = "_04553478-c766-11e1-8775-005056c00008";
+        ShuntCompensator shuntCompensator = network.getShuntCompensator(shuntCompensatorId);
+        String cgmesTerminal = getCgmesTerminal(shuntCompensator.getTerminal());
+
+        // If Q are NaN is not exported
+
+        shuntCompensator.getTerminal().setQ(-13.03);
+        String sv = exportSvAsString(network, 4);
+        assertTrue(sv.contains(cgmesTerminal));
+
+        shuntCompensator.getTerminal().setQ(Double.NaN);
+        String sv1 = exportSvAsString(network, 4);
+        assertFalse(sv1.contains(cgmesTerminal));
+    }
+
+    private static Network importNetwork(ReadOnlyDataSource ds) {
+        Properties properties = new Properties();
+        properties.put("iidm.import.cgmes.profile-used-for-initial-state-values", "SV");
+        properties.put("iidm.import.cgmes.create-cgmes-export-mapping", "true");
+        return new CgmesImport().importData(ds, NetworkFactory.findDefault(), properties);
+    }
+
+    private String exportSvAsString(Network network, int svVersion) throws XMLStreamException, IOException {
+        CgmesExportContext context = new CgmesExportContext(network);
+        Path file = fileSystem.getPath("/work/" + network.getId() + ".xml");
+        OutputStream os = Files.newOutputStream(file);
+        XMLStreamWriter writer = XmlUtil.initializeWriter(true, "    ", os);
+        context.getSvModelDescription().setVersion(svVersion);
+        context.setExportBoundaryPowerFlows(true);
+        StateVariablesExport.write(network, writer, context);
+
+        return Files.readString(file);
+    }
+
+    private static String getCgmesTerminal(Terminal terminal) {
+        return ((Connectable<?>) terminal.getConnectable()).getAliasFromType(Conversion.CGMES_PREFIX_ALIAS_PROPERTIES + CgmesNames.TERMINAL1).orElse(null);
     }
 
     private static void addRepackagerFiles(String tso, Repackager repackager) {
