627de7476fb46ddb3a56281d28d71419c17f14fc
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2021-06-28 10:39:16 +0200

    Add javadoc
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- iidm/iidm-extensions/src/main/java/com/powsybl/iidm/network/extensions/Measurement.java
+++ iidm/iidm-extensions/src/main/java/com/powsybl/iidm/network/extensions/Measurement.java
@@ -9,16 +9,25 @@ package com.powsybl.iidm.network.extensions;
 import java.util.Set;
 
 /**
+ * A measurement with a continuous numeric value (double) such as: angle, voltage, active power and so on.
+ * Can have properties in addition of explicit fields to add precisions about the measurement if necessary.
+ *
  * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
  */
 public interface Measurement {
 
+    /**
+     * Specify which side of the equipment the measurement is applied on.
+     */
     enum Side {
         ONE,
         TWO,
         THREE
     }
 
+    /**
+     * Specify what is measured.
+     */
     enum Type {
         ANGLE,
         ACTIVE_POWER,
@@ -30,31 +39,76 @@ public interface Measurement {
         OTHER
     }
 
+    /**
+     * Get ID of the measurement if it exists. It is optional (can be null).
+     */
     String getId();
 
+    /**
+     * Get the type of measurement (specify what is measured, see {@link Type}).
+     */
     Type getType();
 
+    /**
+     * Get all the property names with values applied to this measurement.
+     */
     Set<String> getPropertyNames();
 
+    /**
+     * Return property value associated to the given property name if it exists, else return null.
+     */
     String getProperty(String name);
 
+    /**
+     * Put a given property value associated with a given property name.
+     */
     Measurement putProperty(String name, String property);
 
+    /**
+     * Remove the property value associated with the given property name if it exists. Else, do nothing.
+     */
     Measurement removeProperty(String name);
 
+    /**
+     * Set measurement value.
+     * Can not be NaN if the measurement is valid.
+     */
     Measurement setValue(double value);
 
+    /**
+     * Get measurement value.
+     */
     double getValue();
 
+    /**
+     * Set the standard deviation.
+     */
     Measurement setStandardDeviation(double standardDeviation);
 
+    /**
+     * Get the standard deviation. Return NaN if unspecified.
+     */
     double getStandardDeviation();
 
+    /**
+     * Get validity status of the measurement.
+     * If it is true (i.e. the measurement is valid), the measured value can not be NaN.
+     */
     boolean isValid();
 
+    /**
+     * Set validity status of the measurement.
+     * If it is true (i.e. the measurement is valid), the measured value can not be NaN.
+     */
     Measurement setValid(boolean valid);
 
+    /**
+     * Get which side the measurement is applied on (see {@link Side}).
+     */
     Side getSide();
 
+    /**
+     * Remove the measurement from the equipment.
+     */
     void remove();
 }
