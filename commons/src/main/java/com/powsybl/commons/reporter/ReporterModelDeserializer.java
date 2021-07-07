0f086db31595efa2b1cdf088f991859373e15dae
Florian Dupuy <florian.dupuy@rte-france.com>
2021-07-07 11:43:18 +0200

    Javadoc: add beta-feature missing warning
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- commons/src/main/java/com/powsybl/commons/reporter/ReporterModelDeserializer.java
+++ commons/src/main/java/com/powsybl/commons/reporter/ReporterModelDeserializer.java
@@ -26,6 +26,8 @@ import java.util.Map;
 import java.util.Objects;
 
 /**
+ * <i>WARNING:</i> <code>Reporter</code> <i>is still a beta feature, structural changes might occur in the future releases</i>
+ *
  * @author Florian Dupuy <florian.dupuy at rte-france.com>
  */
 public class ReporterModelDeserializer extends StdDeserializer<ReporterModel> {
