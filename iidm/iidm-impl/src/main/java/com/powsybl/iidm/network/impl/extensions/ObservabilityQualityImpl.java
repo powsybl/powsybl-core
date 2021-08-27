e93eb66e2418505f3a371ff2c5687f6a40e99173
Thomas ADAM <52197093+tadam50@users.noreply.github.com>
2021-08-27 15:24:34 +0200

    Add new extension : InjectionObservability (#1788)
    * Add InjectionObservability, with only observable field available
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>
    
    * Add standardDeviation + redundant fileds to InjectionObservability
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>
    
    * Increase code coverage + fix code smell
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>
    
    * Fix header
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>
    
    * Change standardDeviation from float to double
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>
    
    * Change observable from required to optional
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>
    
    * Add ObservabilityQuality class object
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>
    
    * Fix ObservabilityQuality setRedundant method parameter from boolean to Boolean
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>
    
    * Fix field creation
    
    Signed-off-by: Thomas ADAM <tadam@silicom.fr>
    
    * Comments improvement.
    
    Co-authored-by: Anne Tilloy <anne.tilloy@rte-france.com>

---
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/extensions/ObservabilityQualityImpl.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.network.impl.extensions;
+
+import com.powsybl.iidm.network.extensions.ObservabilityQuality;
+
+/**
+ * @author Thomas Adam <tadam at silicom.fr>
+ */
+public class ObservabilityQualityImpl<T> implements ObservabilityQuality<T> {
+
+    private double standardDeviation;
+
+    private Boolean redundant;
+
+    public ObservabilityQualityImpl(double standardDeviation, Boolean redundant) {
+        this.standardDeviation = standardDeviation;
+        this.redundant = redundant;
+    }
+
+    @Override
+    public double getStandardDeviation() {
+        return standardDeviation;
+    }
+
+    @Override
+    public ObservabilityQuality<T> setStandardDeviation(double standardDeviation) {
+        this.standardDeviation = standardDeviation;
+        return this;
+    }
+
+    @Override
+    public Boolean isRedundant() {
+        return redundant;
+    }
+
+    @Override
+    public ObservabilityQuality<T> setRedundant(Boolean redundant) {
+        this.redundant = redundant;
+        return this;
+    }
+}
