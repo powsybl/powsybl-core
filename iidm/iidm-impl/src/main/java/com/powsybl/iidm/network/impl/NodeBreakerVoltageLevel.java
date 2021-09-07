487cb604bb1286eafcbdbbd79681275cece8cb4a
Marcos de Miguel <demiguelm@aia.es>
2021-09-07 16:05:52 +0200

    fix NodeBreakerVoltageLevel, copied solution from BusBreakerVoltageLevel
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/NodeBreakerVoltageLevel.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/NodeBreakerVoltageLevel.java
@@ -509,7 +509,7 @@ class NodeBreakerVoltageLevel extends AbstractVoltageLevel {
     NodeBreakerVoltageLevel(String id, String name, boolean fictitious, SubstationImpl substation, Ref<NetworkImpl> ref,
                             double nominalV, double lowVoltageLimit, double highVoltageLimit) {
         super(id, name, fictitious, substation, ref, nominalV, lowVoltageLimit, highVoltageLimit);
-        variants = new VariantArray<>(substation.getNetwork().getRef(), VariantImpl::new);
+        variants = new VariantArray<>(ref == null ? substation.getNetwork().getRef() : ref, VariantImpl::new);
     }
 
     @Override
