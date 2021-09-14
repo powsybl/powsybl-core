8cc94b58ab3f9afd81983b1243cd90b0e3cd47c1
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2021-09-14 16:29:04 +0200

    If b undefined for transformer in CGMES, don't crash
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/transformers/TwoWindingsTransformerConversion.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/transformers/TwoWindingsTransformerConversion.java
@@ -150,7 +150,7 @@ public class TwoWindingsTransformerConversion extends AbstractTransformerConvers
                 .setR(convertedT2xModel.r)
                 .setX(convertedT2xModel.x)
                 .setG(convertedT2xModel.end1.g)
-                .setB(convertedT2xModel.end1.b)
+                .setB(Double.isNaN(convertedT2xModel.end1.b) ? 0.0 : convertedT2xModel.end1.b)
                 .setRatedU1(convertedT2xModel.end1.ratedU)
                 .setRatedU2(convertedT2xModel.end2.ratedU);
         identify(adder);
