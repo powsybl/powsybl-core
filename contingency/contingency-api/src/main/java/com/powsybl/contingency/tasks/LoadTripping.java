bddea8e1a2f8a62f73a509c477df48e2d53da8ae
Hadrien <hadrien.godard@artelys.com>
2021-09-24 12:11:57 +0200

    New type of contingencies : LOAD
    Signed-off-by: Hadrien <hadrien.godard@artelys.com>

---
+++ contingency/contingency-api/src/main/java/com/powsybl/contingency/tasks/LoadTripping.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2016, All partners of the iTesla project (http://www.itesla-project.eu/consortium)
+ * Copyright (c) 2016-2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.contingency.tasks;
+
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.iidm.network.Injection;
+import com.powsybl.iidm.network.Network;
+
+/**
+ * @author Hadrien Godard <hadrien.godard at artelys.com>
+ */
+public class LoadTripping extends AbstractInjectionTripping {
+
+    public LoadTripping(String id) {
+        super(id);
+    }
+
+    @Override
+    protected Injection getInjection(Network network) {
+        Injection injection = network.getLoad(id);
+        if (injection == null) {
+            throw new PowsyblException("Load '" + id + "' not found");
+        }
+
+        return injection;
+    }
+}
