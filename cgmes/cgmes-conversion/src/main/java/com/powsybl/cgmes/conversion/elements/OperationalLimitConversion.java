d9642a29e8243929d7911e804862e95bee1db7aa
Luma <zamarrenolm@aia.es>
2021-10-01 09:14:43 +0200

    Cgmes: Support for CGMES 3.0 (CIM version 100) (#1753)
    * support for CGMES 3 (CIM version 100)
    * More conformity cases
    * Topology test for all the CGMES 3 conformity grids
    * Remove TP dependencies
    * Save voltage and angle for nodes at boundary if we are converting boundary
    * Update queries synchronousMachines and operationalLimits
    * Fix initialP and normalValue
    * Fix operationalLimits
    * Loading test cases without TP file.
    * In CGMES 3 unit tests, import networks with and without TP, SV and check they are equivalent
    
    Signed-off-by: José Antonio Marqués <marquesja@aia.es>
    Signed-off-by: Marcos de Miguel <demiguelm@aia.es>
    Signed-off-by: Luma <zamarrenolm@aia.es>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/OperationalLimitConversion.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/OperationalLimitConversion.java
@@ -192,7 +192,8 @@ public class OperationalLimitConversion extends AbstractIdentifiedObjectConversi
 
     @Override
     public void convert() {
-        double value = p.asDouble("value");
+        double normalValue = p.asDouble("normalValue");
+        double value = p.asDouble("value", normalValue);
         if (value <= 0) {
             context.ignored(OPERATIONAL_LIMIT, "value is <= 0");
             return;
@@ -235,7 +236,7 @@ public class OperationalLimitConversion extends AbstractIdentifiedObjectConversi
     private boolean isPatl() {
         String limitTypeName = p.getLocal(OPERATIONAL_LIMIT_TYPE_NAME);
         String limitType = p.getLocal(LIMIT_TYPE);
-        return limitTypeName.equals("PATL") || "LimitTypeKind.patl".equals(limitType);
+        return limitTypeName.equals("PATL") || "LimitTypeKind.patl".equals(limitType) || "LimitKind.patl".equals(limitType);
     }
 
     private void addPatl(double value, LoadingLimitsAdder<?, ?> adder) {
@@ -269,7 +270,7 @@ public class OperationalLimitConversion extends AbstractIdentifiedObjectConversi
     private boolean isTatl() {
         String limitTypeName = p.getLocal(OPERATIONAL_LIMIT_TYPE_NAME);
         String limitType = p.getLocal(LIMIT_TYPE);
-        return limitTypeName.equals("TATL") || "LimitTypeKind.tatl".equals(limitType);
+        return limitTypeName.equals("TATL") || "LimitTypeKind.tatl".equals(limitType) || "LimitKind.tatl".equals(limitType);
     }
 
     private void addTatl(String name, double value, int acceptableDuration, LoadingLimitsAdder<?, ?> adder) {
