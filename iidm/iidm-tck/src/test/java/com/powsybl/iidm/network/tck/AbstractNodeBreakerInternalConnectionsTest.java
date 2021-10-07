4ae80acd16a3cf29bacbd336d61f25f8cced7882
Florian Dupuy <florian.dupuy@rte-france.com>
2021-10-07 17:15:45 +0200

    Add unit test
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- iidm/iidm-tck/src/test/java/com/powsybl/iidm/network/tck/AbstractNodeBreakerInternalConnectionsTest.java
+++ iidm/iidm-tck/src/test/java/com/powsybl/iidm/network/tck/AbstractNodeBreakerInternalConnectionsTest.java
@@ -53,6 +53,11 @@ public abstract class AbstractNodeBreakerInternalConnectionsTest {
         List<Integer> nodesInternallyConnectedTo3 = vl.getNodeBreakerView().getNodeInternalConnectedToStream(3).boxed().collect(Collectors.toList());
         assertEquals(Arrays.asList(6, 4), nodesInternallyConnectedTo3);
 
+        // Find the first internal connection encountered
+        InternalConnections firstInternalConnectionFound = findFirstInternalConnections(vl);
+        assertEquals(new InternalConnections().add(0, 7), firstInternalConnectionFound);
+
+        // Find the internal connections encountered before encountering a terminal
         InternalConnections foundStoppingAtTerminals = findInternalConnectionsTraverseStoppingAtTerminals(vl);
 
         // If we stop traversal at terminals
@@ -68,6 +73,7 @@ public abstract class AbstractNodeBreakerInternalConnectionsTest {
         InternalConnections actual = findInternalConnections(vl);
         InternalConnections expected = all;
         assertEquals(expected, actual);
+
     }
 
     @Test
@@ -250,6 +256,21 @@ public abstract class AbstractNodeBreakerInternalConnectionsTest {
         topo.traverse(topo.getNodes(), (n1, sw, n2) -> {
             if (sw == null) {
                 cs.add(n1, n2);
+            }
+            return TraverseResult.CONTINUE;
+        });
+
+        return cs;
+    }
+
+    private InternalConnections findFirstInternalConnections(VoltageLevel vl) {
+        VoltageLevel.NodeBreakerView topo = vl.getNodeBreakerView();
+
+        InternalConnections cs = new InternalConnections();
+        topo.traverse(topo.getNodes(), (n1, sw, n2) -> {
+            if (sw == null) {
+                cs.add(n1, n2);
+                return TraverseResult.TERMINATE_TRAVERSER;
             }
             return TraverseResult.CONTINUE;
         });
