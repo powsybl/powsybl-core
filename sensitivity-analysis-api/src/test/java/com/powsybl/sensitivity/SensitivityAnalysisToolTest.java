a33601829cbb673bb34739c84ab2426348221913
Anne Tilloy <anne.tilloy@rte-france.com>
2021-10-20 11:12:34 +0200

    Add unit tests for the tool.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- sensitivity-analysis-api/src/test/java/com/powsybl/sensitivity/SensitivityAnalysisToolTest.java
+++ sensitivity-analysis-api/src/test/java/com/powsybl/sensitivity/SensitivityAnalysisToolTest.java
@@ -9,16 +9,16 @@ package com.powsybl.sensitivity;
 import com.powsybl.tools.AbstractToolTest;
 import com.powsybl.tools.Tool;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Collections;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author Sebastien Murgey {@literal <sebastien.murgey at rte-france.com>}
  */
-@Ignore
 public class SensitivityAnalysisToolTest extends AbstractToolTest {
 
     private static final String COMMAND_NAME = "sensitivity-analysis";
@@ -31,6 +31,7 @@ public class SensitivityAnalysisToolTest extends AbstractToolTest {
         super.setUp();
         createFile("test.uct", "");
         createFile("test.csv", "");
+        createFile("input.xiidm", "");
     }
 
     @Override
@@ -40,10 +41,9 @@ public class SensitivityAnalysisToolTest extends AbstractToolTest {
 
     @Override
     public void assertCommand() {
-        assertCommand(tool.getCommand(), COMMAND_NAME, 8, 2);
+        assertCommand(tool.getCommand(), COMMAND_NAME, 8, 3);
         assertOption(tool.getCommand().getOptions(), "case-file", true, true);
-        assertOption(tool.getCommand().getOptions(), "output-file", false, true);
-        assertOption(tool.getCommand().getOptions(), "output-format", false, true);
+        assertOption(tool.getCommand().getOptions(), "output-file", true, true);
         assertOption(tool.getCommand().getOptions(), "factors-file", true, true);
         assertOption(tool.getCommand().getOptions(), "contingencies-file", false, true);
         assertOption(tool.getCommand().getOptions(), "parameters-file", false, true);
@@ -51,16 +51,23 @@ public class SensitivityAnalysisToolTest extends AbstractToolTest {
 
     @Test
     public void checkFailsWhenNetworkFileNotFound() throws IOException {
-        assertCommand(new String[] {COMMAND_NAME, "--case-file", "wrongFile.uct", "--factors-file", "test.csv"}, 3, null, "com.powsybl.commons.PowsyblException: File wrongFile.uct does not exist or is not a regular file");
+        assertCommand(new String[] {COMMAND_NAME, "--case-file", "wrongFile.uct", "--factors-file", "test.csv", "--output-file", "output.csv"}, 3, null, "com.powsybl.commons.PowsyblException: File wrongFile.uct does not exist or is not a regular file");
     }
 
     @Test
     public void checkFailsWhenFactorsFileNotFound() throws IOException {
-        assertCommand(new String[] {COMMAND_NAME, "--case-file", "test.uct", "--factors-file", "wrongFile.csv"}, 3, null, "PowsyblException: Unsupported file format or invalid file");
+        assertCommand(new String[] {COMMAND_NAME, "--case-file", "test.uct", "--factors-file", "wrongFile.csv", "--output-file", "output.csv"}, 3, null, "PowsyblException: Unsupported file format or invalid file");
     }
 
     @Test
     public void checkThrowsWhenOutputFileAndNoFormat() throws IOException {
-        assertCommand(new String[] {COMMAND_NAME, "--case-file", "test.uct", "--factors-file", "test.csv", "--output-file", "out.txt"}, 2, "", "error: Missing required option: output-format");
+        assertCommand(new String[] {COMMAND_NAME, "--case-file", "test.uct", "--factors-file", "test.csv", "--output-file", "out.txt"}, 3, "", "Unsupported output format: out.txt");
+    }
+
+    @Test
+    public void checkCommand() {
+        assertEquals("sensitivity-analysis", tool.getCommand().getName());
+        assertEquals("Computation", tool.getCommand().getTheme());
+        assertEquals("Run sensitivity analysis", tool.getCommand().getDescription());
     }
 }
