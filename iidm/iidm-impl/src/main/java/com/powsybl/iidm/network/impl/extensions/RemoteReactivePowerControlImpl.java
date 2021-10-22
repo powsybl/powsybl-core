c09811248232e524b2afb3b5c3da5b84484b036a
Mathieu BAGUE <mathieu.bague@rte-france.com>
2021-10-22 13:50:15 +0200

    Assert that regulatingTerminal is never null
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/extensions/RemoteReactivePowerControlImpl.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/extensions/RemoteReactivePowerControlImpl.java
@@ -11,20 +11,22 @@ import com.powsybl.iidm.network.Generator;
 import com.powsybl.iidm.network.Terminal;
 import com.powsybl.iidm.network.extensions.RemoteReactivePowerControl;
 
+import java.util.Objects;
+
 /**
  * @author Bertrand Rix <bertrand.rix at artelys.com>
  */
 public class RemoteReactivePowerControlImpl extends AbstractExtension<Generator> implements RemoteReactivePowerControl {
 
-    double targetQ;
+    private final double targetQ;
 
-    Terminal regulatingTerminal;
+    private final Terminal regulatingTerminal;
 
-    boolean enabled;
+    private final boolean enabled;
 
     public RemoteReactivePowerControlImpl(double targetQ, Terminal regulatingTerminal, boolean enabled) {
         this.targetQ = targetQ;
-        this.regulatingTerminal = regulatingTerminal;
+        this.regulatingTerminal = Objects.requireNonNull(regulatingTerminal);
         this.enabled = enabled;
     }
 
