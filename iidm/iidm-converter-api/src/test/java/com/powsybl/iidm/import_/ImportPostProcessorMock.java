6175529f6cfae98e3871c9b84e17494bc6aa9929
Coline Piloquet <coline.piloquet@rte-france.com>
2021-12-08 14:16:29 +0100

    Add copyright and author
    Signed-off-by: Coline PILOQUET <coline.piloquet@rte-france.com>

--- iidm/iidm-converter-api/src/test/java/com/powsybl/iidm/import_/ImportPostProcessorMock.java
+++ iidm/iidm-converter-api/src/test/java/com/powsybl/iidm/import_/ImportPostProcessorMock.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.iidm.import_;
 
 import com.powsybl.commons.reporter.Report;
@@ -5,6 +11,9 @@ import com.powsybl.commons.reporter.Reporter;
 import com.powsybl.computation.ComputationManager;
 import com.powsybl.iidm.network.Network;
 
+/**
+ * @author Coline Piloquet <coline.piloquet at rte-france.com>
+ */
 public class ImportPostProcessorMock implements ImportPostProcessor {
 
     public ImportPostProcessorMock() {
--- iidm/iidm-converter-api/src/test/java/com/powsybl/iidm/import_/PostProcessorReporterTest.java
+++ iidm/iidm-converter-api/src/test/java/com/powsybl/iidm/import_/PostProcessorReporterTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.iidm.import_;
 
 import com.powsybl.commons.AbstractConverterTest;
@@ -18,6 +24,9 @@ import java.util.Optional;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * @author Coline Piloquet <coline.piloquet at rte-france.com>
+ */
 public class PostProcessorReporterTest extends AbstractConverterTest {
     @Test
     public void postProcessorWithReporter() throws IOException {
