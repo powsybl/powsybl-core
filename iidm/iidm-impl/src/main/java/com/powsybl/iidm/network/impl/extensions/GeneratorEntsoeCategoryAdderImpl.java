cdd88d74c40565e1d3764f7466398c5724402c04
Coline Piloquet <coline.piloquet@rte-france.com>
2021-12-08 17:08:43 +0100

    Add generator entsoe category extension
    Signed-off-by: Coline PILOQUET <coline.piloquet@rte-france.com>

---
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/extensions/GeneratorEntsoeCategoryAdderImpl.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.iidm.network.impl.extensions;
+
+import com.powsybl.commons.extensions.AbstractExtensionAdder;
+import com.powsybl.iidm.network.Generator;
+import com.powsybl.iidm.network.extensions.GeneratorEntsoeCategory;
+import com.powsybl.iidm.network.extensions.GeneratorEntsoeCategoryAdder;
+
+/**
+ * @author Jérémy Labous <jlabous at silicom.fr>
+ */
+public class GeneratorEntsoeCategoryAdderImpl extends AbstractExtensionAdder<Generator, GeneratorEntsoeCategory>
+        implements GeneratorEntsoeCategoryAdder {
+
+    private int code;
+
+    public GeneratorEntsoeCategoryAdderImpl(Generator generator) {
+        super(generator);
+    }
+
+    @Override
+    protected GeneratorEntsoeCategory createExtension(Generator extendable) {
+        return new GeneratorEntsoeCategoryImpl(extendable, code);
+    }
+
+    @Override
+    public GeneratorEntsoeCategoryAdder withCode(int code) {
+        this.code = code;
+        return this;
+    }
+}
