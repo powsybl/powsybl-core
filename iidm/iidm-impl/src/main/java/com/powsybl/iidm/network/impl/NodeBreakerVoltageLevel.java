9a33abc7b30086ca0179335271abed6f66a9599e
José Antonio Marqués <marquesja@aia.es>
2022-03-01 12:31:54 +0100

    Do not allow switches with the same node or bus at both ends
    Signed-off-by: José Antonio Marqués <marquesja@aia.es>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/NodeBreakerVoltageLevel.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/NodeBreakerVoltageLevel.java
@@ -151,6 +151,9 @@ class NodeBreakerVoltageLevel extends AbstractVoltageLevel {
             if (node2 == null) {
                 throw new ValidationException(this, "second connection node is not set");
             }
+            if (node1 == node2) {
+                throw new ValidationException(this, "same node at both ends");
+            }
             if (kind == null) {
                 throw new ValidationException(this, "kind is not set");
             }
