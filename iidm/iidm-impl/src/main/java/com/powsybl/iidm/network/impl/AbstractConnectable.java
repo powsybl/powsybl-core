69574f562447af40993954e35fc9622c5213b8cf
VEDELAGO MIORA <miora.ralambotiana@rte-france.com>
2022-03-07 15:04:55 +0100

    Fix checkstyle
    Signed-off-by: VEDELAGO MIORA <miora.ralambotiana@rte-france.com>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/AbstractConnectable.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/AbstractConnectable.java
@@ -15,7 +15,6 @@ import com.powsybl.iidm.network.impl.util.Ref;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.function.Supplier;
 
 /**
