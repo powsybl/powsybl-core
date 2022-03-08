ca72ff2ba2076fd8c7d6b58f9d4df0cf9a65058e
Coline Piloquet <coline.piloquet@rte-france.com>
2022-03-08 16:22:26 +0100

    Add getter and setter in feeder results
    Signed-off-by: Coline PILOQUET <coline.piloquet@rte-france.com>

--- shortcircuit-api/src/main/java/com/powsybl/shortcircuit/SelectiveStudyFeederResult.java
+++ shortcircuit-api/src/main/java/com/powsybl/shortcircuit/SelectiveStudyFeederResult.java
@@ -18,4 +18,69 @@ public class SelectiveStudyFeederResult {
     private ValuesOnThreePhases feederVoltagePerPhase;
     private double voltageDrop;
 
+    public SelectiveStudyFeederResult(String busId, String connectableId, double feederThreePhaseCurrent, double feederThreePhaseVoltage, ValuesOnThreePhases feederCurrentPerPhase, ValuesOnThreePhases feederVoltagePerPhase, double voltageDrop) {
+        this.busId = busId;
+        this.connectableId = connectableId;
+        this.feederThreePhaseCurrent = feederThreePhaseCurrent;
+        this.feederThreePhaseVoltage = feederThreePhaseVoltage;
+        this.feederCurrentPerPhase = feederCurrentPerPhase;
+        this.feederVoltagePerPhase = feederVoltagePerPhase;
+        this.voltageDrop = voltageDrop;
+    }
+
+    public String getBusId() {
+        return busId;
+    }
+
+    public void setBusId(String busId) {
+        this.busId = busId;
+    }
+
+    public String getConnectableId() {
+        return connectableId;
+    }
+
+    public void setConnectableId(String connectableId) {
+        this.connectableId = connectableId;
+    }
+
+    public double getFeederThreePhaseCurrent() {
+        return feederThreePhaseCurrent;
+    }
+
+    public void setFeederThreePhaseCurrent(double feederThreePhaseCurrent) {
+        this.feederThreePhaseCurrent = feederThreePhaseCurrent;
+    }
+
+    public double getFeederThreePhaseVoltage() {
+        return feederThreePhaseVoltage;
+    }
+
+    public void setFeederThreePhaseVoltage(double feederThreePhaseVoltage) {
+        this.feederThreePhaseVoltage = feederThreePhaseVoltage;
+    }
+
+    public ValuesOnThreePhases getFeederCurrentPerPhase() {
+        return feederCurrentPerPhase;
+    }
+
+    public void setFeederCurrentPerPhase(ValuesOnThreePhases feederCurrentPerPhase) {
+        this.feederCurrentPerPhase = feederCurrentPerPhase;
+    }
+
+    public ValuesOnThreePhases getFeederVoltagePerPhase() {
+        return feederVoltagePerPhase;
+    }
+
+    public void setFeederVoltagePerPhase(ValuesOnThreePhases feederVoltagePerPhase) {
+        this.feederVoltagePerPhase = feederVoltagePerPhase;
+    }
+
+    public double getVoltageDrop() {
+        return voltageDrop;
+    }
+
+    public void setVoltageDrop(double voltageDrop) {
+        this.voltageDrop = voltageDrop;
+    }
 }
