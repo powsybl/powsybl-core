e12dbd9f0d55d1ed795ca141dfcd61723d9ef051
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2022-03-28 20:19:45 +0200

    Add unit test
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- powerfactory/powerfactory-db/src/test/java/com/powsybl/powerfactory/db/DbStudyCaseLoaderTest.java
+++ powerfactory/powerfactory-db/src/test/java/com/powsybl/powerfactory/db/DbStudyCaseLoaderTest.java
@@ -9,10 +9,8 @@ package com.powsybl.powerfactory.db;
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
 import com.powsybl.commons.config.InMemoryPlatformConfig;
-import com.powsybl.powerfactory.model.DataAttribute;
-import com.powsybl.powerfactory.model.DataAttributeType;
-import com.powsybl.powerfactory.model.StudyCase;
-import com.powsybl.powerfactory.model.StudyCaseLoader;
+import com.powsybl.commons.config.PlatformConfig;
+import com.powsybl.powerfactory.model.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,8 +34,12 @@ public class DbStudyCaseLoaderTest {
         public void read(String powerFactoryHome, String projectName, DataObjectBuilder builder) {
             builder.createClass("ElmNet");
             builder.createAttribute("ElmNet", DataAttribute.LOC_NAME, DataAttributeType.STRING.ordinal(), "");
+            builder.createAttribute("ElmNet", "aInt", DataAttributeType.INTEGER.ordinal(), "");
             builder.createObject(0L, "ElmNet", -1);
             builder.setStringAttributeValue(0L, DataAttribute.LOC_NAME, "TestGrid");
+            builder.setIntAttributeValue(0L, "aInt", 3);
+            builder.createClass("ElmSubstat");
+            builder.createObject(1L, "ElmSubstat", 0L);
         }
     }
 
@@ -53,28 +55,36 @@ public class DbStudyCaseLoaderTest {
         fileSystem.close();
     }
 
-    @Test
-    public void readFromConfigTest() {
-        InMemoryPlatformConfig platformConfig = new InMemoryPlatformConfig(fileSystem);
-        var pfModule = platformConfig.createModuleConfig("power-factory");
-        pfModule.setStringProperty("home-dir", "c:\\work");
-        StudyCase studyCase = StudyCaseLoader.load("Test.IntPrj",
+    private StudyCase loadStudy(PlatformConfig platformConfig) {
+        return StudyCaseLoader.load("Test.IntPrj",
             () -> null,
             List.of(new DbStudyCaseLoader(platformConfig, new TestDatabaseReader())))
                 .orElseThrow();
+    }
+
+    @Test
+    public void readInstallFromConfigTest() {
+        InMemoryPlatformConfig platformConfig = new InMemoryPlatformConfig(fileSystem);
+        var pfModule = platformConfig.createModuleConfig("power-factory");
+        pfModule.setStringProperty("home-dir", "c:\\work");
+        StudyCase studyCase = loadStudy(platformConfig);
         assertEquals("???", studyCase.getName());
     }
 
     @Test
-    public void autodetectTest() throws IOException {
+    public void autodetectInstallTest() throws IOException {
         InMemoryPlatformConfig platformConfig = new InMemoryPlatformConfig(fileSystem);
         Path digSilentDir = fileSystem.getPath(DbStudyCaseLoader.DIG_SILENT_DEFAULT_DIR);
         Files.createDirectories(digSilentDir.resolve("PowerFactory 2016 SP3"));
         Files.createDirectories(digSilentDir.resolve("PowerFactory 2022 SP1"));
-        StudyCase studyCase = StudyCaseLoader.load("Test.IntPrj",
-            () -> null,
-            List.of(new DbStudyCaseLoader(platformConfig, new TestDatabaseReader())))
-                .orElseThrow();
+        StudyCase studyCase = loadStudy(platformConfig);
         assertEquals("???", studyCase.getName());
+    }
+
+    @Test
+    public void noInstallTest() {
+        InMemoryPlatformConfig platformConfig = new InMemoryPlatformConfig(fileSystem);
+        PowerFactoryException exception = assertThrows(PowerFactoryException.class, () -> loadStudy(platformConfig));
+        assertEquals("PowerFactory installation not found", exception.getMessage());
     }
 }
