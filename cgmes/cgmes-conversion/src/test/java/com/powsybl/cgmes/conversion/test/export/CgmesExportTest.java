3f639ac3f20567a7a870a635f4d688c05ad9b35c
VEDELAGO MIORA <miora.ralambotiana@rte-france.com>
2022-04-05 15:36:28 +0200

    Add test
    Signed-off-by: VEDELAGO MIORA <miora.ralambotiana@rte-france.com>

--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/export/CgmesExportTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/export/CgmesExportTest.java
@@ -11,9 +11,7 @@ import com.google.common.jimfs.Jimfs;
 import com.powsybl.commons.datasource.GenericReadOnlyDataSource;
 import com.powsybl.iidm.export.Exporters;
 import com.powsybl.iidm.import_.Importers;
-import com.powsybl.iidm.network.Network;
-import com.powsybl.iidm.network.SwitchKind;
-import com.powsybl.iidm.network.VoltageLevel;
+import com.powsybl.iidm.network.*;
 import com.powsybl.iidm.network.test.FictitiousSwitchFactory;
 import com.powsybl.iidm.network.util.Networks;
 import org.junit.Test;
@@ -31,20 +29,54 @@ import static org.junit.Assert.assertNull;
 public class CgmesExportTest {
 
     @Test
-    public void testFromIidm() throws IOException { // Test from IIDM with configuration that does not exist in CGMES (disconnected node)
+    public void testFromIidm() throws IOException {
+        // Test from IIDM with configuration that does not exist in CGMES (disconnected node on switch and HVDC line)
+
         Network network = FictitiousSwitchFactory.create();
-        network.getVoltageLevel("C").getNodeBreakerView().newSwitch().setId("TEST_SW")
+        VoltageLevel vl = network.getVoltageLevel("C");
+
+        // Add disconnected node on switch (side 2)
+        vl.getNodeBreakerView().newSwitch().setId("TEST_SW")
                 .setKind(SwitchKind.DISCONNECTOR)
                 .setOpen(true)
                 .setNode1(0)
                 .setNode2(6)
                 .add();
+
+        // Add disconnected node on DC converter station (side 2)
+        vl.newVscConverterStation()
+                .setId("C1")
+                .setNode(5)
+                .setLossFactor(1.1f)
+                .setVoltageSetpoint(405.0)
+                .setVoltageRegulatorOn(true)
+                .add();
+        vl.getNodeBreakerView().newInternalConnection().setNode1(0).setNode2(5).add();
+        vl.newVscConverterStation()
+                .setId("C2")
+                .setNode(6)
+                .setLossFactor(1.1f)
+                .setReactivePowerSetpoint(123)
+                .setVoltageRegulatorOn(false)
+                .add();
+        network.newHvdcLine()
+                .setId("hvdc_line")
+                .setR(5.0)
+                .setConvertersMode(HvdcLine.ConvertersMode.SIDE_1_INVERTER_SIDE_2_RECTIFIER)
+                .setNominalV(440.0)
+                .setMaxP(50.0)
+                .setActivePowerSetpoint(20.0)
+                .setConverterStationId1("C1")
+                .setConverterStationId2("C2")
+                .add();
+
         try (FileSystem fs = Jimfs.newFileSystem(Configuration.unix())) {
             Path tmpDir = Files.createDirectory(fs.getPath("/cgmes"));
             Exporters.export("CGMES", network, null, tmpDir.resolve("tmp"));
             Network n2 = Importers.loadNetwork(new GenericReadOnlyDataSource(tmpDir, "tmp"));
             VoltageLevel c = n2.getVoltageLevel("C");
             assertNull(Networks.getEquivalentTerminal(c, c.getNodeBreakerView().getNode2("TEST_SW")));
+            assertNull(n2.getVscConverterStation("C2").getTerminal().getBusView().getBus());
         }
     }
 }
