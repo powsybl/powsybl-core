48739f92b41d8802f8110ca9ff6475f5ce3b8d81
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2022-04-05 23:17:36 +0200

    Substation and line position extensions
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

---
+++ iidm/iidm-extensions/src/main/java/com/powsybl/iidm/network/extensions/SubstationPosition.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.network.extensions;
+
+import com.powsybl.commons.extensions.Extension;
+import com.powsybl.iidm.network.Substation;
+
+/**
+ * @author Massimo Ferraro <massimo.ferraro@techrain.eu>
+ */
+public interface SubstationPosition extends Extension<Substation> {
+
+    String NAME = "substationPosition";
+
+    @Override
+    default String getName() {
+        return NAME;
+    }
+
+    Coordinate getCoordinate();
+}
---
+++ iidm/iidm-xml-converter/src/main/java/com/powsybl/iidm/xml/extensions/SubstationPositionXmlSerializer.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.xml.extensions;
+
+import com.google.auto.service.AutoService;
+import com.powsybl.commons.extensions.AbstractExtensionXmlSerializer;
+import com.powsybl.commons.extensions.ExtensionXmlSerializer;
+import com.powsybl.commons.xml.XmlReaderContext;
+import com.powsybl.commons.xml.XmlUtil;
+import com.powsybl.commons.xml.XmlWriterContext;
+import com.powsybl.iidm.network.Substation;
+import com.powsybl.iidm.network.extensions.Coordinate;
+import com.powsybl.iidm.network.extensions.SubstationPosition;
+import com.powsybl.iidm.network.extensions.SubstationPositionAdder;
+
+import javax.xml.stream.XMLStreamException;
+
+/**
+ * @author Massimo Ferraro <massimo.ferraro@techrain.eu>
+ */
+@AutoService(ExtensionXmlSerializer.class)
+public class SubstationPositionXmlSerializer extends AbstractExtensionXmlSerializer<Substation, SubstationPosition> {
+
+    public SubstationPositionXmlSerializer() {
+        super(SubstationPosition.NAME, "network", SubstationPosition.class, true, "substationPosition.xsd",
+                "http://www.powsybl.org/schema/iidm/ext/substation_position/1_0", "sp");
+    }
+
+    @Override
+    public void write(SubstationPosition substationPosition, XmlWriterContext context) throws XMLStreamException {
+        context.getWriter().writeEmptyElement(getNamespaceUri(), "coordinate");
+        XmlUtil.writeDouble("longitude", substationPosition.getCoordinate().getLongitude(), context.getWriter());
+        XmlUtil.writeDouble("latitude", substationPosition.getCoordinate().getLatitude(), context.getWriter());
+    }
+
+    @Override
+    public SubstationPosition read(Substation substation, XmlReaderContext context) throws XMLStreamException {
+        Coordinate[] coordinate = new Coordinate[1];
+        XmlUtil.readUntilEndElement(getExtensionName(), context.getReader(), () -> {
+            double longitude = XmlUtil.readDoubleAttribute(context.getReader(), "longitude");
+            double latitude = XmlUtil.readDoubleAttribute(context.getReader(), "latitude");
+            coordinate[0] = new Coordinate(latitude, longitude);
+        });
+        substation.newExtension(SubstationPositionAdder.class)
+                .withCoordinate(coordinate[0])
+                .add();
+        return substation.getExtension(SubstationPositionAdder.class);
+    }
+
+}
