f1e35e4c3385db4f6de3dd25d891ede713cd1278
Florian Dupuy <florian.dupuy@rte-france.com>
2022-05-17 16:29:39 +0200

    Improve coverage
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

---
+++ commons/src/test/java/com/powsybl/commons/config/EmptyPlatformConfigTest.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.commons.config;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Florian Dupuy <florian.dupuy at rte-france.com>
+ */
+public class EmptyPlatformConfigTest {
+
+    @Test
+    public void test() {
+        assertEquals(Optional.empty(), PlatformConfig.defaultConfig().getConfigDir());
+        assertEquals(Optional.empty(), PlatformConfig.defaultConfig().getOptionalModuleConfig("any"));
+        assertEquals("com.powsybl.commons.config.PlatformConfig$EmptyModuleConfigRepository", PlatformConfig.defaultConfig().getRepository().getClass().getName());
+    }
+
+}
