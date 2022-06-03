dd0653fbb0183054b0244790982e5006ab1f5e4e
marqueslanauja <51124986+marqueslanauja@users.noreply.github.com>
2022-06-03 10:34:48 +0200

    CGMES: support additional configuration for HVDC (#2125)
    Signed-off-by: José Antonio Marqués <marquesja@aia.es>
    Signed-off-by: Luma <zamarrenolm@aia.es>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/hvdc/IslandsEnds.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/hvdc/IslandsEnds.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -25,33 +26,71 @@ class IslandsEnds {
         islandsEndsNodes = new ArrayList<>();
     }
 
-    void add(Adjacency adjacency, List<String> islandNodes) {
+    void add(Adjacency adjacency, NodeEquipment nodeEquipment, List<String> islandNodes) {
         if (islandNodes.isEmpty()) {
             return;
         }
         Set<String> visitedNodes = new HashSet<>();
 
         String nodeEnd1 = islandNodes.get(0);
-        List<String> adjacentNodeEnd1 = computeAdjacentNodes(nodeEnd1,
-            adjacency, visitedNodes);
+        List<String> adjacentNodeEnd1 = computeAdjacentNodes(nodeEnd1, adjacency, visitedNodes);
 
-        String nodeEnd2 = getNodeOtherEnd(islandNodes, visitedNodes);
+        String nodeEnd2 = getNodeOtherEnd(nodeEquipment, islandNodes, visitedNodes);
         if (nodeEnd2 == null) {
             return;
         }
-        List<String> adjacentNodeEnd2 = computeAdjacentNodes(nodeEnd2,
-            adjacency, visitedNodes);
+        List<String> adjacentNodeEnd2 = computeAdjacentNodes(nodeEnd2, adjacency, visitedNodes);
+
+        // Consider configurations where DcLinks are only connected to AC network only in one of both substations
+        // In this configuration DcLink1 and DcLink2 are connected in s2 but not in s1,
+        // on this side they are connected in an adjacent substation sn.
+        // Hvdc configuration analysis only considers the sub-network between s1 and s2
+        // so we can have disconnected components in one side that are connected trough the other side
+        //
+        //     --- LN1 --- s1 --- AcDc11 --- DcLink 1 --- AcDc12 ----
+        //  sn |                                                     |- s2
+        //     --- LN2 --- s1 --- AcDc21 --- DcLink 2 --- AcDc22 ----
+        //
+        // Process unassigned components and assign them to the right end.
+        for (String node : islandNodes) {
+            if (visitedNodes.contains(node)) {
+                continue;
+            }
+            List<String> adjacentNodeEnd = computeAdjacentNodes(node, adjacency, visitedNodes);
+            addToRightEnd(nodeEquipment, adjacentNodeEnd1, adjacentNodeEnd2, adjacentNodeEnd);
+        }
 
         IslandEnd islandEnd = new IslandEnd(adjacentNodeEnd1, adjacentNodeEnd2);
-
         islandsEndsNodes.add(islandEnd);
     }
 
-    private static String getNodeOtherEnd(List<String> islandNodes, Set<String> visitedNodes) {
+    private static String getNodeOtherEnd(NodeEquipment nodeEquipment, List<String> islandNodes, Set<String> visitedNodes) {
         return islandNodes.stream()
-            .filter(n -> !visitedNodes.contains(n))
+            .filter(n -> isNodeOtherEnd(nodeEquipment, visitedNodes, n))
             .findFirst()
             .orElse(null);
+    }
+
+    private static boolean isNodeOtherEnd(NodeEquipment nodeEquipment, Set<String> visitedNodes, String node) {
+        if (visitedNodes.contains(node)) {
+            return false;
+        }
+        return visitedNodes.stream().anyMatch(n -> nodeEquipment.existDcLineSegmentBetweenBothNodes(n, node));
+    }
+
+    private static void addToRightEnd(NodeEquipment nodeEquipment, List<String> nodesEnd1, List<String> nodesEnd2, List<String> nodes) {
+        List<String> nodesConnectedToEnd2 = nodes.stream()
+            .filter(n -> nodesEnd2.stream().anyMatch(n2 -> nodeEquipment.existDcLineSegmentBetweenBothNodes(n, n2)))
+            .collect(Collectors.toList());
+        List<String> nodesConnectedToEnd1 = nodes.stream()
+            .filter(n -> nodesEnd1.stream().anyMatch(n1 -> nodeEquipment.existDcLineSegmentBetweenBothNodes(n, n1)))
+            .collect(Collectors.toList());
+
+        if (nodesConnectedToEnd1.isEmpty() && !nodesConnectedToEnd2.isEmpty()) {
+            nodesEnd1.addAll(nodes);
+        } else if (!nodesConnectedToEnd1.isEmpty() && nodesConnectedToEnd2.isEmpty()) {
+            nodesEnd2.addAll(nodes);
+        }
     }
 
     private static List<String> computeAdjacentNodes(String nodeId,
