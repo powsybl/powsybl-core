df3fbf3fc9f34a0d824cea519fe553d4ef46eff2
miovd <miora.ralambotiana@rte-france.com>
2022-06-07 09:37:38 +0200

    Move scalables in iidm-modification (#2157)
    Signed-off-by: VEDELAGO MIORA <miora.ralambotiana@rte-france.com>
    Co-authored-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/scalable/LoadScalable.java
+++ iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/scalable/LoadScalable.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.powsybl.action.util;
+package com.powsybl.iidm.modification.scalable;
 
 import com.powsybl.iidm.network.*;
 import org.slf4j.Logger;
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Objects;
 
-import static com.powsybl.action.util.Scalable.ScalingConvention.*;
+import static com.powsybl.iidm.modification.scalable.Scalable.ScalingConvention.*;
 
 /**
  * @author Ameni Walha <ameni.walha at rte-france.com>
--- iidm/iidm-modification/src/test/java/com/powsybl/iidm/modification/scalable/UpDownScalableTest.java
+++ iidm/iidm-modification/src/test/java/com/powsybl/iidm/modification/scalable/UpDownScalableTest.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.powsybl.action.util;
+package com.powsybl.iidm.modification.scalable;
 
 import com.powsybl.iidm.network.Injection;
 import com.powsybl.iidm.network.Network;
