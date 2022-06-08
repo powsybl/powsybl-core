f2a3a35df8027ef5a25e2f3ce5efecc62b483891
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2022-06-08 18:52:36 +0200

    Fix
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/LoadingLimitsBugTest.java
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/LoadingLimitsBugTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.iidm.xml;
 
 import com.powsybl.commons.AbstractConverterTest;
@@ -8,6 +14,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+/**
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
 public class LoadingLimitsBugTest extends AbstractConverterTest {
 
     @Test
