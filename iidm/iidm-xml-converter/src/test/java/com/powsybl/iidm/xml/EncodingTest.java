96321968d47e551062f96821bcd1e6cac1108b20
lunaria <goro27@hotmail.fr>
2022-06-15 09:05:26 +0200

    fixing Windows build - using xml comparing
    Signed-off-by: lunaria <goro27@hotmail.fr>

--- iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/EncodingTest.java
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/EncodingTest.java
@@ -1,40 +1,37 @@
 package com.powsybl.iidm.xml;
 
+import com.powsybl.commons.AbstractConverterTest;
 import com.powsybl.iidm.export.ExportOptions;
 import com.powsybl.iidm.network.Network;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 public class EncodingTest {
 
-    public static final Path XML_FILE_PATH_ENCODE_ISO_8859_1 = Paths.get("src/test/resources/encoding/network.xml");
-
     @Test
     public void testEncodingISO88591ToISO88591() throws IOException {
-        Network network = NetworkXml.read(XML_FILE_PATH_ENCODE_ISO_8859_1);
+        Network network = NetworkXml.read(getClass().getResourceAsStream("/encoding/network.xml"));
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ExportOptions options = new ExportOptions();
         options.setCharset(StandardCharsets.ISO_8859_1);
         NetworkXml.write(network, options, baos);
-        assertEquals(new String(Files.readAllBytes(XML_FILE_PATH_ENCODE_ISO_8859_1)), baos.toString());
+        InputStream is = new ByteArrayInputStream(baos.toByteArray());
+        AbstractConverterTest.compareXml(getClass().getResourceAsStream("/encoding/network.xml"), is);
     }
 
     @Test
     public void testEncodingISO88591ToUTF8() throws IOException {
-        Network network = NetworkXml.read(XML_FILE_PATH_ENCODE_ISO_8859_1);
+        Network network = NetworkXml.read(getClass().getResourceAsStream("/encoding/network.xml"));
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ExportOptions options = new ExportOptions();
         options.setCharset(StandardCharsets.UTF_8);
         NetworkXml.write(network, options, baos);
-        assertNotEquals(new String(Files.readAllBytes(XML_FILE_PATH_ENCODE_ISO_8859_1)), baos.toString());
+        InputStream is = new ByteArrayInputStream(baos.toByteArray());
+        AbstractConverterTest.compareXml(getClass().getResourceAsStream("/encoding/network.xml"), is);
     }
 }
