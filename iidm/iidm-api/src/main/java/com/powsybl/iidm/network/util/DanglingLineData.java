4af5f1425e7c31570639ad50747a5995d547158e
José Antonio Marqués <marquesja@aia.es>
2022-06-15 18:15:41 +0200

    minor changes
    Signed-off-by: José Antonio Marqués <marquesja@aia.es>

--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/util/DanglingLineData.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/util/DanglingLineData.java
@@ -21,33 +21,15 @@ import java.util.Objects;
  */
 public class DanglingLineData {
 
-    private final String id;
+    private final DanglingLine danglingLine;
 
-    private final double r;
-    private final double x;
-    private final double g1;
-    private final double g2;
-    private final double b1;
-    private final double b2;
-
-    private final double u1;
-    private final double theta1;
-    private final double p0;
-    private final double q0;
-    private final double targetP;
-    private final double targetQ;
-
-    // flow at the network side
-    double p;
-    double q;
-    // Voltage and angle at the boundary side
     double boundaryBusU;
     double boundaryBusTheta;
-    // Flow at the boundary side
+
     double boundaryP;
     double boundaryQ;
-
-    private final DanglingLine danglingLine;
+    double p;
+    double q;
 
     public DanglingLineData(DanglingLine danglingLine) {
         this(danglingLine, true);
@@ -56,34 +38,20 @@ public class DanglingLineData {
     public DanglingLineData(DanglingLine danglingLine, boolean splitShuntAdmittance) {
         this.danglingLine = Objects.requireNonNull(danglingLine);
 
-        id = danglingLine.getId();
-        r = danglingLine.getR();
-        x = danglingLine.getX();
-        g1 = splitShuntAdmittance ? danglingLine.getG() * 0.5 : danglingLine.getG();
-        b1 = splitShuntAdmittance ? danglingLine.getB() * 0.5 : danglingLine.getB();
-        g2 = splitShuntAdmittance ? danglingLine.getG() * 0.5 : 0.0;
-        b2 = splitShuntAdmittance ? danglingLine.getB() * 0.5 : 0.0;
-        p0 = danglingLine.getP0();
-        q0 = danglingLine.getQ0();
         Optional<DanglingLine.Generation> generation = Optional.ofNullable(danglingLine.getGeneration());
-        if (generation.isPresent()) {
-            targetP = generation.get().getTargetP();
-            targetQ = generation.get().getTargetQ();
-        } else {
-            targetP = 0.0;
-            targetQ = 0.0;
-        }
+        double targetP = generation.isPresent() ? generation.get().getTargetP() : 0.0;
+        double targetQ = generation.isPresent() ? generation.get().getTargetQ() : 0.0;
         // Flow at the boundary side
-        boundaryP = -(p0 - targetP);
-        boundaryQ = -(q0 - targetQ);
+        boundaryP = -(danglingLine.getP0() - targetP);
+        boundaryQ = -(danglingLine.getQ0() - targetQ);
 
         boolean isZ0 = isZ0(danglingLine);
         Complex pq = networkPQZ0(danglingLine, isZ0);
         p = pq.getReal();
         q = pq.getImaginary();
 
-        u1 = getV(danglingLine);
-        theta1 = getTheta(danglingLine);
+        double u1 = getV(danglingLine);
+        double theta1 = getTheta(danglingLine);
 
         if (!valid(u1, theta1)) {
             boundaryBusU = Double.NaN;
@@ -93,27 +61,33 @@ public class DanglingLineData {
 
         Complex v1 = ComplexUtils.polar2Complex(u1, theta1);
 
-        Complex vBoundaryBus = boundaryVoltageAndAngle(v1, isZ0);
+        Complex vBoundaryBus = boundaryVoltageAndAngle(v1, isZ0, splitShuntAdmittance);
         boundaryBusU = vBoundaryBus.abs();
         boundaryBusTheta = vBoundaryBus.getArgument();
 
-        pq = networkPQ(danglingLine, v1, vBoundaryBus, isZ0);
+        pq = networkPQ(danglingLine, v1, vBoundaryBus, isZ0, splitShuntAdmittance);
         p = pq.getReal();
         q = pq.getImaginary();
     }
 
-    private Complex boundaryVoltageAndAngle(Complex v1, boolean isZ0) {
+    private Complex boundaryVoltageAndAngle(Complex v1, boolean isZ0, boolean splitShuntAdmittance) {
+
+        double g1 = splitShuntAdmittance ? danglingLine.getG() * 0.5 : danglingLine.getG();
+        double b1 = splitShuntAdmittance ? danglingLine.getB() * 0.5 : danglingLine.getB();
+        double g2 = splitShuntAdmittance ? danglingLine.getG() * 0.5 : 0.0;
+        double b2 = splitShuntAdmittance ? danglingLine.getB() * 0.5 : 0.0;
+
         Complex vBoundaryBus = new Complex(Double.NaN, Double.NaN);
         if (isZ0) {
             vBoundaryBus = v1;
         } else if (boundaryP == 0.0 && boundaryQ == 0.0) {
-            LinkData.BranchAdmittanceMatrix adm = LinkData.calculateBranchAdmittance(r, x, 1.0, 0.0, 1.0, 0.0, new Complex(g1, b1), new Complex(g2, b2));
+            LinkData.BranchAdmittanceMatrix adm = LinkData.calculateBranchAdmittance(danglingLine.getR(), danglingLine.getX(), 1.0, 0.0, 1.0, 0.0, new Complex(g1, b1), new Complex(g2, b2));
             vBoundaryBus = adm.y21().multiply(v1).negate().divide(adm.y22());
         } else {
 
             // Two buses Loadflow
             Complex sBoundary = new Complex(boundaryP, boundaryQ);
-            Complex ytr = new Complex(r, x).reciprocal();
+            Complex ytr = new Complex(danglingLine.getR(), danglingLine.getX()).reciprocal();
 
             Complex ysh2 = new Complex(g2, b2);
             Complex zt = ytr.add(ysh2).reciprocal();
@@ -144,12 +118,17 @@ public class DanglingLineData {
         return new Complex(networkP, networkQ);
     }
 
-    private Complex networkPQ(DanglingLine danglingLine, Complex vNetwork, Complex vBoundary, boolean isZ0) {
+    private Complex networkPQ(DanglingLine danglingLine, Complex vNetwork, Complex vBoundary, boolean isZ0, boolean splitShuntAdmittance) {
         double networkP = Double.NaN;
         double networkQ = Double.NaN;
 
+        double g1 = splitShuntAdmittance ? danglingLine.getG() * 0.5 : danglingLine.getG();
+        double b1 = splitShuntAdmittance ? danglingLine.getB() * 0.5 : danglingLine.getB();
+        double g2 = splitShuntAdmittance ? danglingLine.getG() * 0.5 : 0.0;
+        double b2 = splitShuntAdmittance ? danglingLine.getB() * 0.5 : 0.0;
+
         if ((Double.isNaN(danglingLine.getTerminal().getP()) || Double.isNaN(danglingLine.getTerminal().getQ())) && !isZ0) {
-            LinkData.BranchAdmittanceMatrix adm = LinkData.calculateBranchAdmittance(r, x, 1.0, 0.0, 1.0, 0.0, new Complex(g1, b1), new Complex(g2, b2));
+            LinkData.BranchAdmittanceMatrix adm = LinkData.calculateBranchAdmittance(danglingLine.getR(), danglingLine.getX(), 1.0, 0.0, 1.0, 0.0, new Complex(g1, b1), new Complex(g2, b2));
             Complex s1 = adm.y11().multiply(vNetwork).add(adm.y12().multiply(vBoundary)).multiply(vNetwork.conjugate()).conjugate();
             networkP = s1.getReal();
             networkQ = s1.getImaginary();
