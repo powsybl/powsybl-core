9c7dcadff01acbdb2cc64dfb20346aae75058460
miovd <miora.ralambotiana@rte-france.com>
2022-06-28 12:59:17 +0200

    Limit getters return optionals (#2169)
    Signed-off-by: VEDELAGO MIORA <miora.ralambotiana@rte-france.com>

--- ucte/ucte-converter/src/test/java/com/powsybl/ucte/converter/UcteImporterTest.java
+++ ucte/ucte-converter/src/test/java/com/powsybl/ucte/converter/UcteImporterTest.java
@@ -122,8 +122,8 @@ public class UcteImporterTest {
         assertEquals("ESNODE11 XXNODE11 1 + FRNODE11 XXNODE11 1", l.getId());
         MergedXnode mergedXnode = l.getExtension(MergedXnode.class);
         assertNotNull(mergedXnode);
-        assertNotNull(l.getCurrentLimits1());
-        assertNotNull(l.getCurrentLimits2());
+        assertTrue(l.getCurrentLimits1().isPresent());
+        assertTrue(l.getCurrentLimits2().isPresent());
     }
 
     @Test
