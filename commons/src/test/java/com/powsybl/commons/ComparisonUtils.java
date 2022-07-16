34c3ec80f287cfa75c5092ea02f51058ff8adbad
sfid98 <s.fidanza@studenti.poliba.it>
2022-07-16 15:39:17 +0200

    Change date copyright
    Signed-off-by: sfid98 <s.fidanza@studenti.poliba.it>

--- commons/src/test/java/com/powsybl/commons/ComparisonUtils.java
+++ commons/src/test/java/com/powsybl/commons/ComparisonUtils.java
@@ -11,7 +11,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 /**
- * Copyright (c) 2021, Stanislao Fidanza
+ * Copyright (c) 2022, Stanislao Fidanza
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
