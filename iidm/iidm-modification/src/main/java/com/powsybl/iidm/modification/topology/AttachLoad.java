80dc8e127ffe9d63a4eb3060384a0b81216cde04
Anne Tilloy <anne.tilloy@rte-france.com>
2022-07-25 09:25:03 +0200

    Proposal.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/topology/AttachLoad.java
+++ iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/topology/AttachLoad.java
@@ -20,16 +20,14 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static com.powsybl.iidm.modification.topology.TopologyModificationUtils.*;
 
 /**
  * This method adds a new load on an existing voltage level. The voltage level should be described
- * in node/breaker.
+ * in node/breaker topology.
  * @author Coline Piloquet <coline.piloquet at rte-france.com>
  */
 public class AttachLoad implements NetworkModification {
@@ -38,16 +36,28 @@ public class AttachLoad implements NetworkModification {
 
     private final LoadAdder loadAdder;
     private final String voltageLevelId;
-    private String bbsId; //Id of the busBar where the switch will be closed
-
-    // Position of the load for the connectablePosition extension
-    private final int loadPositionOrder;
+    private String bbsId;
+    private int loadPositionOrder;
+    private PositionInsideSection loadPositionInsideSection = PositionInsideSection.SPECIFIC;
     private final ConnectablePosition.Direction loadDirection;
 
-    public AttachLoad(LoadAdder loadAdder, String voltageLevelId, String bbsId, int loadPositionOrder) {
-        this(loadAdder, voltageLevelId, bbsId, loadPositionOrder, ConnectablePosition.Direction.BOTTOM);
+    public enum PositionInsideSection {
+        FIRST,
+        LAST,
+        SPECIFIC
     }
 
+    /**
+     * Constructor.
+     *
+     * @param loadAdder                The load adder.
+     * @param voltageLevelId           The voltage level with the given ID that we want to connect to the initial line.
+     * @param bbsId                    The ID of the existing bus bar section of the voltage level voltageLevelId where we want to connect the load.
+     *                                 This switch will be closed.
+     *                                 Please note that there will be switches between this bus or bus bar section and the connection point of the line.
+     * @param loadPositionOrder        The order of the load to be attached from its extension {@link com.powsybl.iidm.network.extensions.ConnectablePosition}.
+     * @param loadDirection            The direction of the load to be attached from its extension {@link com.powsybl.iidm.network.extensions.ConnectablePosition}.
+     */
     public AttachLoad(LoadAdder loadAdder, String voltageLevelId, String bbsId, int loadPositionOrder, ConnectablePosition.Direction loadDirection) {
         this.loadAdder = loadAdder;
         this.voltageLevelId = voltageLevelId;
@@ -56,15 +66,59 @@ public class AttachLoad implements NetworkModification {
         this.loadDirection = loadDirection;
     }
 
-    public AttachLoad(LoadAdder loadAdder, String voltageLevelId, int loadPositionOrder) {
-        this(loadAdder, voltageLevelId, loadPositionOrder, ConnectablePosition.Direction.BOTTOM);
+    public AttachLoad(LoadAdder loadAdder, String voltageLevelId, String bbsId, int loadPositionOrder) {
+        this(loadAdder, voltageLevelId, bbsId, loadPositionOrder, ConnectablePosition.Direction.BOTTOM);
     }
 
-    public AttachLoad(LoadAdder loadAdder, String voltageLevelId, int loadPositionOrder, ConnectablePosition.Direction loadDirection) {
+    /**
+     * Constructor.
+     *
+     * @param loadAdder                The load adder.
+     * @param voltageLevelId           The voltage level with the given ID that we want to connect to the initial line.
+     * @param bbsId                    The ID of the existing bus bar section of the voltage level voltageLevelId where we want to connect the load.
+     *                                 Please note that there will be switches between this bus or bus bar section and the connection point of the line.
+     * @param loadPositionInsideSection  The load position inside the section bbsId, only {@link PositionInsideSection} FIRST and {@link PositionInsideSection} LAST are supported here.
+     * @param loadDirection            The direction of the load to be attached from its extension {@link com.powsybl.iidm.network.extensions.ConnectablePosition}.
+     */
+    public AttachLoad(LoadAdder loadAdder, String voltageLevelId, String bbsId, PositionInsideSection loadPositionInsideSection, ConnectablePosition.Direction loadDirection) {
         this.loadAdder = loadAdder;
         this.voltageLevelId = voltageLevelId;
-        this.loadPositionOrder = loadPositionOrder;
+        this.bbsId = bbsId;
+        if (loadPositionInsideSection == PositionInsideSection.SPECIFIC) {
+            throw new PowsyblException("Load position inside section SPECIFIC is not compatible with this constructor");
+        }
+        this.loadPositionInsideSection = loadPositionInsideSection;
         this.loadDirection = loadDirection;
+    }
+
+    public AttachLoad(LoadAdder loadAdder, String voltageLevelId, String bbsId, PositionInsideSection loadPositionInsideSection) {
+        this(loadAdder, voltageLevelId, bbsId, loadPositionInsideSection, ConnectablePosition.Direction.BOTTOM);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param loadAdder                The load adder.
+     * @param voltageLevelId           The voltage level with the given ID that we want to connect to the initial line. The load will be connected on the first bus bar section.
+     * @param loadPositionInsideSection  The load position inside the section bbsId, only {@link PositionInsideSection} FIRST and {@link PositionInsideSection} LAST are supported here.
+     * @param loadDirection            The direction of the load to be attached from its extension {@link com.powsybl.iidm.network.extensions.ConnectablePosition}.
+     */
+    public AttachLoad(LoadAdder loadAdder, String voltageLevelId, PositionInsideSection loadPositionInsideSection, ConnectablePosition.Direction loadDirection) {
+        this.loadAdder = loadAdder;
+        this.voltageLevelId = voltageLevelId;
+        if (loadPositionInsideSection == PositionInsideSection.SPECIFIC) {
+            throw new PowsyblException("Load position inside section SPECIFIC is not compatible with this constructor");
+        }
+        this.loadPositionInsideSection = loadPositionInsideSection;
+        this.loadDirection = loadDirection;
+    }
+
+    public AttachLoad(LoadAdder loadAdder, String voltageLevelId, PositionInsideSection loadPositionInsideSection) {
+        this(loadAdder, voltageLevelId, loadPositionInsideSection, ConnectablePosition.Direction.BOTTOM);
+    }
+
+    public AttachLoad(LoadAdder loadAdder, String voltageLevelId) {
+        this(loadAdder, voltageLevelId, PositionInsideSection.FIRST, ConnectablePosition.Direction.BOTTOM);
     }
 
     public LoadAdder getLoadAdder() {
@@ -83,7 +137,6 @@ public class AttachLoad implements NetworkModification {
         this.bbsId = bbsId;
     }
 
-    // TODO : javadoc
     public int getLoadPositionOrder() {
         return loadPositionOrder;
     }
@@ -98,30 +151,22 @@ public class AttachLoad implements NetworkModification {
         createNodeBreakerSwitches(loadNode, forkNode, bbsNode, loadId, voltageLevel.getNodeBreakerView());
         BusbarSectionPosition position = bbs.getExtension(BusbarSectionPosition.class);
         if (position == null) {
-            LOGGER.warn("No busbar section position extension found on {}, only one disconnector is created.", bbs.getId());
+            LOGGER.warn("No bus bar section position extension found on {}, only one disconnector is created.", bbs.getId());
             reporter.report(Report.builder()
                     .withKey("noBusbarSectionPositionExtension")
-                    .withDefaultMessage("No busbar section position extension found on ${busbarSectionId}, only one disconnector is created")
-                    .withValue("busbarSectionId", bbs.getId())
+                    .withDefaultMessage("No bus bar section position extension found on ${bbsId}, only one disconnector is created")
+                    .withValue("bbsId", bbs.getId())
                     .withSeverity(TypedValue.WARN_SEVERITY)
                     .build());
         } else {
-            createTopologyFromBusbarList(voltageLevel, forkNode, loadId, voltageLevel.getNodeBreakerView().getBusbarSectionStream()
+            createTopologyFromBusbarSectionList(voltageLevel, forkNode, loadId, voltageLevel.getNodeBreakerView().getBusbarSectionStream()
                     .filter(b -> b.getExtension(BusbarSectionPosition.class) != null)
                     .filter(b -> b.getExtension(BusbarSectionPosition.class).getSectionIndex() == position.getSectionIndex())
                     .filter(b -> !b.getId().equals(bbsId)));
         }
-        voltageLevel.getNodeBreakerView().getBusbarSectionStream().forEach(busbarSection -> {
-            if (busbarSection.getExtension(BusbarSectionPosition.class).getSectionIndex() == position.getSectionIndex() && !busbarSection.getId().equals(bbsId)) {
-                createNBDisconnector(forkNode, busbarSection.getTerminal().getNodeBreakerView().getNode(),
-                        String.valueOf(busbarSection.getId()), loadId, voltageLevel.getNodeBreakerView(), true);
-            }
-        });
-
     }
 
-    private void createTopologyFromBusbarList(VoltageLevel voltageLevel/*, int loadNode*/, int forkNode, String loadId, Stream<BusbarSection> bbsStream) {
-        // createNBBreaker(loadNode, forkNode, "", loadId, voltageLevel.getNodeBreakerView(), false);
+    private void createTopologyFromBusbarSectionList(VoltageLevel voltageLevel, int forkNode, String loadId, Stream<BusbarSection> bbsStream) {
         bbsStream.forEach(b -> {
             int bbsNode = b.getTerminal().getNodeBreakerView().getNode();
             createNBDisconnector(forkNode, bbsNode, String.valueOf(bbsNode), loadId, voltageLevel.getNodeBreakerView(), true);
@@ -138,62 +183,57 @@ public class AttachLoad implements NetworkModification {
         apply(network, false, Reporter.NO_OP);
     }
 
-    // TODO To move into a utility class
-    private static void throwExceptionOrLogError(String message, String key, boolean throwException, Reporter reporter) {
-        LOGGER.error(message);
-        reporter.report(Report.builder()
-                .withKey(key)
-                .withDefaultMessage(message)
-                .withSeverity(TypedValue.ERROR_SEVERITY)
-                .build());
-        if (throwException) {
-            throw new PowsyblException(message);
-        }
-    }
-
     @Override
     public void apply(Network network, boolean throwException, Reporter reporter) {
         VoltageLevel voltageLevel = network.getVoltageLevel(voltageLevelId);
-        BusbarSection bbs = null;
+        TopologyKind topologyKind = voltageLevel.getTopologyKind();
         if (voltageLevel == null) {
             throwExceptionOrLogError(String.format("Voltage level %s is not found", voltageLevelId), "missingVoltageLevel", throwException, reporter);
             return;
         }
-
-        TopologyKind topologyKind = voltageLevel.getTopologyKind();
         if (topologyKind != TopologyKind.NODE_BREAKER) {
             throwExceptionOrLogError(String.format("Voltage level %s is not in node/breaker.", voltageLevelId), "notNodeBreakerVoltageLevel", throwException, reporter);
             return;
         }
 
+        BusbarSection bbs = null;
         if (bbsId != null) {
             bbs = network.getBusbarSection(bbsId);
             if (bbs == null) {
-                throwExceptionOrLogError(String.format("Busbar section %s not found.", bbsId), "notFoundBusbarSection", throwException, reporter);
+                throwExceptionOrLogError(String.format("Bus bar section %s not found.", bbsId), "notFoundBusbarSection", throwException, reporter);
                 return;
             }
             if (bbs.getTerminal().getVoltageLevel() != voltageLevel) {
-                throwExceptionOrLogError(String.format("Busbar section %s is not in voltageLevel %s", bbsId, voltageLevelId), "busbarSectionNotInVoltageLevel", throwException, reporter);
+                throwExceptionOrLogError(String.format("Bus bar section %s is not in voltageLevel %s", bbsId, voltageLevelId), "busbarSectionNotInVoltageLevel", throwException, reporter);
                 return;
             }
         }
 
         if (bbs == null) {
             bbs = voltageLevel.getNodeBreakerView().getBusbarSectionStream().findFirst().orElse(null);
-            bbsId = bbs.getId();
             if (bbs == null) {
-                throwExceptionOrLogError(String.format("Voltage level %s has no busbar section.", voltageLevelId), "noBusbarSectionInVoltageLevel", throwException, reporter);
+                throwExceptionOrLogError(String.format("Voltage level %s has no bus bar section.", voltageLevelId), "noBusbarSectionInVoltageLevel", throwException, reporter);
                 return;
             }
+            bbsId = bbs.getId();
         }
-
-        //TODO: Add check on ConnectablePosition : ask Florian if the method already exists
 
         int loadNode = voltageLevel.getNodeBreakerView().getMaximumNodeIndex() + 1;
         int forkNode = loadNode + 1;
         loadAdder.setNode(loadNode);
         Load load = loadAdder.add();
         String loadId = load.getId();
+
+        if (loadPositionInsideSection != PositionInsideSection.SPECIFIC) {
+            if (loadPositionInsideSection == PositionInsideSection.FIRST) {
+                loadPositionOrder = 0; // FIXME: should be computed.
+            } else if (loadPositionInsideSection == PositionInsideSection.LAST) {
+                loadPositionOrder = Integer.MAX_VALUE; // FIXME: should be computed.
+            }
+        }
+
+        // TODO: add a check for SPECIFIC position inside section.
+
         load.newExtension(ConnectablePositionAdder.class)
                 .newFeeder()
                     .withDirection(loadDirection)
@@ -202,9 +242,8 @@ public class AttachLoad implements NetworkModification {
                 .add()
                 .add();
 
-        //Create switches and a breaker linking the load to the busbar sections
+        // create switches and a breaker linking the load to the bus bar sections.
         createTopologyAutomatically(network, voltageLevel, loadNode, forkNode, loadId, reporter);
-
     }
 
     public static List<Pair<String, Integer>> getFeederPositions(VoltageLevel voltageLevel) {
@@ -268,5 +307,17 @@ public class AttachLoad implements NetworkModification {
             }
         });
         return feederPositionsOrders;
+    }
+
+    private static void throwExceptionOrLogError(String message, String key, boolean throwException, Reporter reporter) {
+        LOGGER.error(message);
+        reporter.report(Report.builder()
+                .withKey(key)
+                .withDefaultMessage(message)
+                .withSeverity(TypedValue.ERROR_SEVERITY)
+                .build());
+        if (throwException) {
+            throw new PowsyblException(message);
+        }
     }
 }
