40a40c1eb5fdc5edf9405ddd004de8642e02d55d
Coline PILOQUET <coline.piloquet@rte-france.com>
2022-07-25 11:19:59 +0200

    Add builder
    Signed-off-by: Coline PILOQUET <coline.piloquet@rte-france.com>

---
+++ iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/topology/AttachLoadBuilder.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.iidm.modification.topology;
+
+import com.powsybl.iidm.network.LoadAdder;
+import com.powsybl.iidm.network.extensions.ConnectablePosition;
+
+/**
+ * @author Coline Piloquet <coline.piloquet at rte-france.com>
+ */
+public class AttachLoadBuilder {
+
+    private LoadAdder loadAdder = null;
+    private String voltageLevelId = null;
+    private String bbsId = null;
+
+    private Integer loadPositionOrder = null;
+    private ConnectablePosition.Direction loadDirection = ConnectablePosition.Direction.BOTTOM;
+
+    public AttachLoad build() {
+        return new AttachLoad(loadAdder, voltageLevelId, bbsId, loadPositionOrder, loadDirection);
+    }
+
+    public AttachLoadBuilder withLoadAdder(LoadAdder loadAdder) {
+        this.loadAdder = loadAdder;
+        return this;
+    }
+
+    public AttachLoadBuilder withVoltageLevelId(String voltageLevelId) {
+        this.voltageLevelId = voltageLevelId;
+        return this;
+    }
+
+    public AttachLoadBuilder withBbsId(String bbsId) {
+        this.bbsId = bbsId;
+        return this;
+    }
+
+    public AttachLoadBuilder withLoadPositionOrder(int loadPositionOrder) {
+        this.loadPositionOrder = loadPositionOrder;
+        return this;
+    }
+
+    public AttachLoadBuilder withLoadDirection(ConnectablePosition.Direction loadDirection) {
+        this.loadDirection = loadDirection;
+        return this;
+    }
+}
