104ceb810a7bb1ffdf778fe4b5aff5e63c2e7af0
Luma <zamarrenolm@aia.es>
2022-08-19 16:47:23 +0200

    CGMES: terminal sequenceNumber can be optional
    Signed-off-by: Luma <zamarrenolm@aia.es>

--- cgmes/cgmes-model/src/test/java/com/powsybl/cgmes/model/test/cim14/Cim14SmallCasesCatalog.java
+++ cgmes/cgmes-model/src/test/java/com/powsybl/cgmes/model/test/cim14/Cim14SmallCasesCatalog.java
@@ -60,6 +60,17 @@ public final class Cim14SmallCasesCatalog {
                                            "ENTSO-E_Boundary_Set_EU_TP.xml"));
     }
 
+    public static TestGridModelResources m7busesNoSequenceNumbers() {
+        return new TestGridModelResources("7buses-no-sequenceNumbers", expectedM7Buses(),
+                new ResourceSet("/cim14/m7buses-no-sequenceNumbers/",
+                        "m7buses-no-sequenceNumbers_EQ.xml",
+                        "m7buses-no-sequenceNumbers_TP.xml"),
+                new ResourceSet("/cim14/m7buses/",
+                        "m7buses_SV.xml"),
+                new ResourceSet("/cim14/", "ENTSO-E_Boundary_Set_EU_EQ.xml",
+                        "ENTSO-E_Boundary_Set_EU_TP.xml"));
+    }
+
     public static TestGridModelResources ieee14() {
         return new TestGridModelResources("ieee14", expectedIeee14(),
                 new ResourceSet("/cim14/ieee14/", "ieee14bus_EQ.xml",
