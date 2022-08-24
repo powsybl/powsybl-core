e19fbb19d42dec6d3fcde0d6130996e5ca96d2ba
Luma <zamarrenolm@aia.es>
2022-08-24 14:51:32 +0200

    minor refactoring
    Signed-off-by: Luma <zamarrenolm@aia.es>

--- powerfactory/powerfactory-converter/src/main/java/com/powsybl/powerfactory/converter/HvdcConverter.java
+++ powerfactory/powerfactory-converter/src/main/java/com/powsybl/powerfactory/converter/HvdcConverter.java
@@ -6,22 +6,16 @@
  */
 package com.powsybl.powerfactory.converter;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import com.powsybl.iidm.network.HvdcLineAdder;
-import com.powsybl.iidm.network.Network;
-import com.powsybl.iidm.network.VoltageLevel;
-import com.powsybl.iidm.network.VscConverterStation;
-import com.powsybl.iidm.network.VscConverterStationAdder;
 import com.powsybl.iidm.network.HvdcLine.ConvertersMode;
+import com.powsybl.iidm.network.*;
 import com.powsybl.powerfactory.converter.PowerFactoryImporter.ImportContext;
 import com.powsybl.powerfactory.model.DataObject;
 import com.powsybl.powerfactory.model.DataObjectRef;
 import com.powsybl.powerfactory.model.PowerFactoryException;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Luma Zamarreño <zamarrenolm at aia.es>
@@ -30,160 +24,132 @@ import com.powsybl.powerfactory.model.PowerFactoryException;
 
 class HvdcConverter extends AbstractConverter {
 
-    private final List<HvdcConfiguration> hvdcConfigurations = new ArrayList<>();
+    private final Map<DataObject, List<DataObject>> elmTermsConnectedToVscs = new HashMap<>();
+    private final List<Configuration> configurations = new ArrayList<>();
+    private final Set<DataObject> dcElmLnes = new HashSet<>();
+    private final Set<DataObject> dcElmTerms = new HashSet<>();
+    private final Set<DataObject> usedVscs = new HashSet<>();
 
     HvdcConverter(ImportContext importContext, Network network) {
         super(importContext, network);
     }
 
-    List<HvdcConfiguration> get() {
-        return hvdcConfigurations;
-    }
-
     boolean isDcLink(DataObject elmLne) {
-        return hvdcConfigurations.stream().anyMatch(hvdcConfiguration -> isDcLink(hvdcConfiguration, elmLne));
-    }
-
-    private static boolean isDcLink(HvdcConfiguration hvdcConfiguration, DataObject elmLne) {
-        return hvdcConfiguration.dcLnes.stream().anyMatch(hvdc -> hvdc.elmLne.equals(elmLne));
+        return dcElmLnes.contains(elmLne);
     }
 
     boolean isDcNode(DataObject elmTerm) {
-        return hvdcConfigurations.stream().anyMatch(hvdcConfiguration -> isDcNode(hvdcConfiguration.dcLnes, elmTerm));
+        return dcElmTerms.contains(elmTerm);
     }
 
-    void createConfigurations(List<DataObject> elmTerms, List<DataObject> elmVscs) {
+    void computeConfigurations(List<DataObject> elmTerms, List<DataObject> elmVscs) {
         if (elmVscs.isEmpty()) {
             return;
         }
-        List<DataObject> elmTermsConnectedToAnyVsc = findElmTermsConnectedToAnyVsc(elmTerms, elmVscs);
+        computeElmTermsConnectedToVscs(elmTerms, elmVscs);
         for (DataObject elmVsc : elmVscs) {
-            if (isUsedInPreviousConfiguration(elmVsc, hvdcConfigurations)) {
-                continue;
-            }
-            Optional<HvdcConfiguration> hvdcConfiguration = createHvdcConfiguration(elmVsc, elmTermsConnectedToAnyVsc);
-            if (hvdcConfiguration.isPresent()) {
-                hvdcConfigurations.add(hvdcConfiguration.get());
+            if (!usedVscs.contains(elmVsc)) {
+                computeConfiguration(elmVsc).ifPresent(this::addConfiguration);
             }
         }
     }
 
-    private static List<DataObject> findElmTermsConnectedToAnyVsc(List<DataObject> elmTerms, List<DataObject> elmVscs) {
-        return elmTerms.stream().filter(elmTerm -> isElmTermConnectedToAnyVsc(elmTerm, elmVscs)).collect(Collectors.toList());
+    private void computeElmTermsConnectedToVscs(List<DataObject> elmTerms, List<DataObject> elmVscs) {
+        for (DataObject elmTerm : elmTerms) {
+            getDataObjectsConnectedToElmTerm(elmTerm)
+                    .filter(elmVscs::contains).findFirst()
+                    .ifPresent(elmVsc -> elmTermsConnectedToVscs.computeIfAbsent(elmVsc, k -> new ArrayList<>()).add(elmTerm));
+        }
     }
 
-    private static List<DataObject> findElmTermsConnectedToVsc(List<DataObject> elmTerms, DataObject elmVsc) {
-        return elmTerms.stream().filter(elmTerm -> isElmTermConnectedToVsc(elmTerm, elmVsc)).collect(Collectors.toList());
+    private void addConfiguration(Configuration c) {
+        configurations.add(c);
+        dcElmLnes.addAll(c.getElmLnes());
+        dcElmTerms.addAll(c.getElmTerms());
+        usedVscs.add(c.vsc0);
+        usedVscs.add(c.vsc1);
     }
 
-    private static boolean isElmTermConnectedToVsc(DataObject elmTerm, DataObject elmVsc) {
-        return isElmTermConnectedToAnyVsc(elmTerm, Collections.singletonList(elmVsc));
-    }
-
-    private static boolean isElmTermConnectedToAnyVsc(DataObject elmTerm, List<DataObject> elmVscs) {
-        return getDataObjectsConnectedToElmTerm(elmTerm).stream().anyMatch(elmVscs::contains);
-    }
-
-    private static List<DataObject> getDataObjectsConnectedToElmTerm(DataObject elmTerm) {
+    private static Stream<DataObject> getDataObjectsConnectedToElmTerm(DataObject elmTerm) {
         return elmTerm.getChildren().stream()
-            .map(staCubic -> staCubic.findObjectAttributeValue(DataAttributeNames.OBJ_ID).flatMap(DataObjectRef::resolve))
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .collect(Collectors.toList());
+                .map(staCubic -> staCubic.findObjectAttributeValue(DataAttributeNames.OBJ_ID)
+                        .flatMap(DataObjectRef::resolve))
+                .flatMap(Optional::stream);
     }
 
-    private static boolean isUsedInPreviousConfiguration(DataObject elmVsc, List<HvdcConfiguration> hvdcConfigurations) {
-        return hvdcConfigurations.stream().anyMatch(hvdcConfiguration -> hvdcConfiguration.vsc0.equals(elmVsc)
-            || hvdcConfiguration.vsc1.equals(elmVsc));
-    }
-
-    private static Optional<HvdcConfiguration> createHvdcConfiguration(DataObject elmVsc, List<DataObject> elmTermsConnectedToAnyVsc) {
-        List<HvdcLne> hvdcLnes = hvdcLinksConnectedToVsc(elmTermsConnectedToAnyVsc, elmVsc);
-        Optional<DataObject> otherVsc = otherVsc(elmTermsConnectedToAnyVsc, hvdcLnes);
-
-        if (otherVsc.isEmpty()) {
-            return Optional.empty();
+    private Optional<Configuration> computeConfiguration(DataObject elmVsc0) {
+        List<DCLink> links = computeDcLinksConnectedToVsc(elmVsc0);
+        Optional<DataObject> elmVsc1 = findVsc1(links);
+        if (elmVsc1.isPresent()) {
+            List<DataObject> configurationDcElmTerms = links.stream().flatMap(l -> Stream.of(l.elmTerm0, l.elmTerm1)).collect(Collectors.toList());
+            Optional<DataObject> elmTermAc0 = findAcElmTerm(elmVsc0, configurationDcElmTerms);
+            if (elmTermAc0.isPresent()) {
+                Optional<DataObject> elmTermAc1 = findAcElmTerm(elmVsc1.get(), configurationDcElmTerms);
+                if (elmTermAc1.isPresent()) {
+                    return Optional.of(new Configuration(elmTermAc0.get(), elmVsc0, links, elmTermAc1.get(), elmVsc1.get()));
+                }
+            }
         }
-
-        Optional<DataObject> elemTermAc0 = findElemTermAc(elmVsc, elmTermsConnectedToAnyVsc, hvdcLnes);
-        Optional<DataObject> elemTermAc1 = findElemTermAc(otherVsc.get(), elmTermsConnectedToAnyVsc, hvdcLnes);
-
-        if (elemTermAc0.isEmpty() || elemTermAc1.isEmpty()) {
-            return Optional.empty();
-        }
-
-        return Optional.of(new HvdcConfiguration(elemTermAc0.get(), elmVsc, hvdcLnes, elemTermAc1.get(), otherVsc.get()));
+        return Optional.empty();
     }
 
-    private static List<HvdcLne> hvdcLinksConnectedToVsc(List<DataObject> elmTermsConnectedToAnyVsc, DataObject elmVsc) {
+    private List<DCLink> computeDcLinksConnectedToVsc(DataObject elmVsc) {
+        List<DCLink> dcLinks = new ArrayList<>();
+        for (DataObject elmTerm : elmTermsConnectedToVscs.get(elmVsc)) {
 
-        List<DataObject> elmTermsConnectedToVsc = findElmTermsConnectedToVsc(elmTermsConnectedToAnyVsc, elmVsc);
-
-        List<HvdcLne> hvdcLnes = new ArrayList<>();
-        for (DataObject elmTerm : elmTermsConnectedToVsc) {
-
-            List<DataObject> elmLnes = getDataObjectsConnectedToElmTerm(elmTerm).stream()
+            List<DataObject> elmLnes = getDataObjectsConnectedToElmTerm(elmTerm)
                 .filter(dataObject -> dataObject.getDataClassName().equals("ElmLne"))
                 .collect(Collectors.toList());
 
             for (DataObject elmLne : elmLnes) {
-                Optional<DataObject> ohterElmTerm = otherElmTerm(elmTermsConnectedToAnyVsc, elmTerm, elmLne);
-                if (ohterElmTerm.isPresent()) {
-                    hvdcLnes.add(new HvdcLne(elmTerm, elmLne, ohterElmTerm.get()));
-                }
+                otherElmTerm(elmTerm, elmLne, elmVsc)
+                        .ifPresent(dataObject -> dcLinks.add(new DCLink(elmTerm, elmLne, dataObject)));
             }
         }
-        return hvdcLnes;
+        return dcLinks;
     }
 
-    private static Optional<DataObject> otherElmTerm(List<DataObject> elmTermsConnectedToAnyVsc, DataObject elmTerm, DataObject elmLne) {
-        return elmTermsConnectedToAnyVsc.stream()
+    private Optional<DataObject> otherElmTerm(DataObject elmTerm, DataObject elmLne, DataObject elmVsc) {
+        // Search only in elmTerms connected to other VSCs
+        return elmTermsConnectedToVscs.entrySet().stream()
+                .filter(e -> !Objects.equals(elmVsc, e.getKey()))
+                .map(Map.Entry::getValue)
+                .flatMap(Collection::stream)
+                // Recheck that elmTerm is different from the given one
             .filter(otherElmTerm -> !elmTerm.equals(otherElmTerm) && isElmTermConnectedToLne(otherElmTerm, elmLne))
             .findFirst();
     }
 
     private static boolean isElmTermConnectedToLne(DataObject elmTerm, DataObject elmLne) {
-        return getDataObjectsConnectedToElmTerm(elmTerm).stream().anyMatch(elmLne::equals);
+        return getDataObjectsConnectedToElmTerm(elmTerm).anyMatch(elmLne::equals);
     }
 
-    private static Optional<DataObject> otherVsc(List<DataObject> elmTermsConnectedToAnyVsc, List<HvdcLne> hvdcLnes) {
-        if (hvdcLnes.isEmpty()) {
-            return Optional.empty();
+    private Optional<DataObject> findVsc1(List<DCLink> links) {
+        if (!links.isEmpty()) {
+            DataObject elmTerm1 = links.get(0).elmTerm1;
+            Optional<DataObject> elmVsc1 = findTheOnlyOneDataObject(vscConnectedToElmTerm(elmTerm1));
+            if (elmVsc1.isPresent()) {
+                // Check other VSC is at end 1 for all the links in the HVDC configuration
+                List<DataObject> elmTermsConnectedToOtherVsc = elmTermsConnectedToVscs.get(elmVsc1.get());
+                if (links.stream().allMatch(l -> elmTermsConnectedToOtherVsc.contains(l.elmTerm1))) {
+                    return elmVsc1;
+                }
+            }
         }
-        DataObject elmTerm1 = hvdcLnes.get(0).elmTerm1;
-        List<DataObject> vscConnecteds = vscConnectedToElmTerm(elmTerm1);
-
-        Optional<DataObject> otherVsc = findTheOnlyOneDataObject(vscConnecteds);
-        if (otherVsc.isPresent()) {
-            List<DataObject> elmTermsConnectedToVsc = findElmTermsConnectedToVsc(elmTermsConnectedToAnyVsc, otherVsc.get());
-
-            return isEnd1ConnectedToVscForAllHvdcLnes(hvdcLnes, elmTermsConnectedToVsc) ? otherVsc : Optional.empty();
-        } else {
-            return Optional.empty();
-        }
+        return Optional.empty();
     }
 
     private static List<DataObject> vscConnectedToElmTerm(DataObject elmTerm) {
-        return getDataObjectsConnectedToElmTerm(elmTerm).stream()
+        return getDataObjectsConnectedToElmTerm(elmTerm)
             .filter(dataObject -> dataObject.getDataClassName().equals("ElmVsc"))
             .collect(Collectors.toList());
     }
 
-    private static boolean isEnd1ConnectedToVscForAllHvdcLnes(List<HvdcLne> hvdcLnes, List<DataObject> elmTermsConnectedToVsc) {
-        return hvdcLnes.stream().allMatch(hvdcLne -> elmTermsConnectedToVsc.contains(hvdcLne.elmTerm1));
-    }
-
-    private static Optional<DataObject> findElemTermAc(DataObject elmVsc, List<DataObject> elmTermsConnectedToAnyVsc, List<HvdcLne> hvdcLnes) {
-        List<DataObject> elmTermsAc = elmTermsConnectedToAnyVsc.stream()
-            .filter(elmTerm -> isElmTermConnectedToVsc(elmTerm, elmVsc))
-            .filter(elemTerm -> !isDcNode(hvdcLnes, elemTerm))
-            .collect(Collectors.toList());
-
+    private Optional<DataObject> findAcElmTerm(DataObject elmVsc, List<DataObject> dcElmTerms) {
+        List<DataObject> elmTermsAc = elmTermsConnectedToVscs.get(elmVsc).stream()
+                .filter(elmTerm -> !dcElmTerms.contains(elmTerm))
+                .collect(Collectors.toList());
         return findTheOnlyOneDataObject(elmTermsAc);
-    }
-
-    private static boolean isDcNode(List<HvdcLne> hvdcLnes, DataObject elmTerm) {
-        return hvdcLnes.stream().anyMatch(hvdcLne -> hvdcLne.elmTerm0.equals(elmTerm) || hvdcLne.elmTerm1.equals(elmTerm));
     }
 
     private static Optional<DataObject> findTheOnlyOneDataObject(List<DataObject> dataObjects) {
@@ -195,45 +161,56 @@ class HvdcConverter extends AbstractConverter {
         return Optional.of(dataObjects.get(0));
     }
 
-    private static final class HvdcConfiguration {
+    private static final class Configuration {
         private final DataObject elmTermAc0;
         private final DataObject vsc0;
-        private final List<HvdcLne> dcLnes;
+        private final List<DCLink> links;
         private final DataObject elmTermAc1;
         private final DataObject vsc1;
 
-        private HvdcConfiguration(DataObject elmTermAc0, DataObject vsc0, List<HvdcLne> dcLnes, DataObject elmTermAc1, DataObject vsc1) {
+        private Configuration(DataObject elmTermAc0, DataObject vsc0, List<DCLink> links, DataObject elmTermAc1, DataObject vsc1) {
             this.elmTermAc0 = elmTermAc0;
             this.vsc0 = vsc0;
-            this.dcLnes = dcLnes;
+            this.links = links;
             this.elmTermAc1 = elmTermAc1;
             this.vsc1 = vsc1;
         }
+
+        Collection<DataObject> getElmLnes() {
+            return links.stream().map(l -> l.elmLne).collect(Collectors.toList());
+        }
+
+        Collection<DataObject> getElmTerms() {
+            return links.stream()
+                    .flatMap(l -> Stream.of(l.elmTerm0, l.elmTerm1))
+                    .collect(Collectors.toList());
+        }
     }
 
-    private static final class HvdcLne {
+    private static final class DCLink {
         private final DataObject elmTerm0;
         private final DataObject elmLne;
         private final DataObject elmTerm1;
 
-        private HvdcLne(DataObject elmTerm0, DataObject elmLne, DataObject elmTerm1) {
+        private DCLink(DataObject elmTerm0, DataObject elmLne, DataObject elmTerm1) {
             this.elmTerm0 = elmTerm0;
             this.elmLne = elmLne;
             this.elmTerm1 = elmTerm1;
+
         }
     }
 
     void create() {
-        hvdcConfigurations.forEach(this::create);
+        configurations.forEach(this::create);
     }
 
-    private void create(HvdcConfiguration hvdcConfiguration) {
+    private void create(Configuration configuration) {
 
-        VscModel vscModelR = VscModel.create(hvdcConfiguration.vsc0);
-        DcLineModel dcLineModel = DcLineModel.create(hvdcConfiguration.dcLnes, hvdcConfiguration.vsc0);
-        VscModel vscModelI = VscModel.create(hvdcConfiguration.vsc1);
+        VscModel vscModelR = VscModel.create(configuration.vsc0);
+        DcLineModel dcLineModel = DcLineModel.create(configuration.links, configuration.vsc0);
+        VscModel vscModelI = VscModel.create(configuration.vsc1);
 
-        NodeRef nodeRefR = getNodeFromElmTerm(hvdcConfiguration.elmTermAc0);
+        NodeRef nodeRefR = getNodeFromElmTerm(configuration.elmTermAc0);
         VoltageLevel voltageLevelR = getNetwork().getVoltageLevel(nodeRefR.voltageLevelId);
 
         // Always with internal connection
@@ -242,7 +219,7 @@ class HvdcConverter extends AbstractConverter {
 
         VscConverterStationAdder adderR = voltageLevelR.newVscConverterStation()
             .setEnsureIdUnicity(true)
-            .setId(hvdcConfiguration.vsc0.getLocName())
+            .setId(configuration.vsc0.getLocName())
             .setNode(nodeR)
             .setLossFactor((float) vscModelR.lossFactor)
             .setVoltageSetpoint(vscModelR.voltageSetpoint)
@@ -250,7 +227,7 @@ class HvdcConverter extends AbstractConverter {
             .setVoltageRegulatorOn(vscModelR.voltageRegulatorOn);
         VscConverterStation cR = adderR.add();
 
-        NodeRef nodeRefI = getNodeFromElmTerm(hvdcConfiguration.elmTermAc1);
+        NodeRef nodeRefI = getNodeFromElmTerm(configuration.elmTermAc1);
         VoltageLevel voltageLevelI = getNetwork().getVoltageLevel(nodeRefI.voltageLevelId);
 
         // Always with internal connection
@@ -259,7 +236,7 @@ class HvdcConverter extends AbstractConverter {
 
         VscConverterStationAdder adderI = voltageLevelI.newVscConverterStation()
             .setEnsureIdUnicity(true)
-            .setId(hvdcConfiguration.vsc1.getLocName())
+            .setId(configuration.vsc1.getLocName())
             .setNode(nodeI)
             .setLossFactor((float) vscModelI.lossFactor)
             .setVoltageSetpoint(vscModelI.voltageSetpoint)
@@ -268,7 +245,7 @@ class HvdcConverter extends AbstractConverter {
         VscConverterStation cI = adderI.add();
 
         HvdcLineAdder adder = getNetwork().newHvdcLine()
-            .setId(hvdcConfiguration.dcLnes.stream().findFirst().orElseThrow().elmLne.getLocName())
+            .setId(configuration.links.stream().findFirst().orElseThrow().elmLne.getLocName())
             .setR(dcLineModel.r)
             .setNominalV(dcLineModel.nominalV)
             .setActivePowerSetpoint(dcLineModel.activePowerSetpoint)
@@ -292,10 +269,10 @@ class HvdcConverter extends AbstractConverter {
             this.maxP = maxP;
         }
 
-        private static DcLineModel create(List<HvdcLne> dcLnes, DataObject elmVscRectifier) {
+        private static DcLineModel create(List<DCLink> dcLnes, DataObject elmVscRectifier) {
 
             double g = 0.0;
-            for (HvdcLne hvdcLne : dcLnes) {
+            for (DCLink hvdcLne : dcLnes) {
                 DataObject typLne = hvdcLne.elmLne.getObjectAttributeValue(DataAttributeNames.TYP_ID).resolve().orElseThrow();
                 double gline = obtainRLine(hvdcLne.elmLne, typLne);
                 g += gline == 0.0 ? 0.0 : 1 / gline;
@@ -307,8 +284,8 @@ class HvdcConverter extends AbstractConverter {
             return new DcLineModel(r, obtainNominalV(dcLnes), activePowerSetpoint, maxP);
         }
 
-        private static double obtainNominalV(List<HvdcLne> dcLnes) {
-            HvdcLne hvdcLne = dcLnes.stream().findFirst().orElseThrow();
+        private static double obtainNominalV(List<DCLink> dcLnes) {
+            DCLink hvdcLne = dcLnes.stream().findFirst().orElseThrow();
             Optional<Float> unom = hvdcLne.elmLne.findFloatAttributeValue("Unom");
             if (unom.isPresent()) {
                 return unom.get();
