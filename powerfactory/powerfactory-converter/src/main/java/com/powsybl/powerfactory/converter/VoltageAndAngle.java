5174887fe40aad2d8fe232f379bf5732ef0ea2af
Luma <zamarrenolm@aia.es>
2022-08-31 17:33:37 +0200

    PowerFactory: fix updates of voltages
    Signed-off-by: Luma <zamarrenolm@aia.es>

--- powerfactory/powerfactory-converter/src/main/java/com/powsybl/powerfactory/converter/VoltageAndAngle.java
+++ powerfactory/powerfactory-converter/src/main/java/com/powsybl/powerfactory/converter/VoltageAndAngle.java
@@ -6,13 +6,14 @@
  */
 package com.powsybl.powerfactory.converter;
 
-import java.util.Optional;
-
 import com.powsybl.iidm.network.Bus;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.Terminal;
+import com.powsybl.iidm.network.VoltageLevel;
 import com.powsybl.powerfactory.converter.PowerFactoryImporter.ImportContext;
 import com.powsybl.powerfactory.model.DataObject;
+
+import java.util.Optional;
 
 /**
  * @author Luma Zamarreño <zamarrenolm at aia.es>
@@ -33,16 +34,41 @@ class VoltageAndAngle extends AbstractConverter {
         Optional<Float> u = elmTerm.findFloatAttributeValue("m:u");
         Optional<Float> phiu = elmTerm.findFloatAttributeValue("m:phiu");
 
-        if (uknom.isPresent() && u.isPresent() && phiu.isPresent()) {
+        // Some voltages may come with a negative sign (DC nodes), ignore them
+        if (uknom.isPresent() && u.isPresent() && phiu.isPresent() && u.get() >= 0) {
             NodeRef nodeRef = getImportContext().elmTermIdToNode.get(elmTerm.getId());
-            Terminal terminal = getNetwork().getVoltageLevel(nodeRef.voltageLevelId).getNodeBreakerView().getTerminal(nodeRef.node);
-            if (terminal != null) {
-                Bus bus = terminal.getBusView().getBus();
-                if (bus != null) {
-                    bus.setV(u.get() * uknom.get());
-                    bus.setAngle(phiu.get());
+            // Registered nodes may have not led to a node in the IIDM network, check that node exists
+            VoltageLevel.NodeBreakerView nb = getNetwork().getVoltageLevel(nodeRef.voltageLevelId).getNodeBreakerView();
+            if (nodeRef.node <= nb.getMaximumNodeIndex()) {
+                double v = u.get() * uknom.get();
+                double angle = phiu.get();
+                update(nb, nodeRef.node, v, angle);
+            }
+        }
+    }
+
+    private static void update(VoltageLevel.NodeBreakerView nb, int node, double v, double angle) {
+        Terminal terminal = nb.getTerminal(node);
+        if (terminal != null) {
+            update(terminal, v, angle);
+        } else {
+            // If the node does not have a terminal,
+            // try to find one at the nodes directly connected to it
+            for (int node1 : nb.getNodesInternalConnectedTo(node)) {
+                terminal = nb.getTerminal(node1);
+                if (terminal != null) {
+                    update(terminal, v, angle);
+                    break;
                 }
             }
+        }
+    }
+
+    private static void update(Terminal terminal, double v, double angle) {
+        Bus bus = terminal.getBusView().getBus();
+        if (bus != null) {
+            bus.setV(v);
+            bus.setAngle(angle);
         }
     }
 }
