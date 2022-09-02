c9eab1ad8b925d8fdf5a334459394844e20235a3
Franck LECUYER <franck.lecuyer@rte-france.com>
2022-09-02 16:10:56 +0200

    Use builder to create a RevertConnectVoltageLevelOnLine modification
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- iidm/iidm-modification/src/test/java/com/powsybl/iidm/modification/topology/RevertConnectVoltageLevelOnLineTest.java
+++ iidm/iidm-modification/src/test/java/com/powsybl/iidm/modification/topology/RevertConnectVoltageLevelOnLineTest.java
@@ -47,16 +47,32 @@ public class RevertConnectVoltageLevelOnLineTest extends AbstractXmlConverterTes
         vl.getNodeBreakerView().newSwitch().setId("breaker4").setName("breaker4").setKind(SwitchKind.BREAKER).setRetained(false).setOpen(true).setFictitious(false).setNode1(0).setNode2(1).add();
         network.newLine().setId("LINE34").setR(0.1).setX(0.1).setG1(0.0).setB1(0.0).setG2(0.0).setB2(0.0).setNode1(1).setVoltageLevel1("VL3").setNode2(1).setVoltageLevel2("VL4").add();
 
-        final NetworkModification modificationWithError1 = new RevertConnectVoltageLevelOnLine("line1NotFound", "CJ_2", "CJ", null);
+        final NetworkModification modificationWithError1 = new RevertConnectVoltageLevelOnLineBuilder()
+                .withLine1Id("line1NotFound")
+                .withLine2Id("CJ_2")
+                .withLineId("CJ")
+                .build();
         assertThrows("Line line1NotFound is not found", PowsyblException.class, () -> modificationWithError1.apply(network));
 
-        final NetworkModification modificationWithError2 = new RevertConnectVoltageLevelOnLine("CJ_1", "line2NotFound", "CJ", null);
+        final NetworkModification modificationWithError2 = new RevertConnectVoltageLevelOnLineBuilder()
+                .withLine1Id("CJ_1")
+                .withLine2Id("line2NotFound")
+                .withLineId("CJ")
+                .build();
         assertThrows("Line line2NotFound is not found", PowsyblException.class, () -> modificationWithError2.apply(network));
 
-        final NetworkModification modificationWithError3 = new RevertConnectVoltageLevelOnLine("CJ_1", "LINE34", "CJ", null);
+        final NetworkModification modificationWithError3 = new RevertConnectVoltageLevelOnLineBuilder()
+                .withLine1Id("CJ_1")
+                .withLine2Id("LINE34")
+                .withLineId("CJ")
+                .build();
         assertThrows("Lines CJ_1 and LINE34 should have one and only one voltage level in common at their extremities", PowsyblException.class, () -> modificationWithError3.apply(network));
 
-        modification = new RevertConnectVoltageLevelOnLine("CJ_1", "CJ_2", "CJ", null);
+        modification = new RevertConnectVoltageLevelOnLineBuilder()
+                .withLine1Id("CJ_1")
+                .withLine2Id("CJ_2")
+                .withLineId("CJ")
+                .build();
         modification.apply(network);
         roundTripXmlTest(network, NetworkXml::writeAndValidate, NetworkXml::validateAndRead,
                 "/fictitious-revert-connect-voltage-level-on-line-vl.xml");
@@ -68,7 +84,11 @@ public class RevertConnectVoltageLevelOnLineTest extends AbstractXmlConverterTes
         NetworkModification modification = new ConnectVoltageLevelOnLine(VOLTAGE_LEVEL_ID, BBS, network.getLine("NHV1_NHV2_1"));
         modification.apply(network);
 
-        modification = new RevertConnectVoltageLevelOnLine("NHV1_NHV2_1_1", "NHV1_NHV2_1_2", "NHV1_NHV2_1", null);
+        modification = new RevertConnectVoltageLevelOnLineBuilder()
+                .withLine1Id("NHV1_NHV2_1_1")
+                .withLine2Id("NHV1_NHV2_1_2")
+                .withLineId("NHV1_NHV2_1")
+                .build();
         modification.apply(network);
         roundTripXmlTest(network, NetworkXml::writeAndValidate, NetworkXml::validateAndRead,
                 "/eurostag-revert-connect-voltage-level-on-line-nb-vl.xml");
@@ -80,7 +100,11 @@ public class RevertConnectVoltageLevelOnLineTest extends AbstractXmlConverterTes
         NetworkModification modification = new ConnectVoltageLevelOnLine(VOLTAGE_LEVEL_ID, "bus", network.getLine("NHV1_NHV2_1"));
         modification.apply(network);
 
-        modification = new RevertConnectVoltageLevelOnLine("NHV1_NHV2_1_1", "NHV1_NHV2_1_2", "NHV1_NHV2_1", null);
+        modification = new RevertConnectVoltageLevelOnLineBuilder()
+                .withLine1Id("NHV1_NHV2_1_1")
+                .withLine2Id("NHV1_NHV2_1_2")
+                .withLineId("NHV1_NHV2_1")
+                .build();
         modification.apply(network);
         roundTripXmlTest(network, NetworkXml::writeAndValidate, NetworkXml::validateAndRead,
                 "/eurostag-revert-connect-voltage-level-on-line-bb-vl.xml");
@@ -91,13 +115,22 @@ public class RevertConnectVoltageLevelOnLineTest extends AbstractXmlConverterTes
         Network network = createNbBbNetwork();
         Line line1 = network.getLine("NHV1_NHV2_1");
         Line line2 = network.getLine("NHV1_NHV2_2");
-        RevertConnectVoltageLevelOnLine modification = new RevertConnectVoltageLevelOnLine(line1.getId(), line2.getId(), "NEW LINE ID", null);
+        RevertConnectVoltageLevelOnLine modification = new RevertConnectVoltageLevelOnLineBuilder()
+                .withLine1Id(line1.getId())
+                .withLine2Id(line2.getId())
+                .withLineId("NEW LINE ID")
+                .build();
         assertEquals("NHV1_NHV2_1", modification.getLine1Id());
         assertEquals("NHV1_NHV2_2", modification.getLine2Id());
         assertEquals("NEW LINE ID", modification.getLineId());
         assertNull(modification.getLineName());
 
-        modification = new RevertConnectVoltageLevelOnLine(line1.getId(), line2.getId(), "NEW LINE ID", "NEW LINE NAME");
+        modification = new RevertConnectVoltageLevelOnLineBuilder()
+                .withLine1Id(line1.getId())
+                .withLine2Id(line2.getId())
+                .withLineId("NEW LINE ID")
+                .withLineName("NEW LINE NAME")
+                .build();
         assertEquals("NEW LINE NAME", modification.getLineName());
     }
 
@@ -106,7 +139,11 @@ public class RevertConnectVoltageLevelOnLineTest extends AbstractXmlConverterTes
         Network network = createNbBbNetwork();
         Line line1 = network.getLine("NHV1_NHV2_1");
         Line line2 = network.getLine("NHV1_NHV2_2");
-        RevertConnectVoltageLevelOnLine modification = new RevertConnectVoltageLevelOnLine(line1.getId(), line2.getId(), "NEW LINE ID", null);
+        RevertConnectVoltageLevelOnLine modification = new RevertConnectVoltageLevelOnLineBuilder()
+                .withLine1Id(line1.getId())
+                .withLine2Id(line2.getId())
+                .withLineId("NEW LINE ID")
+                .build();
         modification.setLine1Id(line1.getId() + "_A")
                 .setLine2Id(line2.getId() + "_B")
                 .setLineId("NEW LINE ID_C")
