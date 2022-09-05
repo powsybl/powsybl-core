d2455dc09d2f4610ce23491300db449405d2c2ff
Hadrien-Godard <64086499+Hadrien-Godard@users.noreply.github.com>
2022-09-05 14:34:39 +0200

    Phase tap changer tap position action (#2242)
    Signed-off-by: Hadrien <hadrien.godard@artelys.com>
    Co-authored-by: Anne Tilloy <anne.tilloy@rte-france.com>
    Co-authored-by: Etienne LESOT <etienne.lesot@rte-france.com>
    Co-authored-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- security-analysis/security-analysis-api/src/main/java/com/powsybl/security/json/ActionSerializer.java
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/json/ActionSerializer.java
@@ -10,12 +10,10 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import com.powsybl.security.action.Action;
-import com.powsybl.security.action.LineConnectionAction;
-import com.powsybl.security.action.MultipleActionsAction;
-import com.powsybl.security.action.SwitchAction;
+import com.powsybl.security.action.*;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 
 /**
  * @author Etienne Lesot <etienne.lesot@rte-france.com>
@@ -42,8 +40,21 @@ public class ActionSerializer extends StdSerializer<Action> {
                 jsonGenerator.writeBooleanField("openSide1", ((LineConnectionAction) action).isOpenSide1());
                 jsonGenerator.writeBooleanField("openSide2", ((LineConnectionAction) action).isOpenSide2());
                 break;
+            case PhaseTapChangerTapPositionAction.NAME:
+                var tapPositionAction = (PhaseTapChangerTapPositionAction) action;
+                jsonGenerator.writeStringField("id", action.getId());
+                jsonGenerator.writeStringField("transformerId", tapPositionAction.getTransformerId());
+                jsonGenerator.writeNumberField("value", tapPositionAction.getValue());
+                jsonGenerator.writeBooleanField("relativeValue", tapPositionAction.isRelativeValue());
+                tapPositionAction.getSide().ifPresent(side -> {
+                    try {
+                        jsonGenerator.writeStringField("side", side.toString());
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                });
+                break;
             case MultipleActionsAction.NAME:
-
                 jsonGenerator.writeStringField("id", action.getId());
                 jsonGenerator.writeObjectField("actions", ((MultipleActionsAction) action).getActions());
                 break;
