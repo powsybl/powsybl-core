7acc2ca54bc88cee4d25c3192b44572df34f52c0
sfid98 <57460392+sfid98@users.noreply.github.com>
2022-09-06 10:15:04 +0200

    Create utility class for compareXml/compareTxt for refactoring purposes (#2209)
    Signed-off-by: sfid98 <s.fidanza@studenti.poliba.it>

--- ucte/ucte-converter/src/test/java/com/powsybl/ucte/converter/UcteExporterTest.java
+++ ucte/ucte-converter/src/test/java/com/powsybl/ucte/converter/UcteExporterTest.java
@@ -22,6 +22,7 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Properties;
 
+import static com.powsybl.commons.ComparisonUtils.compareTxt;
 import static org.junit.Assert.*;
 
 /**
