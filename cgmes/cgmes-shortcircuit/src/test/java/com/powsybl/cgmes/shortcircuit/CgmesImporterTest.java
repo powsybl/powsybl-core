eaac555fea5b3d877e932e79ccd5da73f2651c22
Coline PILOQUET <coline.piloquet@rte-france.com>
2022-10-04 18:48:35 +0200

    Move tests into package.
    Signed-off-by: Coline PILOQUET <coline.piloquet@rte-france.com>

--- cgmes/cgmes-shortcircuit/src/test/java/com/powsybl/cgmes/shortcircuit/CgmesImporterTest.java
+++ cgmes/cgmes-shortcircuit/src/test/java/com/powsybl/cgmes/shortcircuit/CgmesImporterTest.java
@@ -1,4 +1,4 @@
-/**
+package com.powsybl.cgmes.shortcircuit; /**
  * Copyright (c) 2021, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
