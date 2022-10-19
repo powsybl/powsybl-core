81eda998b00aba268262d2c3d88ac2a0a5180de4
Anne Tilloy <anne.tilloy@rte-france.com>
2022-10-19 11:15:44 +0200

    Fix javadoc.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- security-analysis/security-analysis-api/src/main/java/com/powsybl/security/action/GeneratorAction.java
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/action/GeneratorAction.java
@@ -11,10 +11,12 @@ import java.util.Optional;
 
 /**
  * An action to:
- * - change the targetP of a generator, either by specifying a new absolute value (MW) or a relative change (MW).
- * - enable or disable the generator voltage control.
- * - change targetQ of a generator (MVar).
- * - change targetV of a generator (kV).
+ * <ul>
+ *     <li>change the targetP of a generator, either by specifying a new absolute value (MW) or a relative change (MW).</li>
+ *     <li>enable or disable the generator voltage control.</li>
+ *     <li>change targetQ of a generator (MVar). </li>
+ *     <li>change targetV of a generator (kV).</li>
+ * </ul>
  *
  * @author Hadrien Godard <hadrien.godard@artelys.com>
  * @author Anne Tilloy <anne.tilloy@rte-france.com>
@@ -24,11 +26,11 @@ public class GeneratorAction extends AbstractAction {
     public static final String NAME = "GENERATOR";
 
     private final String generatorId;
-    private final Boolean activePowerRelativeValue; // true if it is a relative variation of targetP, false if it is a new targetP
-    private final Double activePowerValue; // could be a new targetP if relativeVariation equals false or a relative variation of targetP.
-    private final Boolean voltageRegulatorOn; // to change voltage control.
-    private final Double targetV; // absolute value only.
-    private final Double targetQ; // absolute value only.
+    private final Boolean activePowerRelativeValue;
+    private final Double activePowerValue;
+    private final Boolean voltageRegulatorOn;
+    private final Double targetV;
+    private final Double targetQ;
 
     /**
      * @param id the id of the action.
@@ -36,8 +38,8 @@ public class GeneratorAction extends AbstractAction {
      * @param activePowerRelativeValue True if the generator targetP variation is relative, False if absolute.
      * @param activePowerValue The new generator targetP (MW) if activePowerRelativeValue equals False, otherwise the relative variation of generator targetP (MW).
      * @param voltageRegulatorOn The new generator voltage regulator status.
-     * @param targetV The new generator targetV (kV).
-     * @param targetQ The new generator targetQ (MVar).
+     * @param targetV The new generator targetV (kV), absolute value only.
+     * @param targetQ The new generator targetQ (MVar), absolute value only.
      */
     GeneratorAction(String id, String generatorId, Boolean activePowerRelativeValue, Double activePowerValue,
                     Boolean voltageRegulatorOn, Double targetV, Double targetQ) {
