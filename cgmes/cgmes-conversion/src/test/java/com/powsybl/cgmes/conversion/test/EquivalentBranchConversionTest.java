618225212fc3218d633b2806c72014177163e593
José Antonio Marqués <marquesja@aia.es>
2022-10-20 13:38:14 +0200

    Add unitary test
    Signed-off-by: José Antonio Marqués <marquesja@aia.es>

---
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/EquivalentBranchConversionTest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.cgmes.conversion.test;
+
+import com.powsybl.cgmes.conformity.CgmesConformity1ModifiedCatalog;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import com.powsybl.iidm.network.*;
+import org.junit.Test;
+
+import com.powsybl.cgmes.conversion.Conversion;
+import com.powsybl.cgmes.model.CgmesModel;
+import com.powsybl.cgmes.model.CgmesModelFactory;
+import com.powsybl.cgmes.model.test.TestGridModel;
+import com.powsybl.commons.datasource.ReadOnlyDataSource;
+import com.powsybl.triplestore.api.TripleStoreFactory;
+
+/**
+ * @author Luma Zamarreño <zamarrenolm at aia.es>
+ * @author José Antonio Marqués <marquesja at aia.es>
+ */
+public class EquivalentBranchConversionTest {
+
+    @Test
+    public void microGridBaseCaseBEEquivalentBranchWithDifferentNominalsTest() throws IOException {
+        Conversion.Config config = new Conversion.Config();
+        Network n = networkModel(CgmesConformity1ModifiedCatalog.microGridBaseCaseBEEquivalentBranchWithDifferentNominals(), config);
+
+        Line lineBaseVoltage1 = n.getLine("ffbabc27-1ccd-4fdc-b037-e341706c8d29");
+
+        assertEquals(5.087377777778, lineBaseVoltage1.getR(), 1.0e-6);
+        assertEquals(69.422222222222, lineBaseVoltage1.getX(), 1.0e-6);
+        assertEquals(-0.000023332348, lineBaseVoltage1.getG1(), 1.0e-6);
+        assertEquals(0.000318392599, lineBaseVoltage1.getB1(), 1.0e-6);
+        assertEquals(0.000023862628, lineBaseVoltage1.getG2(), 1.0e-6);
+        assertEquals(-0.000325628794, lineBaseVoltage1.getB2(), 1.0e-6);
+
+        Line lineBaseVoltage2 = n.getLine("b58bf21a-096a-4dae-9a01-3f03b60c24c7");
+
+        assertEquals(1.978977272727, lineBaseVoltage2.getR(), 1.0e-6);
+        assertEquals(34.977272727273, lineBaseVoltage2.getX(), 1.0e-6);
+        assertEquals(-0.000035831779, lineBaseVoltage2.getG1(), 1.0e-6);
+        assertEquals(0.000633305865, lineBaseVoltage2.getB1(), 1.0e-6);
+        assertEquals(0.000036646138, lineBaseVoltage2.getG2(), 1.0e-6);
+        assertEquals(-0.000647699180, lineBaseVoltage2.getB2(), 1.0e-6);
+    }
+
+    private Network networkModel(TestGridModel testGridModel, Conversion.Config config) throws IOException {
+
+        ReadOnlyDataSource ds = testGridModel.dataSource();
+        String impl = TripleStoreFactory.defaultImplementation();
+
+        CgmesModel cgmes = CgmesModelFactory.create(ds, impl);
+
+        config.setConvertSvInjections(true);
+        Conversion c = new Conversion(cgmes, config);
+        Network n = c.convert();
+
+        return n;
+    }
+}
