60bead4c8249ee44821d2a1808a7f67c2266b024
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2022-11-16 19:36:40 +0100

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- scripting-test/src/main/java/com/powsybl/scripting/test/AbstractGroovyScriptTest.java
+++ scripting-test/src/main/java/com/powsybl/scripting/test/AbstractGroovyScriptTest.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.powsybl.scripting;
+package com.powsybl.scripting.test;
 
 import com.powsybl.computation.ComputationManager;
 import com.powsybl.scripting.groovy.GroovyScriptExtension;
