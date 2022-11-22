c82e77edefa73f38dd344210e6cfbed11cea0dad
Coline PILOQUET <coline.piloquet@rte-france.com>
2022-11-22 18:38:09 +0100

    Fix javadoc.
    Signed-off-by: Coline PILOQUET <coline.piloquet@rte-france.com>

--- shortcircuit-api/src/main/java/com/powsybl/shortcircuit/FaultParameters.java
+++ shortcircuit-api/src/main/java/com/powsybl/shortcircuit/FaultParameters.java
@@ -25,7 +25,7 @@ import java.util.Objects;
 public class FaultParameters {
 
     // VERSION = 1.0 withLimitViolations, withVoltageProfileResult, withFeederResult, studyType and minVoltageDropProportionalThreshold
-    // VERSION = 1.1 voltageMapType, nominalVoltageMapType, useResistances, useLoads, useCapacities, useShunts, withVoltageDropProfileResult and withVoltageProfileResult -> withVoltageProfileResult
+    // VERSION = 1.1 withVoltageDropProfileResult and withVoltageMap -> withVoltageProfileResult
     public static final String VERSION = "1.1";
 
     private final String id;
@@ -52,7 +52,7 @@ public class FaultParameters {
         return withLimitViolations;
     }
 
-    /** Whether the results should include the voltage map on the whole network */
+    /** Whether the results should include the voltages on every bus of the network */
     public boolean isWithVoltageProfileResult() {
         return withVoltageProfileResult;
     }
