62dc1fee424a3595c48dfb9598cc7e875941ea13
VEDELAGO MIORA <miora.ralambotiana@rte-france.com>
2022-12-09 15:46:42 +0100

    Small fix in CGMES import to be uniform
    Signed-off-by: VEDELAGO MIORA <miora.ralambotiana@rte-france.com>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/ACLineSegmentConversion.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/ACLineSegmentConversion.java
@@ -14,6 +14,7 @@ import com.powsybl.iidm.network.util.ReorientedBranchCharacteristics;
 import com.powsybl.cgmes.extensions.CgmesLineBoundaryNodeAdder;
 import com.powsybl.cgmes.model.CgmesNames;
 import com.powsybl.iidm.network.*;
+import com.powsybl.iidm.network.util.TieLineUtil;
 import com.powsybl.triplestore.api.PropertyBag;
 
 /**
@@ -64,7 +65,7 @@ public class ACLineSegmentConversion extends AbstractBranchConversion implements
         double x = p.asDouble("x");
         double g = p.asDouble("gch", 0);
         double b = p.asDouble("bch", 0);
-        boundaryLine.setParameters(r, x, g / 2, b / 2, g / 2, b / 2);
+        boundaryLine.setParameters(r, x, g, b, 0, 0);
         return boundaryLine;
     }
 
@@ -126,8 +127,8 @@ public class ACLineSegmentConversion extends AbstractBranchConversion implements
             isLine2Reoriented(boundaryLine2.getBoundarySide()));
 
         TieLineAdder adder = context.network().newTieLine()
-            .setId(context.namingStrategy().getIidmId("TieLine", boundaryLine1.getId() + " + " + boundaryLine2.getId())) // TODO: maybe refine for merged lines
-            .setName(boundaryLine1.getName() + " + " + boundaryLine2.getName())
+            .setId(context.namingStrategy().getIidmId("TieLine", TieLineUtil.buildMergedId(boundaryLine1.getId(), boundaryLine2.getId())))
+            .setName(TieLineUtil.buildMergedName(boundaryLine1.getId(), boundaryLine2.getId(), boundaryLine1.getName(), boundaryLine2.getName()))
             .newHalfLine1()
             .setId(boundaryLine1.getId())
             .setName(boundaryLine1.getName())
