8fca0334b80452c62fb4b332caea5a71e8797d3f
José Antonio Marqués <marquesja@aia.es>
2022-12-22 07:57:33 +0100

    Add slackConverter class
    Signed-off-by: José Antonio Marqués <marquesja@aia.es>

---
+++ powerfactory/powerfactory-converter/src/main/java/com/powsybl/powerfactory/converter/SlackConverter.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.powerfactory.converter;
+
+import java.util.List;
+
+import com.powsybl.iidm.network.Generator;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.extensions.SlackTerminal;
+import com.powsybl.powerfactory.converter.PowerFactoryImporter.ImportContext;
+import com.powsybl.powerfactory.model.DataObject;
+
+/**
+ * @author Luma Zamarreño <zamarrenolm at aia.es>
+ * @author José Antonio Marqués <marquesja at aia.es>
+ */
+class SlackConverter extends AbstractConverter {
+
+    SlackConverter(ImportContext importContext, Network network) {
+        super(importContext, network);
+    }
+
+    void create(List<DataObject> slackObjects) {
+
+        for (DataObject slackObject : slackObjects) {
+            System.err.printf("SlackObject %d %s %n", slackObject.getId(), slackObject.getFullName());
+
+            Generator generator = getNetwork().getGenerator(GeneratorConverter.getId(slackObject));
+            if (generator != null) {
+                SlackTerminal.reset(generator.getTerminal().getVoltageLevel(), generator.getTerminal());
+            }
+        }
+    }
+}
