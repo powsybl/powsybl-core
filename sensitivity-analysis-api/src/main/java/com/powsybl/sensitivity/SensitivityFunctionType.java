2ed3fa45cdbf48ac34937122885db9f0b1041557
Florian Dupuy <florian.dupuy@rte-france.com>
2023-01-18 13:58:34 +0100

    Format javadoc
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- sensitivity-analysis-api/src/main/java/com/powsybl/sensitivity/SensitivityFunctionType.java
+++ sensitivity-analysis-api/src/main/java/com/powsybl/sensitivity/SensitivityFunctionType.java
@@ -10,32 +10,42 @@ import java.util.OptionalInt;
 
 /**
  * Supported function types related to the equipment to monitor. Use:
- * - BRANCH_ACTIVE_POWER_1 and BRANCH_ACTIVE_POWER_2 if you want to monitor the active power in MW of a network branch (lines,
+ * <ul>
+ *     <li>{@link #BRANCH_ACTIVE_POWER_1} and {@link #BRANCH_ACTIVE_POWER_2} if you want to monitor the active power in MW of a network branch (lines,
  * two windings transformer, dangling lines, etc.). Use 1 for side 1 and 2 for side 2. In case of a three windings transformer,
- * use BRANCH_ACTIVE_POWER_3 to monitor the active power in MW of the leg 3 (network side).
- * - BRANCH_CURRENT_1 and BRANCH_CURRENT_2 if you want to monitor the current in A of a network branch (lines,
+ * use {@link #BRANCH_ACTIVE_POWER_3} to monitor the active power in MW of the leg 3 (network side).</li>
+ *     <li>{@link #BRANCH_CURRENT_1} and {@link #BRANCH_CURRENT_2} if you want to monitor the current in A of a network branch (lines,
  * two windings transformer, dangling lines, etc.). Use 1 for side 1 and use 2 for side 2. In case of a three windings transformer,
- * use BRANCH_CURRENT_3 to monitor the current in A of the leg 3 (network side).
- * - BUS_VOLTAGE if you want to monitor the voltage in KV of a specific network bus.
- *
+ * use {@link #BRANCH_CURRENT_3} to monitor the current in A of the leg 3 (network side).</li>
+ *     <li>{@link #BUS_VOLTAGE} if you want to monitor the voltage in KV of a specific network bus.</li>
+ * </ul>
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
  */
 public enum SensitivityFunctionType {
     /**
+     * in MW
      * @deprecated Use {@link #BRANCH_ACTIVE_POWER_1} instead.
      */
-    @Deprecated BRANCH_ACTIVE_POWER, // MW
+    @Deprecated BRANCH_ACTIVE_POWER,
     /**
+     * in A
      * @deprecated Use {@link #BRANCH_CURRENT_1} instead.
      */
-    @Deprecated BRANCH_CURRENT, // A
-    BRANCH_ACTIVE_POWER_1(1), // MW
-    BRANCH_CURRENT_1(1), // A
-    BRANCH_ACTIVE_POWER_2(2), // MW
-    BRANCH_CURRENT_2(2), // A
-    BRANCH_ACTIVE_POWER_3(3), // MW
-    BRANCH_CURRENT_3(3), // A
-    BUS_VOLTAGE; // KV
+    @Deprecated BRANCH_CURRENT,
+    /** in MW */
+    BRANCH_ACTIVE_POWER_1(1),
+    /** in A */
+    BRANCH_CURRENT_1(1),
+    /** in MW */
+    BRANCH_ACTIVE_POWER_2(2),
+    /** in A */
+    BRANCH_CURRENT_2(2),
+    /** in MW */
+    BRANCH_ACTIVE_POWER_3(3),
+    /** in A */
+    BRANCH_CURRENT_3(3),
+    /** in kV */
+    BUS_VOLTAGE;
 
     private final Integer side;
 
