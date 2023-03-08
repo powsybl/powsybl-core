24b620b58a00d16785c0233d730bce509c246e2a
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2023-03-08 10:01:50 +0100

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- iidm/iidm-extensions/src/main/java/com/powsybl/iidm/network/extensions/FortescueConstants.java
+++ iidm/iidm-extensions/src/main/java/com/powsybl/iidm/network/extensions/FortescueConstants.java
@@ -19,7 +19,7 @@ public final class FortescueConstants {
     public static final double DEFAULT_GROUNDING_R = 0;
     public static final double DEFAULT_GROUNDING_X = 0;
     public static final boolean DEFAULT_FREE_FLUXES = true;
-    public static final WindingConnectionType DEFAULT_LEG1_CONNECTION_TYPE = WindingConnectionType.DELTA; // TODO : check if default connection acceptable
-    public static final WindingConnectionType DEFAULT_LEG2_CONNECTION_TYPE = WindingConnectionType.Y_GROUNDED; // TODO : check if default connection acceptable
-    public static final WindingConnectionType DEFAULT_LEG3_CONNECTION_TYPE = WindingConnectionType.DELTA; // TODO : check if default connection acceptable
+    public static final WindingConnectionType DEFAULT_LEG1_CONNECTION_TYPE = WindingConnectionType.DELTA;
+    public static final WindingConnectionType DEFAULT_LEG2_CONNECTION_TYPE = WindingConnectionType.Y_GROUNDED;
+    public static final WindingConnectionType DEFAULT_LEG3_CONNECTION_TYPE = WindingConnectionType.DELTA;
 }
