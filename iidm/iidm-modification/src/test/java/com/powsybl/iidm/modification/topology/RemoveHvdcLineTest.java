218d9623e611cf73013bd2493ed41873d1b39063
Florian Dupuy <florian.dupuy@rte-france.com>
2023-03-15 17:07:56 +0100

    Add unit tests
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- iidm/iidm-modification/src/test/java/com/powsybl/iidm/modification/topology/RemoveHvdcLineTest.java
+++ iidm/iidm-modification/src/test/java/com/powsybl/iidm/modification/topology/RemoveHvdcLineTest.java
@@ -7,6 +7,8 @@
  */
 package com.powsybl.iidm.modification.topology;
 
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.commons.reporter.Reporter;
 import com.powsybl.commons.test.AbstractConverterTest;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.Substation;
@@ -17,8 +19,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Anis Touri <anis-1.touri@rte-france.com>
@@ -37,6 +38,52 @@ class RemoveHvdcLineTest extends AbstractConverterTest {
     @Test
     void testRemoveHvdcLineLccWithShuntCompensator() {
         Network network = HvdcTestNetwork.createLcc();
+        addVoltageLevelWithShuntCompensator(network);
+        new RemoveHvdcLineBuilder().withHvdcLineId("L").withShuntCompensatorIds(List.of("C1_Filter1", "C1_Filter2", "C5_Filter5")).build().apply(network);
+        assertNull(network.getHvdcLine("L"));
+        assertNull(network.getLccConverterStation("C1"));
+        assertNull(network.getShuntCompensator("C1_Filter1"));
+        assertNull(network.getShuntCompensator("C1_Filter2"));
+        assertNotNull(network.getShuntCompensator("C5_Filter5"));
+    }
+
+    @Test
+    void testRemoveHvdcLineVsc() {
+        Network network = HvdcTestNetwork.createVsc();
+        new RemoveHvdcLineBuilder().withHvdcLineId("L").build().apply(network);
+        assertNull(network.getHvdcLine("L"));
+        assertNull(network.getVscConverterStation("C1"));
+        assertNull(network.getVscConverterStation("C2"));
+    }
+
+    @Test
+    void testRemoveHvdcLineVscWithIgnoredShunt() {
+        Network network = HvdcTestNetwork.createVsc();
+        addVoltageLevelWithShuntCompensator(network);
+        new RemoveHvdcLineBuilder().withHvdcLineId("L").withShuntCompensatorIds("C5_Filter5").build().apply(network);
+        assertNull(network.getHvdcLine("L"));
+        assertNull(network.getVscConverterStation("C1"));
+        assertNull(network.getVscConverterStation("C2"));
+        assertNotNull(network.getShuntCompensator("C5_Filter5"));
+    }
+
+    @Test
+    void testRemoveHvdcLineUnknownShunt() {
+        Network network = HvdcTestNetwork.createLcc();
+        RemoveHvdcLine removeHvdcLine = new RemoveHvdcLineBuilder().withHvdcLineId("L").withShuntCompensatorIds("UnknownShunt").build();
+        PowsyblException e = assertThrows(PowsyblException.class, () -> removeHvdcLine.apply(network, true, Reporter.NO_OP));
+        assertEquals("Shunt UnknownShunt not found", e.getMessage());
+    }
+
+    @Test
+    void testRemoveHvdcLineUnknownLine() {
+        Network network = Network.create("empty", "test");
+        RemoveHvdcLine removeHvdcLine = new RemoveHvdcLineBuilder().withHvdcLineId("L").build();
+        PowsyblException e = assertThrows(PowsyblException.class, () -> removeHvdcLine.apply(network, true, Reporter.NO_OP));
+        assertEquals("Hvdc Line L not found", e.getMessage());
+    }
+
+    private static void addVoltageLevelWithShuntCompensator(Network network) {
         Substation s1 = network.getSubstation("S1");
         VoltageLevel vl3 = s1.newVoltageLevel()
                 .setId("VL3")
@@ -57,20 +104,5 @@ class RemoveHvdcLineTest extends AbstractConverterTest {
                 .setMaximumSectionCount(1)
                 .add()
                 .add();
-        new RemoveHvdcLineBuilder().withHvdcLineId("L").withShuntCompensatorIds(List.of("C1_Filter1", "C1_Filter2", "C5_Filter5")).build().apply(network);
-        assertNull(network.getHvdcLine("L"));
-        assertNull(network.getLccConverterStation("C1"));
-        assertNull(network.getShuntCompensator("C1_Filter1"));
-        assertNull(network.getShuntCompensator("C1_Filter2"));
-        assertNotNull(network.getShuntCompensator("C5_Filter5"));
-    }
-
-    @Test
-    void testRemoveHvdcLineVsc() {
-        Network network = HvdcTestNetwork.createVsc();
-        new RemoveHvdcLineBuilder().withHvdcLineId("L").build().apply(network);
-        assertNull(network.getHvdcLine("L"));
-        assertNull(network.getVscConverterStation("C1"));
-        assertNull(network.getVscConverterStation("C2"));
     }
 }
