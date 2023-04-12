7f1f81706416311ec7cca675e56f77e9ea8bb4c5
Luma <zamarrenolm@aia.es>
2023-04-12 09:55:53 +0200

    copyright and author
    Signed-off-by: Luma <zamarrenolm@aia.es>

--- psse/psse-converter/src/test/java/com/powsybl/psse/converter/PsseImporterIllinoisTest.java
+++ psse/psse-converter/src/test/java/com/powsybl/psse/converter/PsseImporterIllinoisTest.java
@@ -1,8 +1,9 @@
 /**
- * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
  */
 package com.powsybl.psse.converter;
 
@@ -14,6 +15,9 @@ import com.powsybl.psse.model.PsseException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * @author Luma Zamarreño <zamarrenolm at aia.es>
+ */
 class PsseImporterIllinoisTest extends AbstractConverterTest {
 
     // From Illinois Center for a Smarter Electric Grid (ICSEG) (https://icseg.iti.illinois.edu/power-cases/)
