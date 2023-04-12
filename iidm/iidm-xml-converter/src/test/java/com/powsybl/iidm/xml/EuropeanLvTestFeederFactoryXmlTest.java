185828bf60f96ee2d239a09130aebe065cb0127d
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-04-12 16:45:25 +0200

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/EuropeanLvTestFeederFactoryXmlTest.java
+++ iidm/iidm-xml-converter/src/test/java/com/powsybl/iidm/xml/EuropeanLvTestFeederFactoryXmlTest.java
@@ -21,8 +21,8 @@ class EuropeanLvTestFeederFactoryXmlTest extends AbstractXmlConverterTest {
     @Test
     void roundTripTest() throws IOException {
         roundTripTest(EuropeanLvTestFeederFactory.create(),
-                (n, xmlFile) -> NetworkXml.writeAndValidate(n, new ExportOptions().setSorted(true), xmlFile),
-                NetworkXml::read,
-                getVersionedNetworkPath("europeanLvTestFeederRef.xml", CURRENT_IIDM_XML_VERSION));
+            (n, xmlFile) -> NetworkXml.writeAndValidate(n, new ExportOptions().setSorted(true), xmlFile),
+            NetworkXml::read,
+            getVersionedNetworkPath("europeanLvTestFeederRef.xml", CURRENT_IIDM_XML_VERSION));
     }
 }
