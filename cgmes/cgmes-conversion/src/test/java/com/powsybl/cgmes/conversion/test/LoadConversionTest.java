5d938b7b36145160132360734daf6901ecf96a13
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-04-24 22:37:49 +0200

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/LoadConversionTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/LoadConversionTest.java
@@ -7,9 +7,12 @@
 package com.powsybl.cgmes.conversion.test;
 
 import com.powsybl.cgmes.conformity.CgmesConformity1Catalog;
+import com.powsybl.cgmes.conformity.CgmesConformity3Catalog;
 import com.powsybl.cgmes.conversion.Conversion;
 import com.powsybl.iidm.network.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static com.powsybl.cgmes.conversion.test.ConversionUtil.networkModel;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,21 +26,24 @@ class LoadConversionTest {
     @Test
     void microGridBaseCaseNL() {
         Conversion.Config config = new Conversion.Config();
-        Network n = networkModel(CgmesConformity1Catalog.microGridBaseCaseNL(), config);
-        Load l1 = n.getLoad("69add5b4-70bd-4360-8a93-286256c0d38b");
-        Load l2 = n.getLoad("25ab1b5b-6803-47e2-805a-ab7b2072e034");
-        Load l3 = n.getLoad("b1e03a8f-6a11-4454-af58-4a4a680e857f");
-        assertTrue(l1.getModel().isEmpty());
-        assertTrue(l2.getModel().isEmpty());
-        assertTrue(l3.getModel().isPresent());
-        LoadModel loadModel = l3.getModel().orElseThrow();
-        assertEquals(LoadModelType.ZIP, loadModel.getType());
-        ZipLoadModel zipLoadModel = (ZipLoadModel) loadModel;
-        assertEquals(0.8, zipLoadModel.getPp());
-        assertEquals(0.2, zipLoadModel.getIp());
-        assertEquals(0, zipLoadModel.getZp());
-        assertEquals(0.7, zipLoadModel.getPq());
-        assertEquals(0.3, zipLoadModel.getIq());
-        assertEquals(0, zipLoadModel.getZq());
+        for (var referenceResource : List.of(CgmesConformity1Catalog.microGridBaseCaseNL(),
+                                             CgmesConformity3Catalog.microGridBaseCaseNL())) {
+            Network n = networkModel(referenceResource, config);
+            Load l1 = n.getLoad("69add5b4-70bd-4360-8a93-286256c0d38b");
+            Load l2 = n.getLoad("25ab1b5b-6803-47e2-805a-ab7b2072e034");
+            Load l3 = n.getLoad("b1e03a8f-6a11-4454-af58-4a4a680e857f");
+            assertTrue(l1.getModel().isEmpty());
+            assertTrue(l2.getModel().isEmpty());
+            assertTrue(l3.getModel().isPresent());
+            LoadModel loadModel = l3.getModel().orElseThrow();
+            assertEquals(LoadModelType.ZIP, loadModel.getType());
+            ZipLoadModel zipLoadModel = (ZipLoadModel) loadModel;
+            assertEquals(0.8, zipLoadModel.getPp());
+            assertEquals(0.2, zipLoadModel.getIp());
+            assertEquals(0, zipLoadModel.getZp());
+            assertEquals(0.7, zipLoadModel.getPq());
+            assertEquals(0.3, zipLoadModel.getIq());
+            assertEquals(0, zipLoadModel.getZq());
+        }
     }
 }
