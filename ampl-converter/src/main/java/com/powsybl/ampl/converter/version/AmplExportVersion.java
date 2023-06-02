8be12fe5d9c5613b024a167813f334a38474995a
Nicolas Pierre <nicolas.pierre@artelys.com>
2023-06-02 15:59:06 +0200

    missing author and licence
    Signed-off-by: Nicolas Pierre <nicolas.pierre@artelys.com>

--- ampl-converter/src/main/java/com/powsybl/ampl/converter/version/AmplExportVersion.java
+++ ampl-converter/src/main/java/com/powsybl/ampl/converter/version/AmplExportVersion.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.ampl.converter.version;
 
 import com.powsybl.ampl.converter.AmplExportConfig;
@@ -5,6 +11,9 @@ import com.powsybl.ampl.converter.AmplSubset;
 import com.powsybl.commons.util.StringToIntMapper;
 import com.powsybl.iidm.network.Network;
 
+/**
+ * @author Nicolas Pierre <nicolas.pierre at artelys.com>
+ */
 public interface AmplExportVersion {
     interface Factory {
         AmplColumnsExporter create(AmplExportConfig config, Network network, StringToIntMapper<AmplSubset> mapper,
