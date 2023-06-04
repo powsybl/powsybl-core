7c464bed7b2919568f8022465c3636c5a19bb528
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-06-04 17:46:24 +0200

    Add unit test
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

---
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/MultipleDataSourceTest.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.cgmes.conversion.test;
+
+import com.google.common.io.ByteStreams;
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import com.powsybl.cgmes.conformity.CgmesConformity1Catalog;
+import com.powsybl.cgmes.conversion.Conversion;
+import com.powsybl.cgmes.model.CgmesModel;
+import com.powsybl.cgmes.model.CgmesModelFactory;
+import com.powsybl.commons.datasource.DataSource;
+import com.powsybl.commons.datasource.MultipleReadOnlyDataSource;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.triplestore.api.TripleStoreFactory;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+class MultipleDataSourceTest {
+
+    @Test
+    void oneZipPerProfileTest() throws Exception {
+        var testDataSource = CgmesConformity1Catalog.microGridBaseCaseBE().dataSource();
+        Set<String> profiles = testDataSource.listNames(".*");
+        try (FileSystem fileSystem = Jimfs.newFileSystem(Configuration.unix())) {
+            // copy and compress each of the profile to the file system
+            Path workDir = fileSystem.getPath("/work");
+            for (String profile : profiles) {
+                try (var is = testDataSource.newInputStream(profile);
+                     var os = new ZipOutputStream(Files.newOutputStream(workDir.resolve(profile + ".zip")))) {
+                    os.putNextEntry(new ZipEntry(profile));
+                    ByteStreams.copy(is, os);
+                    os.closeEntry();
+                }
+            }
+
+            // create a multiple data source with all zipped profiles
+            MultipleReadOnlyDataSource dataSource = new MultipleReadOnlyDataSource(profiles.stream().map(profile -> DataSource.fromPath(workDir.resolve(profile + ".zip"))).collect(Collectors.toList()));
+            String impl = TripleStoreFactory.defaultImplementation();
+            CgmesModel cgmes = CgmesModelFactory.create(dataSource, impl);
+            Conversion.Config config = new Conversion.Config();
+            Network network = new Conversion(cgmes, config)
+                    .convert();
+            assertNotNull(network);
+        }
+    }
+}
