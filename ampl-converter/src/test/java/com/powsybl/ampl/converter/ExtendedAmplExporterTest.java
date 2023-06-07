bde0378d144e49b28c920074892b5f7988f45387
Nicolas Pierre <nicolas.pierre@artelys.com>
2023-06-07 17:08:09 +0200

    wip tests
    Signed-off-by: Nicolas Pierre <nicolas.pierre@artelys.com>

---
+++ ampl-converter/src/test/java/com/powsybl/ampl/converter/ExtendedAmplExporterTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.ampl.converter;
+
+import com.powsybl.ampl.converter.version.AmplExportVersionImpl;
+import com.powsybl.commons.datasource.MemDataSource;
+import com.powsybl.commons.test.AbstractConverterTest;
+import com.powsybl.iidm.network.Bus;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.StaticVarCompensator;
+import com.powsybl.iidm.network.VoltageLevel;
+import com.powsybl.iidm.network.extensions.SlackTerminalAdder;
+import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
+import com.powsybl.iidm.network.test.SvcTestCaseFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.powsybl.commons.test.ComparisonUtils.compareTxt;
+
+/**
+ * @author Nicolas PIERRE <nicolas.pierre at artelys.com>
+ */
+public class ExtendedAmplExporterTest extends AbstractConverterTest {
+
+    private AmplExportConfig v2Config;
+
+    private void assertEqualsToRef(MemDataSource dataSource, String suffix, String refFileName) throws IOException {
+        try (InputStream actual = new ByteArrayInputStream(dataSource.getData(suffix, "txt"))) {
+            compareTxt(getClass().getResourceAsStream("/inputs/extended_exporter/" + refFileName), actual);
+        }
+    }
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        super.setUp();
+        v2Config = new AmplExportConfig(AmplExportConfig.ExportScope.ALL, false,
+            AmplExportConfig.ExportActionType.CURATIVE, false, false,
+            AmplExportVersionImpl.V2);
+    }
+
+    @Test
+    public void testSlackBusExport() throws IOException {
+        Network network = EurostagTutorialExample1Factory.create();
+        VoltageLevel vlGen = network.getVoltageLevel("VLLOAD");
+        Bus bus = vlGen.getBusBreakerView().getBus("NLOAD");
+        SlackTerminalAdder adder = vlGen.newExtension(SlackTerminalAdder.class);
+        adder.withTerminal(bus.getConnectedTerminals().iterator().next()).add();
+
+        MemDataSource dataSource = new MemDataSource();
+        new AmplNetworkWriter(network, dataSource, v2Config).write();
+
+        assertEqualsToRef(dataSource, "_network_buses",
+            "eurostag-tutorial-example1-buses.txt");
+    }
+
+    @Test
+    public void testNewTapExport() throws IOException {
+
+    }
+
+    @Test
+    public void testRegulatingBusIdExportGenerators() throws IOException {
+        Network network = EurostagTutorialExample1Factory.createWithMoreGenerators();
+        network.getGenerator("GEN").setVoltageRegulatorOn(false);
+
+        MemDataSource dataSource = new MemDataSource();
+        new AmplNetworkWriter(network, dataSource, v2Config).write();
+
+        assertEqualsToRef(dataSource, "_network_generators",
+            "eurostag-tutorial-example1-generators-regulating-bus.txt");
+    }
+
+    @Test
+    public void testRegulatingBusIdExportSvc() throws IOException {
+        Network network = SvcTestCaseFactory.createWithMoreSVCs();
+        network.getStaticVarCompensator("SVC2").setRegulationMode(StaticVarCompensator.RegulationMode.OFF);
+        MemDataSource dataSource = new MemDataSource();
+        new AmplNetworkWriter(network, dataSource, v2Config).write();
+
+        assertEqualsToRef(dataSource, "_network_static_var_compensators", "svc-test-case-regulating-bus.txt");
+    }
+}
