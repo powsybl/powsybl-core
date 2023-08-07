6df7da12a7836b82b756dd515b757a07e82f12ca
Nicolas Pierre <nicolas.pierre@artelys.com>
2023-08-07 16:39:38 +0200

    comments and clean
    Signed-off-by: Nicolas Pierre <nicolas.pierre@artelys.com>

--- iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/util/RegulationUtils.java
+++ iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/util/RegulationUtils.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.iidm.modification.util;
 
 import com.powsybl.iidm.network.Bus;
@@ -13,35 +20,73 @@ public final class RegulationUtils {
     private RegulationUtils() {
     }
 
+    /**
+     * Get the stream of all generators of the network regulating the bus.
+     */
     public static Stream<Generator> getRegulatingGenerators(Network network, Bus regulatingBus) {
         return network.getGeneratorStream().filter(Generator::isVoltageRegulatorOn)
             .filter(g -> regulatingBus.equals(g.getRegulatingTerminal().getBusView().getBus()));
     }
 
+    /**
+     * Get the stream of all shunts compensators of the network regulating the bus.
+     */
     public static Stream<ShuntCompensator> getRegulatingShuntCompensators(Network network, Bus regulatingBus) {
         return network.getShuntCompensatorStream().filter(ShuntCompensator::isVoltageRegulatorOn)
             .filter(shunt -> regulatingBus.equals(shunt.getRegulatingTerminal().getBusView().getBus()));
     }
 
-    private static OptionalDouble getTargetVForRegulatingElement(Network network, Bus regulatingBus, String shuntId,
-                                                                 String generatorId) {
-        Optional<Double> optDouble = getRegulatingShuntCompensators(network, regulatingBus)
-            .filter(sc -> !sc.getId().equals(shuntId)).map(ShuntCompensator::getTargetV).findFirst()
-            .or(() -> getRegulatingGenerators(network, regulatingBus).filter(
-                generator -> !generator.getId().equals(generatorId)).map(Generator::getTargetV).findFirst())
-            .or(() -> Double.isNaN(regulatingBus.getV()) ? Optional.empty() : Optional.of(regulatingBus.getV()));
+    /**
+     * Finds the correct target voltage for a regulating element.
+     *
+     * @param regulatingBus       the regulating bus on which the regulating element will be connected.
+     * @param shuntToIgnoreId     allow to ignore one shunt in the search, useful when we have reconnected a shunt and want to set its target v
+     * @param generatorToIgnoreId same as param shuntToIgnoreId, but for a generator
+     * @return the targetV, if there is no regulating shunts, generators, and the regulating bus does not have a valid voltage,
+     * then it returns {@link OptionalDouble#empty()}
+     */
+    private static OptionalDouble getTargetVForRegulatingElement(Network network, Bus regulatingBus,
+                                                                 String shuntToIgnoreId,
+                                                                 String generatorToIgnoreId) {
+        // first we search for a shunt regulating the bus (appart from the one given in parameters)
+        Optional<Double> optDouble =
+            getRegulatingShuntCompensators(network, regulatingBus)
+                .filter(sc -> !sc.getId().equals(shuntToIgnoreId))
+                .map(ShuntCompensator::getTargetV).findFirst()
+                // if none were found, we search for a generator regulating the bus (appart from the one given in parameters)
+                .or(() -> getRegulatingGenerators(network, regulatingBus)
+                    .filter(generator -> !generator.getId().equals(generatorToIgnoreId))
+                    .map(Generator::getTargetV).findFirst())
+                // if none were found, we use the network voltage, if it is not NaN else we return an empty optional.
+                .or(() -> Double.isNaN(regulatingBus.getV()) ? Optional.empty() : Optional.of(regulatingBus.getV()));
         return optDouble.isPresent() ? OptionalDouble.of(optDouble.get()) : OptionalDouble.empty();
     }
 
-    public static OptionalDouble getTargetVForRegulatingShunt(Network network, Bus regulatingBus, String shuntId) {
-        return getTargetVForRegulatingElement(network, regulatingBus, shuntId, null);
+    /**
+     * Tries to find the regulating voltage for a regulating shunt on the given regulating bus.
+     *
+     * @return {@link OptionalDouble#empty()} If no valid values are found (no regulating elements and NaN network voltage on the bus)
+     */
+    public static OptionalDouble getTargetVForRegulatingShunt(Network network, Bus regulatingBus,
+                                                              String shuntToIgnoreId) {
+        return getTargetVForRegulatingElement(network, regulatingBus, shuntToIgnoreId, null);
     }
 
+    /**
+     * Tries to find the regulating voltage for a regulating generator on the given regulating bus.
+     *
+     * @return {@link OptionalDouble#empty()} If no valid values are found (no regulating elements and NaN network voltage on the bus)
+     */
     public static OptionalDouble getTargetVForRegulatingGenerator(Network network, Bus regulatingBus,
-                                                                  String generatorId) {
-        return getTargetVForRegulatingElement(network, regulatingBus, null, generatorId);
+                                                                  String generatorToIgnoreId) {
+        return getTargetVForRegulatingElement(network, regulatingBus, null, generatorToIgnoreId);
     }
 
+    /**
+     * Tries to find the regulating voltage for a regulating shunt.
+     *
+     * @return {@link OptionalDouble#empty()} If no valid values are found (no regulating elements and NaN network voltage on the bus)
+     */
     public static OptionalDouble getTargetVForRegulatingShunt(ShuntCompensator shuntCompensator) {
         Bus regulatingBus = shuntCompensator.getRegulatingTerminal().getBusView().getBus();
         if (regulatingBus != null) {
@@ -51,6 +96,11 @@ public final class RegulationUtils {
         return OptionalDouble.empty();
     }
 
+    /**
+     * Tries to find the regulating voltage for a regulating generator.
+     *
+     * @return {@link OptionalDouble#empty()} If no valid values are found (no regulating elements and NaN network voltage on the bus)
+     */
     public static OptionalDouble getTargetVForRegulatingGenerator(Generator generator) {
         Bus regulatingBus = generator.getRegulatingTerminal().getBusView().getBus();
         if (regulatingBus != null) {
