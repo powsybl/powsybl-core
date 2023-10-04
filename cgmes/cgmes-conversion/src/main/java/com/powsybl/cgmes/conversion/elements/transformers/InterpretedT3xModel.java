powsybl/powsybl-core

commit 4c469160c833447be34eb844651cab27468f26a3

CGMES import: support of optional ratedS value of transformers (#2648)
Signed-off-by: Luma <zamarrenolm@aia.es>
Signed-off-by: José Antonio Marqués <marquesja@aia.es>

cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/transformers/InterpretedT3xModel.java
@@ -66,6 +66,7 @@ public class InterpretedT3xModel {
         final TapChangerConversion.InterpretedEnd end1;
         final InterpretedEnd2 end2;
         final boolean structuralRatioAtEnd2;
+        final double ratedS;
 
         /**
          * Maps Cgmes ratioTapChangers, phaseTapChangers, shuntAdmittances and
@@ -86,6 +87,7 @@ public class InterpretedT3xModel {
             this.end2 = new InterpretedEnd2(windingInterpretedShunt.g2, windingInterpretedShunt.b2,
                     windingInterpretedTapChanger.ratioTapChanger2, windingInterpretedTapChanger.phaseTapChanger2);
             this.structuralRatioAtEnd2 = windingStructuralRatioAtEnd2;
+            this.ratedS = cgmesEnd.ratedS;
         }
 
         /**

cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/transformers/CgmesT2xModel.java
@@ -23,6 +23,7 @@ public class CgmesT2xModel {
     final CgmesPartialEnd end1;
     final CgmesPartialEnd end2;
     final boolean x1IsZero;
+    final Double ratedS;
 
     public CgmesT2xModel(PropertyBags ends, Context context) {
         PropertyBag bagEnd1 = ends.get(0);
@@ -36,6 +37,19 @@ public class CgmesT2xModel {
         this.end1 = new CgmesPartialEnd(bagEnd1, x, context);
         this.end2 = new CgmesPartialEnd(bagEnd2, x, context);
         this.x1IsZero = x1 == 0.0;
+
+        this.ratedS = getRatedS(bagEnd1, bagEnd2);
+    }
+
+    static Double getRatedS(PropertyBag end1, PropertyBag end2) {
+        // For a two-winding transformer the values for the high and low voltage sides shall be identical
+        // But is an optional attribute, it may be specified at any end
+        double ratedS1 = end1.asDouble(CgmesNames.RATEDS, 0);
+        if (ratedS1 > 0) {
+            return ratedS1;
+        }
+        double ratedS2 = end2.asDouble(CgmesNames.RATEDS, 0);
+        return ratedS2 > 0.0 ? ratedS2 : null;
     }
 
     static class CgmesPartialEnd {

cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/transformers/CgmesT3xModel.java
@@ -37,6 +37,7 @@ public class CgmesT3xModel {
         final TapChanger phaseTapChanger;
         final double ratedU;
         final String terminal;
+        final Double ratedS;
 
         CgmesEnd(PropertyBag end, Context context) {
             this.r = end.asDouble(CgmesNames.R);
@@ -47,6 +48,8 @@ public class CgmesT3xModel {
             this.phaseTapChanger = TapChanger.phaseTapChangerFromEnd(end, x, context);
             this.ratedU = end.asDouble(CgmesNames.RATEDU);
             this.terminal = end.getId(CgmesNames.TERMINAL);
+            double ratedS0 = end.asDouble(CgmesNames.RATEDS, 0);
+            this.ratedS = ratedS0 > 0 ? ratedS0 : null;
         }
     }
 }

cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/transformers/ConvertedT2xModel.java
@@ -19,6 +19,7 @@ public class ConvertedT2xModel {
     final double x;
     final TapChangerConversion.ConvertedEnd1 end1;
     final ConvertedEnd2 end2;
+    final Double ratedS;
 
     /**
      * ratioTapChanger and phaseTapChanger of end2 are moved to end1 and then
@@ -53,7 +54,7 @@ public class ConvertedT2xModel {
         this.end1 = new TapChangerConversion.ConvertedEnd1(rc0.g1 + rc0.g2, rc0.b1 + rc0.b2, ratioTapChanger, phaseTapChanger,
             interpretedT2xModel.end1.ratedU, interpretedT2xModel.end1.terminal);
         this.end2 = new ConvertedEnd2(interpretedT2xModel.end2.ratedU, interpretedT2xModel.end2.terminal);
-
+        this.ratedS = interpretedT2xModel.ratedS;
     }
 
     static class ConvertedEnd2 {

cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/transformers/ConvertedT3xModel.java
@@ -35,6 +35,7 @@ public class ConvertedT3xModel {
         final double r;
         final double x;
         final TapChangerConversion.ConvertedEnd1 end1;
+        final Double ratedS;
 
         /**
          * At each winding or leg:
@@ -56,6 +57,7 @@ public class ConvertedT3xModel {
                 windingTapChanger.phaseTapChanger,
                 interpretedWinding.end1.ratedU,
                 interpretedWinding.end1.terminal);
+            this.ratedS = interpretedWinding.ratedS;
         }
 
         private TapChangerWinding moveCombineTapChangerWinding(InterpretedWinding interpretedWinding, TapChangerConversion tcc) {

cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/transformers/InterpretedT2xModel.java
@@ -21,6 +21,7 @@ public class InterpretedT2xModel {
     final TapChangerConversion.InterpretedEnd end1;
     final TapChangerConversion.InterpretedEnd end2;
     final boolean structuralRatioAtEnd2;
+    final Double ratedS;
 
     /**
      * Maps Cgmes ratioTapChangers, phaseTapChangers, shuntAdmittances and
@@ -43,6 +44,7 @@ public class InterpretedT2xModel {
             interpretedTapChanger.ratioTapChanger2, interpretedTapChanger.phaseTapChanger2,
             cgmesT2xModel.end2.ratedU, cgmesT2xModel.end2.terminal);
         this.structuralRatioAtEnd2 = structuralRatioAlternative(cgmesT2xModel, alternative);
+        this.ratedS = cgmesT2xModel.ratedS;
     }
 
     /**

cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/transformers/ThreeWindingsTransformerConversion.java
@@ -136,6 +136,9 @@ public class ThreeWindingsTransformerConversion extends AbstractTransformerConve
             .setG(convertedModelWinding.end1.g)
             .setB(convertedModelWinding.end1.b)
             .setRatedU(convertedModelWinding.end1.ratedU);
+        if (convertedModelWinding.ratedS != null) {
+            ladder.setRatedS(convertedModelWinding.ratedS);
+        }
     }
 
     private static void setToIidmWindingTapChanger(ConvertedT3xModel convertedT3xModel, ConvertedT3xModel.ConvertedWinding convertedModelWinding,

cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/transformers/TwoWindingsTransformerConversion.java
@@ -153,6 +153,9 @@ public class TwoWindingsTransformerConversion extends AbstractTransformerConvers
                 .setB(Double.isNaN(convertedT2xModel.end1.b) ? 0.0 : convertedT2xModel.end1.b)
                 .setRatedU1(convertedT2xModel.end1.ratedU)
                 .setRatedU2(convertedT2xModel.end2.ratedU);
+        if (convertedT2xModel.ratedS != null) {
+            adder.setRatedS(convertedT2xModel.ratedS);
+        }
         identify(adder);
         connect(adder);
         TwoWindingsTransformer tx = adder.add();

commit 64c80de8437fc681ee34db56c9f9050fc2c7db57

Fix optional ratedS regression introduced in #2648 (#2733)
Signed-off-by: Luma <zamarrenolm@aia.es>

cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/elements/transformers/InterpretedT3xModel.java
@@ -66,7 +66,7 @@ public class InterpretedT3xModel {
         final TapChangerConversion.InterpretedEnd end1;
         final InterpretedEnd2 end2;
         final boolean structuralRatioAtEnd2;
-        final double ratedS;
+        final Double ratedS;
 
         /**
          * Maps Cgmes ratioTapChangers, phaseTapChangers, shuntAdmittances and