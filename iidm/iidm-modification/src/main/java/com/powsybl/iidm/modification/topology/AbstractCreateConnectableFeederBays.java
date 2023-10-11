18dd232f85ecbf326926c390f33c12ec4ec9e37f
Nicolas Rol <nicolas.rol@rte-france.com>
2023-10-11 14:17:02 +0200

    checkstyle correction + licence correction
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/topology/AbstractCreateConnectableFeederBays.java
+++ iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/topology/AbstractCreateConnectableFeederBays.java
@@ -20,7 +20,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static com.powsybl.iidm.modification.topology.TopologyModificationUtils.*;
 import static com.powsybl.iidm.modification.util.ModificationReports.*;
--- iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/topology/NamingStrategy.java
+++ iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/topology/NamingStrategy.java
@@ -1,8 +1,9 @@
 /**
- * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
  */
 package com.powsybl.iidm.modification.topology;
 
@@ -13,7 +14,7 @@ import java.util.List;
 /**
  * The naming strategy aims at clarifying and facilitating the naming of the different network elements created via the
  * different {@link com.powsybl.iidm.modification.NetworkModification} classes. Based on the name of the network element
- * the user wish to create (a VoltageLeevl, a BranchFeederBay, etc.), all the other elements created during the
+ * the user wish to create (a VoltageLevel, a BranchFeederBay, etc.), all the other elements created during the
  * NetworkModification will be given a name using this name as baseline and prefixes/suffixes according to the naming
  * strategy chosen by the user. The naming strategy can be either the default one
  * ({@link com.powsybl.iidm.modification.topology.DefaultNamingStrategy}) or a new implementation of the present class
