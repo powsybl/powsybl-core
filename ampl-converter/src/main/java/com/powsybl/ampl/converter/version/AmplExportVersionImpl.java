54cdc68d6aabab00b15a36629ef89eafc1fd3d2a
pjeanmarie <72982611+pjeanmarie@users.noreply.github.com>
2023-11-06 15:57:32 +0100

    Add missing Override
    Co-authored-by: Olivier Perrin <olivier.perrin@rte-france.com>
    Signed-off-by: Pauline Jean-Marie <pauline.jean-marie@artelys.com>

--- ampl-converter/src/main/java/com/powsybl/ampl/converter/version/AmplExportVersionImpl.java
+++ ampl-converter/src/main/java/com/powsybl/ampl/converter/version/AmplExportVersionImpl.java
@@ -22,6 +22,7 @@ public enum AmplExportVersionImpl implements AmplExportVersion {
         this.factory = factory;
     }
 
+    @Override
     public AmplExportVersion.Factory getColumnsExporter() {
         return factory;
     }
