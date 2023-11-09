29e4a4f4f6227d342f3886b7633dfae6086e34f0
Sophie Frasnedo <sophie.frasnedo@rte-france.com>
2023-11-09 11:27:44 +0100

    Split temporary duration criterion classes in different files
    Signed-off-by: Sophie Frasnedo <sophie.frasnedo@rte-france.com>

---
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/detectors/criterion/duration/IntervalTemporaryDurationCriterion.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.security.detectors.criterion.duration;
+
+/**
+ * Describes interval temporary duration criterion
+ *
+ * @author Sophie Frasnedo {@literal <sophie.frasnedo at rte-france.com>}
+ */
+
+public class IntervalTemporaryDurationCriterion extends AbstractTemporaryDurationCriterion {
+    private static final TemporaryDurationCriterionType COMPARISON_TYPE = TemporaryDurationCriterionType.INTERVAL;
+    private double lowBound;
+    private double highBound;
+    private boolean lowClosed;
+    private boolean highClosed;
+
+    @Override
+    public TemporaryDurationCriterionType getComparisonType() {
+        return COMPARISON_TYPE;
+    }
+
+    public double getLowBound() {
+        return lowBound;
+    }
+
+    public IntervalTemporaryDurationCriterion setLowBound(double lowBound) {
+        this.lowBound = lowBound;
+        return this;
+    }
+
+    public double getHighBound() {
+        return highBound;
+    }
+
+    public IntervalTemporaryDurationCriterion setHighBound(double highBound) {
+        this.highBound = highBound;
+        return this;
+    }
+
+    public boolean isLowClosed() {
+        return lowClosed;
+    }
+
+    public IntervalTemporaryDurationCriterion setLowClosed(boolean lowClosed) {
+        this.lowClosed = lowClosed;
+        return this;
+    }
+
+    public boolean isHighClosed() {
+        return highClosed;
+    }
+
+    public IntervalTemporaryDurationCriterion setHighClosed(boolean highClosed) {
+        this.highClosed = highClosed;
+        return this;
+    }
+}
