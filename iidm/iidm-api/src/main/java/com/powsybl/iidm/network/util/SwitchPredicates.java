44b0758ad60d73cd1b3c2186a0e793af23a87e3a
Nicolas Rol <135979730+rolnico@users.noreply.github.com>
2023-11-09 12:33:38 +0100

    Custom connection/disconnection of terminals (#2754)
    * new methods Terminal::connect / Terminal::disconnect added with Predicate<Switch> isSwitchOperable parameter
    * add several static predicates in SwitchPredicates class
    * disconnect: open switches only if all paths can be opened
    * disconnect: open first openable switch on each paths for the sake of simplicity, no minimization of operations done
    * new method UndirectedGraph::findAllPaths with paths comparator for sorting
    * methods UndirectedGraph::findAllPaths methods take Predicate<V> instead of Function<V, Boolean>
    
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

---
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/util/SwitchPredicates.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023. , RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.iidm.network.util;
+
+import com.powsybl.iidm.network.Switch;
+import com.powsybl.iidm.network.SwitchKind;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+
+/**
+ * @author Nicolas Rol {@literal <nicolas.rol at rte-france.com>}
+ */
+public final class SwitchPredicates {
+
+    private SwitchPredicates() { }
+
+    public static final Predicate<Switch> IS_NONFICTIONAL_CLOSED_BREAKER = switchObject -> switchObject != null && switchObject.getKind() == SwitchKind.BREAKER && !switchObject.isOpen() && !switchObject.isFictitious();
+    public static final Predicate<Switch> IS_NONFICTIONAL_BREAKER = switchObject -> switchObject != null && switchObject.getKind() == SwitchKind.BREAKER && !switchObject.isFictitious();
+    public static final Predicate<Switch> IS_CLOSED_BREAKER = switchObject -> switchObject != null && switchObject.getKind() == SwitchKind.BREAKER && !switchObject.isOpen();
+    public static final Predicate<Switch> IS_BREAKER_OR_DISCONNECTOR = switchObject -> switchObject != null && (switchObject.getKind() == SwitchKind.BREAKER || switchObject.getKind() == SwitchKind.DISCONNECTOR);
+    public static final Predicate<Switch> IS_OPEN_DISCONNECTOR = switchObject -> switchObject != null && switchObject.getKind() == SwitchKind.DISCONNECTOR && switchObject.isOpen();
+    public static final Predicate<Switch> IS_OPEN = switchObject -> switchObject != null && switchObject.isOpen();
+    public static final Predicate<Switch> IS_NON_NULL = Objects::nonNull;
+}
