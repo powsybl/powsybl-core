d8b1aea29d43e7c013b4f0ee835794927c728ead
Florian Dupuy <florian.dupuy@rte-france.com>
2023-12-01 15:48:23 +0100

    Add javadoc in AbstracetSerDeTest
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- commons-test/src/main/java/com/powsybl/commons/test/AbstractSerDeTest.java
+++ commons-test/src/main/java/com/powsybl/commons/test/AbstractSerDeTest.java
@@ -19,6 +19,9 @@ import java.nio.file.Path;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
  */
@@ -39,18 +42,51 @@ public abstract class AbstractSerDeTest {
         fileSystem.close();
     }
 
+    /**
+     * XML roundtrip test on the given data. The following steps occur:
+     * <ul>
+     *     <li>Transform the given data with the transformer function provided. This might be used to add a roundtrip on
+     *     other formats prior to the XML roundtrip.</li>
+     *     <li>Write the transformed data to a temporary XML file thanks to the given write function.</li>
+     *     <li>Compare the obtained XML file with the given reference file thanks to {@link ComparisonUtils#compareXml}.</li>
+     *     <li>Read the same XML file with the given read function.</li>
+     *     <li>Write the data read in the previous step to a new temporary XML file.</li>
+     *     <li>Compare the new obtained XML file with the first obtained XML file byte by byte using {@link Files#mismatch}.</li>
+     *     <li>Return the data read 3 steps above.</li>
+     * </ul>
+     */
     protected <T> T roundTripXmlTest(T data, BiFunction<T, Path, T> transformer, BiConsumer<T, Path> write, Function<Path, T> read, String ref) throws IOException {
         return roundTripTest(data, transformer, write, read, ComparisonUtils::compareXml, ref);
     }
 
+    /**
+     * Roundtrip test on the given data. The following steps occur:
+     * <ul>
+     *     <li>Write the given data to a temporary file thanks to the given write function.</li>
+     *     <li>Compare the obtained file with the given reference file thanks to {@link ComparisonUtils#compareTxt}.</li>
+     *     <li>Read the same file with the given read function.</li>
+     *     <li>Write the data read in the previous step to a new temporary file.</li>
+     *     <li>Compare the new obtained file with the first obtained file byte by byte using {@link Files#mismatch}.</li>
+     *     <li>Return the data read 3 steps above.</li>
+     * </ul>
+     */
     protected <T> T roundTripTest(T data, BiConsumer<T, Path> write, Function<Path, T> read, String ref) throws IOException {
         return roundTripTest(data, write, read, ComparisonUtils::compareTxt, ref);
     }
 
+    /**
+     * Write the given data with given write function, and compare the resulting XML file to given reference.
+     * @return the path of written file.
+     */
     protected <T> Path writeXmlTest(T data, BiConsumer<T, Path> write, String ref) throws IOException {
         return writeTest(data, write, ComparisonUtils::compareXml, ref);
     }
 
+    /**
+     * Write the given data with given write function, and compare the resulting XML file to given reference with the
+     * comparison method provided.
+     * @return the path of written XML file.
+     */
     protected <T> Path writeTest(T data, BiConsumer<T, Path> write, BiConsumer<InputStream, InputStream> compare, String ref) throws IOException {
         Path xmlFile = tmpDir.resolve("data");
         write.accept(data, xmlFile);
@@ -75,8 +111,13 @@ public abstract class AbstractSerDeTest {
         // Read the exported data
         T data2 = read.apply(xmlFile);
 
-        // Export the retrieved data and check the result with the reference
-        writeTest(data2, write, compare, ref);
+        // Export the retrieved data and check the result with first written xml file
+        // This is done to avoid the complexity of the DiffBuilder
+        Path xmlFile2 = tmpDir.resolve("data2");
+        write.accept(data, xmlFile2);
+        assertEquals(-1, Files.mismatch(xmlFile, xmlFile2),
+                "XML file A written after transforming given data " +
+                        "differs from XML file B obtained after reading A and writing the resulting data");
 
         return data2;
     }
