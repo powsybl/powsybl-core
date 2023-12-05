ce804d2b6e014b2b637ba99728cfb27a8b5e381f
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2023-12-05 11:47:01 +0100

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- math/src/test/java/com/powsybl/math/solver/KinsolTest.java
+++ math/src/test/java/com/powsybl/math/solver/KinsolTest.java
@@ -43,7 +43,7 @@ class KinsolTest {
             j2.setAtIndex(2, dq2dv2);
             j2.setAtIndex(3, dq2dph2);
         });
-        double[] x = new double[] {1, 0};
+        double[] x = new double[] {1, 0}; // initial guess
         KinsolParameters parameters = new KinsolParameters();
         KinsolResult result = solver.solve(x, parameters);
         assertSame(KinsolStatus.KIN_SUCCESS, result.getStatus());
