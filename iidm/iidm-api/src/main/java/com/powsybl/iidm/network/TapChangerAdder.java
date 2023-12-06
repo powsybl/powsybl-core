2c6733901aac645b95c117671ce90b6df84be415
Florent MILLOT <millotflo@gmail.com>
2023-12-06 17:34:06 +0100

    Clean code
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/TapChangerAdder.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/TapChangerAdder.java
@@ -4,8 +4,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 package com.powsybl.iidm.network;
 
+/**
+ * @author Florent MILLOT {@literal <florent.millot at rte-france.com>}
+ */
 public interface TapChangerAdder<S extends TapChangerAdder<S, A, C>, A extends TapChangerStepAdder<A, S>, C extends TapChanger<C, ?>> {
 
     S setLowTapPosition(int lowTapPosition);
--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/TapChangerStepAdder.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/TapChangerStepAdder.java
@@ -1,5 +1,15 @@
+/**
+ * Copyright (c) 2023, All partners of the iTesla project (http://www.itesla-project.eu/consortium)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package com.powsybl.iidm.network;
 
+/**
+ * @author Florent MILLOT {@literal <florent.millot at rte-france.com>}
+ */
 public interface TapChangerStepAdder<S extends com.powsybl.iidm.network.TapChangerStepAdder<S, A>, A extends TapChangerAdder<A, S, ?>> {
     S setRho(double rho);
 
