774cda399ab8b7dd8bd771099953d2e83ddeb7b9
Anne Tilloy <anne.tilloy@rte-france.com>
2024-01-03 11:11:29 +0100

    Increase version.
    Signed-off-by: Anne Tilloy <anne.tilloy@rte-france.com>

--- security-analysis/security-analysis-api/src/main/java/com/powsybl/security/action/ActionList.java
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/action/ActionList.java
@@ -29,7 +29,7 @@ import java.util.Objects;
  */
 public class ActionList {
     private final List<Action> actions;
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     public ActionList(List<Action> actions) {
         this.actions = ImmutableList.copyOf(Objects.requireNonNull(actions));
