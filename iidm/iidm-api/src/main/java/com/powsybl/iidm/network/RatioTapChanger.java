d4c77e62846a3ee674b7370bd743949a8e9c7abe
Florent MILLOT <millotflo@gmail.com>
2024-01-05 14:18:39 +0100

    Remove one wildcard on TapChanger interface
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/RatioTapChanger.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/RatioTapChanger.java
@@ -11,7 +11,7 @@ package com.powsybl.iidm.network;
  *
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
  */
-public interface RatioTapChanger extends TapChanger<RatioTapChanger, RatioTapChangerStep, RatioTapChangerStepsReplacer> {
+public interface RatioTapChanger extends TapChanger<RatioTapChanger, RatioTapChangerStep, RatioTapChangerStepsReplacer, RatioTapChangerStepsReplacerStepAdder> {
 
     /**
      * Get the target voltage in kV.
--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/TapChanger.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/TapChanger.java
@@ -6,13 +6,16 @@
  */
 package com.powsybl.iidm.network;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalInt;
 
 /**
  *
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
  */
-public interface TapChanger<C extends TapChanger<C, S, R>, S extends TapChangerStep<S>, R extends TapChangerStepsReplacer<R, ?>> {
+public interface TapChanger<C extends TapChanger<C, S, R, A>, S extends TapChangerStep<S>, R extends TapChangerStepsReplacer<R, A>, A extends TapChangerStepAdder<A, R>> {
 
     /**
      * Get the lowest tap position corresponding to the first step of the tap changer.
