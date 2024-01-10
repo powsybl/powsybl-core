9d596330e7ff7355cd81a14bd72356dd8bc9aecb
Florian Dupuy <florian.dupuy@rte-france.com>
2024-01-10 17:34:52 +0100

    Read with UTF8
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- commons/src/main/java/com/powsybl/commons/binary/BinReader.java
+++ commons/src/main/java/com/powsybl/commons/binary/BinReader.java
@@ -14,6 +14,7 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.function.Supplier;
 
@@ -51,7 +52,7 @@ public class BinReader implements TreeDataReader {
             if (nbBytesRead != stringNbBytes) {
                 throw new PowsyblException("Cannot read the full string, bytes missing: " + (stringNbBytes - nbBytesRead));
             }
-            return new String(stringBytes);
+            return new String(stringBytes, StandardCharsets.UTF_8);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
