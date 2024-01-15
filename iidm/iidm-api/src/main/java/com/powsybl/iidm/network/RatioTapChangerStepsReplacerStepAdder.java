158cb24764840d9bec1bd6d728d40005b595bae2
Florent MILLOT <75525996+flomillot@users.noreply.github.com>
2024-01-15 12:08:36 +0100

    Update iidm/iidm-api/src/main/java/com/powsybl/iidm/network/RatioTapChangerStepsReplacerStepAdder.java
    Co-authored-by: Olivier Perrin <olivier.perrin@rte-france.com>
    Signed-off-by: Florent MILLOT <75525996+flomillot@users.noreply.github.com>

--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/RatioTapChangerStepsReplacerStepAdder.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/RatioTapChangerStepsReplacerStepAdder.java
@@ -9,7 +9,7 @@
 package com.powsybl.iidm.network;
 
 /**
- * Interface to build a single step when using {@link RatioTapChangerStepsReplacer}.
+ * Interface for classes responsible for building a single step when using {@link RatioTapChangerStepsReplacer}.
  *
  * @author Florent MILLOT {@literal <florent.millot at rte-france.com>}
  */
