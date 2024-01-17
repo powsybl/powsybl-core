23ed1e2c23bc6da611c09bb0dd4a611e8335b0b5
Sophie Frasnedo <sophie.frasnedo@rte-france.com>
2024-01-17 13:59:58 +0100

    Add test on ground conversion
    Signed-off-by: Sophie Frasnedo <sophie.frasnedo@rte-france.com>

---
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/GroundConversionTest.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+package com.powsybl.cgmes.conversion.test;
+
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.VoltageLevel;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Sophie Frasnedo {@literal <sophie.frasnedo at rte-france.com>}
+ */
+
+class GroundConversionTest {
+
+    @Test
+    void groundConversionTest() {
+        Network network = Network.read("groundTest.xml", getClass().getResourceAsStream("/groundTest.xml"));
+        long groundCount = network.getVoltageLevelStream().map(VoltageLevel::getGrounds).count();
+        assertEquals(2, groundCount);
+    }
+}
