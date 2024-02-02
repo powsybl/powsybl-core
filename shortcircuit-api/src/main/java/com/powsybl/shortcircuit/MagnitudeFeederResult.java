b3fd47fdd2531779a710a156ba5eba43d3e56bda
Coline PILOQUET <coline.piloquet@rte-france.com>
2024-02-02 13:02:09 +0100

    square brackets for unit
    Signed-off-by: Coline PILOQUET <coline.piloquet@rte-france.com>

--- shortcircuit-api/src/main/java/com/powsybl/shortcircuit/MagnitudeFeederResult.java
+++ shortcircuit-api/src/main/java/com/powsybl/shortcircuit/MagnitudeFeederResult.java
@@ -19,7 +19,7 @@ public class MagnitudeFeederResult extends AbstractFeederResult {
     }
 
     /**
-     * The three-phase current magnitude (in A).
+     * The three-phase current magnitude [in A].
      */
     public double getCurrent() {
         return current;
