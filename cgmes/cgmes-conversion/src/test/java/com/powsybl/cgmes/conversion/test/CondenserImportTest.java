5d6e6eda990384141296e2d7cc145da7a868191e
Luma <zamarrenolm@aia.es>
2024-02-22 23:22:23 +0100

    CGMES condensers shall not have generating units
    Signed-off-by: Luma <zamarrenolm@aia.es>

---
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/CondenserImportTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+package com.powsybl.cgmes.conversion.test;
+
+import com.powsybl.commons.test.AbstractSerDeTest;
+import com.powsybl.iidm.network.Generator;
+import com.powsybl.iidm.network.Network;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Luma Zamarreño {@literal <zamarrenolm at aia.es>}
+ */
+
+class CondenserImportTest extends AbstractSerDeTest {
+
+    @Test
+    void cgmes2condenserConversionTest() {
+        Network network = Network.read("condenser2_EQ.xml", getClass().getResourceAsStream("/issues/condenser2_EQ.xml"));
+        assertEquals(1, network.getGeneratorCount());
+        Generator g = network.getGenerators().iterator().next();
+        assertEquals(0, g.getMinP());
+        assertEquals(0, g.getMaxP());
+    }
+
+    @Test
+    void cgmes3condenserConversionTest() {
+        Network network = Network.read("condenser3_EQ.xml", getClass().getResourceAsStream("/issues/condenser3_EQ.xml"));
+        assertEquals(1, network.getGeneratorCount());
+        Generator g = network.getGenerators().iterator().next();
+        assertEquals(0, g.getMinP());
+        assertEquals(0, g.getMaxP());
+    }
+}
