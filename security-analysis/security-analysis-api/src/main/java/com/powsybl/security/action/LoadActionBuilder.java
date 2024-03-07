aa51d1d7e8f48def27eb3a2ec7b05c561385afa6
Bertrand Rix <bertrand.rix@artelys.com>
2024-03-07 10:10:44 +0100

    Author and copyrights 2.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- security-analysis/security-analysis-api/src/main/java/com/powsybl/security/action/LoadActionBuilder.java
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/action/LoadActionBuilder.java
@@ -1,5 +1,14 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.security.action;
 
+/**
+ * @author Anne Tilloy {@literal <anne.tilloy@rte-france.com>}
+ */
 public class LoadActionBuilder extends AbstractLoadActionBuilder<LoadAction> {
     public LoadAction build() {
         if (relativeValue == null) {
