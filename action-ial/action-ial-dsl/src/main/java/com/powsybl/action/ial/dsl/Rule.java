9d1b984614f26943dbc3cdbaccbaafc434dbfa6c
Bertrand Rix <bertrand.rix@artelys.com>
2024-03-08 14:27:04 +0100

    Rename powsybl-action module to powsybl-action-ial.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

---
+++ action-ial/action-ial-dsl/src/main/java/com/powsybl/action/ial/dsl/Rule.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2017, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.action.ial.dsl;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
+ */
+public class Rule {
+
+    private final String id;
+
+    private String description;
+
+    private final Condition condition;
+
+    private final int life;
+
+    private final List<String> actions;
+
+    private final RuleType type;
+
+    public Rule(String id, Condition condition, int life, String... actions) {
+        this(id, condition, life, Arrays.asList(actions));
+    }
+
+    public Rule(String id, Condition condition, int life, List<String> actions) {
+        this(id, condition, life, RuleType.APPLY, actions);
+    }
+
+    public Rule(String id, Condition condition, int life, RuleType type, String... actions) {
+        this(id, condition, life, type, Arrays.asList(actions));
+    }
+
+    public Rule(String id, Condition condition, int life, RuleType type, List<String> actions) {
+        if (life < 0) {
+            throw new IllegalArgumentException("Invalid life value, has to be >= 0");
+        }
+        this.id = Objects.requireNonNull(id);
+        this.condition = Objects.requireNonNull(condition);
+        this.type = Objects.requireNonNull(type);
+        if (type.equals(RuleType.TEST) && life != 1) {
+            throw new IllegalArgumentException("Invalid life value, has to be 1 in test rule.");
+        }
+        this.life = life;
+        this.actions = Objects.requireNonNull(actions);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Condition getCondition() {
+        return condition;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public List<String> getActions() {
+        return Collections.unmodifiableList(actions);
+    }
+
+    public RuleType getType() {
+        return type;
+    }
+}
---
+++ action-ial/action-ial-simulator/src/test/java/com/powsybl/action/ial/simulator/AlternativeTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.action.ial.simulator;
+
+import com.powsybl.action.ial.simulator.loadflow.LoadFlowActionSimulatorObserver;
+import com.powsybl.iidm.network.Generator;
+import com.powsybl.iidm.network.Load;
+import com.powsybl.iidm.network.Network;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class AlternativeTest extends AbstractLoadFlowRulesEngineTest {
+
+    private final LoadFlowActionSimulatorObserver obs = Mockito.mock(LoadFlowActionSimulatorObserver.class);
+
+    @Override
+    protected Network createNetwork() {
+        Network network = EurostagTutorialExample1WithTemporaryLimitFactory.create();
+        network.getVoltageLevel("VLHV1").getBusBreakerView().getBus("NHV1").setV(380).setAngle(0);
+        network.getLine("NHV1_NHV2_2").getTerminal1().setP(300).setQ(100);
+        return network;
+    }
+
+    @Override
+    protected String getDslFile() {
+        return "/rule-with-test.groovy";
+    }
+
+    @Test
+    void test() {
+        Generator generator = network.getGenerator("GEN");
+        Load load = network.getLoad("LOAD");
+        double targetP = generator.getTargetP();
+        double loadP0 = load.getP0();
+
+        engine.start(actionDb, "contingency1"); // life = 3
+        assertEquals(targetP + 2.0, generator.getTargetP(), 0.0);
+        assertEquals(loadP0, load.getP0(), 0.0);
+
+        // once for action1, once for action2
+        verify(obs, times(2)).beforeTest(Mockito.any(), Mockito.any());
+    }
+
+    @Override
+    protected LoadFlowActionSimulatorObserver createObserver() {
+        return obs;
+    }
+
+}
