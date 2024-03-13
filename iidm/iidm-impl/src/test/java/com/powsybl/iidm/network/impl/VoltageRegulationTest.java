060901c1e6fa1c9c06b6229d9657ab03037e182b
Florian Dupuy <florian.dupuy@rte-france.com>
2024-03-13 13:35:06 +0100

    Fix code smell
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/VoltageRegulationTest.java
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/VoltageRegulationTest.java
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * @author Coline Piloquet {@literal <coline.piloquet@rte-france.fr>}
  */
-public class VoltageRegulationTest {
+class VoltageRegulationTest {
 
     @Test
     void testMultiVariant() {
