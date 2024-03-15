d445265d3f7b8e6a45b7e8f8ea42233177173c53
Olivier Perrin <olivier.perrin@rte-france.com>
2024-03-15 13:56:23 +0100

    Rename old 'limitReduction' float to 'limitReductionValue' (except in powsybl-action-dsl module and in LimitViolation)
    Signed-off-by: Olivier Perrin <olivier.perrin@rte-france.com>

--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/Branch.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/Branch.java
@@ -471,31 +471,31 @@ public interface Branch<I extends Branch<I>> extends Identifiable<I> {
     /**
      * Only checks overloading for LimitType.Current and permanent limits
      */
-    boolean isOverloaded(float limitReduction);
+    boolean isOverloaded(float limitReductionValue);
 
     int getOverloadDuration();
 
-    boolean checkPermanentLimit(TwoSides side, float limitReduction, LimitType type);
+    boolean checkPermanentLimit(TwoSides side, float limitReductionValue, LimitType type);
 
     boolean checkPermanentLimit(TwoSides side, LimitType type);
 
-    boolean checkPermanentLimit1(float limitReduction, LimitType type);
+    boolean checkPermanentLimit1(float limitReductionValue, LimitType type);
 
     boolean checkPermanentLimit1(LimitType type);
 
-    boolean checkPermanentLimit2(float limitReduction, LimitType type);
+    boolean checkPermanentLimit2(float limitReductionValue, LimitType type);
 
     boolean checkPermanentLimit2(LimitType type);
 
-    Overload checkTemporaryLimits(TwoSides side, float limitReduction, LimitType type);
+    Overload checkTemporaryLimits(TwoSides side, float limitReductionValue, LimitType type);
 
     Overload checkTemporaryLimits(TwoSides side, LimitType type);
 
-    Overload checkTemporaryLimits1(float limitReduction, LimitType type);
+    Overload checkTemporaryLimits1(float limitReductionValue, LimitType type);
 
     Overload checkTemporaryLimits1(LimitType type);
 
-    Overload checkTemporaryLimits2(float limitReduction, LimitType type);
+    Overload checkTemporaryLimits2(float limitReductionValue, LimitType type);
 
     Overload checkTemporaryLimits2(LimitType type);
 }
