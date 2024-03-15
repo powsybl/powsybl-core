148dd38650cc3fbe2374b47de51db44fc6cbf290
vidaldid-rte <156446663+vidaldid-rte@users.noreply.github.com>
2024-03-15 09:34:46 +0100

    Sensitivity function: add new SensitivityFunctionType BUS_REACTIVE_POWER (#2930)
    Signed-off-by: VIDAL Didier (Externe) <didier.vidal_externe@rte-france.com>

--- sensitivity-analysis-api/src/main/java/com/powsybl/sensitivity/SensitivityFunctionType.java
+++ sensitivity-analysis-api/src/main/java/com/powsybl/sensitivity/SensitivityFunctionType.java
@@ -20,6 +20,7 @@ import java.util.OptionalInt;
  *     <li>{@link #BRANCH_REACTIVE_POWER_1} and {@link #BRANCH_REACTIVE_POWER_2} if you want to monitor the reactive power in MVar of a network branch (lines,
  * two windings transformer, dangling lines, etc.). Use 1 for side 1 and use 2 for side 2. In case of a three windings transformer,
  * use {@link #BRANCH_REACTIVE_POWER_3} to monitor the reactive power in MVar of the leg 3 (network side).</li>
+ *     <li>{@link #BUS_REACTIVE_POWER} if you want to monitor the reactive power injection in MVar of a specific network bus</li>
  *     <li>{@link #BUS_VOLTAGE} if you want to monitor the voltage in KV of a specific network bus.</li>
  * </ul>
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
@@ -44,6 +45,8 @@ public enum SensitivityFunctionType {
     BRANCH_CURRENT_3(3),
     /** in MVar */
     BRANCH_REACTIVE_POWER_3(3),
+    /** In MVar */
+    BUS_REACTIVE_POWER,
     /** in kV */
     BUS_VOLTAGE;
 
