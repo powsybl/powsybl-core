62d897eceb2ee8dc268161f3c5c60bfbc2e322f2
Nicolas Rol <nicolas.rol@rte-france.com>
2024-03-28 16:25:13 +0100

    comment unseless test
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- commons/src/test/java/com/powsybl/commons/datasource/DataSourceTest.java
+++ commons/src/test/java/com/powsybl/commons/datasource/DataSourceTest.java
@@ -11,18 +11,18 @@ import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Objects;
+import java.util.stream.Stream;
 
 import static com.powsybl.commons.datasource.DataSourceUtil.createNewDataSource;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 /**
  * @author Nicolas Rol {@literal <nicolas.rol at rte-france.com>}
@@ -43,14 +43,17 @@ class DataSourceTest {
         fileSystem.close();
     }
 
-    @Test
-    void test() throws IOException {
-        unitTest("foo.xml", DirectoryDataSource.class);
-        unitTest("foo.xml.gz", GzDataSource.class);
-        unitTest("foo.iidm.bz2", Bzip2DataSource.class);
+    private static Stream<Arguments> provideArgumentsForTestClass() {
+        return Stream.of(
+            Arguments.of("foo.xml", DirectoryDataSource.class),
+            Arguments.of("foo.xml.gz", GzDataSource.class),
+            Arguments.of("foo.iidm.bz2", Bzip2DataSource.class)
+        );
     }
 
-    private void unitTest(String fileName, Class<? extends NewDataSource> dataSourceClass) throws IOException {
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForTestClass")
+    void testClass(String fileName, Class<? extends NewDataSource> dataSourceClass) throws IOException {
         // Create the fake file
         Files.createFile(fileSystem.getPath(directory + fileName));
 
@@ -59,27 +62,26 @@ class DataSourceTest {
 
         // Assert the class of the datasource
         assertInstanceOf(dataSourceClass, dataSource);
-
     }
 
-    @Test
-    void newTest() throws IOException {
-        Bzip2DataSource dataSource = (Bzip2DataSource) new DataSourceBuilder()
-            .withDirectory(Path.of(Objects.requireNonNull(getClass().getClassLoader().getResource("test/foo.txt")).getPath()).getParent())
-            .withSourceFormat(".iidm")
-            .withBaseName("foo")
-            .withCompressionFormat(CompressionFormat.BZIP2)
-            .build();
-
-        // write file
-        try (OutputStream os = dataSource.newOutputStream("test", "iidm", false)) {
-            os.write("line1".getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            fail();
-        }
-
-        for (String name : dataSource.listNames(".*")) {
-            assertTrue(dataSource.exists(name));
-        }
-    }
+//    @Test
+//    void newTest() throws IOException {
+//        Bzip2DataSource dataSource = (Bzip2DataSource) new DataSourceBuilder()
+//            .withDirectory(Path.of(Objects.requireNonNull(getClass().getClassLoader().getResource("test/foo.txt")).getPath()).getParent())
+//            .withSourceFormat(".iidm")
+//            .withBaseName("foo")
+//            .withCompressionFormat(CompressionFormat.BZIP2)
+//            .build();
+//
+//        // write file
+//        try (OutputStream os = dataSource.newOutputStream("test", "iidm", false)) {
+//            os.write("line1".getBytes(StandardCharsets.UTF_8));
+//        } catch (IOException e) {
+//            fail();
+//        }
+//
+//        for (String name : dataSource.listNames(".*")) {
+//            assertTrue(dataSource.exists(name));
+//        }
+//    }
 }
