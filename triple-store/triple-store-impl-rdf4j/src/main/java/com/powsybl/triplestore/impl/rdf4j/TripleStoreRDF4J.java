911fadda9d86fc8256c9e112c27dff2baec1f6c7
Nicolas Rol <nicolas.rol@rte-france.com>
2024-04-15 17:18:56 +0200

    reduce method complexity
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- triple-store/triple-store-impl-rdf4j/src/main/java/com/powsybl/triplestore/impl/rdf4j/TripleStoreRDF4J.java
+++ triple-store/triple-store-impl-rdf4j/src/main/java/com/powsybl/triplestore/impl/rdf4j/TripleStoreRDF4J.java
@@ -363,14 +363,18 @@ public class TripleStoreRDF4J extends AbstractPowsyblTripleStore {
                 throw new TripleStoreException(message);
             }
             // Then all the other statements
-            for (final Statement st : model.filter(subject, null, null)) {
-                if (st.getPredicate().equals(RDF.TYPE)) {
-                    continue;
-                }
-                writer.handleStatement(st);
-            }
+            handleStatement(model, writer, subject);
         }
         writer.endRDF();
+    }
+
+    private void handleStatement(Model model, RDFWriter writer, Resource subject) {
+        for (final Statement st : model.filter(subject, null, null)) {
+            if (st.getPredicate().equals(RDF.TYPE)) {
+                continue;
+            }
+            writer.handleStatement(st);
+        }
     }
 
     private static int statementsCount(RepositoryConnection conn, Resource ctx) {
