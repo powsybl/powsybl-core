24e7d55cd5012cbd2bd5e4532e0cecce6197fe52
vmouradian <valentin.mouradian@artelys.com>
2024-04-19 19:35:47 +0200

    improve Unit tests
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>

--- iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/AreasTests.java
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/AreasTests.java
@@ -1,5 +1,6 @@
 package com.powsybl.iidm.network.impl;
 
+import com.google.common.collect.Iterables;
 import com.powsybl.commons.PowsyblException;
 import com.powsybl.iidm.network.*;
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
@@ -13,8 +14,6 @@ class AreasTests {
 
     final Network network = createEurostagExampleWithAreas();
 
-    final Network subnetwork = network.createSubnetwork("subnetwork_id", "Subnetwork", "json");
-
     final AreaType biddingZone = network.getAreaType("bz");
     final AreaType region = network.getAreaType("rg");
 
@@ -22,15 +21,13 @@ class AreasTests {
     final Area biddingZoneA = network.getArea("bza");
     final Area biddingZoneB = network.getArea("bzb");
     final Area regionA = network.getArea("rga");
-    final Area regionB = network.getArea("rgb");
 
-    final AicArea aicA = network.getAicArea("aic_a");
+    final AicArea aicA = (AicArea) network.getArea("aic_a");
 
     @Test
-    void checkAreasCreated() {
-        assertEquals("bz", biddingZone.getId());
-        assertEquals("Bidding_Zone", biddingZone.getNameOrId());
-        assertEquals(network, biddingZone.getNetwork());
+    void areaAttributesTest() {
+        assertEquals(IdentifiableType.AREA, biddingZoneA.getType());
+        assertEquals(IdentifiableType.AREA_TYPE, biddingZone.getType());
 
         assertEquals("rg", region.getId());
         assertEquals("Region", region.getNameOrId());
@@ -40,76 +37,118 @@ class AreasTests {
         assertEquals("BZ_A", biddingZoneA.getNameOrId());
         assertEquals(biddingZone, biddingZoneA.getAreaType());
 
-        assertEquals("bzb", biddingZoneB.getId());
-        assertEquals("BZ_B", biddingZoneB.getNameOrId());
-        assertEquals(biddingZone, biddingZoneB.getAreaType());
+        assertEquals("aic_a", aicA.getId());
+        assertEquals("Aic_A", aicA.getNameOrId());
+        assertEquals(aic, aicA.getAreaType());
+        assertEquals(10.0, aicA.getAcNetInterchangeTarget());
+        assertEquals(0.1, aicA.getAcNetInterchangeTolerance());
 
-        assertEquals("rga", regionA.getId());
-        assertEquals("Region_A", regionA.getNameOrId());
-        assertEquals(region, regionA.getAreaType());
-        assertEquals("rgb", regionB.getId());
-        assertEquals("Region_B", regionB.getNameOrId());
-        assertEquals(region, regionB.getAreaType());
+    }
 
-        assertEquals(List.of(biddingZoneA, biddingZoneB, regionA, regionB), network.getAreaStream().toList());
+    @Test
+    void areaIterableAndStreamGetterCheck() {
+        assertEquals(4, Iterables.size(network.getAreas()));
+        assertEquals(1, Iterables.size(network.getAicAreas()));
+        assertEquals(3, Iterables.size(network.getAreaTypes()));
+
+        assertEquals(List.of(biddingZoneA, biddingZoneB, regionA, aicA), network.getAreaStream().toList());
         assertEquals(List.of(aicA), network.getAicAreaStream().toList());
         assertEquals(List.of(biddingZone, region, aic), network.getAreaTypeStream().toList());
     }
 
     @Test
-    void chekSubnetworkAreasNotSupported() {
-        List<? extends Throwable> exceptions = List.of(
-                assertThrows(PowsyblException.class, subnetwork::newArea),
-                assertThrows(PowsyblException.class, subnetwork::newAicArea),
-                assertThrows(PowsyblException.class, subnetwork::newAreaType),
-                assertThrows(PowsyblException.class, subnetwork::getAreaTypeStream),
-                assertThrows(PowsyblException.class, subnetwork::getAreaStream),
-                assertThrows(PowsyblException.class, subnetwork::getAicAreaStream),
-                assertThrows(PowsyblException.class, subnetwork::getAreas),
-                assertThrows(PowsyblException.class, subnetwork::getAicAreas),
-                assertThrows(PowsyblException.class, subnetwork::getAreaTypes),
-                assertThrows(PowsyblException.class, () -> subnetwork.getAreaType("type")),
-                assertThrows(PowsyblException.class, () -> subnetwork.getArea("area")),
-                assertThrows(PowsyblException.class, () -> subnetwork.getAicArea("aic")));
-        exceptions.forEach(ex -> assertEquals("Areas are not supported in subnetworks", ex.getMessage()));
+    void checkSubnetworkGetAreasMethods() {
+        Network subnetwork = network.createSubnetwork("subnetwork_id", "Subnetwork", "json");
+        assertEquals(biddingZone, subnetwork.getAreaType("bz"));
+        assertEquals(biddingZoneA, subnetwork.getArea("bza"));
+        assertEquals(aicA, subnetwork.getArea("aic_a"));
+
+        assertEquals(4, Iterables.size(subnetwork.getAreas()));
+        assertEquals(1, Iterables.size(subnetwork.getAicAreas()));
+        assertEquals(3, Iterables.size(subnetwork.getAreaTypes()));
+
+        assertEquals(List.of(biddingZoneA, biddingZoneB, regionA, aicA), subnetwork.getAreaStream().toList());
+        assertEquals(List.of(aicA), subnetwork.getAicAreaStream().toList());
+        assertEquals(List.of(biddingZone, region, aic), subnetwork.getAreaTypeStream().toList());
     }
 
     @Test
-    void addVoltageLevelsTest() {
-        // add voltageLevel to Area
+    void addVoltageLevelsToAreaTest() {
         VoltageLevel vlhv1 = network.getVoltageLevel("VLHV1");
         VoltageLevel vlhv2 = network.getVoltageLevel("VLHV2");
-        VoltageLevel vlload = network.getVoltageLevel("VLLOAD");
-        VoltageLevel vlgen = network.getVoltageLevel("VLGEN");
+        biddingZoneA.addVoltageLevel(vlhv1);
+        vlhv2.addArea(biddingZoneA);
 
+        assertEquals(List.of(biddingZoneA), vlhv1.getAreasStream().toList());
+        assertEquals(List.of(biddingZoneA), vlhv2.getAreasStream().toList());
+        assertEquals(List.of(vlhv1, vlhv2), biddingZoneA.getVoltageLevelStream().toList());
+        assertEquals(2, Iterables.size(biddingZoneA.getVoltageLevels()));
+    }
+
+    @Test
+    void addSameVoltageLevelToAreaTest() {
+        VoltageLevel vlhv1 = network.getVoltageLevel("VLHV1");
+        biddingZoneA.addVoltageLevel(vlhv1);
+        vlhv1.addArea(biddingZoneA);
+
+        assertEquals(List.of(biddingZoneA), vlhv1.getAreasStream().toList());
+        assertEquals(List.of(vlhv1), biddingZoneA.getVoltageLevelStream().toList());
+    }
+
+    @Test
+    void addAreasToVoltageLevelTest() {
+        VoltageLevel vlhv1 = network.getVoltageLevel("VLHV1");
+        vlhv1.addArea(biddingZoneA);
+        regionA.addVoltageLevel(vlhv1);
+
+        assertEquals(List.of(biddingZoneA, regionA), vlhv1.getAreasStream().toList());
+        assertEquals(List.of(vlhv1), biddingZoneA.getVoltageLevelStream().toList());
+        assertEquals(List.of(vlhv1), regionA.getVoltageLevelStream().toList());
+    }
+
+    @Test
+    void throwAddNewAreaSameTypeTest() {
+        VoltageLevel vlhv2 = network.getVoltageLevel("VLHV2");
         biddingZoneA.addVoltageLevel(vlhv2);
-        vlload.addToArea(biddingZoneA);
-        biddingZoneB.addVoltageLevel(vlhv1);
-        vlgen.addToArea(biddingZoneB);
+        var e1 = assertThrows(PowsyblException.class, () -> biddingZoneB.addVoltageLevel(vlhv2));
+        var e2 = assertThrows(PowsyblException.class, () -> vlhv2.addArea(biddingZoneB));
 
-        regionA.addVoltageLevel(vlload);
-        vlhv1.addToArea(regionA);
-        regionB.addVoltageLevel(vlgen);
-        vlhv2.addToArea(regionB);
+        String expectedMessage = "VoltageLevel VLHV2 is already in Area of the same type=Bidding_Zone with id=bza";
+        assertEquals(expectedMessage, e1.getMessage());
+        assertEquals(expectedMessage, e2.getMessage());
+    }
 
-        assertEquals(List.of(biddingZoneA, regionA), vlload.getAreasStream().toList());
-        assertEquals(List.of(biddingZoneB, regionB), vlgen.getAreasStream().toList());
-        assertEquals(List.of(biddingZoneA, regionB), vlhv2.getAreasStream().toList());
-        assertEquals(List.of(biddingZoneB, regionA), vlhv1.getAreasStream().toList());
+    @Test
+    void throwRemovedVoltageLevel() {
 
-        assertEquals(List.of(vlhv2, vlload), biddingZoneA.getVoltageLevelsStream().toList());
-        assertEquals(List.of(vlhv1, vlgen), biddingZoneB.getVoltageLevelsStream().toList());
-        assertEquals(List.of(vlload, vlhv1), regionA.getVoltageLevelsStream().toList());
-        assertEquals(List.of(vlgen, vlhv2), regionB.getVoltageLevelsStream().toList());
+        VoltageLevel dummy = network.newVoltageLevel()
+                .setId("dummy")
+                .setNominalV(400.0)
+                .setTopologyKind(TopologyKind.NODE_BREAKER)
+                .setLowVoltageLimit(0.0)
+                .setHighVoltageLimit(0.0)
+                .add();
+        dummy.remove();
 
-        var e1 = assertThrows(PowsyblException.class, () -> biddingZoneA.addVoltageLevel(vlgen));
-        assertEquals("VoltageLevel VLGEN is already in Area bza", e1.getMessage());
-        var e2 = assertThrows(PowsyblException.class, () -> biddingZoneB.addVoltageLevel(vlhv2));
-        assertEquals("VoltageLevel VLHV2 is already in Area bzb", e2.getMessage());
-        var e3 = assertThrows(PowsyblException.class, () -> regionA.addVoltageLevel(vlgen));
-        assertEquals("VoltageLevel VLGEN is already in Area rga", e3.getMessage());
-        var e4 = assertThrows(PowsyblException.class, () -> regionB.addVoltageLevel(vlhv1));
-        assertEquals("VoltageLevel VLHV1 is already in Area rgb", e4.getMessage());
+        var e1 = assertThrows(PowsyblException.class, () -> dummy.getArea(biddingZone));
+        var e2 = assertThrows(PowsyblException.class, dummy::getAreasStream);
+
+        String expectedMessage = "Cannot access area of removed voltage level dummy";
+        assertEquals(expectedMessage, e1.getMessage());
+        assertEquals(expectedMessage, e2.getMessage());
+    }
+
+    @Test
+    void throwDifferntNetworksTest() {
+        Network network2 = EurostagTutorialExample1Factory.create();
+        VoltageLevel vlhv2Bis = network2.getVoltageLevel("VLHV2");
+
+        var e1 = assertThrows(PowsyblException.class, () -> biddingZoneB.addVoltageLevel(vlhv2Bis));
+        var e2 = assertThrows(PowsyblException.class, () -> vlhv2Bis.addArea(biddingZoneB));
+
+        String expectedMessage = "Identifiable VLHV2 must belong to the same network as the Area to which it is added";
+        assertEquals(expectedMessage, e1.getMessage());
+        assertEquals(expectedMessage, e2.getMessage());
     }
 
     static Network createEurostagExampleWithAreas() {
@@ -137,20 +176,14 @@ class AreasTests {
                 .setName("BZ_B")
                 .setAreaType(biddingZone)
                 .add();
-
         network.newArea()
                 .setId("rga")
                 .setName("Region_A")
                 .setAreaType(region)
                 .add();
-        network.newArea()
-                .setId("rgb")
-                .setName("Region_B")
-                .setAreaType(region)
-                .add();
         network.newAicArea()
-                .setAcNetInterchangeTarget(0.0)
-                .setAcNetInterchangeTolerance(0.0)
+                .setAcNetInterchangeTarget(10.0)
+                .setAcNetInterchangeTolerance(0.1)
                 .setId("aic_a")
                 .setName("Aic_A")
                 .setAreaType(aic)
