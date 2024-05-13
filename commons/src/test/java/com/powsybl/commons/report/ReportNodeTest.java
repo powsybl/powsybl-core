b009d4420e8b442f5011935c9c75f9b831498ae8
Florian Dupuy <florian.dupuy@rte-france.com>
2024-05-13 16:00:03 +0200

    Add unit test
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- commons/src/test/java/com/powsybl/commons/report/ReportNodeTest.java
+++ commons/src/test/java/com/powsybl/commons/report/ReportNodeTest.java
@@ -22,6 +22,64 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class ReportNodeTest extends AbstractSerDeTest {
 
     @Test
+    void testValues() throws IOException {
+        ReportNode root = ReportNode.newRootReportNode()
+                .withMessageTemplate("rootTemplate", """
+                        Root message
+                        doubleUntyped: ${doubleUntyped}
+                        doubleTyped: ${doubleTyped}
+                        floatUntyped: ${floatUntyped}
+                        floatTyped: ${floatTyped}
+                        intUntyped: ${intUntyped}
+                        intTyped: ${intTyped}
+                        longUntyped: ${longUntyped}
+                        longTyped: ${longTyped}
+                        booleanUntyped: ${booleanUntyped}
+                        booleanTyped: ${booleanTyped}
+                        stringUntyped: ${stringUntyped}
+                        stringTyped: ${stringTyped}
+                        severity: ${reportSeverity}""")
+                .withUntypedValue("doubleUntyped", 4.3)
+                .withTypedValue("doubleTyped", 4.4, TypedValue.ACTIVE_POWER)
+                .withUntypedValue("floatUntyped", -1.5f)
+                .withTypedValue("floatTyped", 0.6f, TypedValue.IMPEDANCE)
+                .withUntypedValue("intUntyped", 4)
+                .withTypedValue("intTyped", -2, "count")
+                .withUntypedValue("longUntyped", 5L)
+                .withTypedValue("longTyped", -3L, "count")
+                .withUntypedValue("booleanUntyped", true)
+                .withTypedValue("booleanTyped", false, "protected")
+                .withUntypedValue("stringUntyped", "value")
+                .withTypedValue("stringTyped", "filename", TypedValue.FILENAME)
+                .withSeverity(TypedValue.INFO_SEVERITY)
+                .build();
+        assertEquals("""
+                        Root message
+                        doubleUntyped: 4.3
+                        doubleTyped: 4.4
+                        floatUntyped: -1.5
+                        floatTyped: 0.6
+                        intUntyped: 4
+                        intTyped: -2
+                        longUntyped: 5
+                        longTyped: -3
+                        booleanUntyped: true
+                        booleanTyped: false
+                        stringUntyped: value
+                        stringTyped: filename
+                        severity: INFO""", root.getMessage());
+
+        ReportNode child = root.newReportNode()
+                .withMessageTemplate("child", "Child message with parent value ${stringTyped} and own severity '${reportSeverity}'")
+                .withSeverity("Very important custom severity")
+                .add();
+        assertEquals("Child message with parent value filename and own severity 'Very important custom severity'", child.getMessage());
+        assertEquals(1, root.getChildren().size());
+
+        roundTripTest(root, ReportNodeSerializer::write, ReportNodeDeserializer::read, "/testValuesReportNode.json");
+    }
+
+    @Test
     void testInclude() throws IOException {
         ReportNode root = ReportNode.newRootReportNode()
                 .withMessageTemplate("rootTemplate", "Root message")
