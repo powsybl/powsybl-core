63324c43fa3366d29e2f8c706fd29b7e424bdf5c
Florian Dupuy <florian.dupuy@rte-france.com>
2024-05-14 10:50:44 +0200

    Check template keys when merging contexts
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- commons/src/main/java/com/powsybl/commons/report/RootContext.java
+++ commons/src/main/java/com/powsybl/commons/report/RootContext.java
@@ -35,6 +35,6 @@ public class RootContext {
     }
 
     public synchronized void merge(RootContext otherContext) {
-        dictionary.putAll(otherContext.dictionary);
+        otherContext.dictionary.forEach(this::addDictionaryEntry);
     }
 }
