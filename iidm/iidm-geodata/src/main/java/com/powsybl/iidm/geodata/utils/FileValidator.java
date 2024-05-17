0e33b62078cf57329269920ecb509198161e732e
Hugo KULESZA <hugo.kulesza@rte-france.com>
2024-05-17 09:55:14 +0200

    rework copyright dates
    Signed-off-by: Hugo KULESZA <hugo.kulesza@rte-france.com>

--- iidm/iidm-geodata/src/main/java/com/powsybl/iidm/geodata/utils/FileValidator.java
+++ iidm/iidm-geodata/src/main/java/com/powsybl/iidm/geodata/utils/FileValidator.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -28,6 +28,7 @@ import java.util.stream.Collectors;
 
 /**
  * @author Ahmed Bendaamer {@literal <ahmed.bendaamer at rte-france.com>}
+ * @author Hugo Kulesza {@literal <hugo.kulesza at rte-france.com>}
  */
 public final class FileValidator {
 
--- iidm/iidm-geodata/src/test/java/com/powsybl/iidm/geodata/utils/FileValidatorTest.java
+++ iidm/iidm-geodata/src/test/java/com/powsybl/iidm/geodata/utils/FileValidatorTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -18,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Ahmed Bendaamer {@literal <ahmed.bendaamer at rte-france.com>}
+ * @author Hugo Kulesza {@literal <hugo.kulesza at rte-france.com>}
  */
 class FileValidatorTest {
 
