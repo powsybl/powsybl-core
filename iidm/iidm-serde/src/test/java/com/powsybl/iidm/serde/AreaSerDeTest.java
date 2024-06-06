acd5ff2a912a6a989fdd8f27cdf88513cb531f4e
vmouradian <valentin.mouradian@artelys.com>
2024-06-06 15:50:46 +0200

    test for Serde with subnetworks and Areas
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>

--- iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/AreaSerDeTest.java
+++ iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/AreaSerDeTest.java
@@ -33,11 +33,18 @@ class AreaSerDeTest extends AbstractIidmSerDeTest {
         // Create a base network
         Network network = Network.create("test", "test");
         network.setCaseDate(ZonedDateTime.parse("2020-03-04T13:20:30.476+01:00"));
+        Network subnetwork = network.createSubnetwork("sub", "Sub", "format");
+        subnetwork.setCaseDate(ZonedDateTime.parse("2020-03-04T13:20:30.476+01:00"));
         Substation s1 = network.newSubstation().setId("sub1").add();
         VoltageLevel vl1 = s1.newVoltageLevel().setId("VL1").setNominalV(1).setTopologyKind(TopologyKind.BUS_BREAKER).add();
         network.newSubstation().setId("sub2").add();
         vl1.getBusBreakerView().newBus().setId("N1").add();
         vl1.getBusBreakerView().newBus().setId("N2").add();
+
+        Substation s2 = subnetwork.newSubstation().setId("sub3").add();
+        VoltageLevel vl2 = s2.newVoltageLevel().setId("VL2").setNominalV(1).setTopologyKind(TopologyKind.BUS_BREAKER).add();
+        vl2.getBusBreakerView().newBus().setId("N3").add();
+
         final Load load1 = vl1.newLoad().setId("L1").setBus("N1").setP0(0).setQ0(0).add();
         final DanglingLine danglingLine = vl1.newDanglingLine().setId("DL1").setBus("N2").setR(0.0).setX(0.0).setP0(0).setQ0(0).add();
 
@@ -51,6 +58,9 @@ class AreaSerDeTest extends AbstractIidmSerDeTest {
         network.newArea().setAreaType(controlAreaType).setId("ControlAreaId1").setName("ControlAreaName1").add();
         vl1.addArea(network.getArea("BidZoneId1"));
         vl1.addArea(network.getArea("ControlAreaId1"));
+
+        subnetwork.newArea().setAreaType(biddingZoneType).setId("BidZoneId3").setName("BidZoneName3").add();
+        vl2.addArea(subnetwork.getArea("BidZoneId3"));
         return network;
     }
 
