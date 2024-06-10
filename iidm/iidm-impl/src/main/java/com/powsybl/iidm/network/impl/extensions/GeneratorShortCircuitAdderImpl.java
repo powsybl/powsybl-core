c2d8d399542ad71952a1f869a2cceca8ae5b4bd5
Coline PILOQUET <coline.piloquet@rte-france.com>
2024-06-10 16:11:11 +0200

    Delete shortcircuits module and backwards compatibility for BatteryShortCircuits
    Signed-off-by: Coline PILOQUET <coline.piloquet@rte-france.com>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/extensions/GeneratorShortCircuitAdderImpl.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/extensions/GeneratorShortCircuitAdderImpl.java
@@ -18,7 +18,7 @@ import com.powsybl.iidm.network.extensions.GeneratorShortCircuitAdder;
  * @author Coline Piloquet {@literal <coline.piloquet@rte-france.fr>}
  */
 public class GeneratorShortCircuitAdderImpl extends AbstractExtensionAdder<Generator, GeneratorShortCircuit>
-    implements GeneratorShortCircuitAdder {
+        implements GeneratorShortCircuitAdder {
 
     double directTransX = 0;
     double directSubtransX = Double.NaN;
