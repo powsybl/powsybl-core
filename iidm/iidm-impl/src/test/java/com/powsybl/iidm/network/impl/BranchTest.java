e55dedd6e40a31aec93a06f7bb3bfc19af6b0136
Etienne LESOT <etienne.lesot@rte-france.com>
2024-07-25 16:58:27 +0200

    more test
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/BranchTest.java
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/BranchTest.java
@@ -10,6 +10,7 @@ package com.powsybl.iidm.network.impl;
 import com.powsybl.iidm.network.*;
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -17,75 +18,150 @@ import org.junit.jupiter.api.Test;
  */
 class BranchTest {
 
+    static Network network1;
+    static Network network2;
+    static Branch<?> branch1;
+    static Branch<?> branch2;
+
+    @BeforeAll
+    public static void setUp() {
+        network1 = EurostagTutorialExample1Factory.createWithFixedLimits();
+        branch1 = network1.getBranch("NHV1_NHV2_2");
+        network2 = EurostagTutorialExample1Factory.createWithFixedCurrentLimits();
+        branch2 = network2.getBranch("NHV1_NHV2_2");
+    }
+
     @Test
-    void testPowerLimitsBranches() {
+    void testActivePowerLimits1Branches() {
         // power limits
-        Network network1 = EurostagTutorialExample1Factory.createWithFixedLimits();
-        Branch<?> branch1 = network1.getBranch("NHV1_NHV2_2");
+
         Assertions.assertNotNull(branch1.getNullableActivePowerLimits1());
         Assertions.assertEquals(1100, branch1.getNullableActivePowerLimits1().getPermanentLimit());
         Assertions.assertEquals(LimitType.ACTIVE_POWER, branch1.getNullableActivePowerLimits1().getLimitType());
         Assertions.assertEquals(2, branch1.getNullableActivePowerLimits1().getTemporaryLimits().size());
+
+        Assertions.assertNotNull(branch1.getActivePowerLimits(TwoSides.ONE));
+        Assertions.assertTrue(branch1.getActivePowerLimits(TwoSides.ONE).isPresent());
+        Assertions.assertEquals(1100, branch1.getActivePowerLimits(TwoSides.ONE).get().getPermanentLimit());
+        Assertions.assertEquals(LimitType.ACTIVE_POWER, branch1.getActivePowerLimits(TwoSides.ONE).get().getLimitType());
+        Assertions.assertEquals(2, branch1.getActivePowerLimits(TwoSides.ONE).get().getTemporaryLimits().size());
+
+        Assertions.assertNotNull(branch1.getNullableLimits(LimitType.ACTIVE_POWER, TwoSides.ONE));
+        Assertions.assertEquals(1100, branch1.getNullableLimits(LimitType.ACTIVE_POWER, TwoSides.ONE).getPermanentLimit());
+        Assertions.assertEquals(LimitType.ACTIVE_POWER, branch1.getNullableLimits(LimitType.ACTIVE_POWER, TwoSides.ONE).getLimitType());
+        Assertions.assertEquals(2, branch1.getNullableLimits(LimitType.ACTIVE_POWER, TwoSides.ONE).getTemporaryLimits().size());
+    }
+
+    @Test
+    void testActivePowerLimits2Branches() {
+        // power limits
 
         Assertions.assertNotNull(branch1.getNullableActivePowerLimits2());
         Assertions.assertEquals(500, branch1.getNullableActivePowerLimits2().getPermanentLimit());
         Assertions.assertEquals(LimitType.ACTIVE_POWER, branch1.getNullableActivePowerLimits2().getLimitType());
         Assertions.assertEquals(0, branch1.getNullableActivePowerLimits2().getTemporaryLimits().size());
 
+        Assertions.assertNotNull(branch1.getActivePowerLimits(TwoSides.TWO));
+        Assertions.assertTrue(branch1.getActivePowerLimits(TwoSides.TWO).isPresent());
+        Assertions.assertEquals(500, branch1.getActivePowerLimits(TwoSides.TWO).get().getPermanentLimit());
+        Assertions.assertEquals(LimitType.ACTIVE_POWER, branch1.getActivePowerLimits(TwoSides.TWO).get().getLimitType());
+        Assertions.assertEquals(0, branch1.getActivePowerLimits(TwoSides.TWO).get().getTemporaryLimits().size());
+
+        Assertions.assertNotNull(branch1.getNullableLimits(LimitType.ACTIVE_POWER, TwoSides.TWO));
+        Assertions.assertEquals(500, branch1.getNullableLimits(LimitType.ACTIVE_POWER, TwoSides.TWO).getPermanentLimit());
+        Assertions.assertEquals(LimitType.ACTIVE_POWER, branch1.getNullableLimits(LimitType.ACTIVE_POWER, TwoSides.TWO).getLimitType());
+        Assertions.assertEquals(0, branch1.getNullableLimits(LimitType.ACTIVE_POWER, TwoSides.TWO).getTemporaryLimits().size());
+    }
+
+    @Test
+    void testApparentPowerLimits1Branches() {
+        // power limits
+
         Assertions.assertNotNull(branch1.getNullableApparentPowerLimits1());
         Assertions.assertEquals(1100, branch1.getNullableApparentPowerLimits1().getPermanentLimit());
         Assertions.assertEquals(LimitType.APPARENT_POWER, branch1.getNullableApparentPowerLimits1().getLimitType());
         Assertions.assertEquals(2, branch1.getNullableApparentPowerLimits1().getTemporaryLimits().size());
+
+        Assertions.assertNotNull(branch1.getApparentPowerLimits(TwoSides.ONE));
+        Assertions.assertTrue(branch1.getApparentPowerLimits(TwoSides.ONE).isPresent());
+        Assertions.assertEquals(1100, branch1.getApparentPowerLimits(TwoSides.ONE).get().getPermanentLimit());
+        Assertions.assertEquals(LimitType.APPARENT_POWER, branch1.getApparentPowerLimits(TwoSides.ONE).get().getLimitType());
+        Assertions.assertEquals(2, branch1.getApparentPowerLimits(TwoSides.ONE).get().getTemporaryLimits().size());
+
+        Assertions.assertNotNull(branch1.getNullableLimits(LimitType.APPARENT_POWER, TwoSides.ONE));
+        Assertions.assertEquals(1100, branch1.getNullableLimits(LimitType.APPARENT_POWER, TwoSides.ONE).getPermanentLimit());
+        Assertions.assertEquals(LimitType.APPARENT_POWER, branch1.getNullableLimits(LimitType.APPARENT_POWER, TwoSides.ONE).getLimitType());
+        Assertions.assertEquals(2, branch1.getNullableLimits(LimitType.APPARENT_POWER, TwoSides.ONE).getTemporaryLimits().size());
+    }
+
+    @Test
+    void testApparentPowerLimits2Branches() {
+        // power limits
 
         Assertions.assertNotNull(branch1.getNullableApparentPowerLimits2());
         Assertions.assertEquals(500, branch1.getNullableApparentPowerLimits2().getPermanentLimit());
         Assertions.assertEquals(LimitType.APPARENT_POWER, branch1.getNullableApparentPowerLimits2().getLimitType());
         Assertions.assertEquals(0, branch1.getNullableApparentPowerLimits2().getTemporaryLimits().size());
 
-        Assertions.assertNull(branch1.getNullableCurrentLimits1());
-        Assertions.assertNull(branch1.getNullableCurrentLimits2());
-
-        Assertions.assertNotNull(branch1.getActivePowerLimits(TwoSides.ONE));
-        Assertions.assertNotNull(branch1.getActivePowerLimits(TwoSides.TWO));
-        Assertions.assertNotNull(branch1.getNullableActivePowerLimits(TwoSides.ONE));
-        Assertions.assertNotNull(branch1.getNullableActivePowerLimits(TwoSides.TWO));
-        Assertions.assertNotNull(branch1.getApparentPowerLimits(TwoSides.ONE));
         Assertions.assertNotNull(branch1.getApparentPowerLimits(TwoSides.TWO));
-        Assertions.assertNotNull(branch1.getNullableApparentPowerLimits(TwoSides.ONE));
-        Assertions.assertNotNull(branch1.getNullableApparentPowerLimits(TwoSides.TWO));
+        Assertions.assertTrue(branch1.getApparentPowerLimits(TwoSides.TWO).isPresent());
+        Assertions.assertEquals(500, branch1.getApparentPowerLimits(TwoSides.TWO).get().getPermanentLimit());
+        Assertions.assertEquals(LimitType.APPARENT_POWER, branch1.getApparentPowerLimits(TwoSides.TWO).get().getLimitType());
+        Assertions.assertEquals(0, branch1.getApparentPowerLimits(TwoSides.TWO).get().getTemporaryLimits().size());
 
-        Assertions.assertNotNull(branch1.getNullableLimits(LimitType.ACTIVE_POWER, TwoSides.ONE));
-        Assertions.assertNotNull(branch1.getNullableLimits(LimitType.ACTIVE_POWER, TwoSides.TWO));
-        Assertions.assertNotNull(branch1.getNullableLimits(LimitType.APPARENT_POWER, TwoSides.ONE));
         Assertions.assertNotNull(branch1.getNullableLimits(LimitType.APPARENT_POWER, TwoSides.TWO));
+        Assertions.assertEquals(500, branch1.getNullableLimits(LimitType.APPARENT_POWER, TwoSides.TWO).getPermanentLimit());
+        Assertions.assertEquals(LimitType.APPARENT_POWER, branch1.getNullableLimits(LimitType.APPARENT_POWER, TwoSides.TWO).getLimitType());
+        Assertions.assertEquals(0, branch1.getNullableLimits(LimitType.APPARENT_POWER, TwoSides.TWO).getTemporaryLimits().size());
     }
 
     @Test
-    void testCurrentLimitsBranches() {
+    void testCurrentLimits1Branches() {
         // current limits
-        Network network2 = EurostagTutorialExample1Factory.createWithFixedCurrentLimits();
-        Branch<?> branch2 = network2.getBranch("NHV1_NHV2_2");
-        Assertions.assertNull(branch2.getNullableActivePowerLimits1());
-        Assertions.assertNull(branch2.getNullableActivePowerLimits2());
-        Assertions.assertNull(branch2.getNullableApparentPowerLimits1());
-        Assertions.assertNull(branch2.getNullableApparentPowerLimits2());
-
         Assertions.assertNotNull(branch2.getNullableCurrentLimits1());
         Assertions.assertEquals(LimitType.CURRENT, branch2.getNullableCurrentLimits1().getLimitType());
         Assertions.assertEquals(1100, branch2.getNullableCurrentLimits1().getPermanentLimit());
         Assertions.assertEquals(2, branch2.getNullableCurrentLimits1().getTemporaryLimits().size());
 
+        Assertions.assertNotNull(branch2.getCurrentLimits(TwoSides.ONE));
+        Assertions.assertTrue(branch2.getCurrentLimits(TwoSides.ONE).isPresent());
+        Assertions.assertEquals(LimitType.CURRENT, branch2.getCurrentLimits(TwoSides.ONE).get().getLimitType());
+        Assertions.assertEquals(1100, branch2.getCurrentLimits(TwoSides.ONE).get().getPermanentLimit());
+        Assertions.assertEquals(2, branch2.getCurrentLimits(TwoSides.ONE).get().getTemporaryLimits().size());
+
+        Assertions.assertNotNull(branch2.getNullableCurrentLimits(TwoSides.ONE));
+        Assertions.assertEquals(LimitType.CURRENT, branch2.getNullableCurrentLimits(TwoSides.ONE).getLimitType());
+        Assertions.assertEquals(1100, branch2.getNullableCurrentLimits(TwoSides.ONE).getPermanentLimit());
+        Assertions.assertEquals(2, branch2.getNullableCurrentLimits(TwoSides.ONE).getTemporaryLimits().size());
+
+        Assertions.assertNotNull(branch2.getNullableLimits(LimitType.CURRENT, TwoSides.ONE));
+        Assertions.assertEquals(LimitType.CURRENT, branch2.getNullableLimits(LimitType.CURRENT, TwoSides.ONE).getLimitType());
+        Assertions.assertEquals(1100, branch2.getNullableLimits(LimitType.CURRENT, TwoSides.ONE).getPermanentLimit());
+        Assertions.assertEquals(2, branch2.getNullableLimits(LimitType.CURRENT, TwoSides.ONE).getTemporaryLimits().size());
+    }
+
+    @Test
+    void testCurrentLimits2Branches() {
+        // current limits
         Assertions.assertNotNull(branch2.getNullableCurrentLimits2());
         Assertions.assertEquals(LimitType.CURRENT, branch2.getNullableCurrentLimits2().getLimitType());
         Assertions.assertEquals(500, branch2.getNullableCurrentLimits2().getPermanentLimit());
         Assertions.assertEquals(0, branch2.getNullableCurrentLimits2().getTemporaryLimits().size());
 
-        Assertions.assertNotNull(branch2.getCurrentLimits(TwoSides.ONE));
         Assertions.assertNotNull(branch2.getCurrentLimits(TwoSides.TWO));
-        Assertions.assertNotNull(branch2.getNullableCurrentLimits(TwoSides.ONE));
-        Assertions.assertNotNull(branch2.getNullableCurrentLimits(TwoSides.TWO));
+        Assertions.assertTrue(branch2.getCurrentLimits(TwoSides.TWO).isPresent());
+        Assertions.assertEquals(LimitType.CURRENT, branch2.getCurrentLimits(TwoSides.TWO).get().getLimitType());
+        Assertions.assertEquals(500, branch2.getCurrentLimits(TwoSides.TWO).get().getPermanentLimit());
+        Assertions.assertEquals(0, branch2.getCurrentLimits(TwoSides.TWO).get().getTemporaryLimits().size());
 
-        Assertions.assertNotNull(branch2.getNullableLimits(LimitType.CURRENT, TwoSides.ONE));
+        Assertions.assertNotNull(branch2.getNullableCurrentLimits(TwoSides.TWO));
+        Assertions.assertEquals(LimitType.CURRENT, branch2.getNullableCurrentLimits(TwoSides.TWO).getLimitType());
+        Assertions.assertEquals(500, branch2.getNullableCurrentLimits(TwoSides.TWO).getPermanentLimit());
+        Assertions.assertEquals(0, branch2.getNullableCurrentLimits(TwoSides.TWO).getTemporaryLimits().size());
+
         Assertions.assertNotNull(branch2.getNullableLimits(LimitType.CURRENT, TwoSides.TWO));
+        Assertions.assertEquals(LimitType.CURRENT, branch2.getNullableLimits(LimitType.CURRENT, TwoSides.TWO).getLimitType());
+        Assertions.assertEquals(500, branch2.getNullableLimits(LimitType.CURRENT, TwoSides.TWO).getPermanentLimit());
+        Assertions.assertEquals(0, branch2.getNullableLimits(LimitType.CURRENT, TwoSides.TWO).getTemporaryLimits().size());
     }
 }
