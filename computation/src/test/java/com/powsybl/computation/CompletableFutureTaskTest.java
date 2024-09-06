powsybl/powsybl-core

commit 5bef72fa5eea0b1efc00936b792c0c6b8cc4c3f3

Code cleanup to correct issues reported by Sonar (Consistency) (#2967)
Code cleanup to correct issues reported by Sonar (Consistency) (#2967)

Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

computation/src/main/java/com/powsybl/computation/CompletableFutureTask.java
@@ -45,13 +45,11 @@ public class CompletableFutureTask<R> extends CompletableFuture<R> implements Ru
         future.run();
         try {
             complete(future.get());
-        } catch (ExecutionException exc) {
-            completeExceptionally(exc.getCause());
         } catch (InterruptedException exc) {
             Thread.currentThread().interrupt();
             completeExceptionally(exc);
-        } catch (Throwable exc) {
-            completeExceptionally(exc);
+        } catch (Exception exc) {
+            completeExceptionally(exc.getCause());
         }
     }
 

commit 7cb22445190bb27b86f95c4531fcf58bd259dff9

CompletableFutureTask, fix spurious error logging when cancelling
When calling cancel(true), the CompletableFutureTask was throwing
a NullPointerException in its run method because cancel(true)
causes future.get() to throw a CancellationException without a cause (null)
and completeExceptionally requires a non null Exception

the exception was thrown to the executor and the very end. Executors usually catch and
ignore everything (just logs to stdout) so the problem was just ugly logs and a slight
performance drop.

This was a regression from 5bef72fa5ee, the actual code is reverted to the previous version
(except changing throwable to exception to please avoid sonar warnings I guess)
The code change was not logical, we need to unwrap the cause when we have an ExecutionException
and we also need to not unwrap the cause otherwise.

Signed-off-by: HARPER Jon <jon.harper87@gmail.com>

computation/src/main/java/com/powsybl/computation/CompletableFutureTask.java
@@ -45,11 +45,13 @@ public class CompletableFutureTask<R> extends CompletableFuture<R> implements Ru
         future.run();
         try {
             complete(future.get());
+        } catch (ExecutionException exc) {
+            completeExceptionally(exc.getCause());
         } catch (InterruptedException exc) {
             Thread.currentThread().interrupt();
             completeExceptionally(exc);
         } catch (Exception exc) {
-            completeExceptionally(exc.getCause());
+            completeExceptionally(exc);
         }
     }
 

computation/src/test/java/com/powsybl/computation/CompletableFutureTaskTest.java
@@ -40,8 +40,37 @@ class CompletableFutureTaskTest {
                 Executors.newSingleThreadExecutor(),
                 Executors.newCachedThreadPool(),
                 Executors.newWorkStealingPool(),
+                new MyTestExecutorWithException(),
                 ForkJoinPool.commonPool()
         );
+    }
+
+    // Very basic, spawn a new thread
+    // and allow to wait for the end of the command
+    // just keep exception to be able to assert,
+    // you should only it to launch one command
+    // because it has just one latch and one exception
+    private static class MyTestExecutorWithException implements Executor {
+
+        Exception exception = null;
+        CountDownLatch waitForDone;
+
+        @Override
+        public void execute(Runnable command) {
+            (new Thread() {
+                @Override
+                public void run() {
+                    waitForDone = new CountDownLatch(1);
+                    try {
+                        command.run();
+                    } catch (Exception exception) {
+                        MyTestExecutorWithException.this.exception = exception;
+                    } finally {
+                        waitForDone.countDown();
+                    }
+                }
+            }).start();
+        }
     }
 
     @ParameterizedTest
@@ -125,6 +154,10 @@ class CompletableFutureTaskTest {
         //Second call to cancel should return false
         cancelled = task.cancel(true);
         assertFalse(cancelled);
+        if (executor instanceof MyTestExecutorWithException myTestExecutor) {
+            myTestExecutor.waitForDone.await();
+            assertNull(myTestExecutor.exception);
+        }
     }
 
     @ParameterizedTest