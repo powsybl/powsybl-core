74887227b31aa781b245994f1260ea9867a45c31
Florian Dupuy <66690739+flo-dup@users.noreply.github.com>
2024-09-25 15:13:47 +0200

    Fix ReportNode deserialization by enriching ReportNode API with TreeContext  (#3155)
    * Enriching the ReportNode API to have a generic deserializer
    * Add merge and javadoc to interface
    * Rename RootContext into TreeContext
    
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- commons/src/main/java/com/powsybl/commons/report/ReportNode.java
+++ commons/src/main/java/com/powsybl/commons/report/ReportNode.java
@@ -112,6 +112,11 @@ public interface ReportNode {
     ReportNodeAdder newReportNode();
 
     /**
+     * Get the {@link TreeContext} of the corresponding {@link ReportNode} tree.
+     */
+    TreeContext getTreeContext();
+
+    /**
      * Add a <code>ReportNode</code> as a child of current <code>ReportNode</code>.
      *
      * @param reportRoot the <code>ReportNode</code> to add, it needs to be a root <code>ReportNode</code>
