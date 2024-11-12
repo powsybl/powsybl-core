8fcfd02186eb765e20d3b4efbfb95cee5a9dd728
Olivier Perrin <olivier.perrin@rte-france.com>
2024-11-12 16:00:23 +0100

    Add OverloadManagementExtension extension SerDe test
    Signed-off-by: Olivier Perrin <olivier.perrin@rte-france.com>

---
+++ iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/OverloadManagementSystemMockSerDe.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.iidm.serde;
+
+import com.google.auto.service.AutoService;
+import com.powsybl.commons.extensions.AbstractExtensionSerDe;
+import com.powsybl.commons.extensions.ExtensionSerDe;
+import com.powsybl.commons.io.DeserializerContext;
+import com.powsybl.commons.io.SerializerContext;
+import com.powsybl.iidm.network.OverloadManagementSystem;
+
+/**
+ * @author Olivier Perrin {@literal <olivier.perrin at rte-france.com>}
+ */
+@AutoService(ExtensionSerDe.class)
+public class OverloadManagementSystemMockSerDe extends AbstractExtensionSerDe<OverloadManagementSystem, OverloadManagementSystemMockExt> {
+
+    public OverloadManagementSystemMockSerDe() {
+        super("overloadManagementSystemMock", "network", OverloadManagementSystemMockExt.class, "overloadManagementSystemMock.xsd",
+                "http://www.powsybl.org/schema/iidm/ext/overloadmanagementsystem_mock/1_0", "omsmock");
+    }
+
+    @Override
+    public void write(OverloadManagementSystemMockExt omsFoo, SerializerContext context) {
+        // Nothing inside
+    }
+
+    @Override
+    public OverloadManagementSystemMockExt read(OverloadManagementSystem overloadManagementSystem, DeserializerContext context) {
+        context.getReader().readEndNode();
+        return new OverloadManagementSystemMockExt(overloadManagementSystem);
+    }
+}
