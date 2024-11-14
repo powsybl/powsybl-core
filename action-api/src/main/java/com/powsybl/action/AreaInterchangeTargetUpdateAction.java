d03886243554edf76eb893d42cad8cd2f72a68a1
Bertrand Rix <bertrand.rix@artelys.com>
2024-11-14 11:39:49 +0100

    Equal and hashcode.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- action-api/src/main/java/com/powsybl/action/AreaInterchangeTargetUpdateAction.java
+++ action-api/src/main/java/com/powsybl/action/AreaInterchangeTargetUpdateAction.java
@@ -1,5 +1,7 @@
 package com.powsybl.action;
 
+import java.util.Objects;
+
 public class AreaInterchangeTargetUpdateAction extends AbstractAction {
 
     public static final String NAME = "AREA_INTERCHANGE_TARGET_UPDATE_ACTION";
@@ -24,5 +26,25 @@ public class AreaInterchangeTargetUpdateAction extends AbstractAction {
     @Override
     public String getType() {
         return NAME;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        AreaInterchangeTargetUpdateAction that = (AreaInterchangeTargetUpdateAction) o;
+        return target == that.target && Objects.equals(areaId, that.areaId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), target, areaId);
     }
 }
