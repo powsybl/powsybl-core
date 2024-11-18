946208d79d574d0bd5fd9864cfa751d181daeb92
Bertrand Rix <bertrand.rix@artelys.com>
2024-11-18 10:24:24 +0100

    2024
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- iidm/iidm-modification/src/test/java/com/powsybl/iidm/modification/AreaInterchangeModificationTest.java
+++ iidm/iidm-modification/src/test/java/com/powsybl/iidm/modification/AreaInterchangeModificationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Coreso SA (https://www.coreso.eu/) and TSCNET Services GmbH (https://www.tscnet.eu/)
+ * Copyright (c) 2024, Coreso SA (https://www.coreso.eu/) and TSCNET Services GmbH (https://www.tscnet.eu/)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
