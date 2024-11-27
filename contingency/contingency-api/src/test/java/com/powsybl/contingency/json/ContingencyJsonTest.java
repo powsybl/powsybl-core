32e392d17199ec512ee787d6c08eafbedffa55ed
Etienne LESOT <etienne.lesot@rte-france.com>
2024-11-27 13:52:49 +0100

    add tests
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- contingency/contingency-api/src/test/java/com/powsybl/contingency/json/ContingencyJsonTest.java
+++ contingency/contingency-api/src/test/java/com/powsybl/contingency/json/ContingencyJsonTest.java
@@ -22,8 +22,7 @@ import com.powsybl.contingency.Contingency;
 import com.powsybl.contingency.contingency.list.ContingencyList;
 import com.powsybl.contingency.contingency.list.DefaultContingencyList;
 import com.powsybl.iidm.network.Network;
-import com.powsybl.iidm.network.test.BatteryNetworkFactory;
-import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
+import com.powsybl.iidm.network.test.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -36,6 +35,7 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
 
+import static com.powsybl.contingency.ContingencyElementType.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -52,6 +52,9 @@ class ContingencyJsonTest extends AbstractSerDeTest {
         Files.copy(getClass().getResourceAsStream("/contingencies.json"), fileSystem.getPath("/contingencies.json"));
         Files.copy(getClass().getResourceAsStream("/contingenciesBatteries.json"), fileSystem.getPath("/contingenciesBatteries.json"));
         Files.copy(getClass().getResourceAsStream("/contingenciesWithOptionalName.json"), fileSystem.getPath("/contingenciesWithOptionalName.json"));
+        Files.copy(getClass().getResourceAsStream("/contingenciesWithSeveralElements.json"), fileSystem.getPath("/contingenciesWithSeveralElements.json"));
+        Files.copy(getClass().getResourceAsStream("/contingenciesWith3wt.json"), fileSystem.getPath("/contingenciesWith3wt.json"));
+        Files.copy(getClass().getResourceAsStream("/contingenciesWithDlAndTl.json"), fileSystem.getPath("/contingenciesWithDlAndTl.json"));
     }
 
     private static Contingency create() {
@@ -155,6 +158,68 @@ class ContingencyJsonTest extends AbstractSerDeTest {
         assertEquals(1, contingencies.get(3).getElements().size());
 
         roundTripTest(contingencyList, ContingencyJsonTest::write, ContingencyJsonTest::readContingencyList, "/contingenciesBatteries.json");
+    }
+
+    @Test
+    void readJsonListWithSwitchHvdcSvcShuntBbsTwtContingency() throws IOException {
+        Network network = FourSubstationsNodeBreakerFactory.create();
+
+        ContingencyList contingencyList = ContingencyList.load(fileSystem.getPath("/contingenciesWithSeveralElements.json"));
+        assertEquals("list", contingencyList.getName());
+
+        List<Contingency> contingencies = contingencyList.getContingencies(network);
+        assertEquals(6, contingencies.size());
+        assertEquals("contingency", contingencies.get(0).getId());
+        assertEquals(1, contingencies.get(0).getElements().size());
+        assertEquals(HVDC_LINE, contingencies.get(0).getElements().get(0).getType());
+        assertEquals("contingency2", contingencies.get(1).getId());
+        assertEquals(1, contingencies.get(1).getElements().size());
+        assertEquals(TWO_WINDINGS_TRANSFORMER, contingencies.get(1).getElements().get(0).getType());
+        assertEquals("contingency3", contingencies.get(2).getId());
+        assertEquals(1, contingencies.get(2).getElements().size());
+        assertEquals(SHUNT_COMPENSATOR, contingencies.get(2).getElements().get(0).getType());
+        assertEquals("contingency4", contingencies.get(3).getId());
+        assertEquals(1, contingencies.get(3).getElements().size());
+        assertEquals(STATIC_VAR_COMPENSATOR, contingencies.get(3).getElements().get(0).getType());
+        assertEquals("contingency5", contingencies.get(4).getId());
+        assertEquals(1, contingencies.get(4).getElements().size());
+        assertEquals(BUSBAR_SECTION, contingencies.get(4).getElements().get(0).getType());
+        assertEquals("contingency6", contingencies.get(5).getId());
+        assertEquals(1, contingencies.get(5).getElements().size());
+        assertEquals(SWITCH, contingencies.get(5).getElements().get(0).getType());
+
+        roundTripTest(contingencyList, ContingencyJsonTest::write, ContingencyJsonTest::readContingencyList, "/contingenciesWithSeveralElements.json");
+    }
+
+    @Test
+    void readJsonListWithTwt3Contingency() throws IOException {
+        Network network = ThreeWindingsTransformerNetworkFactory.create();
+        ContingencyList contingencyList = ContingencyList.load(fileSystem.getPath("/contingenciesWith3wt.json"));
+        assertEquals("list", contingencyList.getName());
+
+        List<Contingency> contingencies = contingencyList.getContingencies(network);
+        assertEquals(1, contingencies.size());
+        assertEquals("contingency", contingencies.get(0).getId());
+        assertEquals(1, contingencies.get(0).getElements().size());
+        assertEquals(THREE_WINDINGS_TRANSFORMER, contingencies.get(0).getElements().get(0).getType());
+        roundTripTest(contingencyList, ContingencyJsonTest::write, ContingencyJsonTest::readContingencyList, "/contingenciesWith3wt.json");
+    }
+
+    @Test
+    void readJsonListWithTieLineAndDanglingLineContingency() throws IOException {
+        Network network = EurostagTutorialExample1Factory.createWithTieLine();
+        ContingencyList contingencyList = ContingencyList.load(fileSystem.getPath("/contingenciesWithDlAndTl.json"));
+        assertEquals("list", contingencyList.getName());
+
+        List<Contingency> contingencies = contingencyList.getContingencies(network);
+        assertEquals(2, contingencies.size());
+        assertEquals("contingency", contingencies.get(0).getId());
+        assertEquals(1, contingencies.get(0).getElements().size());
+        assertEquals(DANGLING_LINE, contingencies.get(0).getElements().get(0).getType());
+        assertEquals("contingency2", contingencies.get(1).getId());
+        assertEquals(1, contingencies.get(1).getElements().size());
+        assertEquals(TIE_LINE, contingencies.get(1).getElements().get(0).getType());
+        roundTripTest(contingencyList, ContingencyJsonTest::write, ContingencyJsonTest::readContingencyList, "/contingenciesWithDlAndTl.json");
     }
 
     @Test
