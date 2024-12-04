edca74868811c6357c666cf8c3c71da4695f4671
Florian Dupuy <florian.dupuy@rte-france.com>
2024-12-04 13:55:46 +0100

    Remove unwanted formatting changes
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- iidm/iidm-tck/src/test/java/com/powsybl/iidm/network/tck/AbstractNodeBreakerTest.java
+++ iidm/iidm-tck/src/test/java/com/powsybl/iidm/network/tck/AbstractNodeBreakerTest.java
@@ -27,195 +27,195 @@ public abstract class AbstractNodeBreakerTest {
     private Network createNetwork() {
         Network network = Network.create("test", "test");
         Substation s1 = network.newSubstation()
-                .setId("S1")
-                .setCountry(Country.FR)
-                .add();
+            .setId("S1")
+            .setCountry(Country.FR)
+            .add();
         VoltageLevel vl = s1.newVoltageLevel()
-                .setId("VL1")
-                .setNominalV(1.0)
-                .setTopologyKind(TopologyKind.NODE_BREAKER)
-                .add();
+            .setId("VL1")
+            .setNominalV(1.0)
+            .setTopologyKind(TopologyKind.NODE_BREAKER)
+            .add();
 
         // Busbar sections
         BusbarSection bbs11 = vl.getNodeBreakerView()
-                .newBusbarSection()
-                .setId("BBS11")
-                .setNode(0)
-                .add();
+            .newBusbarSection()
+            .setId("BBS11")
+            .setNode(0)
+            .add();
         bbs11.newExtension(BusbarSectionPositionAdder.class)
-                .withBusbarIndex(1)
-                .withSectionIndex(1)
-                .add();
+            .withBusbarIndex(1)
+            .withSectionIndex(1)
+            .add();
         BusbarSection bbs21 = vl.getNodeBreakerView()
-                .newBusbarSection()
-                .setId("BBS21")
-                .setNode(1)
-                .add();
+            .newBusbarSection()
+            .setId("BBS21")
+            .setNode(1)
+            .add();
         bbs21.newExtension(BusbarSectionPositionAdder.class)
-                .withBusbarIndex(2)
-                .withSectionIndex(1)
-                .add();
+            .withBusbarIndex(2)
+            .withSectionIndex(1)
+            .add();
         BusbarSection bbs12 = vl.getNodeBreakerView()
-                .newBusbarSection()
-                .setId("BBS12")
-                .setNode(2)
-                .add();
+            .newBusbarSection()
+            .setId("BBS12")
+            .setNode(2)
+            .add();
         bbs12.newExtension(BusbarSectionPositionAdder.class)
-                .withBusbarIndex(1)
-                .withSectionIndex(2)
-                .add();
+            .withBusbarIndex(1)
+            .withSectionIndex(2)
+            .add();
         BusbarSection bbs22 = vl.getNodeBreakerView()
-                .newBusbarSection()
-                .setId("BBS22")
-                .setNode(3)
-                .add();
+            .newBusbarSection()
+            .setId("BBS22")
+            .setNode(3)
+            .add();
         bbs22.newExtension(BusbarSectionPositionAdder.class)
-                .withBusbarIndex(2)
-                .withSectionIndex(2)
-                .add();
+            .withBusbarIndex(2)
+            .withSectionIndex(2)
+            .add();
 
         // Disconnectors for coupling
         vl.getNodeBreakerView().newDisconnector()
-                .setId("D_BBS11_BBS12")
-                .setNode1(0)
-                .setNode2(2)
-                .setOpen(true)
-                .add();
+            .setId("D_BBS11_BBS12")
+            .setNode1(0)
+            .setNode2(2)
+            .setOpen(true)
+            .add();
         vl.getNodeBreakerView().newDisconnector()
-                .setId("D_BBS21_BBS22")
-                .setNode1(1)
-                .setNode2(3)
-                .setOpen(false)
-                .add();
+            .setId("D_BBS21_BBS22")
+            .setNode1(1)
+            .setNode2(3)
+            .setOpen(false)
+            .add();
 
         // Generators and loads
         vl.newLoad()
-                .setId("L1")
-                .setNode(4)
-                .setP0(1)
-                .setQ0(1)
-                .add();
+            .setId("L1")
+            .setNode(4)
+            .setP0(1)
+            .setQ0(1)
+            .add();
         vl.newGenerator()
-                .setId("G1")
-                .setNode(5)
-                .setMaxP(100)
-                .setMinP(50)
-                .setTargetP(100)
-                .setTargetV(400)
-                .setVoltageRegulatorOn(true)
-                .add();
+            .setId("G1")
+            .setNode(5)
+            .setMaxP(100)
+            .setMinP(50)
+            .setTargetP(100)
+            .setTargetV(400)
+            .setVoltageRegulatorOn(true)
+            .add();
         vl.newGenerator()
-                .setId("G2")
-                .setNode(6)
-                .setMaxP(100)
-                .setMinP(50)
-                .setTargetP(100)
-                .setTargetV(400)
-                .setVoltageRegulatorOn(true)
-                .add();
+            .setId("G2")
+            .setNode(6)
+            .setMaxP(100)
+            .setMinP(50)
+            .setTargetP(100)
+            .setTargetV(400)
+            .setVoltageRegulatorOn(true)
+            .add();
 
         // Breakers
         vl.getNodeBreakerView().newBreaker()
-                .setId("B_L1_1")
-                .setNode1(4)
-                .setNode2(7)
-                .setOpen(false)
-                .setFictitious(true)
-                .add();
+            .setId("B_L1_1")
+            .setNode1(4)
+            .setNode2(7)
+            .setOpen(false)
+            .setFictitious(true)
+            .add();
         vl.getNodeBreakerView().newBreaker()
-                .setId("B_L1_2")
-                .setNode1(4)
-                .setNode2(7)
-                .setOpen(false)
-                .add();
+            .setId("B_L1_2")
+            .setNode1(4)
+            .setNode2(7)
+            .setOpen(false)
+            .add();
         vl.getNodeBreakerView().newBreaker()
-                .setId("B_G1")
-                .setNode1(5)
-                .setNode2(8)
-                .setOpen(true)
-                .add();
+            .setId("B_G1")
+            .setNode1(5)
+            .setNode2(8)
+            .setOpen(true)
+            .add();
         vl.getNodeBreakerView().newBreaker()
-                .setId("B_G2")
-                .setNode1(6)
-                .setNode2(9)
-                .setOpen(false)
-                .setFictitious(true)
-                .add();
+            .setId("B_G2")
+            .setNode1(6)
+            .setNode2(9)
+            .setOpen(false)
+            .setFictitious(true)
+            .add();
         vl.getNodeBreakerView().newBreaker()
-                .setId("B0")
-                .setNode1(7)
-                .setNode2(17)
-                .setOpen(false)
-                .add();
+            .setId("B0")
+            .setNode1(7)
+            .setNode2(17)
+            .setOpen(false)
+            .add();
         vl.getNodeBreakerView().newBreaker()
-                .setId("B1")
-                .setNode1(8)
-                .setNode2(11)
-                .setOpen(true)
-                .add();
+            .setId("B1")
+            .setNode1(8)
+            .setNode2(11)
+            .setOpen(true)
+            .add();
         vl.getNodeBreakerView().newBreaker()
-                .setId("B2")
-                .setNode1(9)
-                .setNode2(12)
-                .setOpen(false)
-                .setFictitious(true)
-                .add();
+            .setId("B2")
+            .setNode1(9)
+            .setNode2(12)
+            .setOpen(false)
+            .setFictitious(true)
+            .add();
         vl.getNodeBreakerView().newBreaker()
-                .setId("B3")
-                .setNode1(7)
-                .setNode2(8)
-                .setOpen(false)
-                .add();
+            .setId("B3")
+            .setNode1(7)
+            .setNode2(8)
+            .setOpen(false)
+            .add();
         vl.getNodeBreakerView().newBreaker()
-                .setId("B4")
-                .setNode1(8)
-                .setNode2(9)
-                .setOpen(false)
-                .add();
+            .setId("B4")
+            .setNode1(8)
+            .setNode2(9)
+            .setOpen(false)
+            .add();
         vl.getNodeBreakerView().newBreaker()
-                .setId("B5")
-                .setNode1(17)
-                .setNode2(10)
-                .setOpen(false)
-                .add();
+            .setId("B5")
+            .setNode1(17)
+            .setNode2(10)
+            .setOpen(false)
+            .add();
 
         // Disconnectors
         vl.getNodeBreakerView().newDisconnector()
-                .setId("D0")
-                .setNode1(0)
-                .setNode2(10)
-                .setOpen(true)
-                .add();
+            .setId("D0")
+            .setNode1(0)
+            .setNode2(10)
+            .setOpen(true)
+            .add();
         vl.getNodeBreakerView().newDisconnector()
-                .setId("D1")
-                .setNode1(1)
-                .setNode2(10)
-                .setOpen(false)
-                .add();
+            .setId("D1")
+            .setNode1(1)
+            .setNode2(10)
+            .setOpen(false)
+            .add();
         vl.getNodeBreakerView().newDisconnector()
-                .setId("D2")
-                .setNode1(0)
-                .setNode2(11)
-                .setOpen(false)
-                .add();
+            .setId("D2")
+            .setNode1(0)
+            .setNode2(11)
+            .setOpen(false)
+            .add();
         vl.getNodeBreakerView().newDisconnector()
-                .setId("D3")
-                .setNode1(1)
-                .setNode2(11)
-                .setOpen(true)
-                .add();
+            .setId("D3")
+            .setNode1(1)
+            .setNode2(11)
+            .setOpen(true)
+            .add();
         vl.getNodeBreakerView().newDisconnector()
-                .setId("D4")
-                .setNode1(2)
-                .setNode2(12)
-                .setOpen(false)
-                .add();
+            .setId("D4")
+            .setNode1(2)
+            .setNode2(12)
+            .setOpen(false)
+            .add();
         vl.getNodeBreakerView().newDisconnector()
-                .setId("D5")
-                .setNode1(3)
-                .setNode2(12)
-                .setOpen(true)
-                .add();
+            .setId("D5")
+            .setNode1(3)
+            .setNode2(12)
+            .setOpen(true)
+            .add();
         return network;
     }
 
