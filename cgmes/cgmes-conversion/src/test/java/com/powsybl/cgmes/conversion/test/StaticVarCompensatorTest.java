4ededddb9ce2f19c4b26b33131463f0c29be0e24
marqueslanauja <51124986+marqueslanauja@users.noreply.github.com>
2025-01-17 09:35:24 +0100

    Update cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/StaticVarCompensatorTest.java
    Co-authored-by: rcourtier <129156292+rcourtier@users.noreply.github.com>
    Signed-off-by: marqueslanauja <51124986+marqueslanauja@users.noreply.github.com>

--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/StaticVarCompensatorTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/StaticVarCompensatorTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class StaticVarCompensatorTest {
 
-    private static final String DIR = "/staticVarCompensator/";
+    private static final String DIR = "/issues/staticVarCompensator/";
 
     @Test
     void staticVarCompensatorTest() {
