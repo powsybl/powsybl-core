f052643091ff362924c8ad8c7f1e2cc203bb592f
marquesja1 <marquesja@aia.es>
2025-01-30 15:10:40 +0100

    Improve context and remove unused code
    Signed-off-by: marquesja1 <marquesja@aia.es>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/Conversion.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/Conversion.java
@@ -542,12 +542,7 @@ public class Conversion {
 
     private Context createUpdateContext(Network network, ReportNode reportNode) {
         Context context = new Context(cgmes, config, network, reportNode);
-        context.loadCgmesTerminals();
-        context.loadRatioTapChangers();
-        context.loadPhaseTapChangers();
-        context.loadRegulatingControls();
-        context.loadOperationalLimits();
-        context.loadGeneratingUnits();
+        context.buildUpdateCache();
         return context;
     }
 
