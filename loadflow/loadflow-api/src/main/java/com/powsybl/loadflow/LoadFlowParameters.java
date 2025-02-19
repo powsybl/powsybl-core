f07a063cad35462d399431b340f810c4ce35bdfd
vmouradian <valentin.mouradian@artelys.com>
2025-02-19 10:10:24 +0100

    update doc
    Signed-off-by: vmouradian <valentin.mouradian@artelys.com>

--- loadflow/loadflow-api/src/main/java/com/powsybl/loadflow/LoadFlowParameters.java
+++ loadflow/loadflow-api/src/main/java/com/powsybl/loadflow/LoadFlowParameters.java
@@ -49,7 +49,7 @@ public class LoadFlowParameters extends AbstractExtendable<LoadFlowParameters> {
          */
         PROPORTIONAL_TO_GENERATION_P_MAX,
         /**
-         * active power slack distribution on generators, proportional to generator maxP - targetP
+         * active power slack distribution on generators, proportional to generator maxP - targetP if active production is increased, and proportional to targetP - minP if decreased.
          */
         PROPORTIONAL_TO_GENERATION_REMAINING_MARGIN,
         /**
