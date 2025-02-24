a21e830ae53392cbd726e52bc2c20e9b6d8367fd
marquesja1 <marquesja@aia.es>
2025-02-24 14:05:37 +0100

    improve switches created from disconnected terminals
    Signed-off-by: marquesja1 <marquesja@aia.es>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/Conversion.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/Conversion.java
@@ -295,7 +295,7 @@ public class Conversion {
         Context updateContext = createUpdateContext(network, reportNode);
 
         // add processes to create new equipment using update data (ssh and sv data)
-        createSwitchesForDisconnectedTerminalsDuringUpdate(network, cgmes, updateContext);
+        createFictitiousSwitchesForDisconnectedTerminalsDuringUpdate(network, cgmes, updateContext);
 
         update(network, updateContext, reportNode);
     }
