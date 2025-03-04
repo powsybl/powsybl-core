c7ddc6dc770eb246b30d9554f60597f1df175f9e
Olivier Perrin <olivier.perrin@rte-france.com>
2025-03-04 10:41:41 +0100

    Deprecate Equipments.getConnectionInfoInBusBreakerView()
    Signed-off-by: Olivier Perrin <olivier.perrin@rte-france.com>

--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/Equipments.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/Equipments.java
@@ -16,9 +16,24 @@ public final class Equipments {
     private Equipments() {
     }
 
+    /**
+     * @deprecated See {@link #getConnectionInfoInBusBreakerView}
+     */
+    @Deprecated(since = "6.7.0", forRemoval = true)
     public record ConnectionInfo(Bus connectionBus, boolean connected) {
     }
 
+    /**
+     * @deprecated This method was introduced to provide a fallback to handle the very specific case where
+     * {@link com.powsybl.iidm.network.Terminal.BusBreakerView#getConnectableBus()} returned a null bus. This case is no
+     * longer possible, hence this method is now useless.
+     * You can use:
+     * <ul>
+     * <li><code>connected = t.getBusBreakerView().getBus();</code></li>
+     * <li><code>bus = Optional.ofNullable(t.getBusBreakerView().getBus()).orElse(t.getBusBreakerView().getConnectableBus());</code></li>
+     * </ul>
+     */
+    @Deprecated(since = "6.7.0", forRemoval = true)
     public static ConnectionInfo getConnectionInfoInBusBreakerView(Terminal t) {
         Bus bus = t.getBusBreakerView().getBus();
         boolean connected;
