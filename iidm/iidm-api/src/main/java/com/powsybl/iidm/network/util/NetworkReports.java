6b7aa5d249b1c9a06d3aeaafbb6f414ddc046edb
CARON Alice <alice.caron@rte-france.com>
2025-03-07 10:34:54 +0100

    Gather all network reports on NetworkReports class
    Signed-off-by: CARON Alice <alice.caron@rte-france.com>

---
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/util/NetworkReports.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.iidm.network.util;
+
+import com.powsybl.commons.datasource.ReadOnlyDataSource;
+import com.powsybl.commons.report.ReportNode;
+import com.powsybl.commons.report.TypedValue;
+
+import java.util.Objects;
+
+/**
+ * @author Alice Caron {@literal <alice.caron at rte-france.com>}
+ */
+public final class NetworkReports {
+
+    public static final String BUNDLE_BASE_NAME = "com.powsybl.commons.reports";
+
+    private static final String DANGLING_LINE_ID_1 = "danglingLineId1";
+    private static final String DANGLING_LINE_ID_2 = "danglingLineId2";
+
+    private NetworkReports() {
+    }
+
+    public static void alreadyConnectedIdentifiableTerminal(ReportNode reportNode, String identifiableId) {
+        reportNode.newReportNode()
+                .withMessageTemplate("alreadyConnectedTerminal", "A terminal of identifiable ${identifiable} is already connected.")
+                .withUntypedValue("identifiable", identifiableId)
+                .withSeverity(TypedValue.WARN_SEVERITY)
+                .add();
+    }
+
+    public static void alreadyDisconnectedIdentifiableTerminal(ReportNode reportNode, String identifiableId) {
+        reportNode.newReportNode()
+                .withMessageTemplate("alreadyDisconnectedTerminal", "A terminal of identifiable ${identifiable} is already disconnected.")
+                .withUntypedValue("identifiable", identifiableId)
+                .withSeverity(TypedValue.WARN_SEVERITY)
+                .add();
+    }
+
+    public static void parentHasBothRatioAndPhaseTapChanger(ReportNode reportNode, String parentMessage) {
+        reportNode.newReportNode()
+                .withMessageTemplate("validationWarning", "${parent} has both Ratio and Phase Tap Changer.")
+                .withUntypedValue("parent", parentMessage)
+                .withSeverity(TypedValue.WARN_SEVERITY)
+                .add();
+    }
+
+    public static void parentHasDuplicatePointForActivePower(ReportNode reportNode, String ownerMessage, Double p) {
+        reportNode.newReportNode()
+                .withMessageTemplate("validationWarning", "${parent} duplicate point for active power {p}")
+                .withUntypedValue("parent", ownerMessage)
+                .withUntypedValue("p", p)
+                .withSeverity(TypedValue.WARN_SEVERITY)
+                .add();
+    }
+
+    public static ReportNode runIidmNetworkValidationCHecks(ReportNode reportNode, String networkId) {
+        return Objects.requireNonNull(reportNode).newReportNode()
+                .withMessageTemplate("IIDMValidation", "Running validation checks on IIDM network ${networkId}")
+                .withUntypedValue("networkId", networkId)
+                .add();
+    }
+
+    // DEBUG
+    public static void inconsistentPropertyValues(ReportNode reportNode, String propertyName, String propertyValue1, String propertyValue2, String danglingLineId1, String danglingLineId2) {
+        reportNode.newReportNode()
+                .withMessageTemplate("InconsistentPropertyValues", "Inconsistencies of property ${propertyName} between ${danglingLineId1} (value=${propertyValue1}) and ${danglingLineId2} (value=${propertyValue2}). Property is not added to merged line")
+                .withUntypedValue("propertyName", propertyName)
+                .withUntypedValue(DANGLING_LINE_ID_1, danglingLineId1)
+                .withUntypedValue("propertyValue1", propertyValue1)
+                .withUntypedValue(DANGLING_LINE_ID_2, danglingLineId2)
+                .withUntypedValue("propertyValue2", propertyValue2)
+                .withSeverity(TypedValue.DEBUG_SEVERITY)
+                .add();
+    }
+
+    public static void moveCommonAliases(ReportNode reportNode, String alias, String danglingLineId1, String danglingLineId2) {
+        reportNode.newReportNode()
+                .withMessageTemplate("MoveCommonAlias", "Alias ${alias} found in dangling lines ${danglingLineId1} and ${danglingLineId2} is moved to their merged line.")
+                .withUntypedValue("alias", alias)
+                .withUntypedValue(DANGLING_LINE_ID_1, danglingLineId1)
+                .withUntypedValue(DANGLING_LINE_ID_2, danglingLineId2)
+                .withSeverity(TypedValue.DEBUG_SEVERITY)
+                .add();
+    }
+
+    public static void propertyOnlyOnOneSide(ReportNode reportNode, String propertyName, String propertyValue, int emptySide, String danglingLineId1, String danglingLineId2) {
+        reportNode.newReportNode()
+                .withMessageTemplate("PropertyOnlyOnOneSide", "Inconsistencies of property ${propertyName} between both sides (${danglingLineId1) on side 1 and ${danglingLineId2} on side2) of merged line. " +
+                        "Side ${side} has no value. Value on other side is kept.")
+                .withUntypedValue("propertyName", propertyName)
+                .withUntypedValue("side", emptySide)
+                .withUntypedValue(DANGLING_LINE_ID_1, danglingLineId1)
+                .withUntypedValue(DANGLING_LINE_ID_2, danglingLineId2)
+                .withUntypedValue("propertyValue", propertyValue)
+                .withSeverity(TypedValue.DEBUG_SEVERITY)
+                .add();
+    }
+
+    // WARN
+    public static void inconsistentAliasTypes(ReportNode reportNode, String alias, String type1, String type2, String danglingLineId1, String danglingLineId2) {
+        reportNode.newReportNode()
+                .withMessageTemplate("InconsistentAliasTypes", "Inconsistencies of types for alias ${alias} type in dangling lines ${danglingLineId1} (type=${type1}) and ${danglingLineId2} (type=${type2}). Type is lost.")
+                .withUntypedValue("alias", alias)
+                .withUntypedValue(DANGLING_LINE_ID_1, danglingLineId1)
+                .withUntypedValue("type1", type1)
+                .withUntypedValue(DANGLING_LINE_ID_2, danglingLineId2)
+                .withUntypedValue("type2", type2)
+                .withSeverity(TypedValue.WARN_SEVERITY)
+                .add();
+    }
+
+    public static void inconsistentAliasValues(ReportNode reportNode, String alias1, String alias2, String type, String danglingLineId1, String danglingLineId2) {
+        reportNode.newReportNode()
+                .withMessageTemplate("InconsistentAliasValues", "Inconsistencies found for alias type '${type}'('${alias1}' for '${danglingLineId1}' and '${alias2}' for '${danglingLineId2}'). " +
+                        "Types are respectively renamed as '${type}_1' and '${type}_2'.")
+                .withUntypedValue("alias1", alias1)
+                .withUntypedValue("alias2", alias2)
+                .withUntypedValue(DANGLING_LINE_ID_1, danglingLineId1)
+                .withUntypedValue(DANGLING_LINE_ID_2, danglingLineId2)
+                .withUntypedValue("type", type)
+                .withSeverity(TypedValue.WARN_SEVERITY)
+                .add();
+    }
+
+    public static ReportNode createChildReportNode(ReportNode reportNode, ReadOnlyDataSource ds) {
+        return reportNode.newReportNode()
+                .withMessageTemplate("importDataSource", "Import data source ${dataSource}")
+                .withUntypedValue("dataSource", ds.getBaseName())
+                .add();
+    }
+
+    public static void exportMock(ReportNode reportNode) {
+        reportNode.newReportNode()
+                .withMessageTemplate("export_test", "Export mock")
+                .add();
+    }
+
+    public static void testImportPostProcessor(ReportNode reportNode) {
+        reportNode.newReportNode()
+                .withMessageTemplate("testImportPostProcessor", "testing import post processor")
+                .add();
+    }
+}
