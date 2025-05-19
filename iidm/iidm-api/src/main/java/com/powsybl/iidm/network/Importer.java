f0d944d34f800489cb6b6cc5d9169043f2195b9c
marquesja1 <marquesja@aia.es>
2025-05-19 11:47:50 +0200

    Fix the error caused by resolving the conflicts
    Signed-off-by: marquesja1 <marquesja@aia.es>

--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/Importer.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/Importer.java
@@ -356,7 +356,7 @@ public interface Importer {
      * @param reportNode the reportNode used for functional logs
      */
     default void update(Network network, ReadOnlyDataSource dataSource, Properties parameters, ReportNode reportNode) {
-        update(network, dataSource, parameters);
+        throw new UnsupportedOperationException("Importer do not implement updates");
     }
 
     /**
