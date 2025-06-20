72bb749a50b7278040e87402f04b491d4b561a43
CARON Alice <alice.caron@rte-france.com>
2025-06-20 10:01:46 +0200

    Do not fallback in JVM default locale but on base reports
    Signed-off-by: CARON Alice <alice.caron@rte-france.com>

--- commons/src/main/java/com/powsybl/commons/report/BundleMessageTemplateProvider.java
+++ commons/src/main/java/com/powsybl/commons/report/BundleMessageTemplateProvider.java
@@ -19,21 +19,21 @@ public class BundleMessageTemplateProvider implements MessageTemplateProvider {
     private final String bundleBaseName;
     private final boolean throwIfUnknownKey;
     private ResourceBundle resourceBundle;
-
+    public static final ResourceBundle.Control NO_FALLBACK_CONTROL = ResourceBundle.Control.getNoFallbackControl(ResourceBundle.Control.FORMAT_DEFAULT);
     public BundleMessageTemplateProvider(String bundleBaseName) {
         this(bundleBaseName, false);
     }
 
     public BundleMessageTemplateProvider(String bundleBaseName, boolean throwIfUnknownKey) {
         this.bundleBaseName = bundleBaseName;
-        this.resourceBundle = ResourceBundle.getBundle(bundleBaseName, ReportConstants.getDefaultLocale());
+        this.resourceBundle = ResourceBundle.getBundle(bundleBaseName, ReportConstants.getDefaultLocale(), NO_FALLBACK_CONTROL);
         this.throwIfUnknownKey = throwIfUnknownKey;
     }
 
     @Override
     public String getTemplate(String key, Locale locale) {
         if (!this.resourceBundle.getLocale().equals(locale)) {
-            this.resourceBundle = ResourceBundle.getBundle(bundleBaseName, locale);
+            this.resourceBundle = ResourceBundle.getBundle(bundleBaseName, locale, NO_FALLBACK_CONTROL);
         }
         if (!throwIfUnknownKey && !resourceBundle.containsKey(key)) {
             return MessageTemplateProvider.getMissingKeyMessage(key, locale);
