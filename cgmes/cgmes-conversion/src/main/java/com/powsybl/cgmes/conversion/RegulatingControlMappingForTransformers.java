be42b933d76b0b9c851b3287875beb17771a1ab4
Romain Courtier <romain.courtier@rte-france.com>
2025-06-23 14:41:50 +0200

    Add support for CGMES import and export of PhaseTapChanger.ltcFlag
    Signed-off-by: Romain Courtier <romain.courtier@rte-france.com>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/RegulatingControlMappingForTransformers.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/RegulatingControlMappingForTransformers.java
@@ -68,12 +68,11 @@ public class RegulatingControlMappingForTransformers {
     }
 
     public CgmesRegulatingControlPhase buildRegulatingControlPhase(String id, String regulatingControlId,
-        boolean tapChangerControlEnabled, boolean ltcFlag) {
+        boolean tapChangerControlEnabled) {
         CgmesRegulatingControlPhase rtc = new CgmesRegulatingControlPhase();
         rtc.id = id;
         rtc.regulatingControlId = regulatingControlId;
         rtc.tapChangerControlEnabled = tapChangerControlEnabled;
-        rtc.ltcFlag = ltcFlag;
         return rtc;
     }
 
@@ -230,21 +229,21 @@ public class RegulatingControlMappingForTransformers {
 
         boolean okSet = false;
         if (control.mode.endsWith("currentflow")) {
-            okSet = setPtcRegulatingControlCurrentFlow(rc.id, regulating, rc.ltcFlag, control, ptc, context);
+            okSet = setPtcRegulatingControlCurrentFlow(rc.id, regulating, control, ptc, context);
         } else if (control.mode.endsWith("activepower")) {
-            okSet = setPtcRegulatingControlActivePower(rc.id, regulating, rc.ltcFlag, control, ptc, context);
+            okSet = setPtcRegulatingControlActivePower(rc.id, regulating, control, ptc, context);
         }
         control.setCorrectlySet(okSet);
     }
 
-    private boolean setPtcRegulatingControlCurrentFlow(String ptcId, boolean regulating, boolean ltcFlag, RegulatingControl control,
+    private boolean setPtcRegulatingControlCurrentFlow(String ptcId, boolean regulating, RegulatingControl control,
                                                        PhaseTapChanger ptc, Context context) {
-        return setPtcRegulatingControl(ptcId, getPtcRegulating(ltcFlag, regulating), PhaseTapChanger.RegulationMode.CURRENT_LIMITER, control, ptc, context);
+        return setPtcRegulatingControl(ptcId, regulating, PhaseTapChanger.RegulationMode.CURRENT_LIMITER, control, ptc, context);
     }
 
-    private boolean setPtcRegulatingControlActivePower(String ptcId, boolean regulating, boolean ltcFlag, RegulatingControl control,
+    private boolean setPtcRegulatingControlActivePower(String ptcId, boolean regulating, RegulatingControl control,
                                                        PhaseTapChanger ptc, Context context) {
-        return setPtcRegulatingControl(ptcId, getPtcRegulating(ltcFlag, regulating), PhaseTapChanger.RegulationMode.ACTIVE_POWER_CONTROL, control, ptc, context);
+        return setPtcRegulatingControl(ptcId, regulating, PhaseTapChanger.RegulationMode.ACTIVE_POWER_CONTROL, control, ptc, context);
     }
 
     private boolean setPtcRegulatingControl(String ptcId, boolean regulating, PhaseTapChanger.RegulationMode regulationMode,
@@ -272,36 +271,6 @@ public class RegulatingControlMappingForTransformers {
                 .setRegulating(regulating && validTargetDeadband);
 
         return true;
-    }
-
-    private boolean getPtcRegulating(boolean ltcFlag, boolean regulating) {
-        // According to the following CGMES documentation:
-        // IEC TS 61970-600-1, Edition 1.0, 2017-07.
-        // "Energy management system application program interface (EMS-API)
-        // – Part 600-1: Common Grid Model Exchange Specification (CGMES)
-        // – Structure and rules",
-        // "Annex E (normative) implementation guide",
-        // section "E.9 LTCflag" (pages 76-79)
-
-        // The combination: TapChanger.ltcFlag == False
-        // and TapChanger.TapChangerControl Present
-        // Is allowed as:
-        // "An artificial tap changer can be used to simulate control behavior on power
-        // flow"
-
-        // But the ENTSO-E documentation
-        // "QUALITY OF CGMES DATASETS AND CALCULATIONS FOR SYSTEM OPERATIONS"
-        // 3.1 EDITION, 13 June 2019
-
-        // Contains a rule that states that when ltcFlag == False,
-        // Then TapChangerControl should NOT be present
-
-        // Although this combination has been observed in TYNDP test cases,
-        // we will forbid it until an explicit ltcFlag is added to IIDM,
-        // in the meanwhile, when ltcFlag == False,
-        // we avoid regulation by setting regulating to false
-
-        return ltcFlag && regulating;
     }
 
     private RegulatingControl getTapChangerControl(CgmesRegulatingControl rc) {
@@ -361,7 +330,6 @@ public class RegulatingControlMappingForTransformers {
     }
 
     public static class CgmesRegulatingControlPhase extends CgmesRegulatingControl {
-        boolean ltcFlag;
     }
 
     private static class CgmesRegulatingControl {
