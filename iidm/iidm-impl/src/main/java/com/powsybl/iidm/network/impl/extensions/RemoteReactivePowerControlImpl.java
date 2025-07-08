d9fad2a38fb4855796dc80f512f1388633af1e88
Olivier Perrin <olivier.perrin@rte-france.com>
2025-07-08 16:54:09 +0200

    Fix RemoteReactivePowerControlImpl regulating terminal replacement
    Signed-off-by: Olivier Perrin <olivier.perrin@rte-france.com>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/extensions/RemoteReactivePowerControlImpl.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/extensions/RemoteReactivePowerControlImpl.java
@@ -77,8 +77,12 @@ public class RemoteReactivePowerControlImpl extends AbstractMultiVariantIdentifi
     @Override
     public RemoteReactivePowerControl setRegulatingTerminal(Terminal regulatingTerminal) {
         Objects.requireNonNull(regulatingTerminal);
-        checkRegulatingTerminal(regulatingTerminal, getExtendable().getTerminal().getVoltageLevel().getNetwork());
-        this.regulatingTerminal = regulatingTerminal;
+        if (this.regulatingTerminal != regulatingTerminal) {
+            ((TerminalExt) this.regulatingTerminal).getReferrerManager().unregister(this);
+            checkRegulatingTerminal(regulatingTerminal, getExtendable().getTerminal().getVoltageLevel().getNetwork());
+            this.regulatingTerminal = regulatingTerminal;
+            ((TerminalExt) regulatingTerminal).getReferrerManager().register(this);
+        }
         return this;
     }
 
