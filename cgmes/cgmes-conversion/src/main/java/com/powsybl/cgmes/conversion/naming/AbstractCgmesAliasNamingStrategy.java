e405c36032c63c3e7dc421eb2fb52b31247e36b6
Coline Piloquet <coline.piloquet@rte-france.com>
2025-09-17 10:55:34 +0200

    Make attributes of AbstractCgmesAliasNamingStrategy protected Signed-off-by: Coline Piloquet <coline.piloquet@rte-france.com>
    Signed-off-by: Coline Piloquet <coline.piloquet@rte-france.com>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/naming/AbstractCgmesAliasNamingStrategy.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/naming/AbstractCgmesAliasNamingStrategy.java
@@ -32,9 +32,9 @@ import static com.powsybl.cgmes.conversion.naming.CgmesObjectReference.ref;
  */
 public abstract class AbstractCgmesAliasNamingStrategy implements NamingStrategy {
 
-    public final BiMap<String, String> idByUuid = HashBiMap.create();
-    public final Map<String, String> uuidSeed = new HashMap<>();
-    public final NameBasedGenerator nameBasedGenerator;
+    protected final BiMap<String, String> idByUuid = HashBiMap.create();
+    protected final Map<String, String> uuidSeed = new HashMap<>();
+    protected final NameBasedGenerator nameBasedGenerator;
 
     protected AbstractCgmesAliasNamingStrategy(UUID uuidNamespace) {
         // The namespace for generating stable name-based UUIDs is also a UUID
