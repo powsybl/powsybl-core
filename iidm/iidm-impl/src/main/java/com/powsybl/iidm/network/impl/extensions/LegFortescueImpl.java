ce7941678cbae707db2faeb7dc840870f0533001
Etienne LESOT <etienne.lesot@rte-france.com>
2025-09-24 09:59:29 +0200

    author
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/extensions/LegFortescueImpl.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/extensions/LegFortescueImpl.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.iidm.network.impl.extensions;
 
 import com.powsybl.iidm.network.extensions.LegFortescue;
@@ -5,6 +12,9 @@ import com.powsybl.iidm.network.extensions.WindingConnectionType;
 
 import java.util.Objects;
 
+/**
+ * @author Etienne Lesot <etienne.lesot at rte-france.com>
+ */
 class LegFortescueImpl implements LegFortescue {
 
     private double rz;
