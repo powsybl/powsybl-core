33a105e49a2a0c1adf69f9cce8985d61c18111ab
lisrte <laurent.issertial_externe@rte-france.com>
2025-09-25 11:20:34 +0200

    Move DynamicModelInfoTest content to AbstractDynamicModelInfoTest
    Signed-off-by: lisrte <laurent.issertial_externe@rte-france.com>

--- iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/tck/extensions/DynamicModelInfoTest.java
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/tck/extensions/DynamicModelInfoTest.java
@@ -7,81 +7,11 @@
  */
 package com.powsybl.iidm.network.impl.tck.extensions;
 
-import com.powsybl.commons.PowsyblException;
-import com.powsybl.iidm.network.Generator;
-import com.powsybl.iidm.network.Network;
-import com.powsybl.iidm.network.VariantManager;
-import com.powsybl.iidm.network.extensions.DynamicModelInfo;
-import com.powsybl.iidm.network.extensions.DynamicModelInfoAdder;
-import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static com.powsybl.iidm.network.VariantManagerConstants.INITIAL_VARIANT_ID;
-import static org.junit.jupiter.api.Assertions.*;
+import com.powsybl.iidm.network.tck.extensions.AbstractDynamicModelInfoTest;
 
 /**
  * @author Laurent Issertial {@literal <laurent.issertial at rte-france.com>}
  */
-class DynamicModelInfoTest {
-
-    private Network network;
-    private Generator gen;
-
-    @BeforeEach
-    void setUp() {
-        network = EurostagTutorialExample1Factory.create();
-        gen = network.getGenerator("GEN");
-    }
-
-    @Test
-    void testExtension() {
-        gen.newExtension(DynamicModelInfoAdder.class).setModelName("TestModel").add();
-        DynamicModelInfo<Generator> dynamicModelInfo = gen.getExtension(DynamicModelInfo.class);
-        assertEquals("dynamicModel", dynamicModelInfo.getName());
-        assertEquals("GEN", dynamicModelInfo.getExtendable().getId());
-
-        assertEquals("TestModel", dynamicModelInfo.getModelName());
-        dynamicModelInfo.setModelName("TestModel2");
-        assertEquals("TestModel2", dynamicModelInfo.getModelName());
-    }
-
-    @Test
-    void variantsCloneTest() {
-        String variant1 = "variant1";
-        String variant2 = "variant2";
-        String variant3 = "variant3";
-
-        gen.newExtension(DynamicModelInfoAdder.class).setModelName("TestModel").add();
-        DynamicModelInfo<Generator> dynamicModelInfo = gen.getExtension(DynamicModelInfo.class);
-        assertNotNull(dynamicModelInfo);
-
-        // Testing variant cloning
-        VariantManager variantManager = network.getVariantManager();
-        variantManager.cloneVariant(INITIAL_VARIANT_ID, variant1);
-        variantManager.cloneVariant(variant1, variant2);
-        variantManager.setWorkingVariant(variant1);
-        assertEquals("TestModel", dynamicModelInfo.getModelName());
-
-        // Testing setting different values in the cloned variant and going back to the initial one
-        dynamicModelInfo.setModelName("TestModel2");
-        assertEquals("TestModel2", dynamicModelInfo.getModelName());
-        variantManager.setWorkingVariant(INITIAL_VARIANT_ID);
-        assertEquals("TestModel", dynamicModelInfo.getModelName());
-
-        // Removes a variant then adds another variant to test variant recycling (hence calling allocateVariantArrayElement)
-        variantManager.removeVariant(variant1);
-        variantManager.cloneVariant(INITIAL_VARIANT_ID, List.of(variant1, variant3));
-        variantManager.setWorkingVariant(variant1);
-        assertEquals("TestModel", dynamicModelInfo.getModelName());
-        variantManager.setWorkingVariant(variant3);
-        assertEquals("TestModel", dynamicModelInfo.getModelName());
-
-        // Test removing current variant
-        variantManager.removeVariant(variant3);
-        Exception e = assertThrows(PowsyblException.class, dynamicModelInfo::getModelName);
-        assertEquals("Variant index not set", e.getMessage());
-    }
+class DynamicModelInfoTest extends AbstractDynamicModelInfoTest {
 }
