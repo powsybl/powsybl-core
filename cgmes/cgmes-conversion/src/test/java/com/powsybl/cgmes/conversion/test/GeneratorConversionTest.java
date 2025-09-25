b4dd5f7f333989e33a8052a4cfc5b602b15e1ff1
Romain Courtier <romain.courtier@rte-france.com>
2025-09-25 13:24:47 +0200

    CGMES export: don't write injections minQ/maxQ if a ReactiveCapabilityCurve is provided (#3574)
    CGMES export: don't write injections minQ/maxQ if a ReactiveCapabilityCurve is provided (#3574)
    
    * Don't write minQ/maxQ if a ReactiveCapabilityCurve is provided
    * Add unit test
    * Make sure the curve is exported when setting minQ/maxQ to null
    
    Signed-off-by: Romain Courtier <romain.courtier@rte-france.com>
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/GeneratorConversionTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/GeneratorConversionTest.java
@@ -8,16 +8,21 @@
 package com.powsybl.cgmes.conversion.test;
 
 import com.powsybl.cgmes.conversion.Conversion;
+import com.powsybl.commons.test.AbstractSerDeTest;
 import com.powsybl.iidm.network.*;
+import com.powsybl.iidm.network.test.ReactiveLimitsTestNetworkFactory;
 import org.junit.jupiter.api.Test;
 
-import static com.powsybl.cgmes.conversion.test.ConversionUtil.readCgmesResources;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+
+import static com.powsybl.cgmes.conversion.test.ConversionUtil.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Damien Jeandemange {@literal <damien.jeandemange at artelys.com>}
+ * @author Romain Courtier {@literal <romain.courtier at rte-france.com>}
  */
-class GeneratorConversionTest {
+class GeneratorConversionTest extends AbstractSerDeTest {
 
     @Test
     void generatingUnitTypes() {
@@ -31,5 +36,28 @@ class GeneratorConversionTest {
         assertEquals(EnergySource.WIND, network.getGenerator("onshore_wgu_sm").getEnergySource());
         assertEquals("onshore", network.getGenerator("onshore_wgu_sm").getProperty(Conversion.PROPERTY_WIND_GEN_UNIT_TYPE));
         assertEquals(EnergySource.SOLAR, network.getGenerator("sgu_sm").getEnergySource());
+    }
+
+    @Test
+    void reactiveLimitsExportTest() throws IOException {
+        // IIDM network has 2 Generators. G1 has reactive limits of kind curve, G2 has reactive limits of kind min/max.
+        Network network = ReactiveLimitsTestNetworkFactory.create();
+        assertEquals(ReactiveLimitsKind.CURVE, network.getGenerator("G1").getReactiveLimits().getKind());
+        assertEquals(ReactiveLimitsKind.MIN_MAX, network.getGenerator("G2").getReactiveLimits().getKind());
+
+        // Export CGMES EQ profile.
+        String eqFile = writeCgmesProfile(network, "EQ", tmpDir);
+
+        // CGMES SynchronousMachine G1 has a ReactiveCapabilityCurve but no minQ/maxQ.
+        String gen1 = getElement(eqFile, "SynchronousMachine", "G1");
+        assertTrue(gen1.contains("<cim:SynchronousMachine.InitialReactiveCapabilityCurve"));
+        assertFalse(gen1.contains("<cim:SynchronousMachine.minQ>"));
+        assertFalse(gen1.contains("<cim:SynchronousMachine.maxQ>"));
+
+        // CGMES SynchronousMachine G2 has a minQ/maxQ but no ReactiveCapabilityCurve.
+        String gen2 = getElement(eqFile, "SynchronousMachine", "G2");
+        assertFalse(gen2.contains("<cim:SynchronousMachine.InitialReactiveCapabilityCurve"));
+        assertTrue(gen2.contains("<cim:SynchronousMachine.minQ>"));
+        assertTrue(gen2.contains("<cim:SynchronousMachine.maxQ>"));
     }
 }
