b6edaf90c4edb26ebc4a59f8e625d3210c99c2d3
BORSENBERGER Jacques <jacques.borsenberger@rte-france.com>
2025-10-03 13:49:20 +0200

    Add TertiaryReserve extension for Generator  * creation / adder  * test  * serialization / deserialisation
    Signed-off-by: BORSENBERGER Jacques <jacques.borsenberger@rte-france.com>

---
+++ iidm/iidm-extensions/src/main/java/com/powsybl/iidm/network/extensions/TertiaryReserve.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.iidm.network.extensions;
+
+import com.powsybl.commons.extensions.Extension;
+import com.powsybl.iidm.network.Generator;
+
+/**
+ * @author Jacques Borsenberger {literal <jacques.borsenberger at rte-france.com}
+ */
+public interface TertiaryReserve extends Extension<Generator> {
+
+    String NAME = "tertiaryReserve";
+
+    @Override
+    default String getName() {
+        return NAME;
+    }
+
+    boolean isParticipate();
+
+    TertiaryReserve setParticipate(boolean participate);
+}
