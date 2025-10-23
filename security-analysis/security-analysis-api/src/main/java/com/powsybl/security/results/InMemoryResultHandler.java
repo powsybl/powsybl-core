067ca5d6def07b65a40a12cb475d1565d81d8140
Bertrand Rix <bertrand.rix@artelys.com>
2025-10-23 10:21:42 +0200

    clean
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- security-analysis/security-analysis-api/src/main/java/com/powsybl/security/results/InMemoryResultHandler.java
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/results/InMemoryResultHandler.java
@@ -1,7 +1,13 @@
+/**
+ * Copyright (c) 2025, Coreso SA (https://www.coreso.eu/) and TSCNET Services GmbH (https://www.tscnet.eu/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.security.results;
 
 import com.powsybl.contingency.Contingency;
-import com.powsybl.iidm.network.ThreeWindingsTransformer;
 import com.powsybl.security.strategy.OperatorStrategy;
 
 import java.util.ArrayList;
@@ -9,9 +15,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
 
+/**
+ * @author Bertrand Rix {@literal <bertrand.rix at artelys.com>}
+ */
 public class InMemoryResultHandler implements SecurityAnalysisResultHandler {
 
     public static class StateResult {
