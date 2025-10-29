c86bf2fe4d3e998083a6ddd2374206a9afde93c3
Matthieu SAUR <matthieu.saur@rte-france.com>
2025-10-29 08:35:04 +0100

    Use "maxVersion" variable instead a hardCoded version
    Signed-off-by: Matthieu SAUR <matthieu.saur@rte-france.com>

--- iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/StaticVarCompensatorXmlTest.java
+++ iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/StaticVarCompensatorXmlTest.java
@@ -37,7 +37,7 @@ class StaticVarCompensatorXmlTest extends AbstractIidmSerDeTest {
     @Test
     void noRegulatioModeTest() throws IOException {
         // backward compatibility from 1.7 to 1.13 : regulation mode is exported as OFF if it was not set in input file
-        allFormatsRoundTripFromVersionedXmlFromMinToVersion1Dot13Test("staticVarCompensatorNoRegulationMode.xml", "staticVarCompensatorRegulationModeOFF.xml", IidmVersion.V_1_7);
+        allFormatsRoundTripFromVersionedXmlFromMinToMaxVersionTest("staticVarCompensatorNoRegulationMode.xml", "staticVarCompensatorRegulationModeOFF.xml", IidmVersion.V_1_7, IidmVersion.V_1_13);
 
         // regulation mode is exported as VOLTAGE and regulating is set to false if regulation mode was not set in input file
         allFormatsRoundTripFromVersionedXmlTest("staticVarCompensatorNoRegulationMode.xml", "notRegulatingStaticVarCompensatorRoundTripRef.xml", CURRENT_IIDM_VERSION);
