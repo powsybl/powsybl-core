03dd9edd449bdc85e13f808f34c30f5a51201ff6
Denis Bonnand <denis.bonnand@supergrid-institute.com>
2025-10-30 15:00:56 +0100

    Add Droop class and add droop list in AcDcConverters
    Signed-off-by: Denis Bonnand <denis.bonnand@supergrid-institute.com>

---
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/AcDcConverterDroopAdder.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2025, SuperGrid Institute (https://www.supergrid-institute.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.iidm.network;
+
+/**
+ * @author Denis Bonnand {@literal <denis.bonnand at supergrid-institute.com>}
+ */
+public interface AcDcConverterDroopAdder {
+
+    AcDcConverterDroopAdder setUMax(double uMax);
+
+    AcDcConverterDroopAdder setUMin(double uMin);
+
+    AcDcConverterDroopAdder setDroopCoefficient(double k);
+
+    AcDcConverterDroop add();
+}
