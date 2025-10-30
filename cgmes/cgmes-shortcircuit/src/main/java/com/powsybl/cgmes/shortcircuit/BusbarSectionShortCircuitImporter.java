7b8b2652ce2e9bdfe5ad7ac81bc0426de51f7797
Coline P <coline.piloquet@rte-france.com>
2025-10-30 09:04:30 +0100

    Rename package cgmes "shorcircuit" into "shortcircuit". (#3619)
    Signed-off-by: Coline Piloquet <coline.piloquet@rte-france.com>
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- cgmes/cgmes-shortcircuit/src/main/java/com/powsybl/cgmes/shortcircuit/BusbarSectionShortCircuitImporter.java
+++ cgmes/cgmes-shortcircuit/src/main/java/com/powsybl/cgmes/shortcircuit/BusbarSectionShortCircuitImporter.java
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * SPDX-License-Identifier: MPL-2.0
  */
-package com.powsybl.cgmes.shorcircuit;
+package com.powsybl.cgmes.shortcircuit;
 
 import java.util.Objects;
 
--- cgmes/cgmes-shortcircuit/src/main/java/com/powsybl/cgmes/shortcircuit/GeneratorShortCircuitImporter.java
+++ cgmes/cgmes-shortcircuit/src/main/java/com/powsybl/cgmes/shortcircuit/GeneratorShortCircuitImporter.java
@@ -1,10 +1,12 @@
-package com.powsybl.cgmes.shorcircuit; /**
+/**
  * Copyright (c) 2021, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * SPDX-License-Identifier: MPL-2.0
  */
+
+package com.powsybl.cgmes.shortcircuit;
 
 import java.util.Objects;
 
