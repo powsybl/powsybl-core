08e5734444c1c2048ea3c61aa3959a36f7681b7f
vidaldid-rte <didier.vidal_externe@rte-france.com>
2025-11-03 16:31:57 +0100

    Update iidm/iidm-api/src/main/java/com/powsybl/iidm/network/VoltageLevel.java
    Co-authored-by: Romain Courtier <romain.courtier@rte-france.com>
    Signed-off-by: vidaldid-rte <didier.vidal_externe@rte-france.com>

--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/VoltageLevel.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/VoltageLevel.java
@@ -422,7 +422,7 @@ public interface VoltageLevel extends Container<VoltageLevel> {
         }
 
         /**
-         * Returns the fictitious reactive power injection to the node if set, or 0. The value is in MW and uses the load sign convention (a positive value has the same effect as a load connected to the node)
+         * Returns the fictitious reactive power injection to the node if set, or 0. The value is in MVar and uses the load sign convention (a positive value has the same effect as a load connected to the node)
          * A fictitious injection is meant to be considered as a load by simulators or exporters. It is typically used to represent the remainder or a state estimator.
          */
         default double getFictitiousQ0(int node) {
