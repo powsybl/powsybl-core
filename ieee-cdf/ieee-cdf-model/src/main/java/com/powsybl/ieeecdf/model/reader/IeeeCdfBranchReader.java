ada44619fd979bd6f5916e2d6ef12501601eb7f6
Nicolas Rol <nicolas.rol@rte-france.com>
2025-11-19 14:04:15 +0100

    Fix javadoc
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- ieee-cdf/ieee-cdf-model/src/main/java/com/powsybl/ieeecdf/model/reader/IeeeCdfBranchReader.java
+++ ieee-cdf/ieee-cdf-model/src/main/java/com/powsybl/ieeecdf/model/reader/IeeeCdfBranchReader.java
@@ -16,10 +16,6 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- *
- *
- 1         2         3         4         5         6         7         8         9         0         1         2         3
- 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
  * @author Nicolas Rol {@literal <nicolas.rol at rte-france.com>}
  */
 public final class IeeeCdfBranchReader extends AbstractIeeeCdfReader {
