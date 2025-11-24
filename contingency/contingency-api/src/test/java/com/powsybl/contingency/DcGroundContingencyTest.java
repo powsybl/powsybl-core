cb65057ffb0df22bed06022de6567d9f7b16994e
Denis Bonnand <denis.bonnand@supergrid-institute.com>
2025-11-24 16:41:40 +0100

    Update with main
    Signed-off-by: Denis Bonnand <denis.bonnand@supergrid-institute.com>

--- contingency/contingency-api/src/test/java/com/powsybl/contingency/DcGroundContingencyTest.java
+++ contingency/contingency-api/src/test/java/com/powsybl/contingency/DcGroundContingencyTest.java
@@ -8,7 +8,7 @@
 package com.powsybl.contingency;
 
 import com.google.common.testing.EqualsTester;
-import com.powsybl.contingency.contingency.list.ContingencyList;
+import com.powsybl.contingency.list.ContingencyList;
 import com.powsybl.iidm.modification.tripping.DcGroundTripping;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.test.DcDetailedNetworkFactory;
