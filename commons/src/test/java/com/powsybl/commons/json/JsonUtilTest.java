e2383f27b8cb2ab9f0b48d28ee08d81fb98401b9
Clement Philipot <clement.philipot@rte-france.com>
2025-11-25 11:20:15 +0100

    add test (if one of the versions have no second part)
    Signed-off-by: Clement Philipot <clement.philipot@rte-france.com>

--- commons/src/test/java/com/powsybl/commons/json/JsonUtilTest.java
+++ commons/src/test/java/com/powsybl/commons/json/JsonUtilTest.java
@@ -79,6 +79,7 @@ class JsonUtilTest {
         assertTrue(JsonUtil.compareVersions("1.9", "1.10") < 0);
         assertTrue(JsonUtil.compareVersions("2.0", "1.9") > 0);
         assertTrue(JsonUtil.compareVersions("1.10", "1.10.1") < 0);
+        assertTrue(JsonUtil.compareVersions("2.", "2.1.1") < 0);
         assertEquals(0, JsonUtil.compareVersions("1.10.1", "1.10.1"));
         assertTrue(JsonUtil.compareVersions("1.10.1", "1.10.2") < 0);
         assertTrue(JsonUtil.compareVersions("A.2", "B.2") < 0);
