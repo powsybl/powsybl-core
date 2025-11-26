872df76e66cd011881f16daf3f9b042b0d9e3f0b
Fabrice Buscaylet <fabrice.buscaylet@artelys.com>
2025-11-26 14:51:17 +0100

    Sonar code duplication quality gate level fixes (second try)
    Signed-off-by: Fabrice Buscaylet <fabrice.buscaylet@artelys.com>

---
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/limitreduction/result/UnsupportedPropertiesHolder.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.security.limitreduction.result;
+
+import com.powsybl.iidm.network.PropertiesHolder;
+
+import java.util.Set;
+
+/**
+ * Class used to pass Sonar Quality Gates Issues over code duplication in AbstractReducedLoadingLimits ...
+ * @author Fabrice Buscaylet {@literal <fabrice.buscaylet at artelys.com>}
+ */
+public class UnsupportedPropertiesHolder implements PropertiesHolder {
+    @Override
+    public boolean hasProperty() {
+        return false;
+    }
+
+    @Override
+    public boolean hasProperty(String key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getProperty(String key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getProperty(String key, String defaultValue) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String setProperty(String key, String value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeProperty(String key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Set<String> getPropertyNames() {
+        return Set.of();
+    }
+}
