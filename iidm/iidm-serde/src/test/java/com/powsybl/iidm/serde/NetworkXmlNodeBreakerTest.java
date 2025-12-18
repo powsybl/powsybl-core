0fe818c040bee17525ad6139c895e412013e6d2e
Coline Piloquet <coline.piloquet@rte-france.com>
2025-12-18 15:46:46 +0100

    Rebase
    Signed-off-by: Coline Piloquet <coline.piloquet@rte-france.com>

--- iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/NetworkXmlNodeBreakerTest.java
+++ iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/NetworkXmlNodeBreakerTest.java
@@ -1,30 +1,28 @@
-package com.powsybl.iidm.xml;
+package com.powsybl.iidm.serde;
 
-import com.powsybl.commons.AbstractConverterTest;
-import com.powsybl.iidm.import_.Importers;
 import com.powsybl.iidm.network.BusbarSection;
 import com.powsybl.iidm.network.Network;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-public class NetworkXmlNodeBreakerTest extends AbstractConverterTest {
+class NetworkXmlNodeBreakerTest extends AbstractIidmSerDeTest {
 
     @Test
-    public void roundTripTest() throws IOException {
+    void roundTripTest() {
         String filename = "/nodeBreaker.xml";
 
-        Network network = Importers.loadNetwork(filename, getClass().getResourceAsStream(filename));
+        Network network = Network.read("nodeBreaker.xiidm", getClass().getResourceAsStream(filename));
         assertNotNull(network);
         BusbarSection bbs1 = network.getBusbarSection("E1");
         BusbarSection bbs2 = network.getBusbarSection("E2");
         bbs1.getTerminal().getBusBreakerView().getBus().setV(400.0);
         bbs2.getTerminal().getBusBreakerView().getBus().setV(401.0);
+        assertEquals(400.0, bbs1.getTerminal().getBusBreakerView().getBus().getV(), 0.0);
+        assertEquals(401.0, bbs2.getTerminal().getBusBreakerView().getBus().getV(), 0.0);
 
-        Network network2 = NetworkXml.copy(network);
+        Network network2 = NetworkSerDe.copy(network);
         bbs1 = network2.getBusbarSection("E1");
         bbs2 = network2.getBusbarSection("E2");
         assertEquals(400.0, bbs1.getTerminal().getBusBreakerView().getBus().getV(), 0.0);
