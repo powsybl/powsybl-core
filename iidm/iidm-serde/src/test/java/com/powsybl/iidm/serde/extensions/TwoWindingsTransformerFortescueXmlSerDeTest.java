3bf331b81473aac150111f18dbc83576385c4701
Coline Piloquet <coline.piloquet@rte-france.com>
2026-01-28 16:31:17 +0100

    Add test for version 1.0.
    Signed-off-by: Coline Piloquet <coline.piloquet@rte-france.com>

--- iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/extensions/TwoWindingsTransformerFortescueXmlSerDeTest.java
+++ iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/extensions/TwoWindingsTransformerFortescueXmlSerDeTest.java
@@ -14,12 +14,17 @@ import com.powsybl.iidm.network.extensions.TwoWindingsTransformerFortescueAdder;
 import com.powsybl.iidm.network.extensions.WindingConnectionType;
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
 import com.powsybl.iidm.serde.AbstractIidmSerDeTest;
+import com.powsybl.iidm.serde.ExportOptions;
+import com.powsybl.iidm.serde.IidmVersion;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.time.ZonedDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
@@ -90,5 +95,38 @@ class TwoWindingsTransformerFortescueXmlSerDeTest extends AbstractIidmSerDeTest 
 
         assertEquals(fortescue.isFreeFluxes(), fortescue2.isFreeFluxes());
         assertEquals(fortescue.getXm(), fortescue2.getXm());
+    }
+
+    @Test
+    void testV10() throws IOException {
+        Network network = EurostagTutorialExample1Factory.create();
+        network.setCaseDate(ZonedDateTime.parse("2016-12-07T11:18:52.881+01:00"));
+        var twt = network.getTwoWindingsTransformer("NGEN_NHV1");
+        assertNotNull(twt);
+        TwoWindingsTransformerFortescue fortescue = twt.newExtension(TwoWindingsTransformerFortescueAdder.class)
+            .withRz(0.1d)
+            .withXz(2d)
+            .withFreeFluxes(false)
+            .withXm(0.5d)
+            .withConnectionType1(WindingConnectionType.Y_GROUNDED)
+            .withConnectionType2(WindingConnectionType.DELTA)
+            .withGroundingR1(0.02d)
+            .withGroundingX1(0.3d)
+            .withGroundingR2(0.04d)
+            .withGroundingX2(0.95d)
+            .add();
+
+        Network network2 = allFormatsRoundTripTest(network, "/fortescue/twoWindingsTransformerFortescueRef_V1_0.xml",
+            new ExportOptions()
+                .addExtensionVersion(TwoWindingsTransformerFortescue.NAME, "1.0")
+                .setVersion(IidmVersion.V_1_14.toString(".")));
+
+        TwoWindingsTransformer twt2 = network2.getTwoWindingsTransformer("NGEN_NHV1");
+        assertNotNull(twt2);
+        TwoWindingsTransformerFortescue fortescue2 = twt2.getExtension(TwoWindingsTransformerFortescue.class);
+        assertNotNull(fortescue2);
+
+        assertEquals(fortescue.isFreeFluxes(), fortescue2.isFreeFluxes());
+        assertTrue(Double.isNaN(fortescue2.getXm()));
     }
 }
