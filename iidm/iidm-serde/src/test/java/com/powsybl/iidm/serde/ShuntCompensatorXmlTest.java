d4949732cb8af8fa5228a9e96f92da25bcf67751
Samir Romdhani <samir.romdhani_externe@rte-france.com>
2026-02-03 11:11:26 +0100

    feat(IIDM): add tests
    Signed-off-by: Samir Romdhani <samir.romdhani_externe@rte-france.com>

--- iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/ShuntCompensatorXmlTest.java
+++ iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/ShuntCompensatorXmlTest.java
@@ -8,6 +8,7 @@
 package com.powsybl.iidm.serde;
 
 import com.powsybl.commons.PowsyblException;
+import com.powsybl.commons.io.TreeDataFormat;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.ShuntCompensator;
 import com.powsybl.iidm.network.ShuntCompensatorLinearModel;
@@ -15,11 +16,13 @@ import com.powsybl.iidm.network.test.ShuntTestCaseFactory;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static com.powsybl.iidm.serde.IidmSerDeConstants.CURRENT_IIDM_VERSION;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Miora Ralambotiana {@literal <miora.ralambotiana at rte-france.com>}
@@ -96,6 +99,78 @@ class ShuntCompensatorXmlTest extends AbstractIidmSerDeTest {
 
         // backward compatibility
         allFormatsRoundTripFromVersionedXmlFromMinToCurrentVersionTest("shuntWithSolvedSectionCountRoundTripRef.xml", IidmVersion.V_1_14);
+    }
+
+    @Test
+    void shouldRejectShuntOldTagName() {
+        //When
+        //Read XIIDM
+        PowsyblException exception1 = assertThrows(PowsyblException.class,
+                () -> NetworkSerDe.read(getVersionedNetworkAsStream("shuntOldTagName.xml", IidmVersion.V_1_16)));
+        //Read JIIDM
+        ImportOptions importOptions = new ImportOptions().setFormat(TreeDataFormat.JSON);
+        PowsyblException exception2 = assertThrows(PowsyblException.class,
+                () -> NetworkSerDe.read(getVersionedNetworkAsStream("shuntOldTagName.jiidm", IidmVersion.V_1_16), importOptions, null));
+        //Then
+        String expectedErrorMessage = "shunt is not supported for IIDM version 1.16. IIDM version should be <= 1.15";
+        assertEquals(expectedErrorMessage, exception1.getMessage());
+        assertEquals(expectedErrorMessage, exception2.getMessage());
+    }
+
+    @Test
+    void writeXmlShouldUseShuntCompensator() throws IOException {
+        //Given
+        Network network = ShuntTestCaseFactory.create();
+        Path xml = tmpDir.resolve("shuntCompensator_v116.xiidm");
+        //When
+        ExportOptions exportOptions = new ExportOptions().setFormat(TreeDataFormat.XML).setVersion(IidmVersion.V_1_16.toString("."));
+        NetworkSerDe.write(network, exportOptions, xml);
+        //Then
+        String content = Files.readString(xml, StandardCharsets.UTF_8);
+        assertThat(content).contains("<iidm:shuntCompensator ");
+        assertThat(content).doesNotContain("<iidm:shunt ");
+    }
+
+    @Test
+    void writeJsonShouldUseShuntCompensator() throws IOException {
+        //Given
+        Network network = ShuntTestCaseFactory.create();
+        Path xml = tmpDir.resolve("shuntCompensator_v116.jiidm");
+        //When
+        ExportOptions exportOptions = new ExportOptions().setFormat(TreeDataFormat.JSON).setVersion(IidmVersion.V_1_16.toString("."));
+        NetworkSerDe.write(network, exportOptions, xml);
+        //Then
+        String content = Files.readString(xml, StandardCharsets.UTF_8);
+        assertThat(content).contains("shuntCompensators");
+        assertThat(content).doesNotContain("shunts");
+    }
+
+    @Test
+    void writeXmlShouldUseShunt() throws IOException {
+        //Given
+        Network network = ShuntTestCaseFactory.create();
+        Path xml = tmpDir.resolve("shunt_v115.xiidm");
+        //When
+        ExportOptions exportOptions = new ExportOptions().setFormat(TreeDataFormat.XML).setVersion(IidmVersion.V_1_15.toString("."));
+        NetworkSerDe.write(network, exportOptions, xml);
+        String content = Files.readString(xml, StandardCharsets.UTF_8);
+        //Then
+        assertThat(content).contains("<iidm:shunt ");
+        assertThat(content).doesNotContain("<iidm:shuntCompensator ");
+    }
+
+    @Test
+    void writeJsonShouldUseShunt() throws IOException {
+        //Given
+        Network network = ShuntTestCaseFactory.create();
+        Path xml = tmpDir.resolve("shunt_v115.jiidm");
+        //When
+        ExportOptions exportOptions = new ExportOptions().setFormat(TreeDataFormat.JSON).setVersion(IidmVersion.V_1_15.toString("."));
+        NetworkSerDe.write(network, exportOptions, xml);
+        String content = Files.readString(xml, StandardCharsets.UTF_8);
+        //Then
+        assertThat(content).contains("shunts");
+        assertThat(content).doesNotContain("shuntCompensators");
     }
 
     private void write(Network network, String version) {
