3d96c3a69e1be253e4fbad27ec85d9c26affe517
Antoine Bouhours <antoine.bouhours@rte-france.com>
2026-02-06 09:52:52 +0100

    Add flatten in export options (#3680)
    Add flatten in export options (#3680)
    
    * Add flatten in export options
    * Avoid modifying the exported network
    
    ---------
    
    Signed-off-by: BOUHOURS Antoine <antoine.bouhours@rte-france.com>
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- iidm/iidm-serde/src/main/java/com/powsybl/iidm/serde/NetworkSerDe.java
+++ iidm/iidm-serde/src/main/java/com/powsybl/iidm/serde/NetworkSerDe.java
@@ -369,7 +369,9 @@ public final class NetworkSerDe {
         AliasesSerDe.write(n, NETWORK_ROOT_ELEMENT_NAME, context);
         PropertiesSerDe.write(n, context);
 
-        IidmSerDeUtil.runFromMinimumVersion(IidmVersion.V_1_11, context, () -> writeSubnetworks(n, context, extensionsSupplier));
+        if (supportSubnetworksExport(context)) {
+            writeSubnetworks(n, context, extensionsSupplier);
+        }
 
         writeDcDetailed(n, context);
         writeVoltageLevels(n, context);
@@ -569,7 +571,7 @@ public final class NetworkSerDe {
     }
 
     private static boolean supportSubnetworksExport(NetworkSerializerContext context) {
-        return context.getVersion().compareTo(IidmVersion.V_1_11) >= 0;
+        return context.getVersion().compareTo(IidmVersion.V_1_11) >= 0 && !context.getOptions().isFlatten();
     }
 
     private static NetworkSerializerContext createContext(Network n, ExportOptions options, TreeDataWriter writer) {
