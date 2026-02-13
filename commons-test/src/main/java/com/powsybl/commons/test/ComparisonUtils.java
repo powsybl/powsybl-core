2d677737e758761c33d90f303da2cc2b9582808e
Samir Romdhani <samir.romdhani_externe@rte-france.com>
2026-02-13 17:08:24 +0100

    fix line separator on Windows CI
    Signed-off-by: Samir Romdhani <samir.romdhani_externe@rte-france.com>

--- commons-test/src/main/java/com/powsybl/commons/test/ComparisonUtils.java
+++ commons-test/src/main/java/com/powsybl/commons/test/ComparisonUtils.java
@@ -99,7 +99,7 @@ public final class ComparisonUtils {
         String expectedStr = TestUtil.normalizeLineSeparator(expected);
         String actualStr = TestUtil.normalizeLineSeparator(actual);
         // Ensure element ends with exactly one newline before assertion
-        String lineSeparator = System.lineSeparator();
+        String lineSeparator = System.getProperty("line.separator");
         if (!expectedStr.endsWith(lineSeparator)) {
             expectedStr += lineSeparator;
         }
