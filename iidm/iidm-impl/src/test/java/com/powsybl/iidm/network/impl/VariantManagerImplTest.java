d01086b97e80b91d7631fd5d8dff22da9dff0cbb
Nicolas Rol <nicolas.rol@rte-france.com>
2026-02-17 15:40:57 +0100

    Fix merge
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/VariantManagerImplTest.java
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/VariantManagerImplTest.java
@@ -37,6 +37,8 @@ import java.util.concurrent.CountDownLatch;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
