1d4f1935aabf9026cd1795147a513500dc2ca9f4
Samir Romdhani <samir.romdhani_externe@rte-france.com>
2026-02-20 17:05:29 +0100

    fixed anonymizer within extensions BaseVoltageMapping
    Signed-off-by: Samir Romdhani <samir.romdhani_externe@rte-france.com>

--- cgmes/cgmes-extensions/src/test/java/com/powsybl/cgmes/extensions/BaseVoltageMappingSerDeTest.java
+++ cgmes/cgmes-extensions/src/test/java/com/powsybl/cgmes/extensions/BaseVoltageMappingSerDeTest.java
@@ -9,10 +9,19 @@ package com.powsybl.cgmes.extensions;
 
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.test.NoEquipmentNetworkFactory;
+import com.powsybl.iidm.serde.ExportOptions;
+import com.powsybl.iidm.serde.ImportOptions;
+import com.powsybl.iidm.serde.NetworkSerDe;
+import com.powsybl.iidm.serde.anonymizer.Anonymizer;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.ZonedDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Florian Dupuy {@literal <florian.dupuy at rte-france.com>}
@@ -28,5 +37,52 @@ class BaseVoltageMappingSerDeTest extends AbstractCgmesExtensionTest {
                 .addBaseVoltage("id_380", 380, Source.BOUNDARY)
                 .add();
         allFormatsRoundTripTest(network, "/no_equipment_base_voltage_mapping.xml");
+    }
+
+    @Test
+    void testAnonymizedBaseVoltageIds() throws IOException {
+        //Given
+        Network network = NoEquipmentNetworkFactory.create();
+        ExportOptions exportOptions = new ExportOptions().setAnonymized(true);
+        ImportOptions importOptions = new ImportOptions();
+
+        network.newExtension(BaseVoltageMappingAdder.class)
+                .addBaseVoltage("id_400", 400, Source.IGM)
+                .addBaseVoltage("id_380", 380, Source.BOUNDARY)
+                .add();
+        byte[] anonymizedXml;
+        Anonymizer anonymizer;
+        // When Export (with anonymized option)
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+            anonymizer = NetworkSerDe.write(network, exportOptions, os);
+            anonymizedXml = os.toByteArray();
+        }
+        // Then check anonymized id != original ids
+        String anonymizedId400 = anonymizer.anonymizeString("id_400");
+        String anonymizedId380 = anonymizer.anonymizeString("id_380");
+        assertNotEquals("id_400", anonymizedId400);
+        assertNotEquals("id_380", anonymizedId380);
+        // Then check xml content (contain only anonymized id)
+        String xmlContent = new String(anonymizedXml, StandardCharsets.UTF_8);
+        assertTrue(xmlContent.contains("id=\"" + anonymizedId400 + "\""));
+        assertTrue(xmlContent.contains("id=\"" + anonymizedId380 + "\""));
+        assertFalse(xmlContent.contains("id=\"id_400\""));
+        assertFalse(xmlContent.contains("id=\"id_380\""));
+
+        //Then import with anonymizer to restores original ids
+        try (ByteArrayInputStream is = new ByteArrayInputStream(anonymizedXml)) {
+            Network importedNetwork = NetworkSerDe.read(is, importOptions, anonymizer);
+            BaseVoltageMapping importedBaseVoltageMapping = importedNetwork.getExtension(BaseVoltageMapping.class);
+            assertEquals("id_400", importedBaseVoltageMapping.getBaseVoltage(400).getId());
+            assertEquals("id_380", importedBaseVoltageMapping.getBaseVoltage(380).getId());
+        }
+
+        //Then import without anonymizer to check that is still anonymized
+        try (ByteArrayInputStream is = new ByteArrayInputStream(anonymizedXml)) {
+            Network importedNetwork = NetworkSerDe.read(is);
+            BaseVoltageMapping importedBaseVoltageMapping = importedNetwork.getExtension(BaseVoltageMapping.class);
+            assertEquals(anonymizedId400, importedBaseVoltageMapping.getBaseVoltage(400).getId());
+            assertEquals(anonymizedId380, importedBaseVoltageMapping.getBaseVoltage(380).getId());
+        }
     }
 }
