560fb95758512789dd40f240eb52144198861123
Etienne LESOT <etienne.lesot@rte-france.com>
2026-03-02 11:11:02 +0100

    fix
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- commons-test/src/main/java/com/powsybl/commons/test/ComparisonUtils.java
+++ commons-test/src/main/java/com/powsybl/commons/test/ComparisonUtils.java
@@ -19,7 +19,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
