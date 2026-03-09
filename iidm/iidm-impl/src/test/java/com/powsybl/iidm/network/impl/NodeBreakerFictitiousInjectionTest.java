6728c3001c48997d994881f04b5f46fc72c79446
PRABAKARAN Sylvestre <sylvestre.prabakaran@rte-france.com>
2026-03-09 16:20:10 +0100

    Add coverage
    Signed-off-by: PRABAKARAN Sylvestre <sylvestre.prabakaran@rte-france.com>

--- iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/NodeBreakerFictitiousInjectionTest.java
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/NodeBreakerFictitiousInjectionTest.java
@@ -12,8 +12,12 @@ public class NodeBreakerFictitiousInjectionTest {
     @Test
     void testHasFictitiousP0AndFictitiousQ0() {
         Network network = NetworkTest1Factory.create();
+        String initialVariantId = network.getVariantManager().getWorkingVariantId();
+        String duplicateVariantId = "duplicateState";
+        network.getVariantManager().cloneVariant(initialVariantId, duplicateVariantId);
         Bus bus = network.getVoltageLevel("voltageLevel1").getBusBreakerView().getBus("voltageLevel1_0");
 
+        network.getVariantManager().setWorkingVariant(initialVariantId);
         bus.setFictitiousP0(10);
         bus.setFictitiousQ0(20);
         VoltageLevelImpl vl = (VoltageLevelImpl) network.getVoltageLevel("voltageLevel1");
@@ -24,6 +28,13 @@ public class NodeBreakerFictitiousInjectionTest {
 
         bus.setFictitiousP0(0.0);
         bus.setFictitiousQ0(0.0);
+        assertFalse(vl.getNodeBreakerView().hasFictitiousP0());
+        assertFalse(vl.getNodeBreakerView().hasFictitiousQ0());
+        assertEquals(0, bus.getFictitiousP0());
+        assertEquals(0, bus.getFictitiousQ0());
+
+        // Testing hasFictitious with multiple variants (initialState has fictitious values and duplicateState has no fictitious values)
+        network.getVariantManager().setWorkingVariant(duplicateVariantId);
         assertFalse(vl.getNodeBreakerView().hasFictitiousP0());
         assertFalse(vl.getNodeBreakerView().hasFictitiousQ0());
         assertEquals(0, bus.getFictitiousP0());
