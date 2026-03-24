2a56dfa011c60acd44dcf8996f0e9a8bef261bbe
NathanDissoubray <nathan.dissoubray@rte-france.com>
2026-03-24 10:57:47 +0100

    Update LimitViolationComparator and Equivalence with operationalLimitsGroupId
    Signed-off-by: NathanDissoubray <nathan.dissoubray@rte-france.com>

--- security-analysis/security-analysis-api/src/test/java/com/powsybl/security/comparator/LimitViolationEquivalenceTest.java
+++ security-analysis/security-analysis-api/src/test/java/com/powsybl/security/comparator/LimitViolationEquivalenceTest.java
@@ -10,6 +10,7 @@ package com.powsybl.security.comparator;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.powsybl.iidm.network.ThreeSides;
 import com.powsybl.iidm.network.TwoSides;
 import org.junit.jupiter.api.Test;
 
@@ -51,6 +52,13 @@ class LimitViolationEquivalenceTest {
 
         violation2 = new LimitViolation("NHV1_NHV2_2", LimitViolationType.APPARENT_POWER, null, Integer.MAX_VALUE, 1000.0, 0.95f, 1100.0, TwoSides.TWO);
         assertFalse(violationEquivalence.equivalent(violation1, violation2));
+
+        violation1 = new LimitViolation("NHV1_NHV2_1", null, "group_1", LimitViolationType.CURRENT, null, Integer.MAX_VALUE, 1000.0, 0.95f, 1100.0, ThreeSides.ONE, null);
+        violation2 = new LimitViolation("NHV1_NHV2_1", null, "group_1", LimitViolationType.CURRENT, null, Integer.MAX_VALUE, 1000.0, 0.95f, 1100.0, ThreeSides.ONE, null);
+        assertTrue(violationEquivalence.equivalent(violation1, violation2));
+
+        violation2 = new LimitViolation("NHV1_NHV2_1", null, "group_1", LimitViolationType.CURRENT, null, Integer.MAX_VALUE, 1000.0, 0.95f, 1100.09, ThreeSides.ONE, null);
+        assertTrue(violationEquivalence.equivalent(violation1, violation2));
     }
 
 }
