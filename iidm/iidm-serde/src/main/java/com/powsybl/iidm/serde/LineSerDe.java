e2c099226d920c2ca6c04c333806f54dd628d025
Leclerc Clement <clement.leclerc@rte-france.com>
2026-04-01 13:36:00 +0200

    Remove version-based logic for default attribute values in SerDe classes
    Signed-off-by: Leclerc Clement <clement.leclerc@rte-france.com>

--- iidm/iidm-serde/src/main/java/com/powsybl/iidm/serde/LineSerDe.java
+++ iidm/iidm-serde/src/main/java/com/powsybl/iidm/serde/LineSerDe.java
@@ -35,10 +35,10 @@ class LineSerDe extends AbstractSimpleIdentifiableSerDe<Line, LineAdder, Network
     protected void writeRootElementAttributes(Line l, Network n, NetworkSerializerContext context) {
         context.getWriter().writeDoubleAttribute("r", l.getR());
         context.getWriter().writeDoubleAttribute("x", l.getX());
-        context.getWriter().writeDoubleAttribute("g1", l.getG1(), 0.0);
-        context.getWriter().writeDoubleAttribute("b1", l.getB1(), 0.0);
-        context.getWriter().writeDoubleAttribute("g2", l.getG2(), 0.0);
-        context.getWriter().writeDoubleAttribute("b2", l.getB2(), 0.0);
+        context.getWriter().writeDoubleAttribute("g1", l.getG1(),0.0);
+        context.getWriter().writeDoubleAttribute("b1", l.getB1(),0.0);
+        context.getWriter().writeDoubleAttribute("g2", l.getG2(),0.0);
+        context.getWriter().writeDoubleAttribute("b2", l.getB2(),0.0);
         writeNodeOrBus(1, l.getTerminal1(), context);
         writeNodeOrBus(2, l.getTerminal2(), context);
         writeOptionalPQ(1, l.getTerminal1(), context.getWriter(), context.getOptions()::isWithBranchSV);
@@ -55,10 +55,8 @@ class LineSerDe extends AbstractSimpleIdentifiableSerDe<Line, LineAdder, Network
 
     @Override
     protected void writeSubElements(Line l, Network n, NetworkSerializerContext context) {
-        writeLimits(context, 1, ROOT_ELEMENT_NAME, l.getSelectedOperationalLimitsGroup1().orElse(null),
-            context.getOptions().isOnlySelectedOperationalLimitsGroups() ? l.getAllSelectedOperationalLimitsGroups(TwoSides.ONE) : l.getOperationalLimitsGroups1());
-        writeLimits(context, 2, ROOT_ELEMENT_NAME, l.getSelectedOperationalLimitsGroup2().orElse(null),
-            context.getOptions().isOnlySelectedOperationalLimitsGroups() ? l.getAllSelectedOperationalLimitsGroups(TwoSides.TWO) : l.getOperationalLimitsGroups2());
+        writeLimits(context, 1, ROOT_ELEMENT_NAME, l.getSelectedOperationalLimitsGroup1().orElse(null), l.getOperationalLimitsGroups1());
+        writeLimits(context, 2, ROOT_ELEMENT_NAME, l.getSelectedOperationalLimitsGroup2().orElse(null), l.getOperationalLimitsGroups2());
     }
 
     @Override
@@ -101,7 +99,7 @@ class LineSerDe extends AbstractSimpleIdentifiableSerDe<Line, LineAdder, Network
             switch (elementName) {
                 case LIMITS_GROUP_1 -> {
                     IidmSerDeUtil.assertMinimumVersion(ROOT_ELEMENT_NAME, LIMITS_GROUP_1, IidmSerDeUtil.ErrorMessage.NOT_SUPPORTED, IidmVersion.V_1_12, context);
-                    IidmSerDeUtil.runFromMinimumVersion(IidmVersion.V_1_12, context, () -> readLoadingLimitsGroup(l, TwoSides.ONE, LIMITS_GROUP_1, context));
+                    IidmSerDeUtil.runFromMinimumVersion(IidmVersion.V_1_12, context, () -> readLoadingLimitsGroup(l::newOperationalLimitsGroup1, LIMITS_GROUP_1, context));
                 }
                 case ACTIVE_POWER_LIMITS_1 -> {
                     IidmSerDeUtil.assertMinimumVersion(ROOT_ELEMENT_NAME, ACTIVE_POWER_LIMITS_1, IidmSerDeUtil.ErrorMessage.NOT_SUPPORTED, IidmVersion.V_1_5, context);
@@ -114,7 +112,7 @@ class LineSerDe extends AbstractSimpleIdentifiableSerDe<Line, LineAdder, Network
                 case "currentLimits1" -> readCurrentLimits(l.getOrCreateSelectedOperationalLimitsGroup1().newCurrentLimits(), context);
                 case LIMITS_GROUP_2 -> {
                     IidmSerDeUtil.assertMinimumVersion(ROOT_ELEMENT_NAME, LIMITS_GROUPS + "2", IidmSerDeUtil.ErrorMessage.NOT_SUPPORTED, IidmVersion.V_1_12, context);
-                    IidmSerDeUtil.runFromMinimumVersion(IidmVersion.V_1_12, context, () -> readLoadingLimitsGroup(l, TwoSides.TWO, LIMITS_GROUP_2, context));
+                    IidmSerDeUtil.runFromMinimumVersion(IidmVersion.V_1_12, context, () -> readLoadingLimitsGroup(l::newOperationalLimitsGroup2, LIMITS_GROUP_2, context));
                 }
                 case ACTIVE_POWER_LIMITS_2 -> {
                     IidmSerDeUtil.assertMinimumVersion(ROOT_ELEMENT_NAME, ACTIVE_POWER_LIMITS_2, IidmSerDeUtil.ErrorMessage.NOT_SUPPORTED, IidmVersion.V_1_5, context);
