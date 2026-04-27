b312bfdb27fa3489648b818e47ef8bf616b93e6b
Samir Romdhani <samir.romdhani_externe@rte-france.com>
2026-04-27 09:44:21 +0200

    add tests for VscConverterStation related voltage regulation
    Signed-off-by: Samir Romdhani <samir.romdhani_externe@rte-france.com>

--- iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/HvdcXmlTest.java
+++ iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/HvdcXmlTest.java
@@ -7,12 +7,18 @@
  */
 package com.powsybl.iidm.serde;
 
+import com.powsybl.commons.io.TreeDataFormat;
+import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.test.HvdcTestNetwork;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static com.powsybl.iidm.serde.IidmSerDeConstants.CURRENT_IIDM_VERSION;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Mathieu Bague {@literal <mathieu.bague at rte-france.com>}
@@ -34,4 +40,70 @@ class HvdcXmlTest extends AbstractIidmSerDeTest {
         // backward compatibility
         allFormatsRoundTripAllPreviousVersionedXmlTest("VscRoundTripRef.xml");
     }
+
+    @Test
+    void vscWithVoltageRegulationExportedOnNewerVersionShouldSucceed() throws IOException {
+        //Given
+        Network networkImportedV16 = NetworkSerDe.read(getVersionedNetworkAsStream("VscRoundTripRef.xml", IidmVersion.V_1_16));
+        Path xmlV16 = tmpDir.resolve("vsc_v116.xiidm");
+        ExportOptions exportOptionsV16 = new ExportOptions().setFormat(TreeDataFormat.XML).setVersion(IidmVersion.V_1_16.toString("."));
+        NetworkSerDe.write(networkImportedV16, exportOptionsV16, xmlV16);
+        //When
+        //export v17
+        Path xmlV17 = tmpDir.resolve("vsc_v117.xiidm");
+        ExportOptions exportOptionsV17 = new ExportOptions().setFormat(TreeDataFormat.XML).setVersion(IidmVersion.V_1_17.toString("."));
+        NetworkSerDe.write(networkImportedV16, exportOptionsV17, xmlV17);
+        //Then
+        String contentV16 = Files.readString(xmlV16, StandardCharsets.UTF_8);
+        assertThat(contentV16).containsIgnoringWhitespaces("""
+            <iidm:vscConverterStation id="C2" name="Converter2" voltageRegulatorOn="false" lossFactor="1.1" reactivePowerSetpoint="123.0" node="2">
+                <iidm:minMaxReactiveLimits minQ="0.0" maxQ="10.0"/>
+                <iidm:regulatingTerminal id="C1"/>
+            </iidm:vscConverterStation>
+            """);
+        String contentV17 = Files.readString(xmlV17, StandardCharsets.UTF_8);
+        // TODO regulating should be false
+        assertThat(contentV17).containsIgnoringWhitespaces("""
+            <iidm:vscConverterStation id="C2" name="Converter2" lossFactor="1.1" node="2">
+                <iidm:minMaxReactiveLimits minQ="0.0" maxQ="10.0"/>
+                <iidm:voltageRegulation targetValue="123.0" mode="REACTIVE_POWER" regulating="true">
+                    <iidm:terminal id="C1"/>
+                </iidm:voltageRegulation>
+            </iidm:vscConverterStation>
+            """);
+    }
+
+    @Test
+    void vscWithVoltageRegulationExportedOnOlderVersionShouldSucceed() throws IOException {
+        //Given
+        Network networkImportedV16 = NetworkSerDe.read(getVersionedNetworkAsStream("VscRoundTripRef.xml", IidmVersion.V_1_17));
+        Path xmlV17 = tmpDir.resolve("vsc_v117.xiidm");
+        ExportOptions exportOptionsV17 = new ExportOptions().setFormat(TreeDataFormat.XML).setVersion(IidmVersion.V_1_17.toString("."));
+        NetworkSerDe.write(networkImportedV16, exportOptionsV17, xmlV17);
+        //When
+        //export on v16
+        Path xmlV16 = tmpDir.resolve("vsc_v116.xiidm");
+        ExportOptions exportOptionsV16 = new ExportOptions().setFormat(TreeDataFormat.XML).setVersion(IidmVersion.V_1_16.toString("."));
+        NetworkSerDe.write(networkImportedV16, exportOptionsV16, xmlV16);
+        //Then
+        String contentV17 = Files.readString(xmlV17, StandardCharsets.UTF_8);
+        assertThat(contentV17).containsIgnoringWhitespaces("""
+            <iidm:vscConverterStation id="C2" name="Converter2" lossFactor="1.1" node="2">
+                <iidm:minMaxReactiveLimits minQ="0.0" maxQ="10.0"/>
+                <iidm:voltageRegulation targetValue="123.0" mode="REACTIVE_POWER" regulating="true">
+                    <iidm:terminal id="C1"/>
+                </iidm:voltageRegulation>
+            </iidm:vscConverterStation>
+            """);
+        String contentV16 = Files.readString(xmlV16, StandardCharsets.UTF_8);
+        // TODO regulating should be true
+        assertThat(contentV16).containsIgnoringWhitespaces("""
+            <iidm:vscConverterStation id="C2" name="Converter2" voltageRegulatorOn="false" lossFactor="1.1" reactivePowerSetpoint="123.0" node="2">
+                <iidm:minMaxReactiveLimits minQ="0.0" maxQ="10.0"/>
+                <iidm:regulatingTerminal id="C1"/>
+            </iidm:vscConverterStation>
+            """);
+
+    }
+
 }
