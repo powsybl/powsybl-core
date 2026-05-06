1ffa0b51ca1f8d1fd8e7dcf64400f647162bd161
Samir Romdhani <samir.romdhani_externe@rte-france.com>
2026-05-06 15:37:43 +0200

    fix: bus voltage and angle stability
    Signed-off-by: Samir Romdhani <samir.romdhani_externe@rte-france.com>

--- iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/tck/BusTerminalTest.java
+++ iidm/iidm-impl/src/test/java/com/powsybl/iidm/network/impl/tck/BusTerminalTest.java
@@ -7,6 +7,108 @@
  */
 package com.powsybl.iidm.network.impl.tck;
 
+import com.powsybl.iidm.network.*;
 import com.powsybl.iidm.network.tck.AbstractBusTerminalTest;
+import org.junit.jupiter.api.Test;
 
-class BusTerminalTest extends AbstractBusTerminalTest { }
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BusTerminalTest extends AbstractBusTerminalTest {
+
+    @Test
+    void busVoltageShouldNotDependOnNodeInsertionOrder() {
+        Network network = createNetwork();
+        VoltageLevel vlN = network.getVoltageLevel("VL");
+        Load loadNode4 = network.getLoad("L1"); // node 2
+        Terminal.BusView busView = loadNode4.getTerminal().getBusView();
+
+        // LF simulation
+        busView.getBus().setV(400.0);
+        assertEquals(400.0, busView.getBus().getV(), 1e-6);
+
+        // When insert new load in low node
+        vlN.getNodeBreakerView().newInternalConnection().setNode1(2).setNode2(0).add();
+        vlN.newLoad().setId("newLoadLow").setP0(1).setQ0(1)
+                .setNode(0)
+                .add();
+        assertEquals(400.0, busView.getBus().getV(), 1e-6);
+
+        // When insert new load in high node
+        vlN.getNodeBreakerView().newInternalConnection().setNode1(2).setNode2(10).add();
+        vlN.newLoad().setId("newLoadHigh").setP0(1).setQ0(1)
+                .setNode(10)
+                .add();
+        assertEquals(400.0, busView.getBus().getV(), 1e-6);
+    }
+
+    @Test
+    void busAngleShouldNotDependOnNodeInsertionOrder() {
+        Network network = createNetwork();
+        VoltageLevel vlN = network.getVoltageLevel("VL");
+        Load loadNode4 = network.getLoad("L1"); // node 2
+        Terminal.BusView busView = loadNode4.getTerminal().getBusView();
+
+        // LF simulation
+        busView.getBus().setAngle(10.0);
+        assertEquals(10.0, busView.getBus().getAngle(), 1e-6);
+
+        // When insert new load in low node
+        vlN.getNodeBreakerView().newInternalConnection().setNode1(2).setNode2(0).add();
+        vlN.newLoad().setId("newLoadLow").setP0(1).setQ0(1)
+                .setNode(0)
+                .add();
+        assertEquals(10.0, busView.getBus().getAngle(), 1e-6);
+
+        // When insert new load in high node
+        vlN.getNodeBreakerView().newInternalConnection().setNode1(2).setNode2(10).add();
+        vlN.newLoad().setId("newLoadHigh").setP0(1).setQ0(1)
+                .setNode(10)
+                .add();
+        assertEquals(10.0, busView.getBus().getAngle(), 1e-6);
+    }
+
+    //           BBS (node 1)
+    // ---------------(1)-----------------
+    //                 |                |
+    //                 |                |
+    //                (2) L1           (3) G1
+    private Network createNetwork() {
+        Network network = Network.create("test", "test");
+        Substation s = network.newSubstation().setId("S").add();
+        VoltageLevel vl = s.newVoltageLevel()
+                .setId("VL")
+                .setNominalV(400)
+                .setTopologyKind(TopologyKind.NODE_BREAKER)
+                .add();
+        vl.getNodeBreakerView().newBusbarSection().setId("BBS").setNode(1).add();
+        vl.newLoad()
+                .setId("L1")
+                .setNode(2)
+                .setP0(10)
+                .setQ0(-50)
+                .add();
+        vl.newGenerator()
+                .setId("G1")
+                .setNode(3)
+                .setMinP(0)
+                .setMaxP(100)
+                .setTargetP(50)
+                .setTargetV(400)
+                .setVoltageRegulatorOn(true)
+                .add();
+        vl.getNodeBreakerView().newBreaker()
+                .setId("B1")
+                .setNode1(1)
+                .setNode2(2)
+                .setOpen(false)
+                .add();
+        vl.getNodeBreakerView().newBreaker()
+                .setId("B2")
+                .setNode1(1)
+                .setNode2(3)
+                .setOpen(false)
+                .add();
+        return network;
+    }
+
+}
