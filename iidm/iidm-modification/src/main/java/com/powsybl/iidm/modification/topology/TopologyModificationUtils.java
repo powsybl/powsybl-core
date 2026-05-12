4b82de1e33725d721506ed96730a3f8ca7a86d13
Etienne LESOT <etienne.lesot@rte-france.com>
2026-05-12 10:44:00 +0200

    clean log
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/topology/TopologyModificationUtils.java
+++ iidm/iidm-modification/src/main/java/com/powsybl/iidm/modification/topology/TopologyModificationUtils.java
@@ -203,7 +203,7 @@ public final class TopologyModificationUtils {
         boolean noMoreEquipments = voltageLevel.getConnectableStream().noneMatch(c -> c.getType() != IdentifiableType.BUSBAR_SECTION);
         if (!noMoreEquipments) {
             voltageLevelRemovingEquipmentsLeftReport(reportNode, vlId);
-            LOGGER.warn("Voltage level {} still contains equipments.", vlId);
+            LOGGER.warn("Voltage level {} still contains equipments and it is not removed.", vlId);
             return;
         }
         // substation must be gotten before removing the voltageLevel
