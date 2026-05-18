202181951239e3f7e50302069ac7fe308fa2b474
Nicolas Rol <nicolas.rol@rte-france.com>
2026-05-18 12:03:16 +0200

    TopologyKind is already checked
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/AbstractConnectable.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/AbstractConnectable.java
@@ -18,8 +18,6 @@ import java.util.Objects;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-import static com.powsybl.iidm.network.TopologyKind.NODE_BREAKER;
-
 /**
  *
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
@@ -139,7 +137,7 @@ abstract class AbstractConnectable<I extends Connectable<I>> extends AbstractIde
         }
 
         // check bus topology
-        if (voltageLevel.getTopologyKind() != NODE_BREAKER) {
+        if (voltageLevel.getTopologyKind() != TopologyKind.NODE_BREAKER) {
             String msg = String.format(
                     "Trying to move connectable %s to node %d of voltage level %s, which is a bus breaker voltage level",
                     getId(), node, voltageLevel.getId());
@@ -147,7 +145,7 @@ abstract class AbstractConnectable<I extends Connectable<I>> extends AbstractIde
         }
 
         // create the new terminal and attach it to the given voltage level and to the connectable
-        TerminalExt terminalExt = new TerminalBuilder(voltageLevel.getNetworkRef(), voltageLevel.getTopologyKind(), this, oldTerminal.getSide(), oldTerminal.getTerminalNumber())
+        TerminalExt terminalExt = new TerminalBuilder(voltageLevel.getNetworkRef(), TopologyKind.NODE_BREAKER, this, oldTerminal.getSide(), oldTerminal.getTerminalNumber())
                 .setNode(node)
                 .build();
 
@@ -169,7 +167,7 @@ abstract class AbstractConnectable<I extends Connectable<I>> extends AbstractIde
         }
 
         // create the new terminal and attach it to the voltage level of the given bus and links it to the connectable
-        TerminalExt terminalExt = new TerminalBuilder(((VoltageLevelExt) bus.getVoltageLevel()).getNetworkRef(), bus.getVoltageLevel().getTopologyKind(), this, oldTerminal.getSide(), oldTerminal.getTerminalNumber())
+        TerminalExt terminalExt = new TerminalBuilder(((VoltageLevelExt) bus.getVoltageLevel()).getNetworkRef(), TopologyKind.BUS_BREAKER, this, oldTerminal.getSide(), oldTerminal.getTerminalNumber())
                 .setBus(connected ? bus.getId() : null)
                 .setConnectableBus(bus.getId())
                 .build();
