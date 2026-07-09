23fb988b54323986dafb3f15b2d8828e93d65f0c
Samir Romdhani <samir.romdhani_externe@rte-france.com>
2026-07-09 10:18:35 +0200

    review: add getTerminalState test
    Signed-off-by: Samir Romdhani <samir.romdhani_externe@rte-france.com>

--- loadflow/loadflow-validation/src/test/java/com/powsybl/loadflow/validation/ValidationUtilsTest.java
+++ loadflow/loadflow-validation/src/test/java/com/powsybl/loadflow/validation/ValidationUtilsTest.java
@@ -7,9 +7,19 @@
  */
 package com.powsybl.loadflow.validation;
 
+import com.powsybl.iidm.network.Bus;
+import com.powsybl.iidm.network.Terminal;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static java.lang.Double.NaN;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -29,9 +39,9 @@ class ValidationUtilsTest extends AbstractValidationTest {
         assertFalse(ValidationUtils.areNaN(looseConfig, 1.02d));
         assertFalse(ValidationUtils.areNaN(looseConfig, 1d, 3.5d));
         assertFalse(ValidationUtils.areNaN(looseConfig, 3.7d, 2d, .004d));
-        assertTrue(ValidationUtils.areNaN(looseConfig, Double.NaN));
-        assertTrue(ValidationUtils.areNaN(looseConfig, Double.NaN, 2d, .004d));
-        assertTrue(ValidationUtils.areNaN(looseConfig, Double.NaN, 2d, Double.NaN));
+        assertTrue(ValidationUtils.areNaN(looseConfig, NaN));
+        assertTrue(ValidationUtils.areNaN(looseConfig, NaN, 2d, .004d));
+        assertTrue(ValidationUtils.areNaN(looseConfig, NaN, 2d, NaN));
 
         looseConfig.setOkMissingValues(true);
         assertFalse(ValidationUtils.areNaN(looseConfig, 3.7f, 2f, .004f));
@@ -39,9 +49,9 @@ class ValidationUtilsTest extends AbstractValidationTest {
         assertFalse(ValidationUtils.areNaN(looseConfig, Float.NaN, 2f, .004f));
         assertFalse(ValidationUtils.areNaN(looseConfig, Float.NaN, 2f, Float.NaN));
         assertFalse(ValidationUtils.areNaN(looseConfig, 3.7d, 2d, .004d));
-        assertFalse(ValidationUtils.areNaN(looseConfig, Double.NaN));
-        assertFalse(ValidationUtils.areNaN(looseConfig, Double.NaN, 2d, .004d));
-        assertFalse(ValidationUtils.areNaN(looseConfig, Double.NaN, 2d, Double.NaN));
+        assertFalse(ValidationUtils.areNaN(looseConfig, NaN));
+        assertFalse(ValidationUtils.areNaN(looseConfig, NaN, 2d, .004d));
+        assertFalse(ValidationUtils.areNaN(looseConfig, NaN, 2d, NaN));
     }
 
     @Test
@@ -50,14 +60,14 @@ class ValidationUtilsTest extends AbstractValidationTest {
         assertFalse(ValidationUtils.boundedWithin(0.0, 10.0, -5.0, 0.0));
         assertFalse(ValidationUtils.boundedWithin(0.0, 10.0, 15.0, 0.0));
 
-        assertFalse(ValidationUtils.boundedWithin(0.0, 10.0, Double.NaN, 0.0));
-        assertFalse(ValidationUtils.boundedWithin(Double.NaN, Double.NaN, 5.0, 0.0));
+        assertFalse(ValidationUtils.boundedWithin(0.0, 10.0, NaN, 0.0));
+        assertFalse(ValidationUtils.boundedWithin(NaN, NaN, 5.0, 0.0));
 
-        assertTrue(ValidationUtils.boundedWithin(Double.NaN, 10.0, 5.0, 0.0));
-        assertFalse(ValidationUtils.boundedWithin(Double.NaN, 10.0, 15.0, 0.0));
+        assertTrue(ValidationUtils.boundedWithin(NaN, 10.0, 5.0, 0.0));
+        assertFalse(ValidationUtils.boundedWithin(NaN, 10.0, 15.0, 0.0));
 
-        assertTrue(ValidationUtils.boundedWithin(0.0, Double.NaN, 5.0, 0.0));
-        assertFalse(ValidationUtils.boundedWithin(0.0, Double.NaN, -5.0, 0.0));
+        assertTrue(ValidationUtils.boundedWithin(0.0, NaN, 5.0, 0.0));
+        assertFalse(ValidationUtils.boundedWithin(0.0, NaN, -5.0, 0.0));
     }
 
     @Test
@@ -72,7 +82,7 @@ class ValidationUtilsTest extends AbstractValidationTest {
 
     @Test
     void isUndefinedOrZeroShouldSucceed() {
-        assertTrue(ValidationUtils.isUndefinedOrZero(Double.NaN, 0.01));
+        assertTrue(ValidationUtils.isUndefinedOrZero(NaN, 0.01));
         assertTrue(ValidationUtils.isUndefinedOrZero(0.0, 0.01));
         assertTrue(ValidationUtils.isUndefinedOrZero(0.01, 0.02));
         assertFalse(ValidationUtils.isUndefinedOrZero(0.02, 0.01));
@@ -100,10 +110,40 @@ class ValidationUtilsTest extends AbstractValidationTest {
         assertTrue(ValidationUtils.isConnectedAndMainComponent(true, true, looseConfig));
         assertFalse(ValidationUtils.isConnectedAndMainComponent(true, false, looseConfig));
         assertFalse(ValidationUtils.isConnectedAndMainComponent(false, true, looseConfig));
-        //Given (config parameter CheckMainComponentOnly false)
+        // Given (config parameter CheckMainComponentOnly false)
         looseConfig.setCheckMainComponentOnly(false);
         // When Then
         assertTrue(ValidationUtils.isConnectedAndMainComponent(true, false, looseConfig));
         assertFalse(ValidationUtils.isConnectedAndMainComponent(false, false, looseConfig));
     }
+
+    @ParameterizedTest
+    @MethodSource("provideTerminalStateArgument")
+    void getTerminalStateTest(Bus bus, Bus connectableBus, double expectedV, boolean expectedConnected, boolean expectedIsMainComponent) {
+        // Given
+        Terminal terminal = mock(Terminal.class);
+        Terminal.BusView busView = mock(Terminal.BusView.class);
+        when(busView.getBus()).thenReturn(bus);
+        when(busView.getConnectableBus()).thenReturn(connectableBus);
+        when(terminal.getBusView()).thenReturn(busView);
+        // When
+        ValidationUtils.TerminalState terminalState = ValidationUtils.getTerminalState(terminal);
+        // Then
+        assertEquals(expectedV, terminalState.v());
+        assertEquals(expectedConnected, terminalState.connected());
+        assertEquals(expectedIsMainComponent, terminalState.mainComponent());
+    }
+
+    static Stream<Arguments> provideTerminalStateArgument() {
+        Bus bus1 = mock(Bus.class);
+        Bus bus2 = mock(Bus.class);
+        when(bus2.isInMainConnectedComponent()).thenReturn(true);
+        return Stream.of(
+                Arguments.of(null, null, NaN, false, false),
+                Arguments.of(bus1, null, 0.0, true, false),
+                Arguments.of(bus2, null, 0.0, true, true),
+                Arguments.of(null, bus1, NaN, false, false),
+                Arguments.of(null, bus2, NaN, false, true));
+    }
+
 }
