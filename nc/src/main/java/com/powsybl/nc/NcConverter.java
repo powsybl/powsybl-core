855ace05cac2eb53a7d64ee0291acdae06ca9c31
Thomas Bouquet <thomas.bouquet@rte-france.com>
2026-07-24 11:52:30 +0200

    parse operator strategies
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- nc/src/main/java/com/powsybl/nc/NcConverter.java
+++ nc/src/main/java/com/powsybl/nc/NcConverter.java
@@ -8,8 +8,24 @@
 
 package com.powsybl.nc;
 
+import com.powsybl.action.Action;
+import com.powsybl.action.ActionList;
+import com.powsybl.contingency.Contingency;
+import com.powsybl.contingency.list.ContingencyList;
+import com.powsybl.contingency.list.DefaultContingencyList;
+import com.powsybl.contingency.strategy.OperatorStrategy;
+import com.powsybl.contingency.strategy.OperatorStrategyList;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.nc.reader.ContingencyReader;
+import com.powsybl.nc.reader.GridStateAlterationRemedialActionReader;
+import com.powsybl.nc.reader.RotatingMachineActionReader;
+import com.powsybl.nc.reader.ShuntCompensatorModificationReader;
+import com.powsybl.nc.reader.TopologyActionReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
@@ -18,5 +34,38 @@ public final class NcConverter {
     public static final Logger LOGGER = LoggerFactory.getLogger(NcConverter.class);
 
     private NcConverter() {
+    }
+
+    public static NcData read(String ncArchivePath) {
+        Network network = Network.read(ncArchivePath);
+        return read(network, ncArchivePath);
+    }
+
+    public static NcData read(Network network, String ncArchivePath) {
+        QueryManager queryManager = new QueryManager();
+        queryManager.read(ncArchivePath);
+
+        // read contingencies
+        Set<Contingency> contingencies = new ContingencyReader(queryManager, network).readFromProfiles();
+
+        // read elementary actions
+        Set<Action> allActions = new HashSet<>();
+        allActions.addAll(new TopologyActionReader(queryManager, network).readFromProfiles());
+        allActions.addAll(new ShuntCompensatorModificationReader(queryManager, network).readFromProfiles());
+        allActions.addAll(new RotatingMachineActionReader(queryManager, network).readFromProfiles());
+
+        // read operator strategies
+        Set<OperatorStrategy> operatorStrategies = new GridStateAlterationRemedialActionReader(
+            queryManager, network, allActions, contingencies
+        ).readFromProfiles();
+
+        return new NcData(
+            new DefaultContingencyList("NC Contingencies", contingencies.stream().toList()),
+            new OperatorStrategyList(operatorStrategies.stream().toList()),
+            new ActionList(allActions.stream().toList())
+        );
+    }
+
+    public record NcData(ContingencyList contingencyList, OperatorStrategyList operatorStrategyList, ActionList actionList) {
     }
 }
---
+++ nc/src/main/java/com/powsybl/nc/reader/GridStateAlterationRemedialActionReader.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+package com.powsybl.nc.reader;
+
+import com.powsybl.action.Action;
+import com.powsybl.contingency.Contingency;
+import com.powsybl.contingency.ContingencyContext;
+import com.powsybl.contingency.strategy.OperatorStrategy;
+import com.powsybl.contingency.strategy.condition.TrueCondition;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.nc.NcProfile;
+import com.powsybl.nc.QueryManager;
+import com.powsybl.triplestore.api.PropertyBag;
+import com.powsybl.triplestore.api.PropertyBags;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.powsybl.nc.NcConverter.LOGGER;
+import static com.powsybl.nc.reader.ReaderUtils.MRID;
+
+/**
+ * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
+ */
+public class GridStateAlterationRemedialActionReader extends AbstractReader<OperatorStrategy> {
+    private static final String PREVENTIVE_KIND = "http://entsoe.eu/ns/nc#RemedialActionKind.preventive";
+    private static final String CURATIVE_KIND = "http://entsoe.eu/ns/nc#RemedialActionKind.curative";
+    private static final String COMBINATION_CONSTRAINT_KIND_INCLUDED = "http://entsoe.eu/ns/nc#ElementCombinationConstraintKind.included";
+
+    private final Set<String> importedActions;
+    private final Set<String> importedContingencies;
+
+    public GridStateAlterationRemedialActionReader(QueryManager queryManager, Network network, Set<Action> actions, Set<Contingency> contingencies) {
+        super(queryManager, network);
+        this.importedActions = actions.stream().map(Action::getId).collect(Collectors.toSet());
+        this.importedContingencies = contingencies.stream().map(Contingency::getId).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<OperatorStrategy> readFromProfiles() {
+        Set<OperatorStrategy> operatorStrategies = new HashSet<>();
+        PropertyBags gridStateAlterationRemedialActions = queryManager.query("gridStateAlterationRemedialAction", NcProfile.REMEDIAL_ACTION);
+        PropertyBags contingencyWithRemedialActions = queryManager.query("contingencyWithRemedialAction", NcProfile.REMEDIAL_ACTION);
+        PropertyBags gridStateAlterations = queryManager.query("topologyAction", NcProfile.REMEDIAL_ACTION);
+        gridStateAlterations.addAll(queryManager.query("shuntCompensatorModification", NcProfile.REMEDIAL_ACTION));
+        gridStateAlterations.addAll(queryManager.query("rotatingMachineAction", NcProfile.REMEDIAL_ACTION));
+        Map<String, Set<PropertyBag>> contingencyPerRemedialAction = ReaderUtils.groupOnAttribute(contingencyWithRemedialActions, "remedialAction", true);
+        Map<String, Set<PropertyBag>> gridStateAlterationsPerRemedialAction = ReaderUtils.groupOnAttribute(gridStateAlterations, "gridStateAlterationRemedialAction", true);
+        for (PropertyBag gridStateAlterationRemedialAction : gridStateAlterationRemedialActions) {
+            String gridStateAlterationRemedialActionId = gridStateAlterationRemedialAction.get(MRID);
+            operatorStrategies.addAll(processGridStateAlterationRemedialAction(
+                gridStateAlterationRemedialAction,
+                contingencyPerRemedialAction.getOrDefault(gridStateAlterationRemedialActionId, Set.of()),
+                gridStateAlterationsPerRemedialAction.getOrDefault(gridStateAlterationRemedialActionId, Set.of())
+            ));
+        }
+        return operatorStrategies;
+    }
+
+    private Set<OperatorStrategy> processGridStateAlterationRemedialAction(PropertyBag gridStateAlterationRemedialAction,
+                                                                           Set<PropertyBag> contingencyWithRemedialActions,
+                                                                           Set<PropertyBag> gridStateAlterations) {
+        String operatorStrategyId = gridStateAlterationRemedialAction.get(MRID);
+
+        boolean normalAvailable = Boolean.parseBoolean(gridStateAlterationRemedialAction.get("normalAvailable"));
+        if (!normalAvailable) {
+            LOGGER.warn("GridStateAlterationRemedialAction {} should not be available and will be ignored.", operatorStrategyId);
+            return Set.of();
+        }
+
+        List<String> actions = new ArrayList<>();
+        for (PropertyBag gridStateAlteration : gridStateAlterations) {
+            String actionId = gridStateAlteration.get(MRID);
+            if (importedActions.contains(actionId)) {
+                actions.add(actionId);
+            } else {
+                LOGGER.warn("Action {} associated with GridStateAlterationRemedialAction {} was not imported. GridStateAlterationRemedialAction will be ignored.", actionId, operatorStrategyId);
+                return Set.of();
+            }
+        }
+
+        String kind = gridStateAlterationRemedialAction.get("kind");
+        if (PREVENTIVE_KIND.equals(kind)) {
+            if (!contingencyWithRemedialActions.isEmpty()) {
+                LOGGER.info("Contingencies associated with preventive GridStateAlterationRemedialAction {} will not be taken into account.", operatorStrategyId);
+            }
+            return Set.of(new OperatorStrategy(operatorStrategyId, ContingencyContext.none(), new TrueCondition(), actions));
+        } else if (CURATIVE_KIND.equals(kind)) {
+            if (contingencyWithRemedialActions.isEmpty()) {
+                return Set.of(new OperatorStrategy(operatorStrategyId, ContingencyContext.onlyContingencies(), new TrueCondition(), actions));
+            } else {
+                Set<OperatorStrategy> contingencyOperatorStrategies = new HashSet<>();
+                for (PropertyBag contingencyWithRemedialAction : contingencyWithRemedialActions) {
+                    String contingencyId = contingencyWithRemedialAction.get("contingency");
+                    if (!importedContingencies.contains(contingencyId)) {
+                        LOGGER.info("Contingency {} associated with GridStateAlterationRemedialAction {} was not imported. No OperatorStrategy will be created for this contingency.",
+                            contingencyId, operatorStrategyId);
+                    } else {
+                        Optional<OperatorStrategy> curativeOperatorStrategy = processGridStateAlterationRemedialActionForContingency(
+                            operatorStrategyId, contingencyId, actions, contingencyWithRemedialAction
+                        );
+                        curativeOperatorStrategy.ifPresent(contingencyOperatorStrategies::add);
+                    }
+                }
+                if (contingencyOperatorStrategies.isEmpty()) {
+                    LOGGER.warn("GridStateAlterationRemedialAction {} has no valid contingency associated and will be ignored.", operatorStrategyId);
+                    return Set.of();
+                } else {
+                    return contingencyOperatorStrategies;
+                }
+            }
+        } else {
+            LOGGER.warn("GridStateAlterationRemedialAction {} has an unknown kind {} and will be ignored.", operatorStrategyId, kind);
+            return Set.of();
+        }
+    }
+
+    private Optional<OperatorStrategy> processGridStateAlterationRemedialActionForContingency(String operatorStrategyId,
+                                                                                              String contingencyId,
+                                                                                              List<String> actions,
+                                                                                              PropertyBag contingencyWithRemedialAction) {
+        boolean normalEnabled = Boolean.parseBoolean(contingencyWithRemedialAction.getOrDefault("normalEnabled", "true"));
+        if (!normalEnabled) {
+            LOGGER.info("Association between Contingency {} and GridStateAlterationRemedialAction {} is disabled and will be ignored. "
+                    + "No OperatorStrategy will be created for this contingency.",
+                contingencyId, operatorStrategyId);
+            return Optional.empty();
+        }
+
+        String combinationConstraintKind = contingencyWithRemedialAction.get("combinationConstraintKind");
+        if (!COMBINATION_CONSTRAINT_KIND_INCLUDED.equals(combinationConstraintKind)) {
+            LOGGER.info("Association between Contingency {} and GridStateAlterationRemedialAction {} is not included and will be ignored. "
+                    + "No OperatorStrategy will be created for this contingency.",
+                contingencyId, operatorStrategyId);
+            return Optional.empty();
+        }
+
+        return Optional.of(new OperatorStrategy(
+            operatorStrategyId + "@" + contingencyId,
+            ContingencyContext.specificContingency(contingencyId),
+            new TrueCondition(),
+            actions
+        ));
+    }
+}
