07d73995dc4b545b694a649643031d90852c4273
Nayyar <nayyar@bugqore.com>
2026-08-10 17:07:26 +0530

    fix locale-dependent base voltage mRID in CGMES export
    Signed-off-by: Nayyar <nayyar@bugqore.com>

--- cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/export/CgmesExportContext.java
+++ cgmes/cgmes-conversion/src/main/java/com/powsybl/cgmes/conversion/export/CgmesExportContext.java
@@ -24,6 +24,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.time.ZonedDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -287,7 +288,7 @@ public class CgmesExportContext {
     }
 
     private void computeBaseVoltageMapping(Network network) {
-        DecimalFormat noTrailingZerosFormat = new DecimalFormat("0.##");
+        DecimalFormat noTrailingZerosFormat = new DecimalFormat("0.##", new DecimalFormatSymbols(Locale.US));
 
         // Retrieve reference BaseVoltage mapping stored in the dedicated extension.
         // This may not match exactly the base voltages used in the network.
