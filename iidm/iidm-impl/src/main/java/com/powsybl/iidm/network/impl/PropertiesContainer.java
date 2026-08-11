cf2b5f8837059d1ae3cba970287494d5ceb9dae0
CARON Alice <alice.caron@rte-france.com>
2026-08-11 20:08:10 +0200

    Set Properties as null at creation
    Signed-off-by: CARON Alice <alice.caron@rte-france.com>

--- iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/PropertiesContainer.java
+++ iidm/iidm-impl/src/main/java/com/powsybl/iidm/network/impl/PropertiesContainer.java
@@ -16,39 +16,58 @@ import java.util.stream.Collectors;
  * @author Olivier Perrin {@literal <olivier.perrin at rte-france.com>}
  */
 public class PropertiesContainer {
-    protected final Properties properties = new Properties();
+    private Properties properties = null;
 
     public Properties getProperties() {
+        if (properties == null) {
+            properties = new Properties();
+        }
         return properties;
     }
 
     public boolean hasProperty() {
-        return !properties.isEmpty();
+        return properties != null && !properties.isEmpty();
     }
 
     public boolean hasProperty(String key) {
-        return properties.containsKey(key);
+        return properties != null && properties.containsKey(key);
     }
 
     public String getProperty(String key) {
+        if (properties == null) {
+            return null;
+        }
         Object val = properties.get(key);
         return val != null ? val.toString() : null;
     }
 
     public String getProperty(String key, String defaultValue) {
+        if (properties == null) {
+            return defaultValue;
+        }
+
         Object val = properties.getOrDefault(key, defaultValue);
         return val != null ? val.toString() : null;
     }
 
     public String setProperty(String key, String value) {
+        if (properties == null) {
+            properties = new Properties();
+        }
         return (String) properties.put(key, value);
     }
 
     public String removeProperty(String key) {
+        if (properties == null) {
+            return null;
+        }
         return (String) properties.remove(key);
     }
 
     public Set<String> getPropertyNames() {
+        if (properties == null) {
+            properties = new Properties();
+        }
         return properties.keySet().stream().map(Object::toString).collect(Collectors.toSet());
     }
 }
