a185a5f49dac3dd185ffeab808bd3f8d18c91413
NathanDissoubray <nathan.dissoubray@rte-france.com>
2026-08-24 13:03:51 +0200

    Apply review suggestion
    Co-authored-by: Damien Jeandemange <damien.jeandemange@artelys.com>
    Signed-off-by: NathanDissoubray <nathan.dissoubray@rte-france.com>

--- iidm/iidm-api/src/main/java/com/powsybl/iidm/network/DcLineAdder.java
+++ iidm/iidm-api/src/main/java/com/powsybl/iidm/network/DcLineAdder.java
@@ -13,9 +13,9 @@ package com.powsybl.iidm.network;
 public interface DcLineAdder extends IdentifiableAdder<DcLine, DcLineAdder> {
 
     /**
-     * Set the resistance of the DC line in &#937. The resistance value should be positive, since this is a detailed
+     * Set the resistance of the DC line in &#937. The resistance value must be positive, since this is a detailed
      * model, it is a direct physical representation.
-     * @param r the resistance value of the DC line in &#937, should be positive
+     * @param r the resistance value of the DC line in &#937, must be positive
      * @return this adder, for chaining
      */
     DcLineAdder setR(double r);
