fd138624ac6e1b8f3035241ddb8e61bcee4d68f4
Coline Piloquet <coline.piloquet@rte-france.com>
2026-08-28 09:00:53 +0200

    Fix missing import when resolving conflict.
    Signed-off-by: Coline Piloquet <coline.piloquet@rte-france.com>

--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/export/CgmesExportContextTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/export/CgmesExportContextTest.java
@@ -12,6 +12,7 @@ import com.powsybl.cgmes.conversion.export.CgmesExportContext;
 import com.powsybl.cgmes.extensions.CgmesTopologyKind;
 import com.powsybl.cgmes.model.CgmesNamespace;
 import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.TopologyKind;
 import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
 import org.junit.jupiter.api.Test;
 
