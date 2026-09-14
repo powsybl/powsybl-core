35f11376fa622b5a2541dd027864f38015f734a2
Alexandre Le Jean <alexandre.le-jean@artelys.com>
2026-09-14 14:37:05 +0200

    javadoc: small fix in javadoc
    Signed-off-by: Alexandre Le Jean <alexandre.le-jean@artelys.com>

--- iidm/iidm-serde/src/main/java/com/powsybl/iidm/serde/util/IidmSerDeUtil.java
+++ iidm/iidm-serde/src/main/java/com/powsybl/iidm/serde/util/IidmSerDeUtil.java
@@ -426,7 +426,7 @@ public final class IidmSerDeUtil {
 
     /**
      * Sort identifiables by their ids if given export option is activated,
-     * otherwise, by their natural order if the network defines one.
+     * otherwise, by their creation order if the network defines one.
      * In all other cases, do not change the identifiables order.
      */
     public static <T extends Identifiable<?>> Iterable<T> sorted(Network network, Iterable<T> identifiables, ExportOptions exportOptions) {
