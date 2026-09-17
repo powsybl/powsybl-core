43f3cc85385cc8629ae19c4e6638968fd04add03
Alexandre LE JEAN <117461594+lejeana@users.noreply.github.com>
2026-09-17 10:39:40 +0200

    doc: small fixe on javadoc
    Co-authored-by: Coline P <coline.piloquet@rte-france.com>
    Signed-off-by: Alexandre LE JEAN <117461594+lejeana@users.noreply.github.com>

--- security-analysis/security-analysis-api/src/main/java/com/powsybl/security/preprocessor/ContingenciesProviderPreprocessorFactory.java
+++ security-analysis/security-analysis-api/src/main/java/com/powsybl/security/preprocessor/ContingenciesProviderPreprocessorFactory.java
@@ -16,7 +16,7 @@ import java.util.Objects;
  * A factory which creates preprocessors that read contingencies according to a
  * {@link ContingenciesProviderFactory}
  *
- * @deprecated This interface will be removed on later versions, avoid new usages of it.
+ * @deprecated This class will be removed on later versions, avoid new usages of it.
  * @author Sylvain Leclerc {@literal <sylvain.leclerc at rte-france.com>}
  */
 @Deprecated(forRemoval = true, since = "7.4.0")
