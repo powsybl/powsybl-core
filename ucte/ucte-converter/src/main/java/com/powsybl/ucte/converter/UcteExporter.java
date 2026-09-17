4ca7e7235c5c0dcd621b579fc653dd2e37656534
Arthur Michaut <arthur.michaut@artelys.com>
2026-09-17 16:44:56 +0200

    Fix #4077 - Don't crash on boundary lines without power generation in UCTE-DEF exporter
    Signed-off-by: Arthur Michaut <arthur.michaut@artelys.com>

--- ucte/ucte-converter/src/main/java/com/powsybl/ucte/converter/UcteExporter.java
+++ ucte/ucte-converter/src/main/java/com/powsybl/ucte/converter/UcteExporter.java
@@ -328,28 +328,31 @@ public class UcteExporter implements Exporter {
         UcteNode ucteNode = convertXNode(ucteNetwork, xnodeCode, geographicalName, ucteNodeStatus);
         ucteNode.setActiveLoad(boundaryLine.getP0());
         ucteNode.setReactiveLoad(boundaryLine.getQ0());
-        double generatorTargetP = boundaryLine.getGeneration().getTargetP();
-        ucteNode.setActivePowerGeneration(Double.isNaN(generatorTargetP) ? 0 : -generatorTargetP);
-        double generatorTargetQ = boundaryLine.getGeneration().getTargetQ();
-        ucteNode.setReactivePowerGeneration(Double.isNaN(generatorTargetQ) ? 0 : -generatorTargetQ);
-        if (boundaryLine.getGeneration().isVoltageRegulationOn()) {
-            ucteNode.setTypeCode(UcteNodeTypeCode.PU);
-            ucteNode.setVoltageReference(boundaryLine.getGeneration().getTargetV());
-            double minP = boundaryLine.getGeneration().getMinP();
-            double maxP = boundaryLine.getGeneration().getMaxP();
-            double minQ = boundaryLine.getGeneration().getReactiveLimits().getMinQ(boundaryLine.getGeneration().getTargetP());
-            double maxQ = boundaryLine.getGeneration().getReactiveLimits().getMaxQ(boundaryLine.getGeneration().getTargetP());
-            if (minP != -DEFAULT_POWER_LIMIT) {
-                ucteNode.setMinimumPermissibleActivePowerGeneration(-minP);
-            }
-            if (maxP != DEFAULT_POWER_LIMIT) {
-                ucteNode.setMaximumPermissibleActivePowerGeneration(-maxP);
-            }
-            if (minQ != -DEFAULT_POWER_LIMIT) {
-                ucteNode.setMinimumPermissibleReactivePowerGeneration(-minQ);
-            }
-            if (maxQ != DEFAULT_POWER_LIMIT) {
-                ucteNode.setMaximumPermissibleReactivePowerGeneration(-maxQ);
+        BoundaryLine.Generation generation = boundaryLine.getGeneration();
+        if (generation != null) {
+            double generatorTargetP = generation.getTargetP();
+            ucteNode.setActivePowerGeneration(Double.isNaN(generatorTargetP) ? 0 : -generatorTargetP);
+            double generatorTargetQ = generation.getTargetQ();
+            ucteNode.setReactivePowerGeneration(Double.isNaN(generatorTargetQ) ? 0 : -generatorTargetQ);
+            if (generation.isVoltageRegulationOn()) {
+                ucteNode.setTypeCode(UcteNodeTypeCode.PU);
+                ucteNode.setVoltageReference(generation.getTargetV());
+                double minP = generation.getMinP();
+                double maxP = generation.getMaxP();
+                double minQ = generation.getReactiveLimits().getMinQ(generation.getTargetP());
+                double maxQ = generation.getReactiveLimits().getMaxQ(generation.getTargetP());
+                if (minP != -DEFAULT_POWER_LIMIT) {
+                    ucteNode.setMinimumPermissibleActivePowerGeneration(-minP);
+                }
+                if (maxP != DEFAULT_POWER_LIMIT) {
+                    ucteNode.setMaximumPermissibleActivePowerGeneration(-maxP);
+                }
+                if (minQ != -DEFAULT_POWER_LIMIT) {
+                    ucteNode.setMinimumPermissibleReactivePowerGeneration(-minQ);
+                }
+                if (maxQ != DEFAULT_POWER_LIMIT) {
+                    ucteNode.setMaximumPermissibleReactivePowerGeneration(-maxQ);
+                }
             }
         }
     }
