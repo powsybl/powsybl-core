9c321f35a350e1a1e0c8215820742b0d5fa9194d
marquesja1 <marquesja@aia.es>
2026-09-17 14:44:20 +0200

    Remove unused import
    Signed-off-by: marquesja1 <marquesja@aia.es>

--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/LoadUpdateTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/LoadUpdateTest.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Properties;
 
