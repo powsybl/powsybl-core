30acce08a0c0b69ec6bcd53d34426aa0f3192ea1
marqueslanauja <marquesja@aia.es>
2026-09-24 11:13:57 +0200

    Update cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/HvdcUpdateTest.java
    Co-authored-by: Romain Courtier <romain.courtier@rte-france.com>
    Signed-off-by: marqueslanauja <marquesja@aia.es>

--- cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/HvdcUpdateTest.java
+++ cgmes/cgmes-conversion/src/test/java/com/powsybl/cgmes/conversion/test/HvdcUpdateTest.java
@@ -163,7 +163,7 @@ class HvdcUpdateTest extends AbstractSerDeTest {
 
         vsc.setReactivePowerSetpoint(0.0);
 
-        importParameters.put("iidm.import.cgmes.use-previous-values-during-update", "true");
+        importParameters.put(CgmesImport.USE_PREVIOUS_VALUES_DURING_UPDATE, "true");
         network.update(new GenericReadOnlyDataSource(tmpDir.toAbsolutePath(), baseName), importParameters);
 
         assertEquals(30.0, vsc.getReactivePowerSetpoint(), 1e-7);
